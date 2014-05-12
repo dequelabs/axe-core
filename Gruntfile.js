f6bea0a71049ffb154842e1988451666b8b56a87
David Sturley <david.sturley@deque.com>
2014-05-12 09:30:05 -0400

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+/*jshint node: true, camelcase: false */
+
+
+module.exports = function (grunt) {
+	'use strict';
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadTasks('build/tasks');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			files: ['test/**/*'],
+			tasks: ['fixture', 'build']
+		},
+		rules: {
+			lib: {
+				dest: {
+					rules: 'dist/rules.js',
+					full: 'dist/rules.full.js',
+					checks: 'dist/checks.js'
+				}
+			}
+		},
+		uglify: {
+			minify: {
+				files: [{
+					src: ['<%= rules.lib.dest.rules %>'],
+					dest: 'dist/rules.min.js'
+				}, {
+					src: ['<%= rules.lib.dest.full %>'],
+					dest: 'dist/rules.full.min.js'
+				}, {
+					src: ['<%= rules.lib.dest.checks %>'],
+					dest: 'dist/checks.min.js'
+				}]
+			},
+			beautify: {
+				files: [{
+					src: ['<%= rules.lib.dest.rules %>'],
+					dest: '<%= rules.lib.dest.rules %>'
+				}, {
+					src: ['<%= rules.lib.dest.full %>'],
+					dest: '<%= rules.lib.dest.full %>'
+				}, {
+					src: ['<%= rules.lib.dest.checks %>'],
+					dest: '<%= rules.lib.dest.checks %>'
+				}],
+				options: {
+					mangle: false,
+					compress: false,
+					beautify: true
+				}
+			}
+		},
+		connect: {
+			test: {
+				options: {
+					hostname: '0.0.0.0',
+					port: 9876,
+					base: ['.']
+				}
+			}
+		},
+		fixture: {
+			checks: {
+				src: '<%= rules.lib.dest.checks %>',
+				dest: 'test/checks/index.html',
+				options: {
+					fixture: 'test/checks/runner.tmpl',
+					testCwd: 'test/checks'
+				}
+			},
+			rules: {
+				src: '<%= rules.lib.dest.rules %>',
+				dest: 'test/rules/index.html',
+				options: {
+					fixture: 'test/rules/runner.tmpl',
+					testCwd: 'test/rules'
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('server', ['fixture', 'connect:test:keepalive']);
+	grunt.registerTask('test', ['fixture']);
+	grunt.registerTask('build', ['rules', 'uglify']);
+	grunt.registerTask('default', ['build']);
+
+};
---
+++ build/tasks/rules.js
@@ -0,0 +1,101 @@
+/*jshint node: true */
+'use strict';
+
+var path = require('path');
+
+var CHECK_TEMPLATE = 'function (node, options) {\n<%=source%>\n}';
+
+module.exports = function (grunt) {
+
+	function createCheckObject(checks) {
+		var result = {};
+		checks.forEach(function (check) {
+			result[check.id] = check;
+		});
+		return result;
+	}
+
+	function replaceFunctions(string) {
+		return string.replace(/"(?:execute|after)":\s*("[^"]+")/g, function (m, p1) {
+			return m.replace(p1, getSource(p1.replace(/^"|"$/g, '')));
+		});
+	}
+
+	function getSource(file) {
+		return grunt.template.process(CHECK_TEMPLATE, {
+			data: {
+				source: grunt.file.read(file)
+			}
+		});
+	}
+
+	function getChecks(src) {
+		var files = grunt.file.expand(src);
+		return files.map(function (file) {
+			var json = grunt.file.readJSON(file);
+			var dirname = path.dirname(file);
+			if (json.execute) {
+				json.execute = path.resolve(dirname, json.execute);
+			}
+			if (json.after) {
+				json.after = path.resolve(dirname, json.after);
+			}
+
+			return json;
+		});
+	}
+
+	function getRules(src) {
+		var files = grunt.file.expand(src);
+		return files.map(function (file) {
+			var json = grunt.file.readJSON(file);
+			return json;
+		});
+
+	}
+
+	function findCheck(checks, id) {
+		return checks.filter(function (check) {
+			if (check.id === id) {
+				return true;
+			}
+		})[0];
+	}
+
+	grunt.registerMultiTask('rules', function () {
+
+		function blacklist(k, v) {
+			if (options.blacklist.indexOf(k) !== -1) {
+				return undefined;
+			}
+			return v;
+		}
+
+		var options = this.options({
+			rules: ['lib/rules/**/*.json'],
+			checks: ['lib/checks/**/*.json'],
+			blacklist: ['help', 'title']
+		});
+		var rules = getRules(options.rules);
+		var checks = getChecks(options.checks);
+
+		rules.map(function (rule) {
+			rule.checks = rule.checks.map(function (check) {
+				var id = typeof check === 'string' ? check : check.id;
+				var c = findCheck(checks, id);
+				c.options = check.options || c.options;
+
+				return c;
+			});
+			return rule;
+		});
+		var r = replaceFunctions(JSON.stringify(rules, blacklist));
+		var r2 = replaceFunctions(JSON.stringify(rules));
+		var c = replaceFunctions(JSON.stringify(createCheckObject(checks), blacklist));
+
+		grunt.file.write(this.data.dest.rules, 'var dqreRules = ' + r + ';');
+		grunt.file.write(this.data.dest.full, 'var dqreRules = ' + r2 + ';');
+		grunt.file.write(this.data.dest.checks, 'var checks = ' + c + ';');
+
+	});
+};
