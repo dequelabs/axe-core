279c36aa1ab2fa362707e287d703882a8f407a73
Mike Kozlowski <mike.kozlowski@deque.com>
2014-05-22 16:20:31 -0400

    Clean out build stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,29 +3,8 @@
 module.exports = function (grunt) {
 	'use strict';
 
-	grunt.loadTasks('build/tasks');
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
-		npminstall: {
-			'rule-engine': {
-				src: 'bower_components/rule-engine/'
-			},
-			'ks-rules': {
-				src: 'bower_components/ks-rules/'
-			}
-		},
-		rungrunt: {
-			'rule-engine': {
-				src: 'bower_components/rule-engine/'
-			},
-			'ks-rules': {
-				src: 'bower_components/ks-rules/'
-			}
-		},
 	});
-
-	grunt.registerTask('build', ['npminstall', 'rungrunt']);
-	grunt.registerTask('default', ['build']);
 };
