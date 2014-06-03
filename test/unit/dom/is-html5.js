28adc9e9df448dfa7ac08e81f525029313ae57e0
David Sturley <david.sturley@deque.com>
2014-06-03 10:00:44 -0400

    add dom.isHTML5(doc)

---
+++ test/unit/dom/is-html5.js
@@ -0,0 +1,12 @@
+describe('dom.isHTML5', function () {
+	'use strict';
+
+	it('should return false on any document that is not HTML5', function () {
+		var doc = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+		assert.isFalse(kslib.dom.isHTML5(doc));
+	});
+	it('should return true on any document that is HTML5', function () {
+		var doc = document.implementation.createHTMLDocument('Monkeys');
+		assert.isTrue(kslib.dom.isHTML5(doc));
+	});
+});
