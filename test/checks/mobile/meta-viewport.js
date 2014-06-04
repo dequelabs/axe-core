9450b0e3163f12dd5e798977f32a689c396e46ff
David Sturley <david.sturley@deque.com>
2014-06-04 08:24:02 -0400

    KSD-62 Add support for , as a separator

--- test/checks/mobile/meta-viewport.js
+++ test/checks/mobile/meta-viewport.js
@@ -6,57 +6,117 @@ describe('meta-viewport', function () {
 	afterEach(function () {
 		fixture.innerHTML = '';
 	});
+	describe('; separator', function () {
 
-	it('should return false on user-scalable=no', function () {
-		fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=no">';
-		var node = fixture.querySelector('meta');
+		it('should return false on user-scalable=no', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=no">';
+			var node = fixture.querySelector('meta');
 
-		assert.isFalse(checks['meta-viewport'].evaluate(node));
+			assert.isFalse(checks['meta-viewport'].evaluate(node));
+
+		});
+
+		it('should return false on user-scalable=no', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=no; more-stuff=ok">';
+			var node = fixture.querySelector('meta');
+
+			assert.isFalse(checks['meta-viewport'].evaluate(node));
+
+		});
+
+		it('should return true on user-scalable=yes', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=yes; more-stuff=ok">';
+			var node = fixture.querySelector('meta');
+
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
+
+		});
+
+		it('should return true if maximum-scale >= 5', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar; maximum-scale=5; cats= dogs;">';
+			var node = fixture.querySelector('meta');
+
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
+
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar; maximum-scale=6; cats= dogs;">';
+			node = fixture.querySelector('meta');
+
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
+
+		});
+
+		it('should return false on maximum-scale < 5', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=yes; maximum-scale=4">';
+			var node = fixture.querySelector('meta');
+
+			assert.isFalse(checks['meta-viewport'].evaluate(node));
+
+		});
+
+		it('should return true if neither user-scalable or maximum-scale are set', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs;">';
+			var node = fixture.querySelector('meta');
+
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
+
+		});
 
 	});
 
-	it('should return false on user-scalable=no', function () {
-		fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=no; more-stuff=ok">';
-		var node = fixture.querySelector('meta');
+	describe(', separator', function () {
 
-		assert.isFalse(checks['meta-viewport'].evaluate(node));
+		it('should return false on user-scalable=no', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar, cats= dogs, user-scalable=no">';
+			var node = fixture.querySelector('meta');
 
-	});
+			assert.isFalse(checks['meta-viewport'].evaluate(node));
 
-	it('should return true on user-scalable=yes', function () {
-		fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=yes; more-stuff=ok">';
-		var node = fixture.querySelector('meta');
+		});
 
-		assert.isTrue(checks['meta-viewport'].evaluate(node));
+		it('should return false on user-scalable=no', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar, cats= dogs, user-scalable=no, more-stuff=ok">';
+			var node = fixture.querySelector('meta');
 
-	});
+			assert.isFalse(checks['meta-viewport'].evaluate(node));
 
-	it('should return true if maximum-scale >= 5', function () {
-		fixture.innerHTML = '<meta name="viewport" content="foo=bar; maximum-scale=5; cats= dogs;">';
-		var node = fixture.querySelector('meta');
+		});
 
-		assert.isTrue(checks['meta-viewport'].evaluate(node));
+		it('should return true on user-scalable=yes', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar, cats= dogs, user-scalable=yes, more-stuff=ok">';
+			var node = fixture.querySelector('meta');
 
-		fixture.innerHTML = '<meta name="viewport" content="foo=bar; maximum-scale=6; cats= dogs;">';
-		node = fixture.querySelector('meta');
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
 
-		assert.isTrue(checks['meta-viewport'].evaluate(node));
+		});
 
-	});
+		it('should return true if maximum-scale >= 5', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar, maximum-scale=5, cats= dogs;">';
+			var node = fixture.querySelector('meta');
 
-	it('should return false on maximum-scale < 5', function () {
-		fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs; user-scalable=yes; maximum-scale=4">';
-		var node = fixture.querySelector('meta');
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
 
-		assert.isFalse(checks['meta-viewport'].evaluate(node));
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar, maximum-scale=6, cats= dogs;">';
+			node = fixture.querySelector('meta');
 
-	});
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
 
-	it('should return true if neither user-scalable or maximum-scale are set', function () {
-		fixture.innerHTML = '<meta name="viewport" content="foo=bar; cats= dogs;">';
-		var node = fixture.querySelector('meta');
+		});
 
-		assert.isTrue(checks['meta-viewport'].evaluate(node));
+		it('should return false on maximum-scale < 5', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar, cats= dogs, user-scalable=yes, maximum-scale=4">';
+			var node = fixture.querySelector('meta');
+
+			assert.isFalse(checks['meta-viewport'].evaluate(node));
+
+		});
+
+		it('should return true if neither user-scalable or maximum-scale are set', function () {
+			fixture.innerHTML = '<meta name="viewport" content="foo=bar, cats= dogs">';
+			var node = fixture.querySelector('meta');
+
+			assert.isTrue(checks['meta-viewport'].evaluate(node));
+
+		});
 
 	});
 
