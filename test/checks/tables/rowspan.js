362abcbfef678892c74e834b71b5f9f2554b6db4
David Sturley <david.sturley@deque.com>
2014-06-04 09:35:52 -0400

    Invert logic and change message

--- test/checks/tables/rowspan.js
+++ test/checks/tables/rowspan.js
@@ -7,27 +7,27 @@ describe('rowspan', function () {
 		fixture.innerHTML = '';
 	});
 
-	it('should return false when rowSpan is not 1', function () {
+	it('should return true when rowSpan is not 1', function () {
 		fixture.innerHTML = '<table><tr><td rowspan="2">Oh noes</td></tr></table>';
+		var node = fixture.querySelector('td');
+
+		assert.isTrue(checks.rowspan.evaluate(node));
+
+	});
+
+	it('should return false if rowSpan is 1 - explicit', function () {
+		fixture.innerHTML = '<table><tr><td rowspan="1">Oh noes</td></tr></table>';
 		var node = fixture.querySelector('td');
 
 		assert.isFalse(checks.rowspan.evaluate(node));
 
 	});
 
-	it('should return true if rowSpan is 1 - explicit', function () {
-		fixture.innerHTML = '<table><tr><td rowspan="1">Oh noes</td></tr></table>';
-		var node = fixture.querySelector('td');
-
-		assert.isTrue(checks.rowspan.evaluate(node));
-
-	});
-
-	it('should return true if rowSpan is 1 - implicit', function () {
+	it('should return false if rowSpan is 1 - implicit', function () {
 		fixture.innerHTML = '<table><tr><td>Oh noes</td></tr></table>';
 		var node = fixture.querySelector('td');
 
-		assert.isTrue(checks.rowspan.evaluate(node));
+		assert.isFalse(checks.rowspan.evaluate(node));
 
 	});
 
