bc84cddc0dc93156bd793b4eeffa2a6077847794
David Sturley <david.sturley@deque.com>
2014-06-17 11:26:53 -0400

    Add bower again

--- test/unit/utils/index.js
+++ test/unit/utils/index.js
@@ -5,3 +5,19 @@ describe('utils', function () {
 		assert.isObject(window.utils);
 	});
 });
+
+describe('utils.escapeSelector', function () {
+	'use strict';
+
+	it('should be a function', function () {
+		assert.isFunction(utils.escapeSelector);
+	});
+});
+
+describe('utils.matchesSelector', function () {
+	'use strict';
+
+	it('should be a function', function () {
+		assert.isFunction(utils.matchesSelector);
+	});
+});
