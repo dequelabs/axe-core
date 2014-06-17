485c669f64baf6266edcfe3df97429ba3ee24970
David Sturley <david.sturley@deque.com>
2014-06-17 10:41:10 -0400

    Add bower.json; move escapeSelector, matchesSelector

---
+++ test/unit/utils/index.js
@@ -0,0 +1,17 @@
+
+describe('utils.escapeSelector', function () {
+	'use strict';
+
+	it('should be a function', function () {
+		assert.isFunction(kslib.utils.escapeSelector);
+	});
+
+});
+describe('utils.matchesSelector', function () {
+	'use strict';
+
+	it('should be a function', function () {
+		assert.isFunction(kslib.utils.matchesSelector);
+	});
+
+});
