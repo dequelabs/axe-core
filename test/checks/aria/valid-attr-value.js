b98acf921e7ee89d3a751ca69fd1e8d317d3a036
David Sturley <david.sturley@deque.com>
2014-06-19 09:33:45 -0400

    Add matches

--- test/checks/aria/valid-attr-value.js
+++ test/checks/aria/valid-attr-value.js
@@ -78,4 +78,22 @@ describe('aria-valid-attr-value', function () {
 		kslib.aria.validateAttrValue = orig;
 	});
 
+	describe('matches', function () {
+		it('should return false if an element has no attributes', function () {
+			var div = document.createElement('div');
+			assert.isFalse(checks['aria-valid-attr-value'].matches(div));
+		});
+		it('should return false if an element has no ARIA attributes', function () {
+			var div = document.createElement('div');
+			div.id = 'monkeys';
+			assert.isFalse(checks['aria-valid-attr-value'].matches(div));
+		});
+		it('should return true if an element has ARIA attributes', function () {
+			var div = document.createElement('div');
+			div.setAttribute('aria-bats', 'monkeys');
+			assert.isTrue(checks['aria-valid-attr-value'].matches(div));
+		});
+
+	});
+
 });
