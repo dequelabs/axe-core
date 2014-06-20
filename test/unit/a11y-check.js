c406724609d5722c1c2fd054303aaab18bcdbb70
David Sturley <david.sturley@deque.com>
2014-06-20 15:12:26 -0400

    KSD-108 and whatnot

--- test/unit/a11y-check.js
+++ test/unit/a11y-check.js
@@ -4,9 +4,13 @@ describe('dqre.a11yCheck', function () {
 	var orig,
 		results = [{
 			id: 'gimmeLabel',
-			result: dqre.constants.result.PASS,
 			details: [{
-				checks: [{data: 'minkey'}],
+				result: 'PASS',
+				checks: [{
+					type: 'PASS',
+					result: true,
+					data: 'minkey'
+				}],
 				node: {
 					selector: 'minkey',
 					frames: [],
@@ -15,20 +19,28 @@ describe('dqre.a11yCheck', function () {
 			}]
 		}, {
 			id: 'idkStuff',
-			result: dqre.constants.result.NA,
 			details: [{
-				checks: [{data: 'pillock'}],
+				result: 'FAIL',
+				checks: [{
+					type: 'PASS',
+					result: false,
+					data: 'pillock'
+				}],
 				node: {
 					selector: 'pillock',
-					frames: [],
+					frames: ['q', 'r'],
 					source: '<pillock>george bush</pillock>'
 				}
 			}]
 		}, {
 			id: 'bypass',
-			result: dqre.constants.result.WARN,
 			details: [{
-				checks: [{data: 'foon'}],
+				result: 'FAIL',
+				checks: [{
+					data: 'foon',
+					type: 'FAIL',
+					result: true
+				}],
 				node: {
 					selector: 'foon',
 					frames: [],
@@ -37,9 +49,13 @@ describe('dqre.a11yCheck', function () {
 			}]
 		}, {
 			id: 'blinky',
-			result: dqre.constants.result.FAIL,
 			details: [{
-				checks: [{data: 'clueso'}],
+				result: 'FAO:',
+				checks: [{
+					data: 'clueso',
+					type: 'FAIL',
+					result: true
+				}],
 				node: {
 					selector: 'clueso',
 					frames: ['a', 'b'],
@@ -59,20 +75,33 @@ describe('dqre.a11yCheck', function () {
 		dqre.run = orig;
 	});
 	it('should merge the dqre.run results into violations and passes', function (done) {
+		var orig = window.findHelp;
+		window.findHelp = function () {
+			return 'HALP US';
+		};
 		dqre.a11yCheck(document, {}, function (results) {
 			assert.isObject(results);
 			assert.isArray(results.violations);
-			assert.lengthOf(results.violations, 1);
+			assert.lengthOf(results.violations, 2);
 			assert.isArray(results.passes);
 			assert.lengthOf(results.passes, 2);
+
+			window.findHelp = orig;
 			done();
 		});
 	});
 	it('should add the rule id to the rule result', function (done) {
+		var orig = window.findHelp;
+		window.findHelp = function () {
+			return 'HALP US';
+		};
 		dqre.a11yCheck(document, {}, function (results) {
-			assert.equal(results.violations[0].id, 'blinky');
+			assert.equal(results.violations[0].id, 'idkStuff');
+			assert.equal(results.violations[1].id, 'bypass');
 			assert.equal(results.passes[0].id, 'gimmeLabel');
-			assert.equal(results.passes[1].id, 'idkStuff');
+			assert.equal(results.passes[1].id, 'blinky');
+
+			window.findHelp = orig;
 			done();
 		});
 	});
@@ -90,9 +119,10 @@ describe('dqre.a11yCheck', function () {
 			assert.ok(results.violations[0].nodes);
 			assert.equal(results.violations[0].nodes.length, 1);
 			assert.ok(results.violations[0].nodes[0].checks);
-			assert.equal(results.violations[0].nodes[0].checks[0].data, 'clueso');
+			assert.equal(results.violations[0].nodes[0].checks[0].data, 'pillock');
+			assert.equal(results.violations[1].nodes[0].checks[0].data, 'foon');
 			assert.equal(results.passes[0].nodes[0].checks[0].data, 'minkey');
-			assert.equal(results.passes[1].nodes[0].checks[0].data, 'pillock');
+			assert.equal(results.passes[1].nodes[0].checks[0].data, 'clueso');
 			done();
 		});
 	});
@@ -100,9 +130,10 @@ describe('dqre.a11yCheck', function () {
 		dqre.a11yCheck(document, {}, function (results) {
 			assert.ok(results.violations[0].nodes);
 			assert.equal(results.violations[0].nodes.length, 1);
-			assert.equal(results.violations[0].nodes[0].html, '<clueso>nincompoop</clueso>');
+			assert.equal(results.violations[0].nodes[0].html, '<pillock>george bush</pillock>');
+			assert.equal(results.violations[1].nodes[0].html, '<foon>telephone</foon>');
 			assert.equal(results.passes[0].nodes[0].html, '<minkey>chimp</minky>');
-			assert.equal(results.passes[1].nodes[0].html, '<pillock>george bush</pillock>');
+			assert.equal(results.passes[1].nodes[0].html, '<clueso>nincompoop</clueso>');
 			done();
 		});
 	});
@@ -121,7 +152,7 @@ describe('dqre.a11yCheck', function () {
 		dqre.a11yCheck(document, {}, function (results) {
 			assert.ok(results.violations[0].nodes);
 			assert.equal(results.violations[0].nodes.length, 1);
-			assert.deepEqual(results.violations[0].nodes[0].target, ['a', 'b', 'clueso']);
+			assert.deepEqual(results.violations[0].nodes[0].target, ['q', 'r', 'pillock']);
 			done();
 		});
 	});
@@ -256,7 +287,7 @@ describe('failureSummary', function () {
 
 	});
 
-	it('should skip checks that have no help', function () {
+	it('should NOT skip checks that have no help', function () {
 		var summary = failureSummary({
 			result: 'FAIL',
 			checks: [{
@@ -266,7 +297,7 @@ describe('failureSummary', function () {
 			}]
 		});
 
-		assert.deepEqual(summary, []);
+		assert.deepEqual(summary, ['']);
 
 	});
 });
