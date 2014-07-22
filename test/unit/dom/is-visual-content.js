d59a57f7073840f1915dc3355ac5f247d8b87ab0
Mike Kozlowski <mike.kozlowski@deque.com>
2014-07-22 13:28:31 -0400

    Added docs and renamed

--- test/unit/dom/is-visual-content.js
+++ test/unit/dom/is-visual-content.js
@@ -1,4 +1,4 @@
-describe('dom.hasVisualContent', function () {
+describe('dom.isVisualContent', function () {
 	'use strict';
 
 	var fixture = document.getElementById('fixture');
@@ -7,90 +7,90 @@ describe('dom.hasVisualContent', function () {
 		document.getElementById('fixture').innerHTML = '';
 	});
 
-	describe('hasVisualContent', function () {
+	describe('isVisualContent', function () {
 		it('should return true for img', function () {
 			fixture.innerHTML = '<img src="something.jpg">';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for iframe', function () {
 			fixture.innerHTML = '<iframe src="something.html"></iframe>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for object', function () {
 			fixture.innerHTML = '<object data="something.swf"></object>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for video', function () {
 			fixture.innerHTML = '<video src="something.mp4"></video>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for audio', function () {
 			fixture.innerHTML = '<audio src="something.mp3"></audio>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for canvas', function () {
 			fixture.innerHTML = '<canvas></canvas>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for svg', function () {
 			fixture.innerHTML = '<svg></svg>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for math', function () {
 			fixture.innerHTML = '<math></math>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for button', function () {
 			fixture.innerHTML = '<button></button>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for select', function () {
 			fixture.innerHTML = '<select></select>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for textarea', function () {
 			fixture.innerHTML = '<textarea></textarea>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for keygen', function () {
 			fixture.innerHTML = '<keygen>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for progress', function () {
 			fixture.innerHTML = '<progress></progress>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for meter', function () {
 			fixture.innerHTML = '<meter></meter>';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return true for non-hidden input', function () {
 			fixture.innerHTML = '<input type="text">';
-			assert.isTrue(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isTrue(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return false for hidden input', function () {
 			fixture.innerHTML = '<input type="hidden">';
-			assert.isFalse(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isFalse(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 		it('should return false for p', function () {
 			fixture.innerHTML = '<p>Paragraph!</p>';
-			assert.isFalse(kslib.dom.hasVisualContent(fixture.children[0]));
+			assert.isFalse(kslib.dom.isVisualContent(fixture.children[0]));
 		});
 
 	});
