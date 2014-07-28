e034236c2280804348cd5d338f8e425fd871ec26
Mike Kozlowski <mike.kozlowski@deque.com>
2014-07-28 13:46:36 -0400

    Fixes for Firefox

--- test/unit/dom/get-background-color.js
+++ test/unit/dom/get-background-color.js
@@ -7,6 +7,19 @@ describe('dom.getBackgroundColor', function () {
 		document.getElementById('fixture').innerHTML = '';
 	});
 
+	it('should return the blended color if it has no background set', function () {
+		fixture.innerHTML = '<div style="height: 40px; width: 30px; background-color: #800000;">' +
+			'<div id="target" style="height: 20px; width: 15px;">' +
+			'</div></div>';
+		var target = fixture.querySelector('#target');
+		var actual = kslib.dom.getBackgroundColor(target);
+		var expected = new kslib.color.Color(128, 0, 0, 1);
+		assert.closeTo(actual.red, expected.red, 0.5);
+		assert.closeTo(actual.green, expected.green, 0.5);
+		assert.closeTo(actual.blue, expected.blue, 0.5);
+		assert.closeTo(actual.alpha, expected.alpha, 0.1);
+	});
+
 	it('should return the blended color if it has alpha set', function () {
 		fixture.innerHTML = '<div style="height: 40px; width: 30px; background-color: #800000;">' +
 			'<div id="target" style="height: 20px; width: 15px; background-color: rgba(0, 128, 0, 0.5);">' +
