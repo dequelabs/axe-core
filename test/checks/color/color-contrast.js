0d02cfa3b8a321b099b5a18822d9c643177f09a1
Mike Kozlowski <mike.kozlowski@deque.com>
2014-07-28 17:15:32 -0400

    Add richer messaging

--- test/checks/color/color-contrast.js
+++ test/checks/color/color-contrast.js
@@ -3,64 +3,89 @@ describe('color-contrast', function () {
 
 	var fixture = document.getElementById('fixture');
 
+	var checkContext = {
+		_relatedNodes: [],
+		_data: null,
+		data: function (d) {
+			this._data = d;
+		},
+		relatedNodes: function (rn) {
+			this._relatedNodes = rn;
+		}
+	};
+
 	afterEach(function () {
-		document.getElementById('fixture').innerHTML = '';
+		fixture.innerHTML = '';
+		checkContext._relatedNodes = [];
+		checkContext._data = null;
+	});
+
+	it('should return store the proper values in data', function () {
+		fixture.innerHTML = '<div style="color: black; background-color: white; font-size: 14pt"><b id="target">' +
+			'My text</b></div>';
+		var target = fixture.querySelector('#target');
+		assert.isTrue(checks['color-contrast'].evaluate.call(checkContext, target));
+		var white = new kslib.color.Color(255, 255, 255, 1);
+		var black = new kslib.color.Color(0, 0, 0, 1);
+		assert.equal(checkContext._data.bgColor, white.toHexString());
+		assert.equal(checkContext._data.fgColor, black.toHexString());
+		assert.equal(checkContext._data.contrastRatio, '21.00');
 	});
 
 	it('should return true when there is sufficient contrast because of bold tag', function () {
 		fixture.innerHTML = '<div style="color: gray; background-color: white; font-size: 14pt"><b id="target">' +
 			'My text</b></div>';
 		var target = fixture.querySelector('#target');
-		assert.isTrue(checks['color-contrast'].evaluate(target));
+		assert.isTrue(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should return true when there is sufficient contrast because of font weight', function () {
 		fixture.innerHTML = '<div style="color: gray; background-color: white; font-size: 14pt; font-weight: bold" id="target">' +
 			'My text</div>';
 		var target = fixture.querySelector('#target');
-		assert.isTrue(checks['color-contrast'].evaluate(target));
+		assert.isTrue(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should return true when there is sufficient contrast because of font size', function () {
 		fixture.innerHTML = '<div style="color: gray; background-color: white; font-size: 18pt;" id="target">' +
 			'My text</div>';
 		var target = fixture.querySelector('#target');
-		assert.isTrue(checks['color-contrast'].evaluate(target));
+		assert.isTrue(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should return false when there is not sufficient contrast because of font size', function () {
 		fixture.innerHTML = '<div style="color: gray; background-color: white; font-size: 8pt;" id="target">' +
 			'My text</div>';
 		var target = fixture.querySelector('#target');
-		assert.isFalse(checks['color-contrast'].evaluate(target));
+		assert.isFalse(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should return true when there is sufficient contrast with explicit transparency', function () {
 		fixture.innerHTML = '<div style="color: white; background-color: white;">' +
 			'<span style="color: black; background-color: rgba(0,0,0,0)" id="target">My text</span></div>';
 		var target = fixture.querySelector('#target');
-		assert.isTrue(checks['color-contrast'].evaluate(target));
+		assert.isTrue(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should return true when there is sufficient contrast with implicit transparency', function () {
 		fixture.innerHTML = '<div style="color: white; background-color: white;">' +
 			'<span style="color: black;" id="target">My text</span></div>';
 		var target = fixture.querySelector('#target');
-		assert.isTrue(checks['color-contrast'].evaluate(target));
+		assert.isTrue(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should return true when there is sufficient contrast', function () {
 		fixture.innerHTML = '<div style="color: black; background-color: white;" id="target">' +
 			'My text</div>';
 		var target = fixture.querySelector('#target');
-		assert.isTrue(checks['color-contrast'].evaluate(target));
+		assert.isTrue(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should return false when there is not sufficient contrast', function () {
 		fixture.innerHTML = '<div style="color: yellow; background-color: white;" id="target">' +
 			'My text</div>';
 		var target = fixture.querySelector('#target');
-		assert.isFalse(checks['color-contrast'].evaluate(target));
+		assert.isFalse(checks['color-contrast'].evaluate.call(checkContext, target));
 	});
 
 	it('should not match when there is no text', function () {
