2b984b3c2870971bbc8a434ff5f50ad53b781afc
David Sturley <david.sturley@deque.com>
2014-08-06 16:03:57 -0400

    Attempt #3

--- build/tasks/rules.js
+++ build/tasks/rules.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
 		return string.replace(/"(evaluate|after|gather|matches)":\s*("[^"]+")/g, function (m, p1, p2) {
 			return m.replace(p2, getSource(p2.replace(/^"|"$/g, ''), p1));
 		}).replace(/"(function anonymous\([\s\S]+?\) {)([\s\S]+?)(})"/g, function (m, p1, p2, p3) {
-			return p1 + (p2.replace(/\\(n|r|t)/g, ' ').replace(/\\\\/g, '\\')) + p3;
+			return JSON.parse(m);
 		});
 	}
 
