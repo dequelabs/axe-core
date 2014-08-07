532e69bcba8b7f455122c67072598b7c6ba7d27d
David Sturley <david.sturley@deque.com>
2014-08-07 16:11:18 -0400

    PhantomJS example

---
+++ doc/examples/phantomjs/kensington.js
@@ -0,0 +1,40 @@
+/*global phantom */
+
+
+var args = require('system').args;
+var fs = require('fs');
+var page = require('webpage').create();
+
+if (args.length < 2) {
+	console.log('kensington.js accepts 1 argument, the URL to test');
+}
+
+page.open(args[1], function (status) {
+	// Check for page load success
+	if (status !== 'success') {
+		console.log('Unable to access network');
+		return;
+	}
+
+	page.injectJs('../../../kensington.js');
+	page.framesName.forEach(function (name) {
+		page.switchToFrame(name);
+		page.injectJs('../../../kensington.js');
+	});
+	page.switchToMainFrame();
+	page.evaluateAsync(function () {
+		/*global window, dqre */
+		dqre.a11yCheck(window.document, null, function (results) {
+			window.callPhantom(results);
+		});
+	});
+
+
+	page.onCallback = function (msg) {
+		if (args[2]) {
+			fs.write(args[2], JSON.stringify(msg, null, '  '), 'w');
+		}
+
+		phantom.exit();
+	};
+});
