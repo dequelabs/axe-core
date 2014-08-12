ce5dcb400db38590914f472935838a75beea30ff
Mike Kozlowski <mike.kozlowski@deque.com>
2014-08-12 15:03:54 -0400

    Fix alpha blending order, positioning of checks, and add related nodes

--- test/unit/dom/get-background-color.js
+++ test/unit/dom/get-background-color.js
@@ -8,16 +8,19 @@ describe('dom.getBackgroundColor', function () {
 	});
 
 	it('should return the blended color if it has no background set', function () {
-		fixture.innerHTML = '<div style="height: 40px; width: 30px; background-color: #800000;">' +
+		fixture.innerHTML = '<div id="parent" style="height: 40px; width: 30px; background-color: #800000;">' +
 			'<div id="target" style="height: 20px; width: 15px;">' +
 			'</div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var parent = fixture.querySelector('#parent');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		var expected = new kslib.color.Color(128, 0, 0, 1);
 		assert.closeTo(actual.red, expected.red, 0.5);
 		assert.closeTo(actual.green, expected.green, 0.5);
 		assert.closeTo(actual.blue, expected.blue, 0.5);
 		assert.closeTo(actual.alpha, expected.alpha, 0.1);
+		assert.deepEqual(bgNodes, [target, parent]);
 	});
 
 	it('should return the blended color if it is transparent and positioned', function () {
@@ -31,62 +34,124 @@ describe('dom.getBackgroundColor', function () {
 			'width: 15px; background-color: rgba(0, 128, 0, 0.5);">' +
 			'</div></div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var pos = fixture.querySelector('#pos');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		var expected = new kslib.color.Color(64, 64, 0, 1);
 		if (kslib.dom.supportsElementsFromPoint(target.ownerDocument)) {
 			assert.closeTo(actual.red, expected.red, 0.5);
 			assert.closeTo(actual.green, expected.green, 0.5);
 			assert.closeTo(actual.blue, expected.blue, 0.5);
 			assert.closeTo(actual.alpha, expected.alpha, 0.1);
+			assert.deepEqual(bgNodes, [target, pos]);
+		} else {
+			assert.isNull(actual);
+		}
+	});
+
+	it('should do alpha blending from the back forward', function () {
+		fixture.innerHTML = '<div id="under" style="height: 20px; width: 15px; background-color: #800000;">' +
+			'<div id="transparent" style="height: 20px; width: 15px; background-color: rgba(0, 0, 0, 0);">' +
+			'<div id="target" style="height: 20px; width: 15px; background-color: rgba(0, 128, 0, 0.5);">' +
+			'</div></div></div>';
+		var target = fixture.querySelector('#target');
+		var transparent = fixture.querySelector('#transparent');
+		var under = fixture.querySelector('#under');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
+		var expected = new kslib.color.Color(64, 64, 0, 1);
+		assert.closeTo(actual.red, expected.red, 0.5);
+		assert.closeTo(actual.green, expected.green, 0.5);
+		assert.closeTo(actual.blue, expected.blue, 0.5);
+		assert.closeTo(actual.alpha, expected.alpha, 0.1);
+		assert.deepEqual(bgNodes, [target, transparent, under]);
+	});
+
+	it('should only look at what is underneath original element when blended and positioned', function () {
+		fixture.innerHTML = '<div style="position: absolute; top: 0px; left: 0px; height: 100px; ' +
+			'width: 90px; background-color: #000080;">' +
+			'<div id="under" style="position: absolute; top: 50px; left: 40px; height: 40px; ' +
+			'width: 30px; background-color: #800000;"></div>' +
+			'<div id="pos" style="position: absolute; top: 0px; left: 0px; height: 90px; ' +
+			'width: 70px; background-color: rgba(0, 0, 0, 0);"></div>' +
+			'<div id="parent" style="position: absolute; top: 0px; left: 0px; height: 40px; ' +
+			'width: 30px; background-color: #ffffff">' +
+			'<div id="target" style="position: absolute; top: 60px; left: 45px; height: 20px; ' +
+			'width: 15px; background-color: rgba(0, 128, 0, 0.5);">' +
+			'</div></div></div>';
+		var target = fixture.querySelector('#target');
+		var pos = fixture.querySelector('#pos');
+		var under = fixture.querySelector('#under');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
+		var expected = new kslib.color.Color(64, 64, 0, 1);
+		if (kslib.dom.supportsElementsFromPoint(target.ownerDocument)) {
+			assert.closeTo(actual.red, expected.red, 0.5);
+			assert.closeTo(actual.green, expected.green, 0.5);
+			assert.closeTo(actual.blue, expected.blue, 0.5);
+			assert.closeTo(actual.alpha, expected.alpha, 0.1);
+			assert.deepEqual(bgNodes, [target, pos, under]);
 		} else {
 			assert.isNull(actual);
 		}
 	});
 
 	it('should return the proper blended color if it has alpha set', function () {
-		fixture.innerHTML = '<div style="height: 40px; width: 30px; background-color: #800000;">' +
+		fixture.innerHTML = '<div id="parent" style="height: 40px; width: 30px; background-color: #800000;">' +
 			'<div id="target" style="height: 20px; width: 15px; background-color: rgba(0, 128, 0, 0.5);">' +
 			'</div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var parent = fixture.querySelector('#parent');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		var expected = new kslib.color.Color(64, 64, 0, 1);
 		assert.closeTo(actual.red, expected.red, 0.5);
 		assert.closeTo(actual.green, expected.green, 0.5);
 		assert.closeTo(actual.blue, expected.blue, 0.5);
 		assert.closeTo(actual.alpha, expected.alpha, 0.1);
+		assert.deepEqual(bgNodes, [target, parent]);
 	});
 
 	it('should return the blended color if it has opacity set', function () {
-		fixture.innerHTML = '<div style="height: 40px; width: 30px; background-color: #800000;">' +
+		fixture.innerHTML = '<div id="parent" style="height: 40px; width: 30px; background-color: #800000;">' +
 			'<div id="target" style="height: 20px; width: 15px; opacity: 0.5; background-color: green;">' +
 			'</div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var parent = fixture.querySelector('#parent');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		var expected = new kslib.color.Color(64, 64, 0, 1);
 		assert.equal(actual.red, expected.red);
 		assert.equal(actual.green, expected.green);
 		assert.equal(actual.blue, expected.blue);
 		assert.equal(actual.alpha, expected.alpha);
+		assert.deepEqual(bgNodes, [target, parent]);
 	});
 
 	it('should return null if containing parent has a background image and is non-opaque', function () {
-		fixture.innerHTML = '<div style="height: 40px; width: 30px;' +
+		fixture.innerHTML = '<div id="parent" style="height: 40px; width: 30px;' +
 			'background-color: #800000; background-image: url(image.png);">' +
 			'<div id="target" style="height: 20px; width: 15px; background-color: green; opacity: 0.5;">' +
 			'</div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var parent = fixture.querySelector('#parent');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		assert.isNull(actual);
+		assert.deepEqual(bgNodes, [target, parent]);
 	});
 
 	it('should return null if parent does not fully contain element', function () {
 		fixture.innerHTML = '<div style="background-color: white;">' +
-			'<div style="height: 10px; width: 30px; background-color: #800000;">' +
+			'<div id="parent" style="height: 10px; width: 30px; background-color: #800000;">' +
 			'<div id="target" style="height: 20px; width: 15px; background-color: green; opacity: 0.5;">' +
 			'</div></div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var parent = fixture.querySelector('#parent');
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		assert.isNull(actual);
+		assert.deepEqual(bgNodes, [target, parent]);
 	});
 
 
@@ -104,8 +169,10 @@ describe('dom.getBackgroundColor', function () {
 			'<div id="target" style="height: 20px; width: 15px; background-color: green; background-image: url(image.png);">' +
 			'</div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		assert.isNull(actual);
+		assert.deepEqual(bgNodes, [target]);
 	});
 
 	it('should return the bgcolor if it is solid', function () {
@@ -113,12 +180,14 @@ describe('dom.getBackgroundColor', function () {
 			'<div id="target" style="height: 20px; width: 15px; background-color: green;">' +
 			'</div></div>';
 		var target = fixture.querySelector('#target');
-		var actual = kslib.dom.getBackgroundColor(target);
+		var bgNodes = [];
+		var actual = kslib.dom.getBackgroundColor(target, bgNodes);
 		var expected = new kslib.color.Color(0, 128, 0, 1);
 		assert.equal(actual.red, expected.red);
 		assert.equal(actual.green, expected.green);
 		assert.equal(actual.blue, expected.blue);
 		assert.equal(actual.alpha, expected.alpha);
+		assert.deepEqual(bgNodes, [target]);
 	});
 
 });
