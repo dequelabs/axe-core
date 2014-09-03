f910468fa45bd2fa614f4fd99806570f9ccc8383
David Sturley <david.sturley@deque.com>
2014-09-03 17:22:22 -0400

    KSD-195 Reduce violations and remove nogroup, noname

--- test/checks/forms/labelledby.js
+++ test/checks/forms/labelledby.js
@@ -3,61 +3,121 @@ describe('labelledby', function () {
 
 	var fixture = document.getElementById('fixture');
 
+	var checkContext = {
+		_data: null,
+		data: function (d) {
+			this._data = d;
+		}
+	};
+
 	afterEach(function () {
 		fixture.innerHTML = '';
+		checkContext._data = null;
 	});
 
-	it('should return false if the radio element has no name', function () {
-		fixture.innerHTML = '<input type="radio" id="target">Choice one';
-		var node = fixture.querySelector('#target');
-		assert.isFalse(checks.labelledby.evaluate(node, 'radio'));
-	});
+	function tests(type) {
 
-	it('should return false if there is only one radio element with the same name', function () {
-		fixture.innerHTML = '<input type="radio" id="target" name="uniqueradioname">Choice one<input type="radio" name="differentname">Choice 1a';
-		var node = fixture.querySelector('#target');
-		assert.isFalse(checks.labelledby.evaluate(node, 'radio'));
-	});
+		return function () {
 
-	it('should return false if there are two ungrouped radio elements with the same name', function () {
-		fixture.innerHTML = '<input type="radio" id="target" name="uniqueradioname">Choice one<input type="radio" name="uniqueradioname">Choice 1a';
-		var node = fixture.querySelector('#target');
-		assert.isFalse(checks.labelledby.evaluate(node, 'radio'));
-	});
+			it('should return true if there is only one ' + type + ' element with the same name', function () {
+				fixture.innerHTML = '<input type="' + type + '" id="target" name="uniqueyname">' +
+					'<input type="' + type + '" name="differentname">';
 
-	it('should return false if there are ungrouped radio elements with the same name and without shared labelledby', function () {
-		fixture.innerHTML = '<input type="radio" id="target" aria-labelledby="unique one" name="uniqueradioname">Choice one' +
-			'<input type="radio" aria-labelledby="notshared two" name="uniqueradioname">Choice 1a<input type="radio" aria-labelledby="different three" name="uniqueradioname">Choice 1b';
-		var node = fixture.querySelector('#target');
-		assert.isFalse(checks.labelledby.evaluate(node, 'radio'));
-	});
+				var node = fixture.querySelector('#target');
+				assert.isTrue(checks.labelledby.evaluate.call(checkContext, node));
+			});
 
-	it('should return false if there are ungrouped radio elements with the same name and with shared labelledby pointing to no real node', function () {
-		fixture.innerHTML = '<input type="radio" id="target" aria-labelledby="shared one" name="uniqueradioname">Choice one' +
-			'<input type="radio" aria-labelledby="shared two" name="uniqueradioname">Choice 1a<input type="radio" aria-labelledby="shared three" name="uniqueradioname">Choice 1b';
-		var node = fixture.querySelector('#target');
-		assert.isFalse(checks.labelledby.evaluate(node, 'radio'));
-	});
+			it('should return false if there are two ungrouped ' + type + ' elements with the same name', function () {
+				fixture.innerHTML = '<input type="' + type + '" id="target" name="uniqueyname">' +
+					'<input type="' + type + '" name="uniqueyname">';
 
-	it('should return false if there are ungrouped radio elements with the same name and with shared labelledby pointing to an empty node', function () {
-		fixture.innerHTML = '<p id="shared"></p><input type="radio" id="target" aria-labelledby="shared one" name="uniqueradioname">Choice one' +
-			'<input type="radio" aria-labelledby="shared two" name="uniqueradioname">Choice 1a<input type="radio" aria-labelledby="shared three" name="uniqueradioname">Choice 1b';
-		var node = fixture.querySelector('#target');
-		assert.isFalse(checks.labelledby.evaluate(node, 'radio'));
-	});
+				var node = fixture.querySelector('#target');
+				assert.isFalse(checks.labelledby.evaluate.call(checkContext, node));
+				assert.deepEqual(checkContext._data, {
+					name: 'uniqueyname',
+					type: type
+				});
+			});
 
-	it('should return true if there are ungrouped radio elements with the same name and with shared labelledby pointing to a node with text content', function () {
-		fixture.innerHTML = '<p id="shared">Label</p><input type="radio" id="target" aria-labelledby="shared one" name="uniqueradioname">Choice one' +
-			'<input type="radio" aria-labelledby="shared two" name="uniqueradioname">Choice 1a<input type="radio" aria-labelledby="shared three" name="uniqueradioname">Choice 1b';
-		var node = fixture.querySelector('#target');
-		assert.isTrue(checks.labelledby.evaluate(node, 'radio'));
-	});
+			it('should return false if there are ungrouped ' + type + ' elements with the same name and without shared labelledby', function () {
+				fixture.innerHTML = '<input type="' + type + '" id="target" aria-labelledby="unique one" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="notshared two" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="different three" name="uniqueyname">';
+				var node = fixture.querySelector('#target');
+				assert.isFalse(checks.labelledby.evaluate.call(checkContext, node));
+				assert.deepEqual(checkContext._data, {
+					name: 'uniqueyname',
+					type: type
+				});
+			});
 
-	it('should return true if there are ungrouped radio elements with the same name and with shared labelledby pointing to a node with text content - SPECIAL CHARACTERS', function () {
-		fixture.innerHTML = '<p id="shared">Label</p><input type="radio" id="target" aria-labelledby="shared one" name="s$.#0">Choice one' +
-			'<input type="radio" aria-labelledby="shared two" name="s$.#0">Choice 1a<input type="radio" aria-labelledby="shared three" name="s$.#0">Choice 1b';
-		var node = fixture.querySelector('#target');
-		assert.isTrue(checks.labelledby.evaluate(node, 'radio'));
-	});
+			it('should return false if there are ungrouped ' + type + ' elements with the same name and with shared labelledby ' +
+				'pointing to no real node', function () {
+
+				fixture.innerHTML = '<input type="' + type + '" id="target" aria-labelledby="shared one" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="shared two" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="shared three" name="uniqueyname">';
+
+				var node = fixture.querySelector('#target');
+				assert.isFalse(checks.labelledby.evaluate.call(checkContext, node));
+				assert.deepEqual(checkContext._data, {
+					name: 'uniqueyname',
+					type: type
+				});
+			});
+
+			it('should return false if there are ungrouped ' + type + ' elements with the same name and with shared labelledby ' +
+				'pointing to an empty node', function () {
+				fixture.innerHTML = '<p id="shared"></p>' +
+					'<input type="' + type + '" id="target" aria-labelledby="shared one" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="shared two" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="shared three" name="uniqueyname">';
+
+				var node = fixture.querySelector('#target');
+				assert.isFalse(checks.labelledby.evaluate.call(checkContext, node));
+				assert.deepEqual(checkContext._data, {
+					name: 'uniqueyname',
+					type: type
+				});
+			});
+
+			it('should return true if there are ungrouped ' + type + ' elements with the same name and with shared labelledby' +
+				'pointing to a node with text content', function () {
+
+				fixture.innerHTML = '<p id="shared">Label</p>' +
+					'<input type="' + type + '" id="target" aria-labelledby="shared one" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="shared two" name="uniqueyname">' +
+					'<input type="' + type + '" aria-labelledby="shared three" name="uniqueyname">';
+
+				var node = fixture.querySelector('#target');
+				assert.isTrue(checks.labelledby.evaluate.call(checkContext, node));
+				assert.deepEqual(checkContext._data, {
+					name: 'uniqueyname',
+					type: type
+				});
+			});
+
+			it('should return true if there are ungrouped ' + type + ' elements with the same name and with shared labelledby ' +
+				'pointing to a node with text content - SPECIAL CHARACTERS', function () {
+
+				fixture.innerHTML = '<p id="shared">Label</p>' +
+					'<input type="' + type + '" id="target" aria-labelledby="shared one" name="s$.#0">' +
+					'<input type="' + type + '" aria-labelledby="shared two" name="s$.#0">' +
+					'<input type="' + type + '" aria-labelledby="shared three" name="s$.#0">';
+
+				var node = fixture.querySelector('#target');
+				assert.isTrue(checks.labelledby.evaluate.call(checkContext, node));
+				assert.deepEqual(checkContext._data, {
+					name: 's$.#0',
+					type: type
+				});
+			});
+
+		};
+
+	}
+
+	describe('radio', tests('radio'));
+	describe('checkbox', tests('checkbox'));
 
 });
