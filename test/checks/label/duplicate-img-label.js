76185b94b14d1d860a2b17fea7b24ed4af8085e7
Mike Kozlowski <mike.kozlowski@deque.com>
2014-09-05 15:41:53 -0400

    Changes to button/link name rules

---
+++ test/checks/label/duplicate-img-label.js
@@ -0,0 +1,45 @@
+describe('duplicate-img-label', function () {
+	'use strict';
+
+	var fixture = document.getElementById('fixture');
+
+	afterEach(function () {
+		fixture.innerHTML = '';
+	});
+
+	it('should return false if no img is present', function () {
+		fixture.innerHTML = '<button id="target">Plain text</button>';
+		var node = fixture.querySelector('#target');
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+	});
+
+	it('should return false if no text is present', function () {
+		fixture.innerHTML = '<button id="target"><img alt="Plain text"></button>';
+		var node = fixture.querySelector('#target');
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+	});
+
+	it('should return false if aria-label duplicates img alt', function () {
+		fixture.innerHTML = '<button id="target" aria-label="Plain text"><img alt="Plain text"></button>';
+		var node = fixture.querySelector('#target');
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+	});
+
+	it('should return false if img and text have different text', function () {
+		fixture.innerHTML = '<button id="target"><img alt="Alt text">Plain text</button>';
+		var node = fixture.querySelector('#target');
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+	});
+
+	it('should return true if img and text have the same text', function () {
+		fixture.innerHTML = '<button id="target"><img alt="Plain text">Plain text</button>';
+		var node = fixture.querySelector('#target');
+		assert.isTrue(checks['duplicate-img-label'].evaluate(node));
+	});
+
+	it('should return false if img and text have superset/subset text', function () {
+		fixture.innerHTML = '<button id="target"><img alt="Plain text and more">Plain text</button>';
+		var node = fixture.querySelector('#target');
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+	});
+});
