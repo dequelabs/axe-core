2867e583e968d98bfc6cab864cfba7f14dce87b2
Mike Kozlowski <mike.kozlowski@deque.com>
2014-09-08 15:20:00 -0400

    KSD-203 Check for timed meta refresh

---
+++ test/checks/navigation/meta-refresh.js
@@ -0,0 +1,36 @@
+describe('meta-refresh', function () {
+	'use strict';
+
+	var fixture = document.getElementById('fixture');
+
+	afterEach(function () {
+		fixture.innerHTML = '';
+	});
+	describe('; separator', function () {
+
+		it('should return false if content value is not 0', function () {
+			fixture.innerHTML = '<meta name="refresh" content="300">';
+			var node = fixture.querySelector('meta');
+
+			assert.isFalse(checks['meta-refresh'].evaluate(node));
+
+		});
+
+		it('should return false if content value does not start with 0', function () {
+			fixture.innerHTML = '<meta name="refresh" content="300;URL=something.html">';
+			var node = fixture.querySelector('meta');
+
+			assert.isFalse(checks['meta-refresh'].evaluate(node));
+
+		});
+
+		it('should return true if there is no content value', function () {
+			fixture.innerHTML = '<meta name="refresh">';
+			var node = fixture.querySelector('meta');
+
+			assert.isTrue(checks['meta-refresh'].evaluate(node));
+
+		});
+
+	});
+});
