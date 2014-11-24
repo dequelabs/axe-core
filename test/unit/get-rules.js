44df864d459b0c14c823986202bc923aaac8f3ab
David Sturley <david.sturley@deque.com>
2014-11-24 13:19:16 -0500

    KSD-430 - If given no tags, getRules should return all rules

--- test/unit/get-rules.js
+++ test/unit/get-rules.js
@@ -41,4 +41,20 @@ describe('dqre.getRules', function () {
 		assert.lengthOf(retValue, 0);
 	});
 
+	it('should return all rules if given no tags - undefined', function () {
+		var retValue = dqre.getRules();
+		assert.equal(retValue[0].ruleId, 'awesomeRule1');
+		assert.equal(retValue[0].description, 'some interesting information');
+		assert.equal(retValue[1].ruleId, 'awesomeRule2');
+		assert.equal(retValue[1].description, 'also some interesting information');
+	});
+
+	it('should return all rules if given empty array', function () {
+		var retValue = dqre.getRules([]);
+		assert.equal(retValue[0].ruleId, 'awesomeRule1');
+		assert.equal(retValue[0].description, 'some interesting information');
+		assert.equal(retValue[1].ruleId, 'awesomeRule2');
+		assert.equal(retValue[1].description, 'also some interesting information');
+	});
+
 });
