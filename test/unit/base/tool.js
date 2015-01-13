dde02af824cd63f927abbf56c3b474bf910f05ed
David Sturley <david.sturley@deque.com>
2015-01-13 12:36:01 -0500

    Add tools; refactor configure to reduce code duplication

---
+++ test/unit/base/tool.js
@@ -0,0 +1,122 @@
+/*global Tool */
+describe('Tool', function () {
+  'use strict';
+
+  var fixture = document.getElementById('fixture');
+
+  afterEach(function () {
+    fixture.innerHTML = '';
+  });
+
+  it('should be a function', function () {
+    assert.isFunction(Tool);
+  });
+
+  it('should default active to false', function () {
+    var tool = new Tool({});
+    assert.isFalse(tool.active);
+  });
+
+  describe('prototype', function () {
+    describe('run', function () {
+      it('should be a function', function () {
+        assert.isFunction(Tool.prototype.run);
+      });
+
+      it('should set active to true', function () {
+        var tool = new Tool({
+          run: function () {}
+        });
+        tool.run(null, null, function () {
+          assert.isTrue(tool.active);
+        });
+      });
+
+      it('should call `run` as defined on spec object', function () {
+        var div = document.createElement('div');
+        fixture.appendChild(div);
+        var expectedCb = function () {};
+        var called = false;
+
+        var tool = new Tool({
+          run: function (node, options, callback) {
+            assert.equal(node, div);
+            assert.equal(options, 'monkeys');
+            assert.equal(callback, expectedCb);
+            called = true;
+          }
+        });
+
+        tool.run(div, 'monkeys', expectedCb);
+        assert.isTrue(called);
+      });
+    });
+    describe('cleanup', function () {
+      it('should be a function', function () {
+        assert.isFunction(Tool.prototype.cleanup);
+      });
+
+      it('should set active to false', function () {
+        var tool = new Tool({
+          cleanup: function () {}
+        });
+        tool.active = true;
+        tool.cleanup(function () {});
+        assert.isFalse(tool.active);
+      });
+
+      it('should call `cleanup` as defined on spec object', function () {
+        var expectedCb = function () {};
+        var called = false;
+
+        var tool = new Tool({
+          cleanup: function (callback) {
+            assert.equal(callback, expectedCb);
+            called = true;
+          }
+        });
+
+        tool.cleanup(expectedCb);
+        assert.isTrue(called);
+      });
+    });
+  });
+
+
+  describe('spec object', function () {
+
+    describe('.id', function () {
+      it('should be set', function () {
+        var spec = {
+          id: 'monkeys'
+        };
+        assert.equal(new Tool(spec).id, spec.id);
+      });
+
+      it('should have no default', function () {
+        var spec = {};
+        assert.equal(new Tool(spec).id, spec.id);
+
+      });
+
+    });
+
+
+    describe('.options', function () {
+      it('should be set', function () {
+        var spec = {
+          options: ['monkeys', 'bananas']
+        };
+        assert.equal(new Tool(spec).options, spec.options);
+      });
+
+      it('should have no default', function () {
+        var spec = {};
+        assert.equal(new Tool(spec).options, spec.options);
+
+      });
+
+    });
+
+  });
+});
