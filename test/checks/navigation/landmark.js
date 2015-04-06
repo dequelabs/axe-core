b046d034d3317a605e2093058b9f238740f57efa
David Sturley <david.sturley@deque.com>
2015-04-06 14:05:13 -0400

    KSD-732 - Make all possible messages display for bypass blocks KSD-916 - any heading should satisfy rule

--- test/checks/navigation/landmark.js
+++ test/checks/navigation/landmark.js
@@ -1,8 +1,20 @@
 describe('landmark', function () {
 	'use strict';
 
-	it('should always return true', function () {
-		assert.isTrue(checks.landmark.evaluate());
+	var fixture = document.getElementById('fixture');
+
+	afterEach(function () {
+		fixture.innerHTML = '';
+	});
+
+	it('should return true when role=main is found', function () {
+		fixture.innerHTML = '<div role="main"></div>';
+		assert.isTrue(checks.landmark.evaluate(fixture));
+	});
+
+	it('should otherwise return false', function () {
+		fixture.innerHTML = '<div role="contentinfo"></div>';
+		assert.isFalse(checks.landmark.evaluate(fixture));
 	});
 
 });
