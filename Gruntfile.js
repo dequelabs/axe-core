ea26c34665c11b943920f5f1ddc8ba691557b8e5
David Sturley <david.sturley@deque.com>
2015-05-12 14:26:05 -0400

    Update bower deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,8 +20,8 @@ module.exports = function (grunt) {
 		concat: {
 			kensington: {
 				src: ['lib/intro.stub',
-					'bower_components/rule-engine/dist/dqre.js',
 					'bower_components/ks-common-functions/dist/ks-cf.js',
+					'bower_components/rule-engine/dist/dqre.js',
 					'bower_components/ks-rules/dist/rules.js',
 					'lib/outro.stub'
 				],
