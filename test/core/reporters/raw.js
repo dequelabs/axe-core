27f8f8f0cd3b57edea4c3a61a9140fe1d8deb994
David Sturley <david.sturley@deque.com>
2015-07-07 10:57:52 -0400

    One more failing test

--- test/core/reporters/raw.js
+++ test/core/reporters/raw.js
@@ -2,6 +2,7 @@ describe('reporters - raw', function () {
 	'use strict';
 
 	it('should pass through object', function () {
+		axe._load({});
 		var orig = window.runRules;
 		window.runRules = function (_, __, cb) {
 			cb('foo');
