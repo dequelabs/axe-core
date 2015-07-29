33f2bf9e0d5b725fc62691e07147a312142fbdeb
David Sturley <david.sturley@deque.com>
2015-07-29 15:07:19 -0400

    Move DqElement to utils

--- test/core/utils/index.js
+++ test/core/utils/index.js
@@ -4,6 +4,9 @@ describe('utils', function () {
 	it('should be an object', function () {
 		assert.isObject(window.utils);
 	});
+	it('should be attached to axe global', function () {
+		assert.equal(axe.utils, window.utils);
+	});
 });
 
 describe('utils.escapeSelector', function () {
