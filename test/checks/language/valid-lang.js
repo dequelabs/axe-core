5a5b6ad5f6872462dba8f69ba5a4f1e3aed98bf1
Wilco Fiers <wilco@>
2016-02-17 14:11:18 +0100

    reworked valid-lang to fail empty lang attributes
    It's also more performant

--- test/checks/language/valid-lang.js
+++ test/checks/language/valid-lang.js
@@ -17,7 +17,7 @@ describe('valid-lang', function () {
 
 	describe('lang', function () {
 
-		it('should return true if a lang attribute is present in options', function () {
+		it('should return false if a lang attribute is present in options', function () {
 			var node = document.createElement('div');
 			node.setAttribute('lang', 'woohoo');
 			fixture.appendChild(node);
@@ -34,35 +34,35 @@ describe('valid-lang', function () {
 
 		});
 
-		it('should return false if a lang attribute is not present in options', function () {
+		it('should return true if a lang attribute is not present in options', function () {
 			var node = document.createElement('div');
 			node.setAttribute('lang', 'en-FOO');
 			fixture.appendChild(node);
 
 			assert.isTrue(checks['valid-lang'].evaluate.call(checkContext, node, []));
-			assert.deepEqual(checkContext._data, ['lang="en-foo"']);
+			assert.deepEqual(checkContext._data, ['lang="en-FOO"']);
 		});
 
-		it('should return false (and not throw) when given no options', function () {
+		it('should return true (and not throw) when given no options', function () {
 			var node = document.createElement('div');
 			node.setAttribute('lang', 'en-US');
 			fixture.appendChild(node);
 
 			assert.isTrue(checks['valid-lang'].evaluate.call(checkContext, node));
-			assert.deepEqual(checkContext._data, ['lang="en-us"']);
+			assert.deepEqual(checkContext._data, ['lang="en-US"']);
 		});
 
-		it('should return false if the language is badly formatted', function () {
+		it('should return true if the language is badly formatted', function () {
 			var node = document.createElement('div');
 			node.setAttribute('lang', 'en_US');
 			fixture.appendChild(node);
 
 			assert.isTrue(checks['valid-lang'].evaluate.call(checkContext, node, ['en']));
-			assert.deepEqual(checkContext._data, ['lang="en_us"']);
+			assert.deepEqual(checkContext._data, ['lang="en_US"']);
 
 		});
 
-		it('should return true if it matches a substring proceeded by -', function () {
+		it('should return false if it matches a substring proceeded by -', function () {
 			var node = document.createElement('div');
 			node.setAttribute('lang', 'en-LOL');
 			fixture.appendChild(node);
@@ -75,7 +75,7 @@ describe('valid-lang', function () {
 
 	describe('xml:lang', function () {
 
-		it('should return true if a lang attribute is present in options', function () {
+		it('should return false if a lang attribute is present in options', function () {
 			var node = document.createElement('div');
 			node.setAttribute('xml:lang', 'woohoo');
 			fixture.appendChild(node);
@@ -92,35 +92,35 @@ describe('valid-lang', function () {
 
 		});
 
-		it('should return false if a lang attribute is not present in options', function () {
+		it('should return true if a lang attribute is not present in options', function () {
 			var node = document.createElement('div');
 			node.setAttribute('xml:lang', 'en-FOO');
 			fixture.appendChild(node);
 
 			assert.isTrue(checks['valid-lang'].evaluate.call(checkContext, node, []));
-			assert.deepEqual(checkContext._data, ['xml:lang="en-foo"']);
+			assert.deepEqual(checkContext._data, ['xml:lang="en-FOO"']);
 		});
 
-		it('should return false (and not throw) when given no options', function () {
+		it('should return true (and not throw) when given no options', function () {
 			var node = document.createElement('div');
 			node.setAttribute('xml:lang', 'en-US');
 			fixture.appendChild(node);
 
 			assert.isTrue(checks['valid-lang'].evaluate.call(checkContext, node));
-			assert.deepEqual(checkContext._data, ['xml:lang="en-us"']);
+			assert.deepEqual(checkContext._data, ['xml:lang="en-US"']);
 		});
 
-		it('should return false if the language is badly formatted', function () {
+		it('should return true if the language is badly formatted', function () {
 			var node = document.createElement('div');
 			node.setAttribute('xml:lang', 'en_US');
 			fixture.appendChild(node);
 
 			assert.isTrue(checks['valid-lang'].evaluate.call(checkContext, node, ['en']));
-			assert.deepEqual(checkContext._data, ['xml:lang="en_us"']);
+			assert.deepEqual(checkContext._data, ['xml:lang="en_US"']);
 
 		});
 
-		it('should return true if it matches a substring proceeded by -', function () {
+		it('should return false if it matches a substring proceeded by -', function () {
 			var node = document.createElement('div');
 			node.setAttribute('xml:lang', 'en-LOL');
 			fixture.appendChild(node);
