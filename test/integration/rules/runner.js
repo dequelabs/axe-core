724bf60f47865dfcd1f4554924566f9af057e9e4
Wilco Fiers <wilco@>
2016-03-04 12:45:40 +0100

    Easier debugging of rule integration tests

--- test/integration/rules/runner.js
+++ test/integration/rules/runner.js
@@ -2,6 +2,32 @@
 (function () {
 	'use strict';
 
+
+	var deepEqual = function (x, y) {
+	  if ((typeof x === 'object' && x !== null) && (typeof y === 'object' && y !== null)) {
+	    if (Object.keys(x).length !== Object.keys(y).length) {
+	      return false;
+	    }
+
+	    for (var prop in x) {
+	      if (y.hasOwnProperty(prop)) {
+	        if (! deepEqual(x[prop], y[prop])){
+	          return false;
+	        }
+	      }
+	      else {
+	        return false;
+	      }
+	    }
+
+	    return true;
+	  } else if (x !== y) {
+	    return false;
+	  } else {
+	    return true;
+	  }
+	};
+
 	function flattenResult(results) {
 		return {
 			passes: results.passes[0],
@@ -33,15 +59,55 @@
 				describe(test.description, function () {
 
 					function runTest(test, collection) {
-						if (test[collection]) {
-							describe(collection, function () {
-								test[collection].forEach(function (selector, index) {
-									it('should find ' + JSON.stringify(selector), function () {
-										assert.deepEqual(results[collection].nodes[index].target, selector);
+						if (!test[collection]) {
+							return;
+						}
+
+						describe(collection, function () {
+							var nodes;
+							before(function () {
+								if (typeof results[collection] === 'object') {
+									nodes = results[collection].nodes;
+								}
+							});
+
+							test[collection]
+							.forEach(function (selector) {
+								it('should find ' + JSON.stringify(selector), function () {
+									if (!nodes) {
+										assert(false, 'there are no ' + collection);
+										return;
+									}
+
+									var matches = nodes.filter(function (node) {
+										return deepEqual(node.target, selector);
 									});
+									matches.forEach(function (node) {
+										// remove each node we find
+										nodes.splice(nodes.indexOf(node), 1);
+									});
+
+									if (matches.length === 0) {
+										assert(false, 'Element not found');
+
+									} else if (matches.length === 1) {
+										assert(true, 'Element found');
+
+									} else {
+										assert(false, 'Found ' + matches.length + ' elements which match the target');
+									}
 								});
 							});
-						}
+
+							it('should not return other results', function () {
+								if (typeof nodes !== 'undefined') {
+									// check that all nodes are removed
+									assert.deepEqual(nodes, []);
+								} else {
+									assert(false, 'there are no ' + collection);
+								}
+							});
+						});
 					}
 
 					var results;
