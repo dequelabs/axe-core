2e566b7f5514da274bf2bd78455d59368b3af064
Dylan Barrell <dylan@barrell.com>
2016-03-21 15:36:53 -0700

    make reset cascade to iframes

--- test/core/public/reset.js
+++ test/core/public/reset.js
@@ -1,16 +1,38 @@
-/*global Rule*/
+/*global Rule, resetConfiguration*/
 describe('axe.reset', function () {
 	'use strict';
 
-	it('should throw if no audit is configured', function () {
-		axe._audit = null;
+	function createFrames(callback) {
+		var frame;
+		frame = document.createElement('iframe');
+		frame.src = '../mock/frames/nested1.html';
+		frame.addEventListener('load', function () {
+			setTimeout(callback, 500);
+		});
+		fixture.appendChild(frame);
+	}
 
+	var fixture = document.getElementById('fixture');
+	var assertNotCalled = function () {
+		assert.ok(false, 'Should not be called');
+	};
+
+
+	afterEach(function () {
+		fixture.innerHTML = '';
+	});
+
+	beforeEach(function () {
+		axe._audit = null;
+	});
+
+	it('should throw if no audit is configured', function () {
 		assert.throws(function () {
-			axe.reset();
+			axe.reset(function () {}, function () {});
 		}, Error, /^No audit configured/);
 	});
 
-	it('should restore the default configuration', function () {
+	it('should restore the default configuration', function (done) {
 		axe._load({
 			data: {
 				rules: [{
@@ -36,10 +58,30 @@ describe('axe.reset', function () {
 		assert.instanceOf(axe._audit.rules[0], Rule);
 		assert.equal(axe._audit.rules[0].id, 'bob');
 		assert.equal(axe._audit.rules[0].selector, 'pass');
-		axe.reset();
-		assert.lengthOf(axe._audit.rules, 1);
-		assert.instanceOf(axe._audit.rules[0], Rule);
-		assert.equal(axe._audit.rules[0].id, 'bob');
-		assert.equal(axe._audit.rules[0].selector, 'fail');
+		axe.reset(function () {
+			assert.lengthOf(axe._audit.rules, 1);
+			assert.instanceOf(axe._audit.rules[0], Rule);
+			assert.equal(axe._audit.rules[0].id, 'bob');
+			assert.equal(axe._audit.rules[0].selector, 'fail');
+			done();
+		}, function () {});
 	});
+
+	it('should send command to frames to reset', function (done) {
+		createFrames(function () {
+			axe._load({});
+			var orig = axe.utils.sendCommandToFrame;
+			var frame = document.querySelector('iframe');
+			axe.utils.sendCommandToFrame = function (node, opts) {
+				assert.equal(node, frame);
+				assert.deepEqual(opts, {
+					command: 'reset'
+				});
+				axe.utils.sendCommandToFrame = orig;
+				done();
+			};
+			resetConfiguration(function () {}, assertNotCalled);
+		});
+	});
+
 });
