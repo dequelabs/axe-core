d7e2e0923c043dab5de5b8662831e3c62989d286
Jason Karns <jason@karns.name>
2016-04-15 09:41:05 -0400

    remove unused grunt-contrib-copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
