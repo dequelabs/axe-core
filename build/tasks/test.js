bdc24caf888f033c17d6352983f9bd8842908154
Wilco Fiers <wilco.fiers@deque.com>
2016-04-21 14:40:25 +0200

    rewrote test-webdriver grunt task to run with every browser

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,52 +1,96 @@
 /*jshint node: true */
 'use strict';
+
 var Promise = require('promise');
 var WebDriver = require('selenium-webdriver');
+
+function collectTestResults(driver, resolve, reject) {
+	driver.executeAsyncScript(function () {
+		/*global window */
+		var callback = arguments[arguments.length - 1];
+		setTimeout(function isDone() {
+			if (window.mochaResults) {
+				window.mochaResults.url = window.location.href;
+			}
+			callback(window.mochaResults);
+		}, 500);
+	})
+	.then(function (results) {
+		if (!results) {
+			collectTestResults(driver, resolve, reject);
+		} else {
+			resolve(results);
+		}
+	});
+}
+
 module.exports = function (grunt) {
 	grunt.registerMultiTask('test-webdriver', function () {
 		var options = this.options({
 			browser: 'firefox'
 		});
+
 		var done = this.async();
-		var driver = new WebDriver.Builder()
+		var errors = [];
+		var driver;
+
+		try {
+			driver = new WebDriver.Builder()
 			.forBrowser(options.browser)
 			.build();
 
-		driver.manage().timeouts().setScriptTimeout(5000);
+		} catch(err) {
+			grunt.log.writeln();
+			grunt.log.error(err.message);
+			grunt.log.writeln('Aborted testing using ' + options.browser);
+			return done();
+		}
 
+		driver.manage().timeouts().setScriptTimeout(600);
+
+		var runAllTests =
 		Promise.all(options.urls.map(function (url) {
-			return new Promise(function(resolve) {
-				driver
-					.get(url)
-					.then(function () {
-						driver.executeAsyncScript(function () {
-							/*global window */
-							var callback = arguments[arguments.length - 1];
-							setTimeout(function isDone() {
-								if (!window.mochaResults) {
-									return setTimeout(isDone, 250);
-								}
-								window.mochaResults.url = window.location.href;
-								callback(window.mochaResults);
-							}, 250);
-						})
-						.then(function (results) {
-							resolve(results);
-						});
+			return new Promise(function(resolve, reject) {
+				driver.get(url)
+				.then(function () {
+					collectTestResults(driver, resolve, reject);
+				});
+			}).then(function (result) {
+				grunt.log.writeln(result.url);
 
-					});
+				(result.reports || []).forEach(function (err) {
+					grunt.log.error(err.message);
+					err.url = url;
+					errors.push(err);
+				});
+				var msg = 'passes: ' + result.passes +
+						', failures: ' + result.failures +
+						', duration: ' + (result.duration / 1000) +'s'
+				if (result.failures) {
+					grunt.log.error(msg);
+				} else {
+					grunt.log.ok(msg);
+				}
+				grunt.log.writeln();
 			});
 		})).then(function (results) {
-			driver.quit().then(function () {
-				results.forEach(function (result) {
-					grunt.log.writeln(result.url);
-					if (result.failures) {
-						grunt.log.error(result.failures + ' failing tests');
-					} else {
-						grunt.log.ok(result.passes + ' passing tests');
-					}
-				});
-				done(results);
+			driver.quit()
+			.then(function () {
+
+				if (errors.length === 0) {
+					done(true);
+				} else {
+					errors.forEach(function(err) {
+						grunt.log.writeln();
+						grunt.log.error('URL: ' + err.url);
+						grunt.log.error('Describe: ' + err.titles.join(' > '));
+						grunt.log.error('it ' + err.name);
+						grunt.log.error(err.stack);
+						grunt.log.writeln();
+					});
+					done(false);
+				}
+
 			});
 		});
 	});
