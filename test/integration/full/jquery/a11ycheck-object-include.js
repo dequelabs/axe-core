d1e4f94b99a34b76278321a481c5a6c134a9e56e
Marcy Sutton <marcy.sutton@deque.com>
2016-07-01 11:27:41 -0700

    Correct test name

--- test/integration/full/jquery/a11ycheck-object-include.js
+++ test/integration/full/jquery/a11ycheck-object-include.js
@@ -2,7 +2,7 @@
 
 describe('jQuery object as a11yCheck context', function () {
 	'use strict';
-	it('should find no violations', function (done) {
+	it('should find violations', function (done) {
 		var target = $('#target')[0];
 		axe.a11yCheck({ include: [target] }, {}, function (results) {
 			assert.lengthOf(results.violations, 1, 'violations');
