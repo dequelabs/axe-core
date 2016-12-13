400135cf9cf1fa45759be9235ea86a2885e80933
Ian Kelly <iandotkelly@gmail.com>
2016-12-13 16:59:22 -0500

    2.1.7 release

---
+++ README.md
@@ -0,0 +1,92 @@
+# axe-core
+
+[![Join the chat at https://gitter.im/dequelabs/axe-core](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/dequelabs/axe-core?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![Version](https://img.shields.io/npm/v/axe-core.svg)](https://www.npmjs.com/package/axe-core)
+[![Bower](https://img.shields.io/bower/v/axe-core.svg)](http://bower.io/search)
+[![License](https://img.shields.io/npm/l/axe-core.svg)](LICENSE)
+[![Build Status](https://travis-ci.org/dequelabs/axe-core.svg?branch=master)](https://travis-ci.org/dequelabs/axe-core)
+[![Dependency Status](https://gemnasium.com/dequelabs/axe-core.svg)](https://gemnasium.com/dequelabs/axe-core)
+[![Codacy Badge](https://www.codacy.com/project/badge/c2d7835387dd41b3a77eafd439fba559)](https://www.codacy.com/app/dsturley/axe-core)
+[![Package Quality](http://npm.packagequality.com/shield/axe-core.svg)](http://packagequality.com/#?package=axe-core)
+
+The Accessibility Engine for automated testing of HTML-based user interfaces. Drop the aXe on your accessibility defects!
+
+## Philosophy
+
+We believe that automated testing has an important role to play in achieving digital equality and that in order to do that, it must achieve mainstream adoption by professional web developers. That means that the tests must inspire trust, must be fast, must work everywhere and must be available everywhere.
+
+## Manifesto
+
+1. Automated accessibility testing rules must have a zero false-positive rate
+2. Automated accessibility testing rules must be lightweight and fast
+3. Automated accessibility testing rules must work in all modern browsers
+4. Automated accessibility testing rules must, themselves, be tested automatically
+
+## The sharp edge of the aXe
+
+aXe is the third generation of accessibility rules for HTML-based user interfaces that differentiates itself from other approaches and rules repositories in the following ways:
+
+1. It works on all modern browsers,
+2. It supports in-memory fixtures, static fixtures, integration tests and iframes of infinite depth
+3. It has zero false positives (bugs notwithstanding)
+4. It is open source
+5. It is actively supported by a major accessibility vendor
+6. It is designed to work with whatever tools, frameworks, libraries and environments you have today
+7. It is designed to be integrated into your existing functional/acceptance automated tests
+8. It automatically determines which rules to run based on the evaluation context
+9. It is highly configurable
+
+## Getting started
+
+First download the package:
+
+```
+npm install axe-core --save-dev
+```
+
+Now include the javascript file in each of your iframes in your fixtures or test systems:
+
+```html
+<script src="node_modules/axe-core/axe.min.js" ></script>
+```
+
+Now insert calls at each point in your tests where a new piece of UI becomes visible or exposed:
+
+```js
+axe.run(function (err, results) {
+	if (err) throw err;
+    ok(results.violations.length === 0, 'Should be no accessibility issues');
+    // complete the async call
+    ...
+});
+```
+## Supported Browsers
+
+The [aXe API](doc/API.md) supports the following browsers:
+
+* Internet Explorer v9, 10, 11
+* Google Chrome v42 and above
+* Mozilla Firefox v38 and above
+* Apple Safari v7 and above
+
+## The Accessibility Rules
+
+The complete list of rules run by axe-core can be found in [doc/rule-descriptions.md](./doc/rule-descriptions.md).
+
+## Contents of the API Package
+
+The [aXe API](doc/API.md) package consists of:
+
+* `axe.js` - the JavaScript file that should be included in your web site under test (API)
+* `axe.min.js` - a minified version of the above file
+
+
+## Contributing
+
+Read the [documentation on the architecture](./doc/developer-guide.md)
+
+Read the [documentation on contributing](CONTRIBUTING.md)
+
+## Projects using axe-core
+
+[List of projects using axe-core](doc/projects.md)
---
+++ axe.d.ts
@@ -0,0 +1,169 @@
+// Type definitions for axe-core 2.1.7
+// Project: https://github.com/dequelabs/axe-core
+// Definitions by: Marcy Sutton <https://github.com/marcysutton>
+
+declare module axe {
+
+	type ImpactValue = "minor" | "moderate" | "serious" | "critical";
+
+	type TagValue = "wcag2a" | "wcag2aa" | "section508" | "best-practice";
+
+	type ReporterVersion = "v1" | "v2";
+
+	type RunOnlyType = "rule" | "rules" | "tag" | "tags";
+
+	interface ElementContext {
+		node?: Object,
+		selector?: string,
+		include?: any[],
+		exclude?: any[]
+	}
+	interface RunOnly {
+		type: RunOnlyType,
+		value?: {
+			include?: string[],
+			exclude?: string[]
+		}
+		values?: TagValue[]
+	}
+	interface AxeResults {
+		url: string,
+		timestamp: string,
+		passes: Result[],
+		violations: Result[],
+		incomplete: Result[],
+		inapplicable: Result[]
+	}
+	interface Result {
+		description: string,
+		help: string,
+		helpUrl: string,
+		id: string,
+		impact: ImpactValue,
+		tags: TagValue[],
+		nodes: NodeResult[]
+	}
+	interface NodeResult {
+		html: string,
+		impact: ImpactValue,
+		target: string[],
+		any: CheckResult[],
+		all: CheckResult[],
+		none: CheckResult[],
+		failureSummary?: string
+	}
+	interface CheckResult {
+		id: string,
+		impact: string,
+		message: string,
+		data: any,
+		relatedNodes?: RelatedNode[]
+	}
+	interface RelatedNode {
+		target: string[],
+		html: string
+	}
+	interface Spec {
+		branding?: {
+			brand: string,
+			application: string
+		},
+		reporter?: ReporterVersion,
+		checks?: Check[],
+		rules?: Rule[]
+	}
+	interface Check {
+		id: string,
+		evaluate: Function,
+		after?: Function,
+		options?: any,
+		matches?: string,
+		enabled?: boolean
+	}
+	interface Rule {
+		id: string,
+		selector?: string,
+		excludeHidden?: boolean,
+		enabled?: boolean,
+		pageLevel?: boolean,
+		any?: string[],
+		all?: string[],
+		none?: string[],
+		tags?: string[],
+		matches?: string
+	}
+	interface AxePlugin {
+		id: string,
+		run(...args:any[]): any,
+		commands: {
+			id: string,
+			callback(...args:any[]): void
+		}[],
+		cleanup?(callback:Function): void
+	}
+
+	let plugins: any
+
+	/**
+	 * Source string to use as an injected script in Selenium
+	 */
+	let source: string
+
+	/**
+	 * Object for aXe Results
+	 */
+	var AxeResults: AxeResults
+
+	/**
+	 * Runs a number of rules against the provided HTML page and returns the resulting issue list
+	 *
+	 * @param  {Object}   context  Optional The `Context` specification object @see Context
+	 * @param  {Array}    options  Optional Options passed into rules or checks, temporarily modifying them.
+	 * @param  {Function} callback Optional The function to invoke when analysis is complete.
+	 * @returns {any}  	  results  If the callback was not defined, aXe will return a Promise instead.
+	 */
+	function run(context?: ElementContext, options?: {runOnly?: RunOnly, rules?: Object}, callback?: (error: Error, results:AxeResults) => void): any
+
+	/**
+	 * Starts analysis on the current document and its subframes
+	 *
+	 * @param  {Object}   context  The `Context` specification object @see Context
+	 * @param  {Array}    options  Options passed into rules or checks, temporarily modifyint them.
+	 * @param  {Function} callback The function to invoke when analysis is complete.
+	 * @returns {Object}  results  The aXe results object
+	 */
+	function a11yCheck(context: ElementContext, options: {runOnly?: RunOnly, rules?: Object}, callback: (results:AxeResults) => void): AxeResults
+
+	/**
+	 * Method for configuring the data format used by aXe. Helpful for adding new
+	 * rules, which must be registered with the library to execute.
+	 * @param  {Spec}       Spec Object with valid `branding`, `reporter`, `checks` and `rules` data
+	 */
+	function configure(spec: Spec): void
+
+	/**
+	 * Searches and returns rules that contain a tag in the list of tags.
+	 * @param  {Array}  tags  Optional array of tags
+	 * @return {Array}  Array of rules
+	 */
+	function getRules(tags?: string[]): Object[]
+
+	/**
+	 * Restores the default axe configuration
+	 */
+	function reset(): void
+
+	/**
+	 * Function to register a plugin configuration in document and its subframes
+	 * @param  {Object}    plugin    A plugin configuration object
+	 */
+	function registerPlugin(plugin: AxePlugin): void
+
+	/**
+	 * Function to clean up plugin configuration in document and its subframes
+	 */
+	function cleanup(): void
+
+}
+
+export = axe;
