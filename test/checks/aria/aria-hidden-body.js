7c48efb6cc4ba0a30e0e9436dfb9af4bbdd101d2
Chris Rodriguez <chris.rodriguez@deque.com>
2017-06-08 13:24:02 -0400

    Updated test for previous commit

--- test/checks/aria/aria-hidden-body.js
+++ test/checks/aria/aria-hidden-body.js
@@ -19,9 +19,14 @@ describe('aria-hidden', function () {
 		assert.isTrue(checks['aria-hidden-body'].evaluate.call(checkContext, node));
 	});
 
-	it('fails appropriately if aria-hidden on document.body', function () {
+	it('fails appropriately if aria-hidden=true on document.body', function () {
 		node.setAttribute('aria-hidden', 'true');
 		assert.isFalse(checks['aria-hidden-body'].evaluate.call(checkContext, node));
+	});
+
+	it('passes if aria-hidden=false on document.body', function () {
+		node.setAttribute('aria-hidden', 'false');
+		assert.isTrue(checks['aria-hidden-body'].evaluate.call(checkContext, node));
 	});
 
 });
