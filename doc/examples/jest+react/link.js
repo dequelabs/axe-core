288fc9dea1218aea8ff041e04e934ec00caa0c8f
Wilco Fiers <WilcoFiers@users.noreply.github.com>
2017-06-13 13:16:17 +0200

    docs: Add Jest + React example (#346)
    * docs: Add an example with Jest, React and Enzyme
    
    * fix: update jest example package.json
    
    * fix: Avoid linting jsx example
    
    * docs: Add readme to jest+react example

---
+++ doc/examples/jest+react/link.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Link extends React.Component {
+  render() {
+    return (
+      <a href={this.props.page || '#'}>
+        {this.props.children}
+      </a>
+    );
+  }
+};
