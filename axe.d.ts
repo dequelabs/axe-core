d1de79b676234157f474e4a8613ca08106ef1024
Marcy Sutton <marcy.sutton@deque.com>
2017-06-14 12:11:14 -0700

    chore: fix version number for 2.3.0

--- axe.d.ts
+++ axe.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for axe-core 2.3
+// Type definitions for axe-core 2.3.0
 // Project: https://github.com/dequelabs/axe-core
 // Definitions by: Marcy Sutton <https://github.com/marcysutton>
 
