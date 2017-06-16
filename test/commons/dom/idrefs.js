ea7125e20504cf154f87f791aff0f470e0a3eb56
Dylan Barrell <dylan@barrell.com>
2017-06-16 19:03:01 -0400

    change dom.idrefs to use dom.getRootNode

--- test/commons/dom/idrefs.js
+++ test/commons/dom/idrefs.js
@@ -1,3 +1,20 @@
+function createContentIDR() {
+	'use strict';
+	var group = document.createElement('div');
+	group.id = 'target';
+	return group;
+}
+
+function makeShadowTreeIDR(node) {
+	'use strict';
+	var root = node.attachShadow({mode: 'open'});
+	var div = document.createElement('div');
+	div.className = 'parent';
+	div.setAttribute('target', 'target');
+	root.appendChild(div);
+	div.appendChild(createContentIDR());
+}
+
 describe('dom.idrefs', function () {
 	'use strict';
 
@@ -17,6 +34,34 @@ describe('dom.idrefs', function () {
 		assert.deepEqual(axe.commons.dom.idrefs(start, 'aria-cats'), expected, 'Should find it!');
 
 
+	});
+
+	it('should find only referenced nodes within the current root: shadow DOM', function () {
+		if (document.body && typeof document.body.attachShadow === 'function') {
+			// shadow DOM v1 - note: v0 is compatible with this code, so no need
+			// to specifically test this
+			fixture.innerHTML = '<div target="target"><div id="target"></div></div>';
+			makeShadowTreeIDR(fixture.firstChild);
+			var start = fixture.firstChild.shadowRoot.querySelector('.parent');
+			var expected = [fixture.firstChild.shadowRoot.getElementById('target')];
+
+			assert.deepEqual(axe.commons.dom.idrefs(start, 'target'), expected,
+				'should only find stuff in the shadow DOM');
+		}
+	});
+
+	it('should find only referenced nodes within the current root: document', function () {
+		if (document.body && typeof document.body.attachShadow === 'function') {
+			// shadow DOM v1 - note: v0 is compatible with this code, so no need
+			// to specifically test this
+			fixture.innerHTML = '<div target="target" class="parent"><div id="target"></div></div>';
+			makeShadowTreeIDR(fixture.firstChild);
+			var start = fixture.querySelector('.parent');
+			var expected = [document.getElementById('target')];
+
+			assert.deepEqual(axe.commons.dom.idrefs(start, 'target'), expected,
+				'should only find stuff in the document');
+		}
 	});
 
 	it('should insert null if a reference is not found', function () {
