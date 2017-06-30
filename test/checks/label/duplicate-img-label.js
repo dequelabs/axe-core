82e51bc38f6eca33944b4b8006a9140067b44c5a
Marcy Sutton <marcy.sutton@deque.com>
2017-06-30 12:22:11 -0700

    fix: use virtualNode in duplicate-img-label
    Closes https://github.com/dequelabs/axe-core/issues/386

--- test/checks/label/duplicate-img-label.js
+++ test/checks/label/duplicate-img-label.js
@@ -5,53 +5,63 @@ describe('duplicate-img-label', function () {
 
 	afterEach(function () {
 		fixture.innerHTML = '';
+		axe._tree = undefined;
 	});
 
 	it('should return false if no img is present', function () {
 		fixture.innerHTML = '<button id="target">Plain text</button>';
 		var node = fixture.querySelector('#target');
-		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node)));
 	});
 
 	it('should return false if no text is present', function () {
 		fixture.innerHTML = '<button id="target"><img alt="Plain text"></button>';
 		var node = fixture.querySelector('#target');
-		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		var result = checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node));
+		assert.isFalse(result);
 	});
 
 	it('should return false if aria-label duplicates img alt', function () {
 		fixture.innerHTML = '<button id="target" aria-label="Plain text"><img alt="Plain text"></button>';
 		var node = fixture.querySelector('#target');
-		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node)));
 	});
 
 	it('should return false if img and text have different text', function () {
 		fixture.innerHTML = '<button id="target"><img alt="Alt text">Plain text</button>';
 		var node = fixture.querySelector('#target');
-		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node)));
 	});
 
 	it('should return true if img and text have the same text', function () {
 		fixture.innerHTML = '<button id="target"><img alt="Plain text">Plain text</button>';
 		var node = fixture.querySelector('#target');
-		assert.isTrue(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		assert.isTrue(checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node)));
 	});
 
 	it('should return true if img has ARIA label with the same text', function () {
 		fixture.innerHTML = '<button id="target"><img aria-label="Plain text">Plain text</button>';
 		var node = fixture.querySelector('#target');
-		assert.isTrue(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		assert.isTrue(checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node)));
 	});
 
 	it('should return false if img and text are both blank', function () {
 		fixture.innerHTML = '<button id="target"><img alt=""></button>';
 		var node = fixture.querySelector('#target');
-		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node)));
 	});
 
 	it('should return false if img and text have superset/subset text', function () {
 		fixture.innerHTML = '<button id="target"><img alt="Plain text and more">Plain text</button>';
 		var node = fixture.querySelector('#target');
-		assert.isFalse(checks['duplicate-img-label'].evaluate(node));
+		var tree = axe._tree = axe.utils.getFlattenedTree(fixture);
+		assert.isFalse(checks['duplicate-img-label'].evaluate(node, undefined, axe.utils.getNodeFromTree(tree[0], node)));
 	});
 });
