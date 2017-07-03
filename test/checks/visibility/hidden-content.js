2beede6e02885333debde9763d62e194dc49944b
Wilco Fiers <wilco.fiers@deque.com>
2017-07-03 14:51:46 +0200

    Fix: Use virtualNode in hidden-content rule

--- test/checks/visibility/hidden-content.js
+++ test/checks/visibility/hidden-content.js
@@ -10,37 +10,44 @@ describe('hidden content', function () {
 		}
 	};
 
+	function checkSetup (html, options, target) {
+		fixture.innerHTML = html;
+    axe._tree = axe.utils.getFlattenedTree(fixture);
+		var node = fixture.querySelector(target || '#target');
+		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+		return [node, options, virtualNode];
+	}
+
 	afterEach(function () {
 		fixture.innerHTML = '';
 		checkContext._data = null;
+		axe._tree = undefined;
 	});
 
 	it('should return undefined with display:none and children', function () {
-    fixture.innerHTML = '<div id="target" style="display: none;"><p>Some paragraph text.</p></div>';
-		var node = fixture.querySelector('#target');
-		assert.isUndefined(checks['hidden-content'].evaluate.call(checkContext, node));
+    var params = checkSetup('<div id="target" style="display: none;"><p>Some paragraph text.</p></div>');
+		assert.isUndefined(checks['hidden-content'].evaluate.apply(checkContext, params));
 	});
 
 	it('should return undefined with visibility:hidden and children', function () {
-    fixture.innerHTML = '<div id="target" style="visibility: hidden;"><p>Some paragraph text.</p></div>';
-		var node = fixture.querySelector('#target');
-		assert.isUndefined(checks['hidden-content'].evaluate.call(checkContext, node));
+    var params = checkSetup('<div id="target" style="visibility: hidden;"><p>Some paragraph text.</p></div>');
+		assert.isUndefined(checks['hidden-content'].evaluate.apply(checkContext, params));
 	});
 
 	it('should return true with visibility:hidden and parent with visibility:hidden', function () {
-    fixture.innerHTML = '<div style="visibility: hidden;"><p id="target" style="visibility: hidden;">Some paragraph text.</p></div>';
-		var node = fixture.querySelector('#target');
-		assert.isTrue(checks['hidden-content'].evaluate.call(checkContext, node));
+    var params = checkSetup('<div style="visibility: hidden;"><p id="target" style="visibility: hidden;">Some paragraph text.</p></div>');
+		assert.isTrue(checks['hidden-content'].evaluate.apply(checkContext, params));
 	});
 
 	it('should return true with aria-hidden and no content', function () {
-		fixture.innerHTML = '<span id="target" class="icon" aria-hidden="true"></span>';
-		var node = fixture.querySelector('#target');
-		assert.isTrue(checks['hidden-content'].evaluate.call(checkContext, node));
+		var params = checkSetup('<span id="target" class="icon" aria-hidden="true"></span>');
+		assert.isTrue(checks['hidden-content'].evaluate.apply(checkContext, params));
 	});
 
 	it('should skip whitelisted elements', function () {
 		var node = document.querySelector('head');
-		assert.isTrue(checks['hidden-content'].evaluate.call(checkContext, node));
+    axe._tree = axe.utils.getFlattenedTree(document.documentElement);
+		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+		assert.isTrue(checks['hidden-content'].evaluate.call(checkContext, node, undefined, virtualNode));
 	});
 });
