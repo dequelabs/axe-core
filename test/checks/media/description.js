0f215746cd98b9a7280c6e22d65689a6a04cf133
Wilco Fiers <wilco.fiers@deque.com>
2017-07-14 13:53:29 +0200

    feat: ShadowDOM support for media checks

--- test/checks/media/description.js
+++ test/checks/media/description.js
@@ -1,30 +1,37 @@
 describe('description', function () {
 	'use strict';
 
-	var fixture = document.getElementById('fixture');
+	var shadowSupport = axe.testUtils.shadowSupport;
+	var checkSetup = axe.testUtils.checkSetup;
 
 	afterEach(function () {
-		fixture.innerHTML = '';
+		document.getElementById('fixture').innerHTML = '';
 	});
 
 	it('should return undefined if there is no track element', function () {
-		fixture.innerHTML = '<video></video>';
-		var node = fixture.querySelector('video');
-
-		assert.isUndefined(checks.description.evaluate(node));
+		var checkArgs = checkSetup('<video></video>', 'video');
+		assert.isUndefined(checks.description.evaluate.apply(null, checkArgs));
 	});
 
-	it('should fail if there is no kind=descriptions attribute', function () {
-		fixture.innerHTML = '<video><track kind=captions></video>';
-		var node = fixture.querySelector('video');
-
-		assert.isTrue(checks.description.evaluate(node));
+	it('should fail if there is no kind=captions attribute', function () {
+		var checkArgs = checkSetup('<video><track kind=captions></video>', 'video');
+		assert.isTrue(checks.description.evaluate.apply(null, checkArgs));
 	});
 
 	it('should pass if there is a kind=descriptions attribute', function () {
-		fixture.innerHTML = '<video><track kind=descriptions></video>';
-		var node = fixture.querySelector('video');
-
-		assert.isFalse(checks.description.evaluate(node));
+		var checkArgs = checkSetup('<video><track kind=descriptions></video>', 'video');
+		assert.isFalse(checks.description.evaluate.apply(null, checkArgs));
 	});
+
+	(shadowSupport.v1 ? it : xit)('should get track from composed tree', function () {
+		var node = document.createElement('div');
+		node.innerHTML = '<track kind=descriptions>';
+		var shadow = node.attachShadow({ mode: 'open' });
+		shadow.innerHTML = '<video><slot></slot></video>';
+
+		var checkArgs = checkSetup(node, {}, 'video');
+		axe.log(checkArgs);
+		assert.isFalse(checks.description.evaluate.apply(null, checkArgs));
+	});
+
 });
