674408f16e58f1f4fb712ea55f00348ecf178cad
Marcy Sutton <marcy.sutton@deque.com>
2017-08-15 13:25:03 -0700

    fix: try telling circle to skip .git/hooks

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d ".git" ]; then
+if [ -d ".git/hooks" ]; then
 	if [ ! -f ".git/hooks/commit-msg" ]; then
 		echo "Installing pre-commit hook"
 		cd .git/hooks/ && cp ../../node_modules/angular-precommit/index.js commit-msg && cd ../../
