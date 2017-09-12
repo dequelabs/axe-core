025947911dc3394e97a8e35f2a84ec1acb719a74
Marcy Sutton <holla@marcysutton.com>
2017-09-12 11:42:14 -0700

    fix(postinstall): use node, more conditionals (#520)
    * fix(postinstall): use node, more conditionals
    
    Closes https://github.com/dequelabs/axe-core/issues/516
    
    * chore: move fs-extra to devDependencies

--- build/utils/postinstall.js
+++ build/utils/postinstall.js
@@ -1,9 +1,9 @@
 var fs = require('fs')
 
 if (fs.existsSync(".git/hooks")) {
-	var commitMsg = '.git/hooks/commit-msg'
+	let commitMsg = '.git/hooks/commit-msg'
 	if (!fs.existsSync(commitMsg)) {
-		var angularPrecommit = "./node_modules/angular-precommit/index.js"
+		let angularPrecommit = "./node_modules/angular-precommit/index.js"
 		if (fs.existsSync(angularPrecommit)) {
 			var fsExtra = require('fs-extra')
 			fsExtra.copy(angularPrecommit, commitMsg, (err) => {
