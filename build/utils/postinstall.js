920406231faaf27e07099466a17d2ee113b868a9
Marcy Sutton <marcy.sutton@deque.com>
2017-09-12 11:11:31 -0700

    chore: move fs-extra to devDependencies

--- build/utils/postinstall.js
+++ build/utils/postinstall.js
@@ -1,11 +1,12 @@
-var fs = require('fs-extra')
+var fs = require('fs')
 
 if (fs.existsSync(".git/hooks")) {
 	let commitMsg = '.git/hooks/commit-msg'
 	if (!fs.existsSync(commitMsg)) {
 		let angularPrecommit = "./node_modules/angular-precommit/index.js"
 		if (fs.existsSync(angularPrecommit)) {
-			fs.copy(angularPrecommit, commitMsg, (err) => {
+			var fsExtra = require('fs-extra')
+			fsExtra.copy(angularPrecommit, commitMsg, (err) => {
 				if (err) throw err
 				console.log("Installing git pre-commit hook")
 			})
