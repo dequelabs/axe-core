639543fe446bf1133c52a45c8dbd2e6cf55f1db4
Ahmad Nassri <ahmad@ahmadnassri.com>
2017-09-21 17:51:56 -0400

    fix node4 build issue (#542)
    * fix(node4): use var to declare variables
    
    Block-scoped declarations (let, const, function, class) not yet supported outside strict mode
    
    * feat(package): add engine hint to package.json
    
    provides minimal requirements warning

--- build/utils/postinstall.js
+++ build/utils/postinstall.js
@@ -1,9 +1,9 @@
 var fs = require('fs')
 
 if (fs.existsSync(".git/hooks")) {
-	let commitMsg = '.git/hooks/commit-msg'
+	var commitMsg = '.git/hooks/commit-msg'
 	if (!fs.existsSync(commitMsg)) {
-		let angularPrecommit = "./node_modules/angular-precommit/index.js"
+		var angularPrecommit = "./node_modules/angular-precommit/index.js"
 		if (fs.existsSync(angularPrecommit)) {
 			var fsExtra = require('fs-extra')
 			fsExtra.copy(angularPrecommit, commitMsg, (err) => {
