f1d1b2f7cc80cf21ece27dc34b49adb6933705c2
ssanaul <ss7@illinois.edu>
2017-10-10 16:38:58 -0500

    fix: correct faulty check tests

--- test/checks/keyboard/has-no-more-than-one-main.js
+++ test/checks/keyboard/has-no-more-than-one-main.js
@@ -2,50 +2,33 @@ describe('has-no-more-than-one-main', function () {
 	'use strict';
 
 	var fixture = document.getElementById('fixture');
+	var checkContext = new axe.testUtils.MockCheckContext();
+	var checkSetup = axe.testUtils.checkSetup;
 
 	afterEach(function () {
 		fixture.innerHTML = '';
+		checkContext.reset();
 	});
 
 	it('should return false if there is more than one element with role main', function () {
-		var mainDiv = document.createElement('div');
-		mainDiv.setAttribute('role','main');
-		var anotherMain = document.createElement('div');
-		anotherMain.setAttribute('role','main');
-		var node = document.querySelector('html');
-		node.appendChild(mainDiv);
-		node.appendChild(anotherMain);
-		assert.isFalse(checks['has-no-more-than-one-main'].evaluate(node));
-		node.removeChild(mainDiv);
-		node.removeChild(anotherMain);
+		var params = checkSetup('<div id = "target"><div role = "main">one</div><div role = "main">two</div></div>');
+		assert.isFalse(checks['has-no-more-than-one-main'].evaluate.apply(checkContext, params));
+
 	});
 	
 	it('should return false if there is more than one main element', function () {
-		var mainLandmark = document.createElement('main');
-		var anotherMain = document.createElement('main');
-		var node = document.querySelector('html');
-		node.appendChild(mainLandmark);
-		node.appendChild(anotherMain);
-		assert.isFalse(checks['has-no-more-than-one-main'].evaluate(node));
-		node.removeChild(mainLandmark);
-		node.removeChild(anotherMain);
+		var params = checkSetup('<div id = "target"><main>one</main><main>two</main></div>');
+		assert.isFalse(checks['has-no-more-than-one-main'].evaluate.apply(checkContext, params));
 	});
 
 	it('should return true if there is only one element with role main', function(){
-		var mainDiv = document.createElement('div');
-		mainDiv.setAttribute('role','main');
-		var node = document.querySelector('html');
-		node.appendChild(mainDiv);
-		assert.isTrue(checks['has-no-more-than-one-main'].evaluate(node));
-		node.removeChild(mainDiv);
+		var params = checkSetup('<div role = "main" id = "target">just one</div>');
+		assert.isTrue(checks['has-no-more-than-one-main'].evaluate.apply(checkContext, params));
 	});
 	
 	it('should return true if there is only one main element', function(){
-		var mainLandmark = document.createElement('main');
-		var node = document.querySelector('html');
-		node.appendChild(mainLandmark);
-		assert.isTrue(checks['has-no-more-than-one-main'].evaluate(node));
-		node.removeChild(mainLandmark);
+		var params = checkSetup('<main id = "target">just one</main>');
+		assert.isTrue(checks['has-no-more-than-one-main'].evaluate.apply(checkContext, params));
 	});
 
 });
