c96f58ccb5a88b0d4c8b0b6c8fb4fd78e73fdd8e
Paul Grenier <pgrenier@gmail.com>
2017-10-24 21:22:01 -0400

    fix(aria-errormessage): adds support for aria-errormessage (#517)
    * fix(aria-errormessage): adds support for aria-errormessage
    
    * fix(aria-valid-attr-value): switches from `any` mode to `all`

---
+++ test/checks/aria/errormessage.js
@@ -0,0 +1,49 @@
+describe('aria-errormessage', function () {
+	'use strict';
+
+	var fixture = document.getElementById('fixture');
+
+	var checkContext = axe.testUtils.MockCheckContext();
+
+	afterEach(function () {
+		fixture.innerHTML = '';
+		checkContext._data = null;
+	});
+
+	it('should return false if aria-errormessage value is invalid', function () {
+		var testHTML = '<div></div>';
+		testHTML += '<div id="plain"></div>';
+		fixture.innerHTML = testHTML;
+		var target = fixture.children[0];
+		target.setAttribute('aria-errormessage', 'plain');
+		assert.isFalse(checks['aria-errormessage'].evaluate.call(checkContext, target));
+	});
+
+	it('should return true if aria-errormessage id is alert', function () {
+		var testHTML = '<div></div>';
+		testHTML += '<div id="alert" role="alert"></div>';
+		fixture.innerHTML = testHTML;
+		var target = fixture.children[0];
+		target.setAttribute('aria-errormessage', 'alert');
+		assert.isTrue(checks['aria-errormessage'].evaluate.call(checkContext, target));
+	});
+
+	it('should return true if aria-errormessage id is aria-live=assertive', function () {
+		var testHTML = '<div></div>';
+		testHTML += '<div id="live" aria-live="assertive"></div>';
+		fixture.innerHTML = testHTML;
+		var target = fixture.children[0];
+		target.setAttribute('aria-errormessage', 'live');
+		assert.isTrue(checks['aria-errormessage'].evaluate.call(checkContext, target));
+	});
+
+	it('should return true if aria-errormessage id is aria-describedby', function () {
+		var testHTML = '<div></div>';
+		testHTML += '<div id="plain"></div>';
+		fixture.innerHTML = testHTML;
+		var target = fixture.children[0];
+		target.setAttribute('aria-errormessage', 'plain');
+		target.setAttribute('aria-describedby', 'plain');
+		assert.isTrue(checks['aria-errormessage'].evaluate.call(checkContext, target));
+	});
+});
