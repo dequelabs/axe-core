3ae913ea53e3c643e8ad1a9c45831aea28a4ed9e
Daniel Aasen <daniel.aasen@tingtun.no>
2017-12-12 13:53:13 +0100

    feat(rule): add check for applet-alt
    Check for alternative text on applet tags
    
    Close #536

---
+++ test/checks/label/applet-alt-alt.js
@@ -0,0 +1,42 @@
+describe('applet-alt-alt', function () {
+	'use strict';
+
+	var fixture = document.getElementById('fixture');
+
+	afterEach(function () {
+		fixture.innerHTML = '';
+	});
+
+	it('should return true if element got alt text and body text', function () {
+		var node = document.createElement('applet');
+		node.setAttribute('alt', 'woohoo');
+		node.innerHTML = 'Some new text';
+		fixture.appendChild(node);
+
+		assert.isTrue(checks['applet-alt-alt'].evaluate(node));
+	});
+
+	it('should return false if element got alt text and no body text', function () {
+		var node = document.createElement('applet');
+		node.setAttribute('alt', 'woohoo');
+		node.innerHTML = ' ';
+		fixture.appendChild(node);
+
+		assert.isFalse(checks['applet-alt-alt'].evaluate(node));
+	});
+
+	it('should return false if no alt label pressent', function () {
+		var node = document.createElement('div');
+		fixture.appendChild(node);
+
+		assert.isFalse(checks['applet-alt-alt'].evaluate(node));
+	});
+
+	it('should return false if an alt label got no text', function () {
+		var node = document.createElement('div');
+		node.setAttribute('alt', ' ');
+		fixture.appendChild(node);
+
+		assert.isFalse(checks['applet-alt-alt'].evaluate(node));
+	});
+});
