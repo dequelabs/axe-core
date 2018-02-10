2169b5d743a8f8c64fcff577960dcd9a38d2657c
Wilco Fiers <wilco.fiers@deque.com>
2018-02-10 14:50:25 +0100

    test(disableOtherRoles): Add integration test

--- test/integration/full/configure-options/configure-options.js
+++ test/integration/full/configure-options/configure-options.js
@@ -1,48 +1,80 @@
-describe('Check Configure Options', function() {
+describe('Configure Options', function() {
 	'use strict';
 
-	var target = document.querySelector('#target');
+	afterEach(function () {
+		axe.reset();
+	});
 
-	describe('aria-allowed-attr', function() {
-		it('should allow an attribute supplied in options', function(done) {
-			target.setAttribute('role', 'separator');
-			target.setAttribute('aria-valuenow', '0');
+	describe('Check', function() {
 
-			axe.configure({
-				checks: [{
-					id: 'aria-allowed-attr',
-					options: {'separator': ['aria-valuenow']}
-				}]
+		var target = document.querySelector('#target');
+		describe('aria-allowed-attr', function() {
+			it('should allow an attribute supplied in options', function(done) {
+				target.setAttribute('role', 'separator');
+				target.setAttribute('aria-valuenow', '0');
+
+				axe.configure({
+					checks: [{
+						id: 'aria-allowed-attr',
+						options: {'separator': ['aria-valuenow']}
+					}]
+				});
+				axe.run(target, {
+					runOnly: {
+				    type: 'rule',
+				    values: [ 'aria-allowed-attr' ]
+				  }
+				}, function(error, results) {
+					assert.lengthOf(results.violations, 0, 'violations');
+					done();
+				});
 			});
-			axe.run(target, {
-				runOnly: {
-			    type: 'rule',
-			    values: [ 'aria-allowed-attr' ]
-			  }
-			}, function(error, results) {
-				assert.lengthOf(results.violations, 0, 'violations');
-				done();
+		});
+
+		describe('aria-required-attr', function() {
+			it('should report unique attributes when supplied from options', function(done) {
+				target.setAttribute('role', 'slider');
+				axe.configure({
+					checks: [{
+						id: 'aria-required-attr',
+						options: {slider: ['aria-snuggles']}
+					}]
+				});
+				axe.run('#target',  {
+					runOnly: {
+				    type: 'rule',
+				    values: [ 'aria-required-attr' ]
+				  }
+				}, function(error, results) {
+					assert.lengthOf(results.violations, 1, 'violations');
+					assert.sameMembers(results.violations[0].nodes[0].any[0].data, ['aria-valuemax', 'aria-valuemin', 'aria-snuggles']);
+					done();
+				});
 			});
 		});
 	});
 
-	describe('aria-required-attr', function() {
-		it('should report unique attributes when supplied from options', function(done) {
-			target.setAttribute('role', 'slider');
+	describe('disableOtherRules', function (done) {
+		it('disables rules that are not in the `rules` array', function () {
 			axe.configure({
-				checks: [{
-					id: 'aria-required-attr',
-					options: {slider: ['aria-snuggles']}
+				disableOtherRules: true,
+				rules: [{
+					id: 'html-has-lang',
+					enabled: true
+				}, {
+					id: 'html-lang-valid',
+					enabled: false
 				}]
 			});
-			axe.run('#target',  {
-				runOnly: {
-			    type: 'rule',
-			    values: [ 'aria-required-attr' ]
-			  }
-			}, function(error, results) {
-				assert.lengthOf(results.violations, 1, 'violations');
-				assert.sameMembers(results.violations[0].nodes[0].any[0].data, ['aria-valuemax', 'aria-valuemin', 'aria-snuggles']);
+
+			axe.run(function(error, results) {
+				assert.isNull(error);
+				assert.lengthOf(results.passes, 1, 'passes');
+				assert.equal(results.passes[0].id, 'html-has-lang');
+
+				assert.lengthOf(results.violations, 0, 'violations');
+				assert.lengthOf(results.incomplete, 0, 'incomplete');
+				assert.lengthOf(results.inapplicable, 0, 'inapplicable');
 				done();
 			});
 		});
