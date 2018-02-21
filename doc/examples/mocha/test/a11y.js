cf70f9fda609fc1184d2d1488b226e70888a962c
Wilco Fiers <wilco.fiers@deque.com>
2018-02-21 14:59:11 +0100

    feat: Update Mocha example

--- doc/examples/mocha/test/a11y.js
+++ doc/examples/mocha/test/a11y.js
@@ -6,7 +6,7 @@ describe('axe', function () {
 	it('should report that good HTML is good', function (done) {
 		var n = document.getElementById('working');
 		axe.run(n, function (err, result) {
-			expect(err).to.be.null();
+			expect(err).to.be.null;
 			expect(result.violations.length).to.equal(0);
 			done();
 		});
@@ -15,7 +15,7 @@ describe('axe', function () {
 	it('should report that bad HTML is bad', function (done) {
 		var n = document.getElementById('broken');
 		axe.run(n, function (err, result) {
-			expect(err).to.be.null();
+			expect(err).to.be.null;
 			expect(result.violations.length).to.equal(1);
 			done();
 		});
