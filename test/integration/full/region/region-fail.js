8ad2718744d5f2665f20d84c04e377f2cfb8463b
Wilco Fiers <wilco.fiers@deque.com>
2018-03-03 16:28:47 +0100

    fix(region): Ignore forms without accessible name as landmarks

--- test/integration/full/region/region-fail.js
+++ test/integration/full/region/region-fail.js
@@ -17,6 +17,12 @@ describe('region fail test', function () {
 		it('should find html', function () {
 			assert.deepEqual(results.violations[0].nodes[0].target, ['html']);
 		});
+
+		it('should have all text content as related nodes', function () {
+			var wrapper = document.querySelector('#wrapper');
+			assert.equal(results.violations[0].nodes[0].any[0].relatedNodes.length,
+				wrapper.querySelectorAll('h1, li, p, a').length);
+		});
 	});
 
 	describe('passes', function () {
