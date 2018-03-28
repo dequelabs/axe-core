e6563e2dda3d06b0ee6d04a3d1e15eb6050cf89a
Nick Schonning <nschonni@gmail.com>
2018-03-28 12:02:16 -0500

    chore: ESLint the rest of Build JS files(#803)
    * chore: Remove unused variable
    
    * chore(sri-update): Autofix string quoting
    
    * chore(build): Remove unused require
    
    * chore(postinstall): Run ESLint fix
    
    Needed to change arrow function to manual before this file parsed correctly.
    
    * chore(test/engine): ignore exported vars used for harness
    
    Since this is concated into pages, it is purposly re-exporting.
    Using the env: browser along with the `exported` directive didn't work.
    
    * chore(build/config): Opt out of line length
    
    * chore(build/configure): Move parseChecks to avoid use before define

--- build/utils/postinstall.js
+++ build/utils/postinstall.js
@@ -1,14 +1,14 @@
 var fs = require('fs')
 
-if (fs.existsSync(".git/hooks")) {
+if (fs.existsSync('.git/hooks')) {
 	var commitMsg = '.git/hooks/commit-msg'
 	if (!fs.existsSync(commitMsg)) {
-		var angularPrecommit = "./node_modules/angular-precommit/index.js"
+		var angularPrecommit = './node_modules/angular-precommit/index.js'
 		if (fs.existsSync(angularPrecommit)) {
 			var fsExtra = require('fs-extra')
-			fsExtra.copy(angularPrecommit, commitMsg, (err) => {
-				if (err) throw err
-				console.log("Installing git pre-commit hook")
+			fsExtra.copy(angularPrecommit, commitMsg, function (err) {
+				if (err) {throw err}
+				console.log('Installing git pre-commit hook')
 			})
 		}
 	}
