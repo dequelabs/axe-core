eb09248f54827b4db651b3f46961c156b929633f
Daniil Samoylov <xdansam@gmail.com>
2018-04-04 20:54:53 +1200

    fix(typescript): Update ElementContext type (#822)
    * fixes 819: Correct typings for ElementContext
    * Update other issues in typings to get the TS scripts pass

--- axe.d.ts
+++ axe.d.ts
@@ -13,18 +13,14 @@ declare module axe {
 	type RunOnlyType = "rule" | "rules" | "tag" | "tags";
 
 	type RunOnlyObject = {
-		include?: string[],
-		exclude?: string[]
+		include?: string[] | string[][],
+		exclude?: string[] | string[][]
 	}
 
 	type RunCallback = (error: Error, results:AxeResults) => void;
 
-	interface ElementContext {
-		node?: Object,
-		selector?: string,
-		include?: any[],
-		exclude?: any[]
-	}
+	type ElementContext = Node | string | RunOnlyObject;
+
 	interface RunOnly {
 		type: RunOnlyType,
 		values?: TagValue[] | RunOnlyObject
@@ -132,7 +128,7 @@ declare module axe {
 	 * @param   {RunCallback}    callback Optional The function to invoke when analysis is complete.
 	 * @returns {Promise<AxeResults>|void} If the callback was not defined, aXe will return a Promise.
 	 */
-	function run(context: ElementContext): Promise<AxeResults>
+	function run(context?: ElementContext): Promise<AxeResults>
 	function run(options: RunOptions): Promise<AxeResults>
 	function run(callback: (error: Error, results:AxeResults) => void): void
 	function run(context: ElementContext, callback: RunCallback): void
