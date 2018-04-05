1f03ee442166437b1ad7b9948182e653f26e079e
Wahaj Abid <waabid@microsoft.com>
2018-04-05 13:21:48 -0700

    fix(layout-table): tests for layout table matches for focusable and non-presentation/none tables

--- test/rule-matches/layout-table-matches.js
+++ test/rule-matches/layout-table-matches.js
@@ -2,6 +2,7 @@ describe('layout-table-matches', function () {
 	'use strict';
 
 	var fixture = document.getElementById('fixture');
+	var fixtureSetup = axe.testUtils.fixtureSetup;
 	var rule;
 
 	beforeEach(function () {
@@ -10,17 +11,42 @@ describe('layout-table-matches', function () {
 		});
 	});
 
+	afterEach(function () {
+		fixture.innerHTML = '';
+	});
+
 	it('returns false for element that is not focusable and has presentation role', function () {
-		fixture.innerHTML = '<table role="presentation"></table>';
+		fixtureSetup('<table role="presentation"></table>');
 		var target = fixture.querySelector('table');
 
 		assert.isFalse(rule.matches(target));
 	});
 
 	it('returns false for element that is not focusable and has none role', function () {
-		fixture.innerHTML = '<table role="none"></table>';
+		fixtureSetup('<table role="none"></table>');
 		var target = fixture.querySelector('table');
 
 		assert.isFalse(rule.matches(target));
+	});
+
+	it('returns trie for element that is a table without presentation/none role', function () {
+		fixtureSetup('<table></table>');
+		var target = fixture.querySelector('table');
+
+		assert.isTrue(rule.matches(target));
+	});
+
+	it('returns true for element that is focusable and has none role', function () {
+		fixtureSetup('<table role="none" tabindex="0"></table>');
+		var target = fixture.querySelector('table');
+
+		assert.isTrue(rule.matches(target));
+	});
+
+	it('returns true for element that is focusable and has presentation role', function () {
+		fixtureSetup('<table role="presentation" tabindex="0"></table>');
+		var target = fixture.querySelector('table');
+
+		assert.isTrue(rule.matches(target));
 	});
 });
