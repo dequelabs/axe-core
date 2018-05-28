de656ee3252becef588c0bbfc65c88000041919c
Wilco Fiers <wilco.fiers@deque.com>
2018-05-28 15:52:21 +0200

    fix: td-has-heading to ignore td with its role changed

--- test/commons/table/is-data-cell.js
+++ test/commons/table/is-data-cell.js
@@ -40,4 +40,42 @@ describe('table.isDataCell', function () {
 		assert.isFalse(axe.commons.table.isDataCell(target));
 	});
 
+	it('should ignore TDs with a valid role other than (grid)cell', function () {
+		fixture.innerHTML = '<table>' +
+			'<tr><td id="target1" role="columnheader">heading</td></tr>' +
+			'<tr><td id="target2" role="rowheader">heading</td></tr>' +
+			'<tr><td id="target3" role="presentation">heading</td></tr>' +
+			'</table>';
+
+		var target1 = $id('target1');
+		var target2 = $id('target2');
+		var target3 = $id('target3');
+		assert.isFalse(axe.commons.table.isDataCell(target1));
+		assert.isFalse(axe.commons.table.isDataCell(target2));
+		assert.isFalse(axe.commons.table.isDataCell(target3));
+	});
+
+	it('should return true for elements with role="(grid)cell"', function () {
+		fixture.innerHTML = '<table>' +
+			'<tr><th id="target1" role="cell">heading</th></tr>' +
+			'<tr><th id="target2" role="gridcell">heading</th></tr>' +
+			'</table>';
+
+		var target1 = $id('target1');
+		var target2 = $id('target2');
+		assert.isTrue(axe.commons.table.isDataCell(target1));
+		assert.isTrue(axe.commons.table.isDataCell(target2));
+	});
+
+	it('should ignore invalid roles', function () {
+		fixture.innerHTML = '<table>' +
+			'<tr><td id="target1" role="foobar">heading</td></tr>' +
+			'<tr><th id="target2" role="foobar">heading</th></tr>' +
+			'</table>';
+
+		var target1 = $id('target1');
+		var target2 = $id('target2');
+		assert.isTrue(axe.commons.table.isDataCell(target1));
+		assert.isFalse(axe.commons.table.isDataCell(target2));
+	});
 });
