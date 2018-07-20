e3b1e1d8d1aa87c9d3e9a60c742a540deacce948
Wilco Fiers <wilco.fiers@deque.com>
2018-07-20 19:52:45 +0200

    fix: Ignore abstracts in determining element roles

--- test/commons/aria/roles.js
+++ test/commons/aria/roles.js
@@ -13,6 +13,16 @@ describe('aria.isValidRole', function() {
 	it('should return false if role is not found in the lookup table', function() {
 		assert.isFalse(axe.commons.aria.isValidRole('cats'));
 	});
+
+	it('returns false for abstract roles by default', function() {
+		assert.isFalse(axe.commons.aria.isValidRole('input'));
+	});
+
+	it('returns true for abstract roles with { allowAbstract: true } ', function() {
+		assert.isTrue(
+			axe.commons.aria.isValidRole('input', { allowAbstract: true })
+		);
+	});
 });
 
 describe('aria.getRolesWithNameFromContents', function() {
