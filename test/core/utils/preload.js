63cc5b4d05929ed1d49a296bf1118712c24086d2
Jey <jey.nandakumar@gmail.com>
2018-08-08 12:53:58 +0100

    fix: update preload function and tests

--- test/core/utils/preload.js
+++ test/core/utils/preload.js
@@ -1,65 +1,67 @@
-describe('axe.utils.preload unit tests', function() {
+describe('axe.utils.preload', function() {
 	'use strict';
 
-	describe('axe.utils.preload', function() {
-		it('should return a queue', function() {
-			var options = {
-				preload: true
-			};
-			var actual = axe.utils.preload(options);
-			assert.isObject(actual);
-			assert.containsAllKeys(actual, ['then', 'defer', 'catch']);
-		});
+	it('should return a queue', function() {
+		var options = {
+			preload: true
+		};
+		var actual = axe.utils.preload(options);
+		assert.isObject(actual);
+		assert.containsAllKeys(actual, ['then', 'defer', 'catch']);
+	});
 
-		it('should ensure queue is defer(able)', function(done) {
-			var options = {
-				preload: false
-			};
-			var actual = axe.utils.preload(options);
-			actual
-				.defer(function(res, rej) {
-					assert.isFunction(rej);
-					res(true);
-					done();
-				})
-				.catch(function(error) {
-					done(error);
-				});
-		});
+	it('should ensure queue is defer(able)', function(done) {
+		var options = {
+			preload: false
+		};
+		var actual = axe.utils.preload(options);
+		actual
+			.defer(function(res, rej) {
+				assert.isFunction(rej);
+				res(true);
+				done();
+			})
+			.catch(function(error) {
+				done(error);
+			});
+	});
 
-		it('should ensure queue is then(able)', function(done) {
-			var options = {
-				preload: false
-			};
-			var actual = axe.utils.preload(options);
-			actual
-				.then(function(results) {
-					assert.isDefined(results);
-					done();
-				})
-				.catch(function(error) {
-					done(error);
-				});
-		});
+	it('should ensure queue is then(able)', function(done) {
+		var options = {
+			preload: false
+		};
+		var actual = axe.utils.preload(options);
+		actual
+			.then(function(results) {
+				assert.isDefined(results);
+				done();
+			})
+			.catch(function(error) {
+				done(error);
+			});
+	});
 
-		it('should return an object with property cssom', function(done) {
-			var options = {
-				preload: {
-					assets: ['cssom']
-				}
-			};
-			var actual = axe.utils.preload(options);
-			actual
-				.then(function(results) {
-					assert.isDefined(results);
-					assert.isArray(results);
-					assert.property(results[0], 'cssom');
+	it('should return an object with property cssom and verify result is same output from preloadCssom', function(done) {
+		var options = {
+			preload: {
+				assets: ['cssom']
+			}
+		};
+		var actual = axe.utils.preload(options);
+		actual
+			.then(function(results) {
+				assert.isDefined(results);
+				assert.isArray(results);
+				assert.property(results[0], 'cssom');
+				// also verify that result from css matches that of preloadCssom
+				axe.utils.preloadCssom(options).then(function(resultFromPreloadCssom) {
+					assert.deepEqual(results[0].cssom, resultFromPreloadCssom[0]);
 					done();
-				})
-				.catch(function(error) {
-					done(error);
 				});
-		});
+			})
+			.catch(function(error) {
+				done(error);
+			});
 	});
 
 	describe('axe.utils.shouldPreload', function() {
