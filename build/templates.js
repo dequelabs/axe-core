3dbab2849633c05affd69202b86384195a46d562
Jey <jey.nandakumar@gmail.com>
2018-08-14 14:15:08 +0100

    fix: preload within audit.run and pass context to checks

--- build/templates.js
+++ build/templates.js
@@ -1,6 +1,5 @@
 module.exports = {
-	evaluate:
-		'function (node, options, virtualNode, preloadedAssets) {\n<%=source%>\n}',
+	evaluate: 'function (node, options, virtualNode, context) {\n<%=source%>\n}',
 	after: 'function (results, options) {\n<%=source%>\n}',
 	gather: 'function (context) {\n<%=source%>\n}',
 	matches: 'function (node, virtualNode) {\n<%=source%>\n}',
