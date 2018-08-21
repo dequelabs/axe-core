8769dc9232691def378e74ef56f5c0f7a5c66364
Stephen Mathieson <me@stephenmathieson.com>
2018-08-21 14:06:21 -0400

    ci: automate publishing releases to npm
    This patch adds configuration for automatically publishing to npm, pending a manual hold. To accept the hold, a user with write access must login to CircleCI and click the "Approve" button.
    
    All (green) commits to the `develop` branch will result in a "canary" version being published to npm. The published version will resemble `X.Y.Z-canary.COMMITSH`, where `X.Y.Z` are the existing SEMVER numbers, and `COMMITSIH` is the Git SHA1 for the commit. The version is published with the [dist tag](https://docs.npmjs.com/cli/dist-tag) "canary". This enables users to `npm install axe-core@canary` to get the latest version on GitHub.
    
    All (green) commits to the `master` branch will result in a production version being published to npm. It is assumed that `package.json`'s version field will be updated as part of each commit to `master` (CircleCI does not do this for us).
    
    For a further explanation, this process is documented in [Google Drive](https://docs.google.com/document/d/14S2ktHpBVNPAR7JdkptOOgmufbZIV0m7orz6N95tPzk) (private whitepaper, sorry OSS folks).

---
+++ build/canary.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+
+const pkgFile = path.resolve(__dirname, '..', 'package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgFile));
+
+const { CIRCLE_SHA1 } = process.env;
+assert(CIRCLE_SHA1, 'CIRCLE_SHA1 enviornment variable not set');
+
+// Shorten the SHA
+const GIT_SHA = CIRCLE_SHA1.substr(0, 7);
+
+// Strip the "dist tag" from the version (if it exists)
+const version = pkg.version.replace(/-\w+\.\d$/, '');
+const nextVersion = `${version}-canary.${GIT_SHA}`;
+
+pkg.version = nextVersion;
+fs.writeFileSync(pkgFile, JSON.stringify(pkg, null, 2));
+
+console.log(`Updated version to ${nextVersion}`);
