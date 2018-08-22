074329be75fe9abb9e0503f324287f70e93aa994
Stephen Mathieson <me@stephenmathieson.com>
2018-08-22 10:53:07 -0400

    build: fix typo

--- build/next-version.js
+++ build/next-version.js
@@ -8,8 +8,8 @@ const pkgFile = path.resolve(__dirname, '..', 'package.json');
 const pkg = JSON.parse(fs.readFileSync(pkgFile));
 
 const { CIRCLE_SHA1, CIRCLE_BRANCH } = process.env;
-assert(CIRCLE_BRANCH, 'CIRCLE_BRANCH enviornment variable not set');
-assert(CIRCLE_SHA1, 'CIRCLE_SHA1 enviornment variable not set');
+assert(CIRCLE_BRANCH, 'CIRCLE_BRANCH environment variable not set');
+assert(CIRCLE_SHA1, 'CIRCLE_SHA1 environment variable not set');
 assert(
 	CIRCLE_BRANCH === 'develop',
 	'This script should only be run from "develop"'
