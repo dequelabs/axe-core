d29ff172181f34531bf3901660006496e7d786ca
Jey <jey.nandakumar@gmail.com>
2018-08-29 15:12:46 +0100

    fix: remove redundant check in test

--- test/integration/full/preload-cssom/preload-cssom.js
+++ test/integration/full/preload-cssom/preload-cssom.js
@@ -246,20 +246,14 @@ describe('preload cssom integration test', function() {
 							.filter(function(s) {
 								return s.shadowId;
 							});
+						assertStylesheet(
+							nonExternalsheetsWithShadowId[
+								nonExternalsheetsWithShadowId.length - 1
+							].sheet,
+							'.green',
+							'.green{background-color:green;}'
+						);
 
-						// Issue - https://github.com/dequelabs/axe-core/issues/1082
-						if (
-							nonExternalsheetsWithShadowId &&
-							nonExternalsheetsWithShadowId.length
-						) {
-							assertStylesheet(
-								nonExternalsheetsWithShadowId[
-									nonExternalsheetsWithShadowId.length - 1
-								].sheet,
-								'.green',
-								'.green{background-color:green;}'
-							);
-						}
 						done();
 					})
 					.catch(done);
