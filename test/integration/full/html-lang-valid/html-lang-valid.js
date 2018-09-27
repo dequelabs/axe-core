e3ce8469005b327335229267033211e604eb5148
jkodu <jey.nandakumar@gmail.com>
2018-09-27 09:48:40 +0100

    test: add tests for xml:lang for rule html-lang-valid

--- test/integration/full/html-lang-valid/html-lang-valid.js
+++ test/integration/full/html-lang-valid/html-lang-valid.js
@@ -1,6 +1,8 @@
 describe('html-lang-valid test', function() {
 	'use strict';
+
 	var results;
+
 	before(function(done) {
 		axe.testUtils.awaitNestedLoad(function() {
 			axe.run(
@@ -15,34 +17,53 @@ describe('html-lang-valid test', function() {
 	});
 
 	describe('violations', function() {
-		it('should find 2', function() {
-			assert.lengthOf(results.violations[0].nodes, 2);
+		var violations = results.violations[0];
+
+		it('should find 3', function() {
+			assert.lengthOf(violations.nodes, 3);
 		});
+
 		it('should find first level iframe', function() {
-			assert.deepEqual(results.violations[0].nodes[0].target, [
-				'#frame1',
-				'#violation1'
-			]);
+			assert.deepEqual(violations.nodes[0].target, ['#frame1', '#violation1']);
 		});
+
 		it('should find second level iframe', function() {
-			assert.deepEqual(results.violations[0].nodes[1].target, [
+			assert.deepEqual(violations.nodes[1].target, [
 				'#frame1',
 				'#frame2',
 				'#violation2'
 			]);
 		});
+
+		it('should find #violation2c', function() {
+			assert.deepEqual(violations.nodes[2].target, [
+				'#frame1',
+				'#frame5',
+				'#violation2c'
+			]);
+		});
 	});
 
 	describe('passes', function() {
-		it('should find 1', function() {
-			assert.lengthOf(results.passes[0].nodes, 1);
+		var passes = results.passes[0];
+
+		it('should find 2', function() {
+			assert.lengthOf(passes.nodes, 2);
 		});
 
 		it('should find #pass1', function() {
-			assert.deepEqual(results.passes[0].nodes[0].target, [
+			assert.deepEqual(passes.nodes[0].target, [
 				'#frame1',
 				'#frame3',
 				'#pass1'
+			]);
+		});
+
+		it('should find #pass2b', function() {
+			assert.deepEqual(passes.nodes[1].target, [
+				'#frame1',
+				'#frame4',
+				'#pass2b'
 			]);
 		});
 	});
