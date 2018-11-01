4a20f221c601a1feaa0e018801ea7f294ab36535
Daniel Aasen <daniel.aasen@tingtun.no>
2018-11-01 14:30:03 +0100

    refactor(check): update check code, fix documentation and html typos

--- test/integration/full/focus/focus.js
+++ test/integration/full/focus/focus.js
@@ -1,33 +1,33 @@
 
 describe('focus test failure', function () {
-	'use strict';
-	var results;
+  'use strict';
+  var results;
 
-	before(function (done) {
-		axe.run({ runOnly: { type: 'rule', values: ['focus'] } }, function (err, r) {
-			assert.isNull(err);
-			results = r;
-			done();
-		});
-	});
+  before(function (done) {
+    axe.run({ runOnly: { type: 'rule', values: ['focus'] } }, function (err, r) {
+      assert.isNull(err);
+      results = r;
+      done();
+    });
+  });
 
-	describe('violations', function () {
-		it('should find 1', function () {
-			assert.lengthOf(results.violations[0].nodes, 1);
-		});
-	});
+  describe('violations', function () {
+    it('should find 7', function () {
+      assert.lengthOf(results.violations[0].nodes, 7);
+    });
+  });
 
-	describe('passes', function () {
-		it('should find 1', function () {
-			assert.lengthOf(results.passes[0].nodes, 1);
-		});
-	});
+  describe('passes', function () {
+    it('should find 1', function () {
+      assert.lengthOf(results.passes[0].nodes, 1);
+    });
+  });
 
-	it('should find 0 inapplicable', function () {
-		assert.lengthOf(results.inapplicable, 0);
-	});
+  it('should find 0 inapplicable', function () {
+    assert.lengthOf(results.inapplicable, 0);
+  });
 
-	it('should find 1 incomplete', function () {
-		assert.lengthOf(results.incomplete[0].nodes, 1);
-	});
+  it('should find 1 incomplete', function () {
+    assert.lengthOf(results.incomplete[0].nodes, 1);
+  });
 });
