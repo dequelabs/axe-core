ebd28e1cd69be73556558cb126e5f24e75e78d14
Daniel Aasen <daniel.aasen@tingtun.no>
2018-11-05 10:39:39 +0100

    feat(check): add use of ariaDescribedby function to link-text check

--- test/checks/text-alternatives/link-text.js
+++ test/checks/text-alternatives/link-text.js
@@ -1,90 +1,88 @@
 describe('link-text', function() {
-	'use strict';
+  'use strict';
 
-	var fixture = document.getElementById('fixture');
-	var fixtureSetup = axe.testUtils.fixtureSetup;
-	var shadowSupport = axe.testUtils.shadowSupport.v1;
+  var fixture = document.getElementById('fixture');
+  var shadowSupport = axe.testUtils.shadowSupport.v1;
+  var checkSetup = axe.testUtils.checkSetup;
 
-	afterEach(function() {
-		fixture.innerHTML = '';
-		axe._tree = undefined;
-	});
+  afterEach(function() {
+    fixture.innerHTML = '';
+    axe._tree = undefined;
+  });
 
-	it('should return true if title text is different then other "a" tag and link text is same', function() {
-		var node = document.createElement('a');
-		node.setAttribute('title', 'woohoo');
-		node.setAttribute('href', '#1');
-		node.textContent = 'some text';
-		var node2 = document.createElement('a');
-		node2.setAttribute('title', 'woohoo2');
-		node2.setAttribute('href', '#2');
-		node2.textContent = 'some text';
-		fixtureSetup([node, node2]);
+  it('should return true if title text is different then other "a" tag and link text is same', function() {
+    checkSetup('<a id="testElem" title="woohoo" href="#1">some text</a>' +
+               '<a title="woohoo2" href="#2">some text</a>',
+               '#fixture');
+    var testElem = document.getElementById('testElem');
+    assert.isTrue(checks['link-text'].evaluate(testElem));
+  });
 
-		assert.isTrue(checks['link-text'].evaluate(node));
-	});
+  it('should return true if both links have the same href', function() {
+    checkSetup('<a id="testElem" title="woohoo" href="#1">some text</a>' +
+               '<a title="woohoo" href="#1">some text</a>',
+               '#fixture');
+    var testElem = document.getElementById('testElem');
+    assert.isTrue(checks['link-text'].evaluate(testElem));
+  });
 
-	it('should return true if both links have the same href', function() {
-		var node = document.createElement('a');
-		node.setAttribute('title', 'woohoo');
-		node.setAttribute('href', '#1');
-		node.textContent = 'some text';
-		var node2 = document.createElement('a');
-		node2.setAttribute('title', 'woohoo');
-		node2.setAttribute('href', '#1');
-		node2.textContent = 'some text';
-		fixtureSetup([node, node2]);
+  it('should return true if link text different then other "a" tag and title text is same', function() {
+    checkSetup('<a id="testElem" title="woohoo" href="#1">some text</a>' +
+               '<a title="woohoo2" href="#2">some text1</a>',
+               '#fixture');
+    var testElem = document.getElementById('testElem');
+    assert.isTrue(checks['link-text'].evaluate(testElem));
+  });
 
-		assert.isTrue(checks['link-text'].evaluate(node));
-	});
+  it('should return true if link text is the same but description text is different', function() {
+    checkSetup('<a id="testElem" aria-describedby="descriptionText" title="woohoo" href="#1">some text</a>' +
+               '<a title="woohoo" href="#2">some text</a>' +
+               '<div id="descriptionText">This description</div>',
+               '#fixture');
+    var testElem = document.getElementById('testElem');
+    assert.isTrue(checks['link-text'].evaluate(testElem));
+  });
 
-	it('should return true if link text different then other "a" tag and title text is same', function() {
-		var node = document.createElement('a');
-		node.setAttribute('title', 'woohoo');
-		node.setAttribute('href', '#1');
-		node.textContent = 'some text';
-		var node2 = document.createElement('a');
-		node2.setAttribute('title', 'woohoo');
-		node2.setAttribute('href', '#2');
-		node2.textContent = 'some text1';
-		fixtureSetup([node, node2]);
+  it('should return false if link text and description text is the same', function() {
+    checkSetup('<a id="testElem" aria-describedby="descriptionText" title="woohoo" href="#1">some text</a>' +
+               '<a title="woohoo" aria-describedby="descriptionText" href="#2">some text</a>' +
+               '<div id="descriptionText">This description</div>',
+               '#fixture');
+    var testElem = document.getElementById('testElem');
+    assert.isFalse(checks['link-text'].evaluate(testElem));
+  });
 
-		assert.isTrue(checks['link-text'].evaluate(node));
-	});
+  it('should return false if both link and title text is the same as other "a" tag', function() {
+    checkSetup('<a id="testElem" title="woohoo" href="#1">some text</a>' +
+               '<a title="woohoo" href="#2">some text</a>',
+               '#fixture');
+    var testElem = document.getElementById('testElem');
+    assert.isFalse(checks['link-text'].evaluate(testElem));
+  });
 
-	it('should return false if both link and title text is the same as other "a" tag', function() {
-		var node = document.createElement('a');
-		node.setAttribute('title', 'woohoo');
-		node.setAttribute('href', '#1');
-		node.textContent = 'some text';
-		var node2 = document.createElement('a');
-		node2.setAttribute('title', 'woohoo');
-		node2.setAttribute('href', '#2');
-		node2.textContent = 'some text';
-		fixtureSetup([node, node2]);
+  (shadowSupport ? it : xit)('works on elements in a shadow DOM', function() {
+    checkSetup('<div id="shadow"> <a id="anchor1" href="#1" title="1">some text</a> </div>',
+               '#fixture');
+    var shadowRoot = document
+      .getElementById('shadow')
+      .attachShadow({ mode: 'open' });
+    shadowRoot.innerHTML = '<slot></slot><a id="anchor2" href="#2" title="1">some text</a>' +
+                           '<a id="anchor3" href="#3" title="2">some text</a>';
+    axe._tree = axe.utils.getFlattenedTree(fixture);
 
-		assert.isFalse(checks['link-text'].evaluate(node));
-	});
+    var anchor1 = document.querySelector('#anchor1');
+    assert.isFalse(
+      checks['link-text'].evaluate(anchor1)
+    );
 
-	(shadowSupport ? it : xit)('works on elements in a shadow DOM', function() {
-		fixture.innerHTML =
-			'<div id="shadow"> <a id="anchor1" href="#1" title="1">some text</a> </div>';
-		var shadowRoot = document
-			.getElementById('shadow')
-			.attachShadow({ mode: 'open' });
-		shadowRoot.innerHTML = '<a id="anchor2" href="#2" title="1">some text</a>';
-		axe._tree = axe.utils.getFlattenedTree(fixture);
+    var anchor2 = shadowRoot.querySelector('#anchor2');
+    assert.isFalse(
+      checks['link-text'].evaluate(anchor2)
+    );
 
-		var anchor1 = document.querySelector('#anchor1');
-		var virtualAnchor1 = axe.utils.getNodeFromTree(axe._tree[0], anchor1);
-		assert.isTrue(
-			checks['link-text'].evaluate(anchor1, undefined, virtualAnchor1)
-		);
-
-		var anchor2 = shadowRoot.querySelector('#anchor2');
-		var virtualAnchor2 = axe.utils.getNodeFromTree(axe._tree[0], anchor2);
-		assert.isTrue(
-			checks['link-text'].evaluate(anchor2, undefined, virtualAnchor2)
-		);
-	});
+    var anchor3 = shadowRoot.querySelector('#anchor3');
+    assert.isTrue(
+      checks['link-text'].evaluate(anchor3)
+    );
+  });
 });
