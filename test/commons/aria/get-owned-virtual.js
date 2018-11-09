4de54890934becaa932c4235d03caea44c0c80e0
Wilco Fiers <wilco.fiers@deque.com>
2018-11-09 13:16:27 +0100

    chore: Add tests

---
+++ test/commons/aria/get-owned-virtual.js
@@ -0,0 +1,47 @@
+describe('aria.getOwnedVirtual', function() {
+	'use strict';
+	var aria = axe.commons.aria;
+	var fixtureSetup = axe.testUtils.fixtureSetup;
+
+	it('returns a list of children in order', function() {
+		fixtureSetup(
+			'<div id="target">' +
+				'<h1>heading 1</h1>' +
+				'<h2>heading 2</h2>' +
+				'<h3>heading 3</h3>' +
+				'</div>'
+		);
+		var target = axe.utils.querySelectorAll(axe._tree[0], '#target')[0];
+		var owned = aria.getOwnedVirtual(target);
+		assert.lengthOf(owned, 3);
+		assert.equal(owned[0].actualNode.nodeName.toUpperCase(), 'H1');
+		assert.equal(owned[1].actualNode.nodeName.toUpperCase(), 'H2');
+		assert.equal(owned[2].actualNode.nodeName.toUpperCase(), 'H3');
+	});
+
+	it('adds aria-owned reffed elements to the children', function() {
+		fixtureSetup(
+			'<div id="target" aria-owns="hdr3 hdr4">' +
+				'<h1>heading 1</h1>' +
+				'<h2>heading 2</h2>' +
+				'</div>' +
+				'<h4 id="hdr4">heading 4</h4>' +
+				'<h3 id="hdr3">heading 3</h3>'
+		);
+		var target = axe.utils.querySelectorAll(axe._tree[0], '#target')[0];
+		var owned = aria.getOwnedVirtual(target);
+		assert.lengthOf(owned, 4);
+		assert.equal(owned[0].actualNode.nodeName.toUpperCase(), 'H1');
+		assert.equal(owned[1].actualNode.nodeName.toUpperCase(), 'H2');
+		assert.equal(owned[2].actualNode.nodeName.toUpperCase(), 'H3');
+		assert.equal(owned[2].actualNode.nodeName.toUpperCase(), 'H4');
+	});
+
+	it('ignores whitespace-only aria-owned');
+
+	it('ignores broken aria-owned refs');
+
+	it('includes text nodes');
+
+	it('returns an empty array if there are no owned elements');
+});
