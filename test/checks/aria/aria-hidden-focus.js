16fc190bdcc799f2ebcd7483461749fe4e2d1ff9
jkodu <jey.nandakumar@gmail.com>
2018-11-19 16:16:15 +0000

    fix: add matches checks and tests based on review

--- test/checks/aria/aria-hidden-focus.js
+++ test/checks/aria/aria-hidden-focus.js
@@ -1,11 +1,12 @@
 describe('aria-hidden-focus', function() {
 	'use strict';
 
+	var check;
 	var fixture = document.getElementById('fixture');
 	var fixtureSetup = axe.testUtils.fixtureSetup;
-	var check;
 	var shadowSupported = axe.testUtils.shadowSupport.v1;
 	var checkContext = axe.testUtils.MockCheckContext();
+	var checkSetup = axe.testUtils.checkSetup;
 
 	before(function() {
 		check = checks['aria-hidden-focus'];
@@ -20,87 +21,64 @@ describe('aria-hidden-focus', function() {
 
 	// pass
 	it('returns true when content not focusable by default', function() {
-		fixtureSetup('<p id="target" aria-hidden="true">Some text</p>');
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var params = checkSetup('<p id="target" aria-hidden="true">Some text</p>');
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isTrue(actual);
 	});
 
 	it('returns true when content hidden through CSS', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<div id="target" aria-hidden="true"><a href="/" style="display:none">Link</a></div>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isTrue(actual);
 	});
 
 	it('returns true when BUTTON removed from tab order through tabindex', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<div id="target" aria-hidden="true">' +
 				'<button tabindex="-1">Some button</button>' +
 				'</div>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isTrue(actual);
 	});
 
 	it('returns true when TEXTAREA removed from tab order through tabindex', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<div id="target" aria-hidden="true">' +
 				'<label>Enter your comments:' +
 				'<textarea tabindex="-1"></textarea>' +
 				'</label>' +
 				'</div>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isTrue(actual);
 	});
 
 	it('returns true when content made unfocusable through disabled', function() {
-		fixtureSetup('<input id="target" disabled aria-hidden="true" />');
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var params = checkSetup(
+			'<input id="target" disabled aria-hidden="true" />'
+		);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isTrue(actual);
 	});
 
 	it('returns true when aria-hidden=false does not negate aria-hidden true', function() {
 		// Note: aria-hidden can't be reset once you've set it to true on an ancestor
-		fixtureSetup(
+		var params = checkSetup(
 			'<div id="target" aria-hidden="true"><div aria-hidden="false"><button tabindex="-1">Some button</button></div></div>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isTrue(actual);
 	});
 
 	(shadowSupported ? it : xit)(
-		'returns true when content hidden through CSS inside shadowDOM',
-		function() {
-			fixtureSetup('<div id="target"></div>');
-			var node = fixture.querySelector('#target');
-			var shadow = node.attachShadow({ mode: 'open' });
-			shadow.innerHTML =
-				'<div aria-hidden="true"><a href="/" style="display:none">Link</a></div>';
-			axe._tree = axe.utils.getFlattenedTree(fixture);
-			axe._selectorData = axe.utils.getSelectorData(axe._tree);
-			var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-			var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
-			assert.isTrue(actual);
-		}
-	);
-
-	(shadowSupported ? it : xit)(
 		'returns true when BUTTON is removed from tab order through tabindex which coexists with plain text in shadowDOM',
 		function() {
+			// Note:
+			// `testUtils.checkSetup` does not work for shadowDOM
+			// as `axe._tree` and `axe._selectorData` needs to be updated after shadowDOM construction
 			fixtureSetup(
 				'<div aria-hidden="true" id="target"><button tabindex="-1">btn</button></div>`'
 			);
@@ -117,12 +95,10 @@ describe('aria-hidden-focus', function() {
 
 	// fail
 	it('returns false when focusable off screen link', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<div id="target" aria-hidden="true"><a href="/" style="position:absolute; top:-999em">Link</a></div>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isFalse(actual);
 		assert.deepEqual(
 			checkContext._relatedNodes,
@@ -131,12 +107,10 @@ describe('aria-hidden-focus', function() {
 	});
 
 	it('returns false when focusable form field only disabled through ARIA', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<div id="target" aria-hidden="true"><input type="text" aria-disabled="true"/></div>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isFalse(actual);
 		assert.deepEqual(
 			checkContext._relatedNodes,
@@ -145,22 +119,18 @@ describe('aria-hidden-focus', function() {
 	});
 
 	it('returns false when focusable content through tabindex', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<p id="target" tabindex="0" aria-hidden="true">Some text</p>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isFalse(actual);
 	});
 
 	it('returns false when focusable SUMMARY element', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<details id="target" aria-hidden="true"><summary>Some button</summary><p>Some details</p></details>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isFalse(actual);
 		assert.deepEqual(
 			checkContext._relatedNodes,
@@ -169,7 +139,7 @@ describe('aria-hidden-focus', function() {
 	});
 
 	it('returns false when focusable SELECT element', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<div id="target" aria-hidden="true">' +
 				'<label>Choose:' +
 				'<select>' +
@@ -179,9 +149,7 @@ describe('aria-hidden-focus', function() {
 				'</label>' +
 				'</div>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isFalse(actual);
 		assert.deepEqual(
 			checkContext._relatedNodes,
@@ -190,7 +158,7 @@ describe('aria-hidden-focus', function() {
 	});
 
 	it('returns false when focusable AREA element', function() {
-		fixtureSetup(
+		var params = checkSetup(
 			'<main id="target" aria-hidden="true">' +
 				'<map name="infographic">' +
 				'<area shape="rect" coords="184,6,253,27" href="https://mozilla.org"' +
@@ -198,9 +166,8 @@ describe('aria-hidden-focus', function() {
 				'</map>' +
 				'</main>'
 		);
-		var node = fixture.querySelector('#target');
-		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
-		var actual = check.evaluate.call(checkContext, node, {}, virtualNode);
+
+		var actual = check.evaluate.apply(checkContext, params);
 		assert.isFalse(actual);
 		assert.deepEqual(
 			checkContext._relatedNodes,
@@ -211,6 +178,9 @@ describe('aria-hidden-focus', function() {
 	(shadowSupported ? it : xit)(
 		'returns false when focusable content through tabindex inside shadowDOM',
 		function() {
+			// Note:
+			// `testUtils.checkSetup` does not work for shadowDOM
+			// as `axe._tree` and `axe._selectorData` needs to be updated after shadowDOM construction
 			fixtureSetup('<div id="target"></div>');
 			var node = fixture.querySelector('#target');
 			var shadow = node.attachShadow({ mode: 'open' });
