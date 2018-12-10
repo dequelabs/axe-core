f5068fee03e7b5ecb9f4099e7c7573db8e1f251c
jkodu <jey.nandakumar@gmail.com>
2018-12-10 16:00:22 +0000

    fix: cache phantom to a different directory

--- .circleci/install-phantomjs.sh
+++ .circleci/install-phantomjs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if ! [ $(which phantomjs) ]; then
-	sudo curl --output /usr/local/bin/phantomjs https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
+	sudo curl --output phantomjs https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
 fi
+
+sudo cp -r phantomjs /usr/local/bin/phantomjs
 
 sudo chmod ugo+x /usr/local/bin/phantomjs
