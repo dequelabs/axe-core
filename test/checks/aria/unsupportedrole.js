25e998d90a5c15606dd02e7c4a772f44379e1c0e
Steven Lambert <steven.lambert@deque.com>
2019-01-17 09:57:07 -0700

    fix: dont flag invalid roles in unsupportedrole

--- test/checks/aria/unsupportedrole.js
+++ test/checks/aria/unsupportedrole.js
@@ -12,13 +12,19 @@ describe('unsupportedrole', function() {
 			type: 'widget',
 			unsupported: true
 		};
-		fixture.innerHTML = '<div id="target" role="mccheddarton">Contents</div>';
+		fixture.innerHTML = '<div id="target" role="mcheddarton">Contents</div>';
 		var node = fixture.querySelector('#target');
 		assert.isTrue(checks.unsupportedrole.evaluate(node));
 	});
 
 	it('should return false if applied to a supported role', function() {
 		fixture.innerHTML = '<div id="target" role="alert">Contents</div>';
+		var node = fixture.querySelector('#target');
+		assert.isFalse(checks.unsupportedrole.evaluate(node));
+	});
+
+	it('should return false if applied to an invalid role', function() {
+		fixture.innerHTML = '<input id="target" role="foo">';
 		var node = fixture.querySelector('#target');
 		assert.isFalse(checks.unsupportedrole.evaluate(node));
 	});
