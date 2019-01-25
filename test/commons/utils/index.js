c5cc19af8521ed9c10922f5bdeb5aa7e77e817d7
jkodu <jey.nandakumar@gmail.com>
2019-01-25 12:04:19 +0000

    fix: reintroduce commons utils namespace

---
+++ test/commons/utils/index.js
@@ -0,0 +1,21 @@
+describe('utils.escapeSelector', function() {
+	'use strict';
+
+	it('should be a function', function() {
+		assert.isFunction(axe.commons.utils.escapeSelector);
+	});
+});
+describe('utils.matchesSelector', function() {
+	'use strict';
+
+	it('should be a function', function() {
+		assert.isFunction(axe.commons.utils.matchesSelector);
+	});
+});
+describe('utils.clone', function() {
+	'use strict';
+
+	it('should be a function', function() {
+		assert.isFunction(axe.commons.utils.clone);
+	});
+});
