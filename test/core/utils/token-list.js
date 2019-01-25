df932728f660ce59b6555f3c5659a0cc65bda005
Jey <jey.nandakumar@gmail.com>
2019-01-25 16:43:39 +0000

    fix: deprecate axe.commons.utils namespace (#1330)

---
+++ test/core/utils/token-list.js
@@ -0,0 +1,27 @@
+describe('axe.utils.tokenList', function() {
+	'use strict';
+
+	it('should split by space', function() {
+		assert.deepEqual(axe.utils.tokenList('bananas monkeys 42'), [
+			'bananas',
+			'monkeys',
+			'42'
+		]);
+	});
+
+	it('should trim first', function() {
+		assert.deepEqual(axe.utils.tokenList(' \r   bananas monkeys 42	\n  '), [
+			'bananas',
+			'monkeys',
+			'42'
+		]);
+	});
+
+	it('should collapse whitespace', function() {
+		assert.deepEqual(axe.utils.tokenList(' \r   bananas \r \n	monkeys		42	\n  '), [
+			'bananas',
+			'monkeys',
+			'42'
+		]);
+	});
+});
