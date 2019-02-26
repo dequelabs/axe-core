560b9f87606687fbd81397b0678089574fcad731
Wahaj Abid <waabid@microsoft.com>
2019-02-25 18:26:24 -0800

    Re-factored and added tests for matches function

--- test/rule-matches/landmark-unique-matches.js
+++ test/rule-matches/landmark-unique-matches.js
@@ -1,10 +1,14 @@
-describe('landmark-unique', function() {
+describe('landmark-unique-matches', function() {
 	'use strict';
-	var rule;
+	let rule;
+	let fixture;
+	let axeFixtureSetup;
 
 	beforeEach(function() {
+		fixture = document.getElementById('fixture');
+		axeFixtureSetup = axe.testUtils.fixtureSetup;
 		rule = axe._audit.rules.find(function(rule) {
-			return rule.id === 'landmark-uqique';
+			return rule.id === 'landmark-unique';
 		});
 	});
 
@@ -16,6 +20,7 @@ describe('landmark-unique', function() {
 	it('should pass because is a landmark', () => {
 		const node = document.createElement('div');
 		node.setAttribute('role', 'banner');
+		fixture.appendChild(node);
 		assert.isTrue(rule.matches(node, null));
 	});
 
@@ -23,6 +28,44 @@ describe('landmark-unique', function() {
 		const node = document.createElement('div');
 		node.setAttribute('role', 'banner');
 		node.style.display = 'none';
+		fixture.appendChild(node);
+		assert.isFalse(rule.matches(node, null));
+	});
+
+	it('should match because it is a section with a label', () => {
+		axeFixtureSetup(
+			'<section aria-label="sample label">some section</section>'
+		);
+		const node = fixture.querySelector('section');
 		assert.isTrue(rule.matches(node, null));
+	});
+
+	it('should not match because it is a section without a label', () => {
+		axeFixtureSetup('<section>some section</section>');
+		const node = fixture.querySelector('section');
+		assert.isFalse(rule.matches(node, null));
+	});
+
+	describe('header/footers should only match when not inside the excluded descendents', () => {
+		const excludedDescendants = ['article', 'aside', 'main', 'nav', 'section'];
+		const elements = ['header', 'footer'];
+
+		elements.forEach(elementType => {
+			excludedDescendants.forEach(exclusionType => {
+				it(`should not match because ${elementType} is contained inside in ${exclusionType}`, () => {
+					const node = document.createElement(elementType);
+					const sectionNode = document.createElement(exclusionType);
+					sectionNode.append(node);
+					fixture.appendChild(sectionNode);
+					assert.isFalse(rule.matches(node, null));
+				});
+			});
+
+			it(`should match because ${elementType} is not contained inside the excluded descendents`, () => {
+				const node = document.createElement(elementType);
+				fixture.appendChild(node);
+				assert.isTrue(rule.matches(node, null));
+			});
+		});
 	});
 });
