da1a1245cfa05507f8cd4407d06e38721133f819
Wahaj Abid <waabid@microsoft.com>
2019-03-01 14:58:33 -0800

    Use related nodes api instead of failing duplicate landmarks

--- test/checks/landmarks/landmark-is-unique-after.js
+++ test/checks/landmarks/landmark-is-unique-after.js
@@ -1,6 +1,7 @@
 describe('landmark-is-unique-after', function() {
 	'use strict';
 
+	var checkContext = axe.testUtils.MockCheckContext();
 	function createResult(result, data) {
 		return {
 			result: result,
@@ -8,43 +9,76 @@ describe('landmark-is-unique-after', function() {
 		};
 	}
 
+	function createResultWithSameRelatedNodes(result, data) {
+		return {
+			...createResult(result, data),
+			relatedNodes: [createResult(result, data)]
+		};
+	}
+
+	function createResultWithProvidedRelatedNodes(result, data, relatedNodes) {
+		return {
+			...createResult(result, data),
+			relatedNodes
+		};
+	}
+
+	afterEach(function() {
+		axe._tree = undefined;
+		checkContext.reset();
+	});
+
 	it('should update duplicate landmarks with failed result', function() {
 		var result = checks['landmark-is-unique'].after([
-			createResult(true, {
+			createResultWithSameRelatedNodes(true, {
 				role: 'some role',
 				label: 'some label'
 			}),
-			createResult(true, {
+			createResultWithSameRelatedNodes(true, {
 				role: 'some role',
 				label: 'some label'
 			}),
-			createResult(true, {
+			createResultWithSameRelatedNodes(true, {
 				role: 'different role',
 				label: 'some label'
 			}),
-			createResult(true, {
+			createResultWithSameRelatedNodes(true, {
 				role: 'some role',
 				label: 'different label'
 			})
 		]);
 
-		assert.deepEqual(result, [
-			createResult(false, {
-				role: 'some role',
-				label: 'some label'
-			}),
-			createResult(false, {
-				role: 'some role',
-				label: 'some label'
-			}),
-			createResult(true, {
-				role: 'different role',
-				label: 'some label'
-			}),
-			createResult(true, {
-				role: 'some role',
-				label: 'different label'
-			})
-		]);
+		const expectedResult = [
+			createResultWithProvidedRelatedNodes(
+				false,
+				{
+					role: 'some role',
+					label: 'some label'
+				},
+				[
+					createResult(true, {
+						role: 'some role',
+						label: 'some label'
+					})
+				]
+			),
+			createResultWithProvidedRelatedNodes(
+				true,
+				{
+					role: 'different role',
+					label: 'some label'
+				},
+				[]
+			),
+			createResultWithProvidedRelatedNodes(
+				true,
+				{
+					role: 'some role',
+					label: 'different label'
+				},
+				[]
+			)
+		];
+		assert.deepEqual(result, expectedResult);
 	});
 });
