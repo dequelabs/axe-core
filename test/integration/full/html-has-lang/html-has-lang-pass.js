ca8177b929bfacf1d661f26e8a7247fc4cb53050
jkodu <jey.nandakumar@gmail.com>
2019-03-13 10:51:18 +0000

    fix: exclude  iframe for html-has-lang rule

---
+++ test/integration/full/html-has-lang/html-has-lang-pass.js
@@ -0,0 +1,36 @@
+/**
+ * Note:
+ * This rule does not include `iframe` uses matches "window-is-top.js"
+ */
+describe('html-has-lang pass test', function() {
+	'use strict';
+	var results;
+	before(function(done) {
+		axe.testUtils.awaitNestedLoad(function() {
+			axe.run(
+				{ runOnly: { type: 'rule', values: ['html-has-lang'] } },
+				function(err, r) {
+					assert.isNull(err);
+					results = r;
+					done();
+				}
+			);
+		});
+	});
+
+	describe('violations', function() {
+		it('should find 0 violations', function() {
+			assert.lengthOf(results.violations, 0);
+		});
+	});
+
+	describe('passes', function() {
+		it('should find 1', function() {
+			assert.lengthOf(results.passes[0].nodes, 1);
+		});
+
+		it('should find #pass1', function() {
+			assert.deepEqual(results.passes[0].nodes[0].target, ['#pass1']);
+		});
+	});
+});
