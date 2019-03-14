3f9c01e89625371dd08deac5f11203cf90f21ac5
Steven Lambert <steven.lambert@deque.com>
2019-03-14 10:32:56 -0600

    always return NamedNodeMap, don't rely on attr order for test

--- test/core/utils/get-node-attributes.js
+++ test/core/utils/get-node-attributes.js
@@ -1,18 +1,16 @@
 describe('axe.utils.getNodeAttributes', function() {
 	'use strict';
 
-	it('returns the list of attributes', function() {
+	it('should return the list of attributes', function() {
 		var node = document.createElement('div');
-		node.innerHTML = '<div class="foo bar" id="123" aria-hidden="true"></div>';
+		node.innerHTML = '<div class="foo bar"></div>';
 		var actual = axe.utils.getNodeAttributes(node.firstChild);
 		assert.isTrue(actual instanceof NamedNodeMap);
-		assert.equal(actual.length, 3);
+		assert.equal(actual.length, 1);
 		assert.equal(actual[0].name, 'class');
-		assert.equal(actual[1].name, 'id');
-		assert.equal(actual[2].name, 'aria-hidden');
 	});
 
-	it('returns the list of attributes when the DOM is clobbered', function() {
+	it('should return the list of attributes when the DOM is clobbered', function() {
 		var node = document.createElement('form');
 		node.setAttribute('id', '123');
 		node.innerHTML = '<select name="attributes"></select>';
