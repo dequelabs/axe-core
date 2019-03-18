461ce8308e410035cfec08b629f76ff0adb76024
Jason <jason@scurker.com>
2019-03-18 11:32:33 -0500

    fix: use execa instead of child_process.spawn

--- doc/examples/test-examples.js
+++ doc/examples/test-examples.js
@@ -1,36 +1,22 @@
 const { readdirSync, statSync } = require('fs');
 const { join } = require('path');
-const { spawn } = require('child_process');
+const execa = require('execa');
 const exampleDirs = readdirSync(__dirname)
 	.map(dir => join(__dirname, dir))
 	.filter(dir => statSync(dir).isDirectory());
 
 async function runner(dir) {
-	let child = spawn('npm install && npm test', {
-		cwd: dir,
-		shell: true,
-		stdio: 'inherit'
-	});
-
-	return new Promise((resolve, reject) => {
-		child.on('close', code => {
-			if (code === 0) {
-				resolve();
-			} else {
-				reject();
-			}
-		});
-	});
+	const config = { cwd: dir, stdio: 'inherit' };
+	await execa.shell('npm install', config);
+	return execa.shell('npm test', config);
 }
 
-(async () => {
-	await Promise.all(exampleDirs.map(runner))
-		.then(() => {
-			// Return successful exit
-			process.exit();
-		})
-		.catch(err => {
-			console.log(err);
-			process.exit(1);
-		});
-})();
+Promise.all(exampleDirs.map(runner))
+	.then(() => {
+		// Return successful exit
+		process.exit();
+	})
+	.catch(err => {
+		console.error(err);
+		process.exit(1);
+	});
