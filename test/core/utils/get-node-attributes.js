56e832d4756aef5e69ed08753c655befca236c0a
Steven Lambert <steven.lambert@deque.com>
2019-03-28 09:04:04 -0600

    don't use NamedNodeMap without window object

--- test/core/utils/get-node-attributes.js
+++ test/core/utils/get-node-attributes.js
@@ -5,7 +5,7 @@ describe('axe.utils.getNodeAttributes', function() {
 		var node = document.createElement('div');
 		node.setAttribute('class', 'foo bar');
 		var actual = axe.utils.getNodeAttributes(node);
-		assert.isTrue(actual instanceof NamedNodeMap);
+		assert.isTrue(actual instanceof window.NamedNodeMap);
 		assert.equal(actual.length, 1);
 		assert.equal(actual[0].name, 'class');
 	});
@@ -16,10 +16,10 @@ describe('axe.utils.getNodeAttributes', function() {
 		node.innerHTML = '<select name="attributes"></select>';
 
 		// eslint-disable-next-line no-restricted-syntax
-		assert.isFalse(node.attributes instanceof NamedNodeMap);
+		assert.isFalse(node.attributes instanceof window.NamedNodeMap);
 
 		var actual = axe.utils.getNodeAttributes(node);
-		assert.isTrue(actual instanceof NamedNodeMap);
+		assert.isTrue(actual instanceof window.NamedNodeMap);
 		assert.equal(actual.length, 1);
 		assert.equal(actual[0].name, 'id');
 	});
