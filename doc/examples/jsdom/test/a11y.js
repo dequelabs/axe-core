b573b1c1e554e3e93e53716e95e92ed3f4acd4f1
Steven Lambert <steven.lambert@deque.com>
2019-05-01 08:21:15 -0600

    docs: add jsdom example and tests (#1530)
    * docs: add jsdom example and tests
    
    * remove author

---
+++ doc/examples/jsdom/test/a11y.js
@@ -0,0 +1,61 @@
+/* global describe, it */
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const assert = require('assert');
+
+describe('axe', () => {
+	const { window } = new JSDOM(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>JSDOM Example</title>
+    </head>
+    <body>
+      <div id="working">
+        <label for="has-label">Label for this text field.</label>
+        <input type="text" id="has-label">
+      </div>
+      <div id="broken">
+        <p>Not a label</p><input type="text" id="no-label">
+      </div>
+    </body>
+  </html>`);
+
+	global.document = window.document;
+	global.window = window;
+
+	// needed by axios lib/helpers/isURLSameOrigin.js
+	global.navigator = window.navigator;
+
+	// needed by axe /lib/core/public/run.js
+	global.Node = window.Node;
+	global.NodeList = window.NodeList;
+
+	// needed by axe /lib/core/base/context.js
+	global.Element = window.Element;
+	global.Document = window.Document;
+
+	const axe = require('axe-core');
+	const config = {
+		rules: {
+			'color-contrast': { enabled: false }
+		}
+	};
+
+	it('should report that good HTML is good', function(done) {
+		var n = window.document.getElementById('working');
+		axe.run(n, config, function(err, result) {
+			assert.equal(err, null, 'Error is not null');
+			assert.equal(result.violations.length, 0, 'Violations is not empty');
+			done();
+		});
+	});
+
+	it('should report that bad HTML is bad', function(done) {
+		var n = window.document.getElementById('broken');
+		axe.run(n, config, function(err, result) {
+			assert.equal(err, null, 'Error is not null');
+			assert.equal(result.violations.length, 1, 'Violations.length is not 1');
+			done();
+		});
+	});
+});
