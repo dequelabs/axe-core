c8ae0fba4d6a5ce44046e2ad414b132bf981ffce
jkodu <jey.nandakumar@gmail.com>
2019-05-03 16:03:17 +0100

    try running puppeteer

--- build/test/headless/get-test-files.js
+++ build/test/headless/get-test-files.js
@@ -1,15 +1,15 @@
-const globby = require('globby');
+// const globby = require('globby');
 
 const getTestFiles = async () => {
 	const host = `localhost`;
 	const port = `9876`;
 
 	const unitTestFiles = [
-		`http://${host}:${port}/test/core/`,
-		`http://${host}:${port}/test/checks/`,
-		`http://${host}:${port}/test/rule-matches/`,
-		`http://${host}:${port}/test/commons/`,
-		`http://${host}:${port}/test/integration/rules`
+		`http://${host}:${port}/test/core/`
+		// `http://${host}:${port}/test/checks/`,
+		// `http://${host}:${port}/test/rule-matches/`,
+		// `http://${host}:${port}/test/commons/`,
+		// `http://${host}:${port}/test/integration/rules`
 	];
 
 	const integrationFileGlobs = [
@@ -23,13 +23,16 @@ const getTestFiles = async () => {
 		);
 	}
 
-	const integrationFiles = await globby(integrationFileGlobs);
+	// const integrationFiles = await globby(integrationFileGlobs);
 
-	const integrationTestFiles = integrationFiles.map(file => {
-		return `http://${host}:${port}/${file}`;
-	});
+	// const integrationTestFiles = integrationFiles.map(file => {
+	//   return `http://${host}:${port}/${file}`;
+	// });
 
-	return [...unitTestFiles, ...integrationTestFiles];
+	return [
+		...unitTestFiles
+		// ...integrationTestFiles
+	];
 };
 
 module.exports = getTestFiles;
