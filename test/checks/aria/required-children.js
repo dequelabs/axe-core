5580699654334efaf4ed89e30b24ccc9ed41cdcb
Jason <jason@scurker.com>
2019-05-13 11:34:47 -0500

    add check test

--- test/checks/aria/required-children.js
+++ test/checks/aria/required-children.js
@@ -103,6 +103,25 @@ describe('aria-required-children', function() {
 		);
 	});
 
+	it('should return undefined when element is empty and is in reviewEmpty options', function() {
+		var params = checkSetup('<div role="list" id="target"></div>', {
+			reviewEmpty: ['list']
+		});
+		assert.isUndefined(
+			checks['aria-required-children'].evaluate.apply(checkContext, params)
+		);
+	});
+
+	it('should return false when children do not have correct role and is in reviewEmpty options', function() {
+		var params = checkSetup(
+			'<div role="list" id="target"><div role="menuitem"></div></div>',
+			{ reviewEmpty: ['list'] }
+		);
+		assert.isFalse(
+			checks['aria-required-children'].evaluate.apply(checkContext, params)
+		);
+	});
+
 	(shadowSupported ? it : xit)(
 		'should pass all existing required children in shadow tree when all required',
 		function() {
