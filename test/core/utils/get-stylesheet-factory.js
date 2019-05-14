a4e177b3f1f29d4a5e8e58e667a5701e66fc6372
Jey <jey.nandakumar@gmail.com>
2019-05-14 17:56:31 +0100

    feat(utils): Update CSSOM for nested @import computation (#1339)

---
+++ test/core/utils/get-stylesheet-factory.js
@@ -0,0 +1,42 @@
+describe('axe.utils.getStyleSheetFactory', function() {
+	'use strict';
+
+	var dynamicDoc = document.implementation.createHTMLDocument(
+		'Dynamic document for testing axe.utils.getStyleSheetFactory'
+	);
+
+	it('throws if there is no argument of dynamicDocument', function() {
+		assert.throws(function() {
+			axe.utils.getStyleSheetFactory();
+		});
+	});
+
+	it('returns a function when passed argument of dynamicDocument', function() {
+		const actual = axe.utils.getStyleSheetFactory(dynamicDoc);
+		assert.isFunction(actual);
+	});
+
+	it('returns a CSSOM stylesheet, when invoked with data (text)', function() {
+		const stylesheetFactory = axe.utils.getStyleSheetFactory(dynamicDoc);
+		const actual = stylesheetFactory({
+			data: `.someStyle{background-color:red;}`,
+			root: document,
+			priority: [1, 0]
+		});
+
+		assert.isDefined(actual);
+		assert.hasAllKeys(actual, [
+			'sheet',
+			'isCrossOrigin',
+			'shadowId',
+			'root',
+			'priority'
+		]);
+		assert.deepEqual(actual.priority, [1, 0]);
+		axe.testUtils.assertStylesheet(
+			actual.sheet,
+			'.someStyle',
+			'.someStyle{background-color:red;}'
+		);
+	});
+});
