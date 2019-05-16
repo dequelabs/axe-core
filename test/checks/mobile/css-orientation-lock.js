2102eca9e52dfdb548dd435e8f2264b38e218178
Steven Lambert <steven.lambert@deque.com>
2019-05-16 07:44:27 -0600

    test: fix tests failing in IE11 (#1570)
    * test: fix broken tests
    
    * add -ms-transform
    
    * IE11 does not add space between media query properties
    
    * dont string compare css strings
    
    * remove broken IE11 test comment

--- test/checks/mobile/css-orientation-lock.js
+++ test/checks/mobile/css-orientation-lock.js
@@ -6,8 +6,6 @@ describe('css-orientation-lock tests', function() {
 	var dynamicDoc = document.implementation.createHTMLDocument(
 		'Dynamic document for CSS Orientation Lock tests'
 	);
-	var isIE11 = axe.testUtils.isIE11;
-	var isPhantom = window.PHANTOMJS ? true : false;
 
 	afterEach(function() {
 		checks['css-orientation-lock'] = origCheck;
@@ -23,11 +21,11 @@ describe('css-orientation-lock tests', function() {
 		MEDIA_STYLE_ORIENTATION_WITHOUT_TRANSFORM:
 			'@media screen and (min-width: 1px) and (max-width: 2000px) and (orientation: portrait) { #mocha { color: red; } }',
 		MEDIA_STYLE_ORIENTATION_WITH_TRANSFORM_NOT_ROTATE:
-			'@media screen and (min-width: 1px) and (max-width: 3000px) and (orientation: landscape) { #mocha { transform: translateX(10px); } }',
+			'@media screen and (min-width: 1px) and (max-width: 3000px) and (orientation: landscape) { #mocha { transform: translateX(10px); -webkit-transform: translateX(10px); } }',
 		MEDIA_STYLE_ORIENTATION_WITH_TRANSFORM_ROTATE_180:
-			'@media screen and (min-width: 1px) and (max-width: 3000px) and (orientation: landscape) { body { transform: rotate(180deg); } }',
+			'@media screen and (min-width: 1px) and (max-width: 3000px) and (orientation: landscape) { body { transform: rotate(180deg); -webkit-transform: rotate(180deg); } }',
 		MEDIA_STYLE_ORIENTATION_WITH_TRANSFORM_ROTATE_90:
-			'@media screen and (min-width: 1px) and (max-width: 3000px) and (orientation: landscape) { #mocha { transform: rotate(270deg); } }'
+			'@media screen and (min-width: 1px) and (max-width: 3000px) and (orientation: landscape) { #mocha { transform: rotate(270deg); -webkit-transform: rotate(270deg); } }'
 	};
 
 	function getSheet(data) {
@@ -220,30 +218,26 @@ describe('css-orientation-lock tests', function() {
 		assert.isTrue(actual);
 	});
 
-	// This currently breaks in IE11
-	(isIE11 || isPhantom ? it.skip : it)(
-		'returns false if TRANSFORM style applied is ROTATE, and is divisible by 90 and not divisible by 180',
-		function() {
-			var actual = checks['css-orientation-lock'].evaluate.call(
-				checkContext,
-				document,
-				{},
-				undefined,
-				{
-					cssom: [
-						{
-							shadowId: undefined,
-							root: document,
-							sheet: getSheet(
-								SHEET_DATA.MEDIA_STYLE_ORIENTATION_WITH_TRANSFORM_ROTATE_90
-							)
-						}
-					]
-				}
-			);
-			assert.isFalse(actual);
-		}
-	);
+	it('returns false if TRANSFORM style applied is ROTATE, and is divisible by 90 and not divisible by 180', function() {
+		var actual = checks['css-orientation-lock'].evaluate.call(
+			checkContext,
+			document,
+			{},
+			undefined,
+			{
+				cssom: [
+					{
+						shadowId: undefined,
+						root: document,
+						sheet: getSheet(
+							SHEET_DATA.MEDIA_STYLE_ORIENTATION_WITH_TRANSFORM_ROTATE_90
+						)
+					}
+				]
+			}
+		);
+		assert.isFalse(actual);
+	});
 
 	// Note:
 	// external stylesheets is tested in integration tests
