6cd821d064b5c62ef69d8efd4c470067def99392
Steven Lambert <steven.lambert@deque.com>
2019-05-28 14:57:57 -0600

    fix

--- test/core/public/run-virtual-rule.js
+++ test/core/public/run-virtual-rule.js
@@ -75,32 +75,14 @@ describe('axe.runVirtualRule', function() {
 	});
 
 	it('should pass a virtual context to rule.runSync', function() {
+		var node = {};
 		axe._audit.rules = [
 			{
 				id: 'aria-roles',
 				runSync: function(context) {
 					assert.equal(typeof context, 'object');
 					assert.isTrue(Array.isArray(context.include));
-					assert.equal(typeof context.include[0], 'object');
-
-					return {
-						id: 'aria-roles',
-						nodes: []
-					};
-				}
-			}
-		];
-
-		axe.runVirtualRule('aria-roles');
-	});
-
-	it('should pass the virtualNode as the actualNode of the context', function() {
-		var node = {};
-		axe._audit.rules = [
-			{
-				id: 'aria-roles',
-				runSync: function(context) {
-					assert.equal(context.include[0].actualNode, node);
+					assert.equal(context.include[0], node);
 
 					return {
 						id: 'aria-roles',
