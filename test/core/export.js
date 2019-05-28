ce3866c0ad13e44565894d6b5ee4e64fa8292006
Ben Stoddard <bcstodds@users.noreply.github.com>
2019-05-28 10:06:48 -0700

    Update export.js

--- test/core/export.js
+++ test/core/export.js
@@ -2,7 +2,7 @@ describe('export', function() {
 	'use strict';
 
 	it('should publish a global `axe` variable', function() {
-		assert.isDefined(window.axe);
+		assert.isDefined(window.__axe);
 	});
 	it('should define version', function() {
 		assert.equal(axe.version, 'x.y.z');
