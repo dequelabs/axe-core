0088e944bb410bafe40cd003c697fd419b717cf6
Wahaj <32555133+waabid@users.noreply.github.com>
2019-06-05 07:22:58 -0700

    feat(rule): Adding landmark-is-unique rule (#1394)
    * Initial attempt at landmark-unique rule
    
    * Adding matches tests WIP
    
    * Re-factored and added tests for matches function
    
    * Added support for virtual nodes
    
    * Using findUp api instead of looking up the tree ourselves
    
    * Migrated unique-ness logic to after and removed code
    
    * Added tests for landmark-is-unique-after
    
    * Added best practice tag to unique landmark rule
    
    * Added test for form elements
    
    * Added comment to explain filtering in landmark matches function
    
    * Changed usage of actual node with virtual node where possible in landmark-unique-matches
    
    * Use related nodes api instead of failing duplicate landmarks
    
    * Updated landmark is unique check test to validate related nodes
    
    * Added a failing iframe test for landmark-unique
    
    * Added a test case for matching landmark-unique against elements in shadow dom
    
    * Implement own findIndex for landmark-is-unique to unblock tests
    
    * Used find instead of findIndex (from polyfills)
    
    * Updated the landmark-unique-matches to use the virtual node by default
    
    * Updated nodeName usage to upper case
    
    * Exit early when no role is found in landmark unique matches
    
    * Added test for heirarchical exclusions in shadow dom for landmark-unique-matches
    
    * Took out common variables in landmark unique matches tests
    
    * Added more iframe integration tests for landmark-unique rule
    
    * PR changes: label to accessibleText, ES3 support in tests, and remove options since not used
    
    * Added more integration test cases
    
    * Updated text for new rule
    
    * One last const to var
    
    * Updated to remove all const/let uses
    
    * Use accessibletextvirtual
    
    * Fixed test by passing virtual node in to landmark is unique
    
    * sync upstream/develop
    
    * remove es6 syntax from tests
    
    * cleanup
    
    * fix typo

---
+++ test/rule-matches/landmark-unique-matches.js
@@ -0,0 +1,201 @@
+describe('landmark-unique-matches', function() {
+	'use strict';
+	var rule;
+	var fixture;
+	var axeFixtureSetup;
+	var shadowSupport = axe.testUtils.shadowSupport.v1;
+	var excludedDescendantsForHeadersFooters = [
+		'article',
+		'aside',
+		'main',
+		'nav',
+		'section'
+	];
+	var headerFooterElements = ['header', 'footer'];
+
+	beforeEach(function() {
+		fixture = document.getElementById('fixture');
+		axeFixtureSetup = axe.testUtils.fixtureSetup;
+		rule = axe._audit.rules.find(function(rule) {
+			return rule.id === 'landmark-unique';
+		});
+	});
+
+	it('should not match because not a landmark', function() {
+		axeFixtureSetup('<h1>some heading</h1>');
+		var node = fixture.querySelector('h1');
+		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+		assert.isFalse(rule.matches(node, virtualNode));
+	});
+
+	it('should pass because is a landmark', function() {
+		axeFixtureSetup('<div role="banner">some banner</div>');
+		var node = fixture.querySelector('div');
+		fixture.appendChild(node);
+		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+		assert.isTrue(rule.matches(node, virtualNode));
+	});
+
+	it('should not match because landmark is hidden', function() {
+		axeFixtureSetup('<div role="banner">some banner</div>');
+		var node = fixture.querySelector('div');
+		node.style.display = 'none';
+		fixture.appendChild(node);
+		var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+		assert.isFalse(rule.matches(node, virtualNode));
+	});
+
+	describe('form and section elements must have accessible names to be matched', function() {
+		var sectionFormElements = ['section', 'form'];
+
+		sectionFormElements.forEach(function(elementType) {
+			it(
+				'should match because it is a ' + elementType + ' with a label',
+				function() {
+					axeFixtureSetup(
+						'<' +
+							elementType +
+							' aria-label="sample label">some ' +
+							elementType +
+							'</' +
+							elementType +
+							'>'
+					);
+					var node = fixture.querySelector(elementType);
+					var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+					assert.isTrue(rule.matches(node, virtualNode));
+				}
+			);
+
+			it(
+				'should not match because it is a ' + elementType + ' without a label',
+				function() {
+					axeFixtureSetup(
+						'<' +
+							elementType +
+							'>some ' +
+							elementType +
+							'</' +
+							elementType +
+							'>'
+					);
+					var node = fixture.querySelector(elementType);
+					var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+					assert.isFalse(rule.matches(node, virtualNode));
+				}
+			);
+		});
+	});
+
+	describe('header/footers should only match when not inside the excluded descendants', function() {
+		headerFooterElements.forEach(function(elementType) {
+			excludedDescendantsForHeadersFooters.forEach(function(exclusionType) {
+				it(
+					'should not match because ' +
+						elementType +
+						' is contained inside an ' +
+						exclusionType,
+					function() {
+						axeFixtureSetup(
+							'<' +
+								exclusionType +
+								' aria-label="sample label"><' +
+								elementType +
+								'>an element</' +
+								elementType +
+								'></' +
+								exclusionType +
+								'>'
+						);
+						var node = fixture.querySelector(elementType);
+						var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+						assert.isFalse(rule.matches(node, virtualNode));
+					}
+				);
+			});
+
+			it(
+				'should match because ' +
+					elementType +
+					' is not contained inside the excluded descendants',
+				function() {
+					axeFixtureSetup(
+						'<' + elementType + '>an element</' + elementType + '>'
+					);
+					var node = fixture.querySelector(elementType);
+					var virtualNode = axe.utils.getNodeFromTree(axe._tree[0], node);
+					assert.isTrue(rule.matches(node, virtualNode));
+				}
+			);
+		});
+	});
+
+	if (shadowSupport) {
+		it('return true for landmarks contained within shadow dom', function() {
+			var container = document.createElement('div');
+			var shadow = container.attachShadow({ mode: 'open' });
+			shadow.innerHTML = '<footer></footer>';
+
+			axeFixtureSetup(container);
+			var vNode = axe.utils.querySelectorAll(axe._tree[0], 'footer')[0];
+			assert.isTrue(rule.matches(vNode.actualNode, vNode));
+		});
+
+		describe('header/footers should only match when not inside the excluded descendants within shadow dom', function() {
+			var container;
+			var shadow;
+
+			beforeEach(function() {
+				container = document.createElement('div');
+				shadow = container.attachShadow({ mode: 'open' });
+			});
+
+			headerFooterElements.forEach(function(elementType) {
+				excludedDescendantsForHeadersFooters.forEach(function(exclusionType) {
+					it(
+						'should not match because ' +
+							elementType +
+							' is contained inside an ' +
+							exclusionType +
+							'',
+						function() {
+							shadow.innerHTML =
+								'<' +
+								exclusionType +
+								' aria-label="sample label"><' +
+								elementType +
+								'>an element</' +
+								elementType +
+								'></' +
+								exclusionType +
+								'>';
+
+							axeFixtureSetup(container);
+							var virtualNode = axe.utils.querySelectorAll(
+								axe._tree[0],
+								elementType
+							)[0];
+							assert.isFalse(rule.matches(virtualNode.actualNode, virtualNode));
+						}
+					);
+				});
+
+				it(
+					'should match because ' +
+						elementType +
+						' is not contained inside the excluded descendants',
+					function() {
+						shadow.innerHTML =
+							'<' + elementType + '>an element</' + elementType + '>';
+						axeFixtureSetup(container);
+						var virtualNode = axe.utils.querySelectorAll(
+							axe._tree[0],
+							elementType
+						)[0];
+						assert.isTrue(rule.matches(virtualNode.actualNode, virtualNode));
+					}
+				);
+			});
+		});
+	}
+});
