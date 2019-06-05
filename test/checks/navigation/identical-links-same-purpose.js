0e74359490b75df15b89e993de90d059d9df5a7c
jkodu <jey.nandakumar@gmail.com>
2019-06-05 14:10:57 +0100

    initial commit, files generated

---
+++ test/checks/navigation/identical-links-same-purpose.js
@@ -0,0 +1,4 @@
+describe('identical-links-same-purpose tests', function() {
+	'use strict';
+	// TODO: Write tests
+});
---
+++ test/rule-matches/identical-links-same-purpose-matches.js
@@ -0,0 +1,4 @@
+describe('identical-links-same-purpose-matches', function() {
+	'use strict';
+	// TODO: Write tests
+});
