73d52736832637598c416b2e7f2576bc021daa12
Jey <jey.nandakumar@gmail.com>
2019-06-06 07:26:06 -0400

    feat(rule): New aria-input-field-label rule (#1610)
    * feat: initial implementation
    
    * feat: rule aria-foorm-field-label with new check for no-implicit-explicit-label
    
    * update rule
    
    * test: fix lint errors
    
    * add tests and updates based on review
    
    * initial implementation
    
    * remove has-visible-text check
    
    * update tests

---
+++ test/checks/aria/no-implicit-explicit-label.js
@@ -0,0 +1,45 @@
+describe('no-implicit-explicit-label', function() {
+	'use strict';
+
+	var fixture = document.getElementById('fixture');
+	var queryFixture = axe.testUtils.queryFixture;
+	var check = checks['no-implicit-explicit-label'];
+	var checkContext = axe.testUtils.MockCheckContext();
+
+	afterEach(function() {
+		fixture.innerHTML = '';
+		checkContext.reset();
+	});
+
+	it('returns false when there is no label text or accessible text', function() {
+		var vNode = queryFixture(
+			'<div id="target" role="searchbox" contenteditable="true"></div>'
+		);
+		var actual = check.evaluate.call(checkContext, vNode.actualNode, {}, vNode);
+		assert.isFalse(actual);
+	});
+
+	it('returns undefined when there is no accessible text', function() {
+		var vNode = queryFixture(
+			'<label for="target">Choose currency:</label><div id="target" role="searchbox" contenteditable="true"></div>'
+		);
+		var actual = check.evaluate.call(checkContext, vNode.actualNode, {}, vNode);
+		assert.isUndefined(actual);
+	});
+
+	it('returns undefined when accessible text does not contain label text', function() {
+		var vNode = queryFixture(
+			'<label for="target">Choose country:</label><div id="target" aria-label="country" role="combobox">England</div>'
+		);
+		var actual = check.evaluate.call(checkContext, vNode.actualNode, {}, vNode);
+		assert.isUndefined(actual);
+	});
+
+	it('returns false when accessible text contains label text', function() {
+		var vNode = queryFixture(
+			'<label for="target">Country</label><div id="target" aria-label="Choose country" role="combobox">England</div>'
+		);
+		var actual = check.evaluate.call(checkContext, vNode.actualNode, {}, vNode);
+		assert.isFalse(actual);
+	});
+});
