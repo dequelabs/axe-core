e83091356b22666d7ab45991c67223ca5c41732d
Steven Lambert <steven.lambert@deque.com>
2019-07-08 15:40:10 -0600

    revert test-examples.js

--- doc/examples/test-examples.js
+++ doc/examples/test-examples.js
@@ -11,22 +11,12 @@ async function runner(dir) {
 	return execa('npm test', config);
 }
 
-async function main() {
-	for (let i = 0; i < exampleDirs.length; i++) {
-		const dir = exampleDirs[i];
-		await runner(dir);
-	}
-}
-
-main();
-// process.exit();
-
-// Promise.all(exampleDirs.map(runner))
-// 	.then(() => {
-// 		// Return successful exit
-// 		process.exit();
-// 	})
-// 	.catch(err => {
-// 		console.error(err);
-// 		process.exit(1);
-// 	});
+Promise.all(exampleDirs.map(runner))
+	.then(() => {
+		// Return successful exit
+		process.exit();
+	})
+	.catch(err => {
+		console.error(err);
+		process.exit(1);
+	});
