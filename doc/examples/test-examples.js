50e38f20bbf9816f34286928176ec057ed9b6719
Steven Lambert <steven.lambert@deque.com>
2019-07-08 15:49:31 -0600

    sync test-examples code to understand output

--- doc/examples/test-examples.js
+++ doc/examples/test-examples.js
@@ -11,12 +11,11 @@ async function runner(dir) {
 	return execa('npm test', config);
 }
 
-Promise.all(exampleDirs.map(runner))
-	.then(() => {
-		// Return successful exit
-		process.exit();
-	})
-	.catch(err => {
-		console.error(err);
-		process.exit(1);
-	});
+async function main() {
+	for (let i = 0; i < exampleDirs.length; i++) {
+		const dir = exampleDirs[i];
+		await runner(dir);
+	}
+}
+
+main();
