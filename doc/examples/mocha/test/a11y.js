de06cd2eb6768bae7aab20cc74dec19dc79c6935
Steven Lambert <steven.lambert@deque.com>
2019-07-09 08:55:20 -0600

    docs: fix mocha example (#1687)
    * docs: fix broken mocha example
    
    * update readme
    
    * update package.json
    
    * revert test-examples.js
    
    * sync test-examples code to understand output
    
    * rerevert test-examples.js

--- doc/examples/mocha/test/a11y.js
+++ doc/examples/mocha/test/a11y.js
@@ -1,8 +1,20 @@
-/* global describe, it, expect, axe, document */
+/* global describe, it, expect, axe, document, expect */
 
 describe('axe', function() {
-	/* eslint no-unused-expressions: 0 */
 	'use strict';
+
+	document
+		.getElementsByTagName('body')[0]
+		.insertAdjacentHTML(
+			'beforeend',
+			'<div id="working">' +
+				'<label for="has-label">Label for this text field.</label>' +
+				'<input type="text" id="has-label">' +
+				'</div>' +
+				'<div id="broken">' +
+				'<p>Not a label</p><input type="text" id="no-label">' +
+				'</div>'
+		);
 
 	it('should report that good HTML is good', function(done) {
 		var n = document.getElementById('working');
