9ee3bf21179bd198b3d4d8075f71de4b6a4b1700
Steven Lambert <steven.lambert@deque.com>
2019-07-11 10:51:28 -0600

    use split

--- test/test-rule-help-version.js
+++ test/test-rule-help-version.js
@@ -3,14 +3,17 @@ var path = require('path');
 var assert = require('assert');
 var packageJSON = require(path.join(__dirname, '../package.json'));
 
-var version = packageJSON.version.substr(
-	0,
-	packageJSON.version.lastIndexOf('.')
-);
+var versions = packageJSON.version.split('.');
+var version = versions[0] + '.' + versions[1];
 
-it('latest axe version rule help docs should be active', function(done) {
-	https.get('https://dequeuniversity.com/rules/axe/' + version, function(res) {
-		assert(res.statusCode >= 200 && res.statusCode <= 299);
-		done();
-	});
-});
+it(
+	'latest axe version (' + version + ') rule help docs should be active',
+	function(done) {
+		https.get('https://dequeuniversity.com/rules/axe/' + version, function(
+			res
+		) {
+			assert(res.statusCode >= 200 && res.statusCode <= 299);
+			done();
+		});
+	}
+);
