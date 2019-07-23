46c711b74cac261e4086dcd91674f53555069ef8
Steven Lambert <steven.lambert@deque.com>
2019-07-23 11:44:32 -0600

    add subclass and superclass roles

---
+++ test/commons/aria/subclass-implicit-nodes.js
@@ -0,0 +1,63 @@
+describe('aria.subclassImplicitNodes', function() {
+	'use strict';
+
+	var orig = axe.commons.aria.lookupTable.role;
+	afterEach(function() {
+		axe.commons.aria.lookupTable.role = orig;
+	});
+
+	it('should return an array of implicit CSS selectors', function() {
+		axe.commons.aria.lookupTable.role = {
+			planes: {
+				subclassRoles: ['trains']
+			},
+			trains: {
+				implicit: ['[class=trains]']
+			}
+		};
+
+		var subclassRoles = axe.commons.aria.subclassImplicitNodes('planes');
+		assert.deepEqual(subclassRoles, ['[class=trains]']);
+	});
+
+	it('should return CSS selectors for all subclass roles', function() {
+		axe.commons.aria.lookupTable.role = {
+			planes: {
+				subclassRoles: ['trains', 'automobiles']
+			},
+			trains: {
+				implicit: ['[class=trains]']
+			},
+			automobiles: {
+				implicit: ['[class=automobiles]']
+			}
+		};
+
+		var subclassRoles = axe.commons.aria.subclassImplicitNodes('planes');
+		assert.deepEqual(subclassRoles, ['[class=trains]', '[class=automobiles]']);
+	});
+
+	it("should not add roles that don't have implicit nodes", function() {
+		axe.commons.aria.lookupTable.role = {
+			planes: {
+				subclassRoles: ['trains', 'automobiles']
+			},
+			trains: {},
+			automobiles: {
+				implicit: ['[class=automobiles]']
+			}
+		};
+
+		var subclassRoles = axe.commons.aria.subclassImplicitNodes('planes');
+		assert.deepEqual(subclassRoles, ['[class=automobiles]']);
+	});
+
+	it('should return null if the role does not have a subclass role', function() {
+		axe.commons.aria.lookupTable.role = {
+			planes: {}
+		};
+
+		var subclassRoles = axe.commons.aria.subclassImplicitNodes('planes');
+		assert.isNull(subclassRoles);
+	});
+});
