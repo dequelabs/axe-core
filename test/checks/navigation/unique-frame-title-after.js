41350cb28a564fe475bd14d726684b23ba9be2fd
jkodu <jey.nandakumar@gmail.com>
2019-08-27 08:06:13 +0100

    initial impl

--- test/checks/navigation/unique-frame-title-after.js
+++ test/checks/navigation/unique-frame-title-after.js
@@ -2,52 +2,112 @@
 describe('unique-frame-title-after', function() {
 	'use strict';
 
-	var checkContext = axe.testUtils.MockCheckContext();
+	var fixture = document.getElementById('fixture');
+	var queryFixture = axe.testUtils.queryFixture;
+	var check = checks['unique-frame-title'];
 
 	afterEach(function() {
-		checkContext.reset();
+		fixture.innerHTML = '';
 	});
 
-	it('should remove any check whose data only appears once', function() {
-		var result = checks['unique-frame-title'].after([
-			{
-				data: 'bananas'
+	it('sets results of check result to `undefined` if identical `iframes` but different purpose (resource titles will be compared)', function() {
+		var nodeOne = queryFixture(
+			'<iframe id="target" title="Incomplete 1" src="../integration/rules/frame-title-unique/frames/page-one.html"> </iframe>'
+		);
+		var nodeOneData = {
+			data: {
+				name: 'incomplete 1',
+				parsedResource: {
+					hostname: 'localhost',
+					pathname: 'page-one.html',
+					port: '9876'
+				},
+				resourceFrameTitle: 'page one'
 			},
-			{
-				data: 'monkeys'
+			relatedNodes: [nodeOne],
+			result: true
+		};
+		var nodeTwo = queryFixture(
+			'<iframe id="target" title="incomplete 1" src="../integration/rules/frame-title-unique/frames/page-two.html"> </iframe>'
+		);
+		var nodeTwoData = {
+			data: {
+				name: 'incomplete 1',
+				parsedResource: {
+					hostname: 'localhost',
+					pathname: 'page-two.html',
+					port: '9876'
+				},
+				resourceFrameTitle: 'page two'
 			},
-			{
-				data: 'bananas'
-			},
-			{
-				data: 'apples'
-			},
-			{
-				data: 'monkeys'
-			}
-		]);
+			relatedNodes: [nodeTwo],
+			result: true
+		};
+		var checkResults = [nodeOneData, nodeTwoData];
 
-		assert.deepEqual(result, [
-			{
-				data: 'bananas',
-				result: true
+		var results = check.after(checkResults);
+
+		assert.lengthOf(results, 1);
+
+		var result = results[0];
+		assert.equal(result.data.name, nodeOneData.data.name);
+		assert.equal(
+			result.data.parsedResource.hostname,
+			nodeOneData.data.parsedResource.hostname
+		);
+		assert.equal(result.relatedNodes.length, 1);
+		assert.isUndefined(result.result);
+	});
+
+	it('sets results of check result to `true` if identical `iframes` with same purpose (resource tiltes match, although pathnames are different)', function() {
+		var nodeOne = queryFixture(
+			'<iframe id="target" title="Pass Me" src="../integration/rules/frame-title-unique/frames/page-one.html"> </iframe>'
+		);
+		var nodeOneData = {
+			data: {
+				name: 'pass me',
+				parsedResource: {
+					hostname: 'localhost',
+					pathname: 'page-one.html',
+					port: '9876'
+				},
+				resourceFrameTitle: 'page one'
 			},
-			{
-				data: 'monkeys',
-				result: true
+			relatedNodes: [nodeOne],
+			result: true
+		};
+		var nodeTwo = queryFixture(
+			'<iframe id="target" title="Pass Me" src="../integration/rules/frame-title-unique/frames/page-one-copy.html"> </iframe>'
+		);
+		var nodeTwoData = {
+			data: {
+				name: 'pass me',
+				parsedResource: {
+					hostname: 'localhost',
+					pathname: 'page-one-copy.html',
+					port: '9876'
+				},
+				resourceFrameTitle: 'page one'
 			},
-			{
-				data: 'bananas',
-				result: true
-			},
-			{
-				data: 'apples',
-				result: false
-			},
-			{
-				data: 'monkeys',
-				result: true
-			}
-		]);
+			relatedNodes: [nodeTwo],
+			result: true
+		};
+		var checkResults = [nodeOneData, nodeTwoData];
+		var results = check.after(checkResults);
+
+		assert.lengthOf(results, 2);
+
+		var result1 = results[0];
+		assert.equal(result1.data.name, nodeOneData.data.name);
+		assert.equal(
+			result1.data.parsedResource.pathname,
+			nodeOneData.data.parsedResource.pathname
+		);
+		assert.equal(result1.relatedNodes.length, 0);
+		assert.isTrue(result1.result);
+
+		var result2 = results[1];
+		assert.equal(result2.relatedNodes.length, 0);
+		assert.isTrue(result2.result);
 	});
 });
--- test/checks/navigation/unique-frame-title.js
+++ test/checks/navigation/unique-frame-title.js
@@ -1,32 +1,77 @@
 describe('unique-frame-title', function() {
 	'use strict';
 
+	var fixture = document.getElementById('fixture');
+	var shadowSupported = axe.testUtils.shadowSupport.v1;
+	var shadowCheckSetup = axe.testUtils.shadowCheckSetup;
+	var queryFixture = axe.testUtils.queryFixture;
+	var check = checks['unique-frame-title'];
 	var checkContext = axe.testUtils.MockCheckContext();
+	var options = {};
+
+	afterEach(function() {
+		fixture.innerHTML = '';
+		checkContext.reset();
+		axe._tree = undefined;
+	});
 
 	afterEach(function() {
 		checkContext.reset();
 	});
 
-	it('should log title to data and return true', function() {
-		assert.isTrue(
-			checks['unique-frame-title'].evaluate.call(checkContext, {
-				title: 'bananas'
-			})
+	it('returns undefined for `iframe` with no accessible name (no name after unicode and space characters are removed)', function() {
+		var vNode = queryFixture(
+			'<iframe id="target" title=" ☀️ " src="../integration/rules/frame-title-unique/frames/page-one.html"> </iframe>'
 		);
-		assert.equal(checkContext._data, 'bananas');
+		var actual = check.evaluate.call(
+			checkContext,
+			vNode.actualNode,
+			options,
+			vNode
+		);
+		assert.isUndefined(actual);
+		assert.isNull(checkContext._data);
 	});
 
-	it('should convert text to lower case', function() {
-		checks['unique-frame-title'].evaluate.call(checkContext, {
-			title: '\t  app\t \n \rle  '
-		});
-		assert.equal(checkContext._data, 'app le');
+	it('returns true and sets `after` data when `iframe` has accessible name', function() {
+		var vNode = queryFixture(
+			'<iframe id="target" title="I am unique" src="../integration/rules/frame-title-unique/frames/page-one.html"> </iframe>'
+		);
+		var actual = check.evaluate.call(
+			checkContext,
+			vNode.actualNode,
+			options,
+			vNode
+		);
+		assert.isTrue(actual);
+		assert.hasAllKeys(checkContext._data, [
+			'name',
+			'parsedResource',
+			'resourceFrameTitle'
+		]);
+		assert.equal(checkContext._data.name, 'i am unique'.toLowerCase());
+		assert.equal(checkContext._data.parsedResource.pathname, 'page-one.html');
 	});
 
-	it('should take out space differences', function() {
-		checks['unique-frame-title'].evaluate.call(checkContext, {
-			title: 'APPLE'
-		});
-		assert.equal(checkContext._data, 'apple');
-	});
+	(shadowSupported ? it : xit)(
+		'returns true and sets `after` data when `iframe` has accessible name (in shadowDOM)',
+		function() {
+			var params = shadowCheckSetup(
+				'<div id="shadow"></div>',
+				'<iframe id="target" aria-label="I am inside shadowDOM" src="../integration/rules/frame-title-unique/frames/page-one.html"> </iframe>'
+			);
+			var actual = check.evaluate.apply(checkContext, params);
+			assert.isTrue(actual);
+			assert.hasAllKeys(checkContext._data, [
+				'name',
+				'parsedResource',
+				'resourceFrameTitle'
+			]);
+			assert.equal(
+				checkContext._data.name,
+				'i am inside shadowdom'.toLowerCase()
+			);
+			assert.equal(checkContext._data.parsedResource.pathname, 'page-one.html');
+		}
+	);
 });
