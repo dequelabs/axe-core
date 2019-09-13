1253bec710ee0f1d447520112ca628985c4dc8bd
jkodu <jey.nandakumar@gmail.com>
2019-09-13 15:47:35 +0100

    test: skip in phantomJs

--- test/commons/dom/is-visible.js
+++ test/commons/dom/is-visible.js
@@ -3,6 +3,7 @@ describe('dom.isVisible', function() {
 
 	var fixture = document.getElementById('fixture');
 	var queryFixture = axe.testUtils.queryFixture;
+	var isPhantom = window.PHANTOMJS ? true : false;
 	var fixtureSetup = axe.testUtils.fixtureSetup;
 	var isIE11 = axe.testUtils.isIE11;
 	var shadowSupported = axe.testUtils.shadowSupport.v1;
@@ -191,56 +192,71 @@ describe('dom.isVisible', function() {
 			assert.isFalse(axe.commons.dom.isVisible(el));
 		});
 
-		it('returns false for `AREA` without closest `MAP` element', function() {
-			var vNode = queryFixture(
-				'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>'
-			);
-			var actual = axe.commons.dom.isVisible(vNode.actualNode);
-			assert.isFalse(actual);
-		});
+		(isPhantom ? xit : it)(
+			'returns false for `AREA` without closest `MAP` element',
+			function() {
+				var vNode = queryFixture(
+					'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>'
+				);
+				var actual = axe.commons.dom.isVisible(vNode.actualNode);
+				assert.isFalse(actual);
+			}
+		);
 
-		it('returns false for `AREA` with closest `MAP` with no name attribute', function() {
-			var vNode = queryFixture(
-				'<map>' +
-					'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
-					'</map>'
-			);
-			var actual = axe.commons.dom.isVisible(vNode.actualNode);
-			assert.isFalse(actual);
-		});
+		(isPhantom ? xit : it)(
+			'returns false for `AREA` with closest `MAP` with no name attribute',
+			function() {
+				var vNode = queryFixture(
+					'<map>' +
+						'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
+						'</map>'
+				);
+				var actual = axe.commons.dom.isVisible(vNode.actualNode);
+				assert.isFalse(actual);
+			}
+		);
 
-		it('returns false for `AREA` with closest `MAP` with name but not referred by an `IMG` usemap attribute', function() {
-			var vNode = queryFixture(
-				'<map name="infographic">' +
-					'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
-					'</map>' +
-					'<img usemap="#infographic-wrong-name" alt="MDN infographic" />'
-			);
-			var actual = axe.commons.dom.isVisible(vNode.actualNode);
-			assert.isFalse(actual);
-		});
+		(isPhantom ? xit : it)(
+			'returns false for `AREA` with closest `MAP` with name but not referred by an `IMG` usemap attribute',
+			function() {
+				var vNode = queryFixture(
+					'<map name="infographic">' +
+						'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
+						'</map>' +
+						'<img usemap="#infographic-wrong-name" alt="MDN infographic" />'
+				);
+				var actual = axe.commons.dom.isVisible(vNode.actualNode);
+				assert.isFalse(actual);
+			}
+		);
 
-		it('returns false for `AREA` with `MAP` and used in `IMG` but `MAP` has `display:none`)', function() {
-			var vNode = queryFixture(
-				'<map name="infographic"style="display:none;">' +
-					'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
-					'</map>' +
-					'<img usemap="#infographic" alt="MDN infographic" />'
-			);
-			var actual = axe.commons.dom.isVisible(vNode.actualNode);
-			assert.isFalse(actual);
-		});
+		(isPhantom ? xit : it)(
+			'returns false for `AREA` with `MAP` and used in `IMG` but `MAP` has `display:none`)',
+			function() {
+				var vNode = queryFixture(
+					'<map name="infographic"style="display:none;">' +
+						'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
+						'</map>' +
+						'<img usemap="#infographic" alt="MDN infographic" />'
+				);
+				var actual = axe.commons.dom.isVisible(vNode.actualNode);
+				assert.isFalse(actual);
+			}
+		);
 
-		it('returns true for `AREA` with `MAP` and used in `IMG`)', function() {
-			var vNode = queryFixture(
-				'<map name="infographic">' +
-					'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
-					'</map>' +
-					'<img usemap="#infographic" alt="MDN infographic" />'
-			);
-			var actual = axe.commons.dom.isVisible(vNode.actualNode);
-			assert.isTrue(actual);
-		});
+		(isPhantom ? xit : it)(
+			'returns true for `AREA` with `MAP` and used in `IMG`)',
+			function() {
+				var vNode = queryFixture(
+					'<map name="infographic">' +
+						'<area id="target" role="link" shape="circle" coords="130,136,60" aria-label="MDN"/>' +
+						'</map>' +
+						'<img usemap="#infographic" alt="MDN infographic" />'
+				);
+				var actual = axe.commons.dom.isVisible(vNode.actualNode);
+				assert.isTrue(actual);
+			}
+		);
 
 		// IE11 either only supports clip paths defined by url() or not at all,
 		// MDN and caniuse.com give different results...
