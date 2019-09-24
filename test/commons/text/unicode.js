3599c3ac2050c85bce9143ae3117f7b50a752b81
Steven Lambert <steven.lambert@deque.com>
2019-09-24 13:23:15 -0600

    fix(label-content-name-mismatch): ignore private space unicode code characters

--- test/commons/text/unicode.js
+++ test/commons/text/unicode.js
@@ -187,6 +187,13 @@ describe('text.removeUnicode', function() {
 		assert.equal(actual, '');
 	});
 
+	it('returns string removing all private use unicode', function() {
+		var actual = axe.commons.text.removeUnicode('', {
+			punctuations: true
+		});
+		assert.equal(actual, '');
+	});
+
 	it('returns string removing combination of unicode characters', function() {
 		var actual = axe.commons.text.removeUnicode(
 			'The ☀️ is orange, the ◓ is white.',
