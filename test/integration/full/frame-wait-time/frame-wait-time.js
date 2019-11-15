7e6bd29c28f9f09d814933d53639c8c92fe2afba
Steven Lambert <steven.lambert@deque.com>
2019-11-15 11:01:52 -0700

    polyfill Function.name

--- test/integration/full/frame-wait-time/frame-wait-time.js
+++ test/integration/full/frame-wait-time/frame-wait-time.js
@@ -6,6 +6,20 @@ describe('frame-wait-time option', function() {
 	var respondable = axe.utils.respondable;
 
 	before(function(done) {
+		// Fix Function#name on browsers that do not support it (IE):
+		// @see https://stackoverflow.com/a/17056530
+		if (!function f() {}.name) {
+			Object.defineProperty(Function.prototype, 'name', {
+				get: function() {
+					var name = (this.toString().match(/^function\s*([^\s(]+)/) || [])[1];
+					// For better performance only parse once, and then cache the
+					// result through a new accessor for repeated access.
+					Object.defineProperty(this, 'name', { value: name });
+					return name;
+				}
+			});
+		}
+
 		axe.testUtils.awaitNestedLoad(function() {
 			done();
 		});
@@ -26,29 +40,32 @@ describe('frame-wait-time option', function() {
 		spy.restore();
 	});
 
-	describe('when set', function() {
-		var opts = {
-			frameWaitTime: 1,
-			runOnly: {
-				type: 'rule',
-				values: ['html-has-lang']
+	function getTimeoutCall() {
+		var calls = spy.getCalls();
+		var timeoutCall;
+		for (var i = 0; i < calls.length; i++) {
+			var fn = calls[i].args[0];
+			if (fn.name === 'collectResultFramesTimeout') {
+				timeoutCall = calls[i];
+				break;
 			}
-		};
-		it('should modify the default frame timeout', function(done) {
-			axe.run('#frame', opts, function() {
-				var calls = spy.getCalls();
-				var timeoutCall;
-				console.log('##### modify default');
-				for (var i = 0; i < calls.length; i++) {
-					var fn = calls[i].args[0];
-					console.log('\nfn:', fn.name);
-					console.log('timeout:', calls[i].args[1]);
+		}
 
-					if (fn.name === 'collectResultFramesTimeout') {
-						timeoutCall = calls[i];
-						break;
-					}
+		return timeoutCall;
+	}
+
+	describe('when set', function() {
+		it('should modify the default frame timeout', function(done) {
+			var opts = {
+				frameWaitTime: 1,
+				runOnly: {
+					type: 'rule',
+					values: ['html-has-lang']
 				}
+			};
+			axe.run('#frame', opts, function() {
+				var timeoutCall = getTimeoutCall();
+				assert.exists(timeoutCall, 'FrameTimeout not called');
 				assert.equal(timeoutCall.args[1], 1);
 				done();
 			});
@@ -58,19 +75,8 @@ describe('frame-wait-time option', function() {
 	describe('when not set', function() {
 		it('should use the default frame timeout', function(done) {
 			axe.run('#frame', function() {
-				var calls = spy.getCalls();
-				var timeoutCall;
-				console.log('##### use default');
-				for (var i = 0; i < calls.length; i++) {
-					var fn = calls[i].args[0];
-					console.log('\nfn:', fn.name);
-					console.log('timeout:', calls[i].args[1]);
-
-					if (fn.name === 'collectResultFramesTimeout') {
-						timeoutCall = calls[i];
-						break;
-					}
-				}
+				var timeoutCall = getTimeoutCall();
+				assert.exists(timeoutCall, 'FrameTimeout not called');
 				assert.equal(timeoutCall.args[1], 60000);
 				done();
 			});
