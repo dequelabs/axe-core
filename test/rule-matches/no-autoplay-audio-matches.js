583796776aaf62908dde2a9740c5d15d522fea46
jkodu <jey.nandakumar@gmail.com>
2019-11-22 12:46:54 +0000

    add test for matches

---
+++ test/rule-matches/no-autoplay-audio-matches.js
@@ -0,0 +1,41 @@
+describe('no-autoplay-audio-matches', function() {
+	'use strict';
+
+	var fixture = document.getElementById('fixture');
+	var queryFixture = axe.testUtils.queryFixture;
+	var rule;
+
+	beforeEach(function() {
+		rule = axe._audit.rules.find(function(rule) {
+			return rule.id === 'no-autoplay-audio';
+		});
+	});
+
+	afterEach(function() {
+		fixture.innerHTML = '';
+	});
+
+	it('returns false for <video> element that is paused', function() {
+		var vNode = queryFixture(
+			'<video id="target" autoplay="true" paused="true"><source src="https://act-rules.github.io/test-assets/rabbit-video/video.mp4" type="video/mp4" /></video>'
+		);
+		var actual = rule.matches(vNode.actualNode);
+		assert.isFalse(actual);
+	});
+
+	it('returns false for <video> element that is muted', function() {
+		var vNode = queryFixture(
+			'<video id="target" autoplay="true" muted="true"><source src="https://act-rules.github.io/test-assets/rabbit-video/video.mp4" type="video/mp4" /></video>'
+		);
+		var actual = rule.matches(vNode.actualNode);
+		assert.isFalse(actual);
+	});
+
+	it('returns true for <audio> element that has loop attribute', function() {
+		var vNode = queryFixture(
+			'<video id="target" loop="true"><source src="https://act-rules.github.io/test-assets/rabbit-video/video.mp4" type="video/mp4" /></video>'
+		);
+		var actual = rule.matches(vNode.actualNode);
+		assert.isTrue(actual);
+	});
+});
