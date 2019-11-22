c836b7f333fc0534f03bf07b561069a69763471d
Steven Lambert <steven.lambert@deque.com>
2019-11-22 11:34:55 -0700

    start checks

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,10 @@ const path = require('path');
 
 module.exports = {
 	mode: 'development',
+  watch: true,
 	entry: './lib/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'axe.js'
-	},
-  resolve: {
-    alias: {
-      '@/commons': path.resolve(__dirname, 'lib/commons/'),
-      '@/utils': path.resolve(__dirname, 'lib/core/utils/')
-    }
-  }
+	}
 };
