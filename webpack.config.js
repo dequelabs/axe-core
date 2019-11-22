d0695da2d94e9089131dd9b6567b3f00e1977f85
Steven Lambert <steven.lambert@deque.com>
2019-11-21 17:07:02 -0700

    initial checks

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'axe.js'
-	}
+	},
+  resolve: {
+    alias: {
+      '@/commons': path.resolve(__dirname, 'lib/commons/'),
+      '@/utils': path.resolve(__dirname, 'lib/core/utils/')
+    }
+  }
 };
