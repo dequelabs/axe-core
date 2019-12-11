1b6fe5a462836798ea7b9c66aef76540aaffcdd3
Wilco Fiers <wilco.fiers@deque.com>
2019-12-11 11:28:57 +0100

    chore: use serial over virtual in tests

--- test/commons/aria/arialabel-text.js
+++ test/commons/aria/arialabel-text.js
@@ -7,7 +7,7 @@ describe('aria.arialabelText', function() {
 		assert.equal(aria.arialabelText(vNode), '');
 	});
 
-	it('returns the aria-label attribute of serialised nodes', function() {
+	it('returns the aria-label attribute', function() {
 		var label = ' my label ';
 		var vNode = new axe.SerialVirtualNode({
 			nodeName: 'div',
@@ -16,31 +16,22 @@ describe('aria.arialabelText', function() {
 		assert.equal(aria.arialabelText(vNode), label);
 	});
 
-	it('returns the aria-label attribute of virtual nodes', function() {
-		var node = document.createElement('div');
-		var label = ' my label ';
-		node.setAttribute('aria-label', label);
-		var vNode = new axe.VirtualNode(node);
-		assert.equal(aria.arialabelText(vNode), label);
-	});
-
 	it('returns "" if there is no aria-label', function() {
-		var node = document.createElement('div');
-		var vNode = new axe.VirtualNode(node);
+		var vNode = new axe.SerialVirtualNode({ nodeName: 'div' });
 		assert.equal(aria.arialabelText(vNode), '');
 	});
 
-	it('returns "" if the node is not an element', function() {
-		var node = document.createTextNode('my text node');
-		assert.equal(aria.arialabelText(node), '');
-	});
-
-	it('looks up the node in the virtualNode tree', function() {
+	it('looks up the node in the flat tree', function() {
 		var label = 'harambe';
 		var node = document.createElement('div');
 		node.setAttribute('aria-label', label);
 
 		axe.utils.getFlattenedTree(node);
 		assert.equal(aria.arialabelText(node), label);
+	});
+
+	it('returns "" if the node is not an element', function() {
+		var node = document.createTextNode('my text node');
+		assert.equal(aria.arialabelText(node), '');
 	});
 });
