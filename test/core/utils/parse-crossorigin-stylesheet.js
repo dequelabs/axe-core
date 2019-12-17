9740164b4b793a47e302bfff04600013e21a21c2
Steven Lambert <steven.lambert@deque.com>
2019-12-17 10:22:16 -0700

    revert

--- test/core/utils/parse-crossorigin-stylesheet.js
+++ test/core/utils/parse-crossorigin-stylesheet.js
@@ -60,8 +60,6 @@ describe('axe.utils.parseCrossOriginStylesheet', function() {
 	});
 
 	it('rejects when given url to fetch is not found', function(done) {
-		this.timeout(6000);
-
 		var importUrl =
 			'https://make-up-a-website-that-does-not-exist.com/style.css';
 		var options = {
