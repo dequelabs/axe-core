3352e4b4315049b25790cc260209eafc31e63ce0
Steven Lambert <steven.lambert@deque.com>
2019-12-18 10:17:43 -0700

    chore: update from Master

--- test/checks/lists/listitem.js
+++ test/checks/lists/listitem.js
@@ -41,6 +41,7 @@ describe('listitem', function() {
 		var target = fixture.querySelector('#target');
 		assert.isFalse(checks.listitem.evaluate.call(checkContext, target));
 		assert.equal(checkContext._data.messageKey, 'roleNotValid');
+		assert.equal(checkContext._data, 'roleNotValid');
 	});
 
 	it('should pass if the listitem has a parent <ol> with an invalid role', function() {
