1912178c2a8590ebcd440b055e01210353df50ac
jkodu <jey.nandakumar@gmail.com>
2020-01-03 16:47:26 +0000

    test: revert fail test

--- test/core/base/audit.js
+++ test/core/base/audit.js
@@ -94,7 +94,7 @@ describe('Audit', function() {
 	});
 
 	it('should be a function', function() {
-		assert.isFunction({});
+		assert.isFunction(Audit);
 	});
 
 	describe('Audit#_constructHelpUrls', function() {
