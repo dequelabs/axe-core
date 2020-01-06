f1f95e715a26f2231cde386aaaf3e7ed35131aa5
jkodu <jey.nandakumar@gmail.com>
2020-01-06 14:46:12 +0000

    test: update tests

--- test/core/utils/preload-media.js
+++ test/core/utils/preload-media.js
@@ -3,61 +3,50 @@
 describe('axe.utils.preloadMedia', function() {
 	'use strict';
 
-	var treeRoot;
 	var origFn = axe.utils.preloadMedia;
 	var fixture = document.getElementById('fixture');
+	var fixtureSetup = axe.testUtils.fixtureSetup;
 
-	var audioSrc =
-		'https://act-rules.github.io/test-assets/moon-audio/moon-speech.mp3';
-	var videoSrc =
-		'https://act-rules.github.io/test-assets/rabbit-video/video.mp4';
-
-	beforeEach(function() {
-		rebuildTree();
-	});
+	function rebuildTree() {
+		axe._tree = axe.utils.getFlattenedTree(document);
+	}
 
 	afterEach(function() {
 		axe.utils.preloadMedia = origFn;
 		fixture.innerHTML = '';
 	});
 
-	function rebuildTree() {
-		treeRoot = axe._tree = axe.utils.getFlattenedTree(document);
-	}
-
-	function addMediaNode(type, src) {
-		var node = document.createElement(type);
-		node.setAttribute('src', src);
-		fixture.appendChild(node);
-	}
-
-	it('passes the treeRoot property to utils.preloadMedia', function(done) {
+	it('invokes utils.preloadMedia and passes the treeRoot property', function(done) {
 		var isCalled = false;
 		axe.utils.preloadMedia = function(options) {
-			assert.equal(options.treeRoot, treeRoot);
+			assert.isDefined(options.treeRoot);
 			isCalled = true;
 			return Promise.resolve();
 		};
 
-		axe.utils.preloadMedia({ treeRoot: treeRoot }).then(function() {
+		rebuildTree();
+
+		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function() {
 			assert.ok(isCalled);
 			done();
 		});
 	});
 
 	it('returns empty array when there are no media nodes to be preloaded', function(done) {
-		axe.utils.preloadMedia({ treeRoot: treeRoot }).then(function(result) {
+		rebuildTree();
+
+		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
 			assert.equal(result.length, 0);
 			done();
 		});
 	});
 
 	it('returns media node (audio) after their metadata has been preloaded', function(done) {
-		addMediaNode('audio', audioSrc);
+		fixtureSetup(
+			'<audio src="https://act-rules.github.io/test-assets/moon-audio/moon-speech.mp3" autoplay="true" controls></audio>'
+		);
 
-		rebuildTree();
-
-		axe.utils.preloadMedia({ treeRoot: treeRoot }).then(function(result) {
+		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
 			assert.equal(result.length, 1);
 			assert.isTrue(result[0].readyState > 0);
 
@@ -66,12 +55,17 @@ describe('axe.utils.preloadMedia', function() {
 	});
 
 	it('returns media nodes (audio, video) after their metadata has been preloaded', function(done) {
-		addMediaNode('audio', audioSrc);
-		addMediaNode('video', videoSrc);
+		fixtureSetup(
+			// 1 audio elm
+			'<audio src="https://act-rules.github.io/test-assets/moon-audio/moon-speech.mp3"></audio>' +
+				// 1 video elm
+				'<video>' +
+				'<source src="https://act-rules.github.io/test-assets/rabbit-video/video.mp4" type="video/mp4" />' +
+				'<source src="https://act-rules.github.io/test-assets/rabbit-video/video.webm" type="video/webm" />' +
+				'</video>'
+		);
 
-		rebuildTree();
-
-		axe.utils.preloadMedia({ treeRoot: treeRoot }).then(function(result) {
+		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
 			assert.equal(result.length, 2);
 			assert.isTrue(result[0].readyState > 0);
 			assert.notEqual(result[1].duration, NaN);
