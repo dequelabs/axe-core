c046f72481036b071c3a790f60a4b1b3919e5ab1
jkodu <jey.nandakumar@gmail.com>
2020-01-10 13:55:58 +0000

    test: skip when navigator is IE

--- test/core/utils/preload-media.js
+++ test/core/utils/preload-media.js
@@ -6,6 +6,7 @@ describe('axe.utils.preloadMedia', function() {
 	var origFn = axe.utils.preloadMedia;
 	var fixture = document.getElementById('fixture');
 	var fixtureSetup = axe.testUtils.fixtureSetup;
+	var isIE11 = axe.testUtils.isIE11;
 
 	afterEach(function() {
 		axe.utils.preloadMedia = origFn;
@@ -37,40 +38,46 @@ describe('axe.utils.preloadMedia', function() {
 		});
 	});
 
-	it('returns media node (audio) after their metadata has been preloaded', function(done) {
-		fixtureSetup(
-			'<audio src="/test/assets/moon-speech.mp3" autoplay="true" controls></audio>'
-		);
+	(isIE11 ? it.skip : it)(
+		'returns media node (audio) after their metadata has been preloaded',
+		function(done) {
+			fixtureSetup(
+				'<audio src="/test/assets/moon-speech.mp3" autoplay="true" controls></audio>'
+			);
 
-		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
-			assert.equal(result.length, 1);
-			assert.isTrue(result[0].readyState > 0);
-			assert.equal(Math.round(result[0].duration), 27);
+			axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
+				assert.equal(result.length, 1);
+				assert.isTrue(result[0].readyState > 0);
+				assert.equal(Math.round(result[0].duration), 27);
 
-			done();
-		});
-	});
+				done();
+			});
+		}
+	);
 
-	it('returns media nodes (audio, video) after their metadata has been preloaded', function(done) {
-		fixtureSetup(
-			// 1 audio elm
-			'<audio src="/test/assets/moon-speech.mp3"></audio>' +
-				// 1 video elm
-				'<video>' +
-				'<source src="/test/assets/video.mp4" type="video/mp4" />' +
-				'<source src="/test/assets/video.webm" type="video/webm" />' +
-				'</video>'
-		);
+	(isIE11 ? it.skip : it)(
+		'returns media nodes (audio, video) after their metadata has been preloaded',
+		function(done) {
+			fixtureSetup(
+				// 1 audio elm
+				'<audio src="/test/assets/moon-speech.mp3"></audio>' +
+					// 1 video elm
+					'<video>' +
+					'<source src="/test/assets/video.mp4" type="video/mp4" />' +
+					'<source src="/test/assets/video.webm" type="video/webm" />' +
+					'</video>'
+			);
 
-		axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
-			assert.equal(result.length, 2);
-			assert.isTrue(result[0].readyState > 0);
-			assert.equal(Math.round(result[0].duration), 27);
+			axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function(result) {
+				assert.equal(result.length, 2);
+				assert.isTrue(result[0].readyState > 0);
+				assert.equal(Math.round(result[0].duration), 27);
 
-			assert.isTrue(result[1].readyState > 0);
-			assert.equal(Math.round(result[1].duration), 14);
+				assert.isTrue(result[1].readyState > 0);
+				assert.equal(Math.round(result[1].duration), 14);
 
-			done();
-		});
-	});
+				done();
+			});
+		}
+	);
 });
