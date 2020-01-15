73713b4aaf8a55a0951706acca72e189585cf0ac
jkodu <jey.nandakumar@gmail.com>
2020-01-15 14:04:59 +0000

    only handle audio media node with no controls in ishidden

--- test/core/utils/is-hidden.js
+++ test/core/utils/is-hidden.js
@@ -40,14 +40,25 @@ describe('axe.utils.isHidden', function() {
 		assert.isFalse(axe.utils.isHidden(document));
 	});
 
-	it('should return false on a `video`', function() {
-		var el = document.createElement('video');
+	it('should return false on a `audio` element without `controls` attribute', function() {
+		fixture.innerHTML =
+			'<audio id="target" src="/test/assets/moon-speech.mp3"></audio>';
+		var el = document.getElementById('target');
 		assert.isFalse(axe.utils.isHidden(el));
 	});
 
-	it('should return false on an `audio`', function() {
-		var el = document.createElement('audio');
+	it('should return false on a `audio` element with `controls` attribute', function() {
+		fixture.innerHTML =
+			'<audio id="target" src="/test/assets/moon-speech.mp3" controls></audio>';
+		var el = document.getElementById('target');
 		assert.isFalse(axe.utils.isHidden(el));
+	});
+
+	it('should return false on a `audio` element with `controls` attribute but `display:none`', function() {
+		fixture.innerHTML =
+			'<audio id="target" src="/test/assets/moon-speech.mp3" style="display: none" controls></audio>';
+		var el = document.getElementById('target');
+		assert.isTrue(axe.utils.isHidden(el));
 	});
 
 	it('should return true if `aria-hidden` is set', function() {
