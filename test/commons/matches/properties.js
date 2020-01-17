ea6009f653e84cb018ae83922921b2a70e10cefd
Steven Lambert <steven.lambert@deque.com>
2020-01-17 08:09:26 -0700

    [WIP] feat(matches, convert-selector, matchers): use virtual node for commons.matchers

--- test/commons/matches/properties.js
+++ test/commons/matches/properties.js
@@ -1,82 +1,57 @@
 describe('matches.properties', function() {
 	var properties = axe.commons.matches.properties;
+	var fixture = document.querySelector('#fixture');
+	var queryFixture = axe.testUtils.queryFixture;
+
+	beforeEach(function() {
+		fixture.innerHTML = '';
+	});
 
 	it('returns true if all properties match', function() {
+		var virtualNode = queryFixture('<input type="text" id="target"></input>');
+
 		assert.isTrue(
-			properties(
-				{
-					foo: 'baz',
-					bar: 'foo',
-					baz: 'bar'
-				},
-				{
-					foo: 'baz',
-					baz: 'bar'
-				}
-			)
+			properties(virtualNode, {
+				nodeName: 'input',
+				id: 'target',
+				type: 'text'
+			})
 		);
 	});
 
 	it('returns false if some properties do not match', function() {
+		var virtualNode = queryFixture('<input type="text" id="target"></input>');
+
 		assert.isFalse(
-			properties(
-				{
-					foo: 'baz',
-					bar: 'foo',
-					baz: 'bar'
-				},
-				{
-					foo: 'baz',
-					bar: 'foo',
-					baz: 'baz'
-				}
-			)
+			properties(virtualNode, {
+				nodeName: 'input',
+				id: 'target',
+				type: 'num'
+			})
 		);
 	});
 
 	it('returns false if any properties are missing', function() {
+		var virtualNode = queryFixture('<h1 id="target">foo</h1>');
+
 		assert.isFalse(
-			properties(
-				{
-					foo: 'baz',
-					baz: 'bar'
-				},
-				{
-					foo: 'baz',
-					bar: 'foo',
-					baz: 'bar'
-				}
-			)
+			properties(virtualNode, {
+				nodeName: 'h1',
+				id: 'target',
+				type: 'text'
+			})
 		);
 	});
 
-	it('works with virtual nodes', function() {
+	it('works with actual nodes', function() {
+		var virtualNode = queryFixture('<input type="text" id="target"></input>');
+
 		assert.isTrue(
-			properties(
-				{
-					actualNode: {
-						foo: 'bar',
-						bar: 'foo'
-					}
-				},
-				{
-					foo: 'bar',
-					bar: 'foo'
-				}
-			)
-		);
-		assert.isFalse(
-			properties(
-				{
-					actualNode: {
-						foo: 'bar',
-						bar: 'foo'
-					}
-				},
-				{
-					baz: 'baz'
-				}
-			)
+			properties(virtualNode.actualNode, {
+				nodeName: 'input',
+				id: 'target',
+				type: 'text'
+			})
 		);
 	});
 });
