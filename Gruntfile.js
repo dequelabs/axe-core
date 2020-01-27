523a31c19fefd330e0b4f4c45f51d400c6f66164
Steven Lambert <steven.lambert@deque.com>
2020-01-27 09:32:11 -0700

    fix(unicode): stop parsing escaped unicode strings (#1997)

--- Gruntfile.js
+++ Gruntfile.js
@@ -204,6 +204,7 @@ module.exports = function(grunt) {
 					compress: false,
 					beautify: {
 						beautify: true,
+						ascii_only: true,
 						indent_level: 2,
 						braces: true,
 						quote_style: 1
