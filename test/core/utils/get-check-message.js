1531d24cd28ffa718528b4e307c8146f8d1648e2
Steven Lambert <steven.lambert@deque.com>
2020-02-26 13:45:11 -0700

    feat(getCheckMessage): add API to return check message

---
+++ test/core/utils/get-check-message.js
@@ -0,0 +1,55 @@
+describe('axe.utils.getCheckMessage', function() {
+	var getCheckMessage = axe.utils.getCheckMessage;
+
+	beforeEach(function() {
+		axe._audit = {
+			data: {
+				checks: {
+					'my-check': {
+						messages: {
+							pass: 'Pass message',
+							fail: 'Fail message',
+							incomplete: 'Incomplete message'
+						}
+					}
+				}
+			}
+		};
+	});
+
+	afterEach(function() {
+		axe._audit = undefined;
+	});
+
+	it('should return the pass message', function() {
+		assert.equal(getCheckMessage('my-check', 'pass'), 'Pass message');
+	});
+
+	it('should return the fail message', function() {
+		assert.equal(getCheckMessage('my-check', 'fail'), 'Fail message');
+	});
+
+	it('should return the incomplete message', function() {
+		assert.equal(
+			getCheckMessage('my-check', 'incomplete'),
+			'Incomplete message'
+		);
+	});
+
+	it('should handle data', function() {
+		axe._audit.data.checks['my-check'].messages.pass =
+			'Pass message with ${data.message}';
+		assert.equal(
+			getCheckMessage('my-check', 'pass', { message: 'hello world!' }),
+			'Pass message with hello world!'
+		);
+	});
+
+	it('returns empty string when check does not exist', function() {
+		assert.equal(getCheckMessage('invalid-check', 'pass'), '');
+	});
+
+	it('returns empty string when check message does not exist', function() {
+		assert.equal(getCheckMessage('invalid-check', 'invalid'), '');
+	});
+});
