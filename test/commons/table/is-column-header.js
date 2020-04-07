da96435a9a8fd07a5c2331418f993140da52a5ce
Steven Lambert <steven.lambert@deque.com>
2020-04-07 14:55:25 -0600

    tests

--- test/commons/table/is-column-header.js
+++ test/commons/table/is-column-header.js
@@ -2,40 +2,44 @@ describe('table.isColumnHeader', function() {
 	'use strict';
 
 	var table = axe.commons.table;
-	var fixture = document.querySelector('#fixture');
+	var fixtureSetup = axe.testUtils.fixtureSetup;
 
-	var tableFixture =
-		'<table>' +
-		'<tr>' +
-		'<th id="ch1">column header 1</th>' +
-		'<th scope="col" id="ch2">column header 2</th>' +
-		'</tr>' +
-		'<tr>' +
-		'<th scope="row" id="rh">row header 1</th>' +
-		'<td id="cell">cell</td>' +
-		'</table>';
+	beforeEach(function() {
+		fixtureSetup(
+			'<table>' +
+				'<tr>' +
+				'<th id="ch1">column header 1</th>' +
+				'<th scope="col" id="ch2">column header 2</th>' +
+				'</tr>' +
+				'<tr>' +
+				'<th id="rh1">row header 1</th>' +
+				'<td id="cell1">cell 1</td>' +
+				'</tr>' +
+				'<tr>' +
+				'<th scope="row" id="rh2">row header 2</th>' +
+				'<td id="cell2">cell 2</td>' +
+				'</tr>' +
+				'</table>'
+		);
+	});
 
 	it('returns false if not a column header', function() {
-		fixture.innerHTML = tableFixture;
-		var cell = document.querySelector('#cell');
+		var cell = document.querySelector('#cell1');
 		assert.isFalse(table.isColumnHeader(cell));
 	});
 
 	it('returns true if scope="auto"', function() {
-		fixture.innerHTML = tableFixture;
 		var cell = document.querySelector('#ch1');
 		assert.isTrue(table.isColumnHeader(cell));
 	});
 
 	it('returns true if scope="col"', function() {
-		fixture.innerHTML = tableFixture;
 		var cell = document.querySelector('#ch2');
 		assert.isTrue(table.isColumnHeader(cell));
 	});
 
 	it('returns false if scope="row"', function() {
-		fixture.innerHTML = tableFixture;
-		var cell = document.querySelector('#rh');
+		var cell = document.querySelector('#rh2');
 		assert.isFalse(table.isColumnHeader(cell));
 	});
 });
