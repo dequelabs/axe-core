a10f645a7be14dc0b4bf02b15d3e51050cfa4703
Steven Lambert <steven.lambert@deque.com>
2020-04-10 10:25:55 -0600

    test

--- rename.js
+++ rename.js
@@ -5,8 +5,9 @@ const { execSync } = require('child_process');
 
 // get all checks
 glob.sync('lib/checks/visibility/*.js').forEach(filePath => {
-
   const fileName = path.basename(filePath, '.js');
+  const fileDir = path.dirname(filePath);
 
-  console.log(fileName);
+  // rename
+  execSync(`git mv ${filePath} ${fileDir}/${fileName}-evaluate.js`);
 });
