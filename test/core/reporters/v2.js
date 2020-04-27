5a7c04c225896fb6c930752e95287823ea7ae864
Steven Lambert <steven.lambert@deque.com>
2020-04-27 14:10:15 -0600

    tests: make tests true unit rather than integration tests

--- test/core/reporters/v2.js
+++ test/core/reporters/v2.js
@@ -1,7 +1,6 @@
 describe('reporters - v2', function() {
 	'use strict';
-	var orig,
-		results,
+	var runResults,
 		_results = [
 			{
 				id: 'gimmeLabel',
@@ -87,64 +86,47 @@ describe('reporters - v2', function() {
 			}
 		];
 	beforeEach(function() {
-		results = JSON.parse(JSON.stringify(_results));
+		runResults = JSON.parse(JSON.stringify(_results));
 		axe._load({
 			messages: {},
 			rules: [],
 			data: {}
 		});
-		orig = axe._runRules;
-		axe._runRules = function(ctxt, options, cb) {
-			cb(results, function noop() {});
-		};
 	});
 
 	afterEach(function() {
 		axe._audit = null;
-		axe._runRules = orig;
 	});
 
-	var optionsV2 = { reporter: 'v2' };
-
-	it('should merge the runRules results into violations and passes', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should merge the runRules results into violations and passes', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.isObject(results);
 			assert.isArray(results.violations);
 			assert.lengthOf(results.violations, 1);
 			assert.isArray(results.passes);
 			assert.lengthOf(results.passes, 1);
-
-			done();
 		});
 	});
-	it('should add the rule id to the rule result', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add the rule id to the rule result', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.equal(results.violations[0].id, 'idkStuff');
 			assert.equal(results.passes[0].id, 'gimmeLabel');
-			done();
 		});
 	});
-	it('should add tags to the rule result', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add tags to the rule result', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.deepEqual(results.violations[0].tags, ['tag2']);
 			assert.deepEqual(results.passes[0].tags, ['tag1']);
-			done();
 		});
 	});
-	it('should add the rule help to the rule result', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add the rule help to the rule result', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.isNotOk(results.violations[0].helpUrl);
 			assert.equal(results.passes[0].helpUrl, 'things');
-			done();
 		});
 	});
-	it('should add the html to the node data', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add the html to the node data', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.ok(results.violations[0].nodes);
 			assert.equal(results.violations[0].nodes.length, 1);
 			assert.equal(
@@ -152,12 +134,10 @@ describe('reporters - v2', function() {
 				'<pillock>george bush</pillock>'
 			);
 			assert.equal(results.passes[0].nodes[0].html, '<minkey>chimp</minky>');
-			done();
 		});
 	});
-	it('should add the target selector array to the node data', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add the target selector array to the node data', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.ok(results.violations[0].nodes);
 			assert.equal(results.violations[0].nodes.length, 1);
 			assert.deepEqual(results.violations[0].nodes[0].target, [
@@ -165,30 +145,24 @@ describe('reporters - v2', function() {
 				'r',
 				'pillock'
 			]);
-			done();
 		});
 	});
-	it('should add the description to the rule result', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add the description to the rule result', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.equal(results.violations[0].description, 'something more nifty');
 			assert.equal(results.passes[0].description, 'something nifty');
-			done();
 		});
 	});
-	it('should add the impact to the rule result', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add the impact to the rule result', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.equal(results.violations[0].impact, 'cats');
 			assert.equal(results.violations[0].nodes[0].impact, 'cats');
 			assert.isNotOk(results.passes[0].impact);
 			assert.isNotOk(results.passes[0].nodes[0].impact);
-			done();
 		});
 	});
-	it('should map relatedNodes', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should map relatedNodes', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.lengthOf(results.violations[0].nodes[0].all[0].relatedNodes, 1);
 			assert.equal(
 				results.violations[0].nodes[0].all[0].relatedNodes[0].target,
@@ -208,24 +182,19 @@ describe('reporters - v2', function() {
 				results.passes[0].nodes[0].any[0].relatedNodes[0].html,
 				'fred'
 			);
-			done();
 		});
 	});
-	it('should add environment data', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add environment data', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.isNotNull(results.url);
 			assert.isNotNull(results.timestamp);
 			assert.isNotNull(results.testEnvironement);
 			assert.isNotNull(results.testRunner);
-			done();
 		});
 	});
-	it('should add toolOptions property', function(done) {
-		axe.run(optionsV2, function(err, results) {
-			assert.isNull(err);
+	it('should add toolOptions property', function() {
+		axe.getReporter('v2')(runResults, {}, function(results) {
 			assert.isNotNull(results.toolOptions);
-			done();
 		});
 	});
 });
