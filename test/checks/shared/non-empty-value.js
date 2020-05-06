0237d7a0e4f4983a7d399646509abafcdc11e533
Steven Lambert <steven.lambert@deque.com>
2020-05-06 15:38:36 -0600

    feat(non-space-content): switch all non-empty checks to new generic check

--- test/checks/shared/non-empty-value.js
+++ test/checks/shared/non-empty-value.js
@@ -2,39 +2,40 @@ describe('non-empty-value', function() {
 	'use strict';
 
 	var fixture = document.getElementById('fixture');
+	var checkSetup = axe.testUtils.checkSetup;
 
 	afterEach(function() {
 		fixture.innerHTML = '';
 	});
 
 	it('should return true if an value is present', function() {
-		var node = document.createElement('input');
-		node.setAttribute('value', 'woohoo');
-		fixture.appendChild(node);
+		var params = checkSetup('<input id="target" value="woohoo" />', {
+			attribute: 'value'
+		});
 
-		assert.isTrue(checks['non-empty-value'].evaluate(node));
+		assert.isTrue(checks['non-empty-value'].evaluate.apply(null, params));
 	});
 
 	it('should return false if an value is not present', function() {
-		var node = document.createElement('input');
-		fixture.appendChild(node);
+		var params = checkSetup('<input id="target" />', { attribute: 'value' });
 
-		assert.isFalse(checks['non-empty-value'].evaluate(node));
+		assert.isFalse(checks['non-empty-value'].evaluate.apply(null, params));
 	});
 
 	it('should return false if an value is present, but empty', function() {
-		var node = document.createElement('input');
-		node.setAttribute('value', ' ');
-		fixture.appendChild(node);
+		var params = checkSetup('<input id="target" value=" " />', {
+			attribute: 'value'
+		});
 
-		assert.isFalse(checks['non-empty-value'].evaluate(node));
+		assert.isFalse(checks['non-empty-value'].evaluate.apply(null, params));
 	});
 
 	it('should collapse whitespace', function() {
-		var node = document.createElement('div');
-		node.setAttribute('value', ' \t \n \r \t  \t\r\n ');
-		fixture.appendChild(node);
+		var params = checkSetup(
+			'<input id="target" value=" \t \n \r \t  \t\r\n " />',
+			{ attribute: 'value' }
+		);
 
-		assert.isFalse(checks['non-empty-value'].evaluate(node));
+		assert.isFalse(checks['non-empty-value'].evaluate.apply(null, params));
 	});
 });
