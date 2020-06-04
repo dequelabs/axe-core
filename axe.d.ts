23674d4403a7088996a758d0081b869516be4aea
Stephen Mathieson <me@stephenmathieson.com>
2020-06-04 12:08:26 -0400

    fix(types): Add "wcag21aa" and "wcag21a" to our TS definition (#2272)

--- axe.d.ts
+++ axe.d.ts
@@ -5,7 +5,7 @@
 declare namespace axe {
 	type ImpactValue = 'minor' | 'moderate' | 'serious' | 'critical';
 
-	type TagValue = 'wcag2a' | 'wcag2aa' | 'section508' | 'best-practice';
+	type TagValue = 'wcag2a' | 'wcag2aa' | 'section508' | 'best-practice' | 'wcag21a' | 'wcag21aa';
 
 	type ReporterVersion = 'v1' | 'v2' | 'raw' | 'raw-env' | 'no-passes';
 
