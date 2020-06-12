30efbfffc8b83ca03b4b2697e4202027826195f9
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-06-12 08:33:01 -0600

    feat(matches): add explicitRole, implicitRole, and semanticRole matches functions (#2286)

---
+++ test/commons/matches/explicit-role.js
@@ -0,0 +1,41 @@
+describe('matches.explicitRole', function() {
+	var explicitRole = axe.commons.matches.explicitRole;
+	var fixture = document.querySelector('#fixture');
+	var queryFixture = axe.testUtils.queryFixture;
+
+	beforeEach(function() {
+		fixture.innerHTML = '';
+	});
+
+	it('should return true if explicit role matches', function() {
+		var virtualNode = queryFixture('<span id="target" role="textbox"></span>');
+		assert.isTrue(explicitRole(virtualNode, 'textbox'));
+	});
+
+	it('should return true if explicit role matches array', function() {
+		var virtualNode = queryFixture('<span id="target" role="textbox"></span>');
+		assert.isTrue(explicitRole(virtualNode, ['combobox', 'textbox']));
+	});
+
+	it('should return false if explicit role does not match', function() {
+		var virtualNode = queryFixture('<span id="target" role="main"></span>');
+		assert.isFalse(explicitRole(virtualNode, 'textbox'));
+	});
+
+	it('should return false if matching implicit role', function() {
+		var virtualNode = queryFixture('<ul><li id="target"></li></ul>');
+		assert.isFalse(explicitRole(virtualNode, 'listitem'));
+	});
+
+	// TODO: will only work when get-role works exclusively with virtual
+	// nodes
+	it.skip('works with SerialVirtualNode', function() {
+		var serialNode = new axe.SerialVirtualNode({
+			nodeName: 'span',
+			attributes: {
+				role: 'textbox'
+			}
+		});
+		assert.isTrue(explicitRole(serialNode, 'textbox'));
+	});
+});
