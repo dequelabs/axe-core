7c23dbd1d04d95e3bd2b56a0bd14571f906eb3d5
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-07-01 09:37:41 -0600

    fix(bypass): find headings in iframes (#2310)
    * fix(bypass): find headings in iframes
    
    * name tests
    
    * fix test
    
    * test
    
    * ci fail
    
    * i think i found the issue
    
    * put back pageLevel
    
    * fix test
    
    * Remove leading "./" from axe-linter excludes
    
    * Revert "Remove leading "./" from axe-linter excludes"
    
    This reverts commit be62002f876b32cc02f361bee7533419b3811f14.
    
    Co-authored-by: Stephen Mathieson <me@stephenmathieson.com>

---
+++ test/integration/full/bypass/header-iframe-pass.js
@@ -0,0 +1,39 @@
+describe('bypass iframe test pass', function() {
+	'use strict';
+	var results;
+	before(function(done) {
+		this.timeout = 50000;
+		axe.testUtils.awaitNestedLoad(function() {
+			// Stop messing with my tests Mocha!
+			var heading = document.querySelector('#mocha h1');
+			if (heading) {
+				heading.outerHTML = '<div><b>bypass pass test fail</b></div>';
+			}
+
+			axe.run({ runOnly: { type: 'rule', values: ['bypass'] } }, function(
+				err,
+				r
+			) {
+				assert.isNull(err);
+				results = r;
+				done();
+			});
+		});
+	});
+
+	describe('violations', function() {
+		it('should find none', function() {
+			assert.lengthOf(results.violations, 0);
+		});
+	});
+
+	describe('passes', function() {
+		it('should find 1', function() {
+			assert.lengthOf(results.passes[0].nodes, 1);
+		});
+
+		it('should find #pass1', function() {
+			assert.deepEqual(results.passes[0].nodes[0].target, ['#pass1']);
+		});
+	});
+});
