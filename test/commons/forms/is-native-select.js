8ad41af457125a268256f84de7623a79e905f782
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-07-07 08:25:54 -0600

    fix(forms/*): allow all form control value checks to work with virtual nodes (#2343)
    * fix(forms/*): allow all form control value checks to work with virtual nodes
    
    * ignore case

--- test/commons/forms/is-native-select.js
+++ test/commons/forms/is-native-select.js
@@ -1,16 +1,19 @@
 describe('forms.isNativeSelect', function() {
 	'use strict';
 	var isNativeSelect = axe.commons.forms.isNativeSelect;
+	var queryFixture = axe.testUtils.queryFixture;
 
 	it('returns true for a select element', function() {
-		var node = document.createElement('select');
+		var node = queryFixture('<select id="target"></select>');
 		assert.isTrue(isNativeSelect(node));
 	});
 
 	it('returns false for non-select elements', function() {
 		var nonSelectElements = ['a', 'h1', 'div', 'span', 'main'];
 		nonSelectElements.forEach(function(nodeName) {
-			var node = document.createElement(nodeName);
+			var node = queryFixture(
+				'<' + nodeName + ' id="target"></' + nodeName + '>'
+			);
 			assert.isFalse(
 				isNativeSelect(node),
 				'<' + nodeName + '> is not a native select element'
