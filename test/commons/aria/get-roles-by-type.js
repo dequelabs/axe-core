2efc869d29a7fcc54242e9ff88dd85d3b5824785
Steven Lambert <steven.lambert@deque.com>
2020-07-10 10:38:43 -0600

    feat(aria/get-roles-by-type): deprecate in favor of standards/get-aria-roles-by-type

---
+++ test/commons/aria/get-roles-by-type.js
@@ -0,0 +1,31 @@
+describe('aria.getRolesByType', function() {
+	'use strict';
+
+	before(function() {
+		axe._load({});
+	});
+
+	afterEach(function() {
+		axe.reset();
+	});
+
+	it('should return array if roletype is found in the lookup table', function() {
+		axe.configure({
+			standards: {
+				ariaRoles: {
+					dogs: {
+						type: 'things'
+					},
+					cats: {
+						type: 'stuff'
+					}
+				}
+			}
+		});
+		assert.deepEqual(axe.commons.aria.getRolesByType('stuff'), ['cats']);
+	});
+
+	it('should return empty array if role is not found in the lookup table', function() {
+		assert.deepEqual(axe.commons.aria.getRolesByType('blahblahblah'), []);
+	});
+});
