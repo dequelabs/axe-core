f89a14ee0160a6325e2f06afdcc4eae6072885c7
Steven Lambert <steven.lambert@deque.com>
2020-07-14 09:50:44 -0600

    chore(validat-attr,unsupported-attr): work with standards object

--- test/commons/aria/attributes.js
+++ test/commons/aria/attributes.js
@@ -28,30 +28,3 @@ describe('aria.requiredAttr', function() {
 		assert.deepEqual(result, []);
 	});
 });
-
-describe('aria.validateAttr', function() {
-	'use strict';
-
-	var orig;
-	beforeEach(function() {
-		orig = axe.commons.aria.lookupTable.attributes;
-	});
-
-	afterEach(function() {
-		axe.commons.aria.lookupTable.attributes = orig;
-	});
-
-	it('should return true if attribute is found in lut', function() {
-		axe.commons.aria.lookupTable.attributes = {
-			cats: {}
-		};
-
-		assert.isTrue(axe.commons.aria.validateAttr('cats'));
-	});
-
-	it('should return false if attribute is found in lut', function() {
-		axe.commons.aria.lookupTable.attributes = {};
-
-		assert.isFalse(axe.commons.aria.validateAttr('cats'));
-	});
-});
