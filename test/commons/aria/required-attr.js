7f874437c3d61f5eb82f7c870b9eb6cd55a8eef4
Steven Lambert <steven.lambert@deque.com>
2020-07-15 09:14:48 -0600

    clone array

--- test/commons/aria/required-attr.js
+++ test/commons/aria/required-attr.js
@@ -10,18 +10,48 @@ describe('aria.requiredAttr', function() {
 			standards: {
 				ariaRoles: {
 					cats: {
-						requiredAttrs: 'yes'
+						requiredAttrs: ['yes']
 					}
 				}
 			}
 		});
 
-		assert.equal(axe.commons.aria.requiredAttr('cats'), 'yes');
+		assert.deepEqual(axe.commons.aria.requiredAttr('cats'), ['yes']);
+	});
+
+	it('should returned empty array if the required attributes is not an array', function() {
+		axe.configure({
+			standards: {
+				ariaRoles: {
+					cats: {
+						requiredAttrs: 'yes'
+					}
+				}
+			}
+		});
+		assert.deepEqual(axe.commons.aria.requiredAttr('cats'), []);
 	});
 
 	it('should return an empty array if there are no required attributes', function() {
 		var result = axe.commons.aria.requiredAttr('cats');
 
 		assert.deepEqual(result, []);
+	});
+
+	it('should return a unique copy of the attributes', function() {
+		var attrs = ['yes', 'no'];
+
+		axe.configure({
+			standards: {
+				ariaRoles: {
+					cats: {
+						requiredAttrs: attrs
+					}
+				}
+			}
+		});
+
+		var result = axe.commons.aria.requiredAttr('cats');
+		assert.notEqual(result, attrs);
 	});
 });
