5f758cc78b15b5cda8291ffa00195662ee28e50d
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-07-15 08:41:32 -0600

    chore(is-unsupported-role,is-valid-role): work with standards object (#2376)

--- test/commons/aria/roles.js
+++ test/commons/aria/roles.js
@@ -1,30 +1,3 @@
-describe('aria.isValidRole', function() {
-	'use strict';
-
-	it('should return true if role is found in the lookup table', function() {
-		var orig = axe.commons.aria.lookupTable.role;
-		axe.commons.aria.lookupTable.role = {
-			cats: true
-		};
-		assert.isTrue(axe.commons.aria.isValidRole('cats'));
-		axe.commons.aria.lookupTable.role = orig;
-	});
-
-	it('should return false if role is not found in the lookup table', function() {
-		assert.isFalse(axe.commons.aria.isValidRole('cats'));
-	});
-
-	it('returns false for abstract roles by default', function() {
-		assert.isFalse(axe.commons.aria.isValidRole('input'));
-	});
-
-	it('returns true for abstract roles with { allowAbstract: true } ', function() {
-		assert.isTrue(
-			axe.commons.aria.isValidRole('input', { allowAbstract: true })
-		);
-	});
-});
-
 describe('aria.requiredOwned', function() {
 	'use strict';
 
