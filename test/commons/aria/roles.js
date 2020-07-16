9512df710c9017e4384a2dd952723a2149fae3e2
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-07-16 08:19:19 -0600

    chore(aria-required-parent): work with standards object (#2379)
    * chore(aria-required-parent): work with standards object
    
    * fixes for ie11
    
    * clone array

--- test/commons/aria/roles.js
+++ test/commons/aria/roles.js
@@ -27,35 +27,6 @@ describe('aria.requiredOwned', function() {
 	});
 });
 
-describe('aria.requiredContext', function() {
-	'use strict';
-
-	var orig;
-	beforeEach(function() {
-		orig = axe.commons.aria.lookupTable.role;
-	});
-
-	afterEach(function() {
-		axe.commons.aria.lookupTable.role = orig;
-	});
-
-	it('should returned the context property for the proper role', function() {
-		axe.commons.aria.lookupTable.role = {
-			cats: {
-				context: 'yes'
-			}
-		};
-		assert.equal(axe.commons.aria.requiredContext('cats'), 'yes');
-	});
-
-	it('should return null if there are no required context nodes', function() {
-		axe.commons.aria.lookupTable.role = {};
-		var result = axe.commons.aria.requiredContext('cats');
-
-		assert.isNull(result);
-	});
-});
-
 describe('aria.implicitNodes', function() {
 	'use strict';
 
