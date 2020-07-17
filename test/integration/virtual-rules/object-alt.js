0210634b6a37f8b71706c2b9063bfdb500f35412
Steven Lambert <steven.lambert@deque.com>
2020-07-17 08:09:00 -0600

    fix test

--- test/integration/virtual-rules/object-alt.js
+++ test/integration/virtual-rules/object-alt.js
@@ -71,9 +71,9 @@ describe('object-alt', function() {
 		var child = new axe.SerialVirtualNode({
 			nodeName: '#text',
 			nodeType: 3,
-			textContent: 'foobar'
+			nodeValue: 'foobar'
 		});
-		node.children.push(child);
+		node.children = [child];
 
 		var results = axe.runVirtualRule('object-alt', node);
 
@@ -82,11 +82,24 @@ describe('object-alt', function() {
 		assert.lengthOf(results.incomplete, 0);
 	});
 
-	it('should fail when alt and children are missing', function() {
+	it('should incomplete when alt and children are missing', function() {
 		var results = axe.runVirtualRule('object-alt', {
 			nodeName: 'object',
 			attributes: {}
 		});
+
+		assert.lengthOf(results.passes, 0);
+		assert.lengthOf(results.violations, 0);
+		assert.lengthOf(results.incomplete, 1);
+	});
+
+	it('should fail children contain no visible text', function() {
+		var node = new axe.SerialVirtualNode({
+			nodeName: 'object'
+		});
+		node.children = [];
+
+		var results = axe.runVirtualRule('object-alt', node);
 
 		assert.lengthOf(results.passes, 0);
 		assert.lengthOf(results.violations, 1);
@@ -94,12 +107,15 @@ describe('object-alt', function() {
 	});
 
 	it('should fail when alt contains only whitespace', function() {
-		var results = axe.runVirtualRule('object-alt', {
+		var node = new axe.SerialVirtualNode({
 			nodeName: 'object',
 			attributes: {
 				alt: ' \t   \n   '
 			}
 		});
+		node.children = [];
+
+		var results = axe.runVirtualRule('object-alt', node);
 
 		assert.lengthOf(results.passes, 0);
 		assert.lengthOf(results.violations, 1);
@@ -107,12 +123,15 @@ describe('object-alt', function() {
 	});
 
 	it('should fail when aria-label is empty', function() {
-		var results = axe.runVirtualRule('object-alt', {
+		var node = new axe.SerialVirtualNode({
 			nodeName: 'object',
 			attributes: {
-				'aria-label': ''
+				alt: ''
 			}
 		});
+		node.children = [];
+
+		var results = axe.runVirtualRule('object-alt', node);
 
 		assert.lengthOf(results.passes, 0);
 		assert.lengthOf(results.violations, 1);
@@ -120,28 +139,13 @@ describe('object-alt', function() {
 	});
 
 	it('should fail when title is empty', function() {
-		var results = axe.runVirtualRule('object-alt', {
+		var node = new axe.SerialVirtualNode({
 			nodeName: 'object',
 			attributes: {
 				title: ''
 			}
 		});
-
-		assert.lengthOf(results.passes, 0);
-		assert.lengthOf(results.violations, 1);
-		assert.lengthOf(results.incomplete, 0);
-	});
-
-	it('should fail children contain no visible text', function() {
-		var node = new axe.SerialVirtualNode({
-			nodeName: 'object'
-		});
-		var child = new axe.SerialVirtualNode({
-			nodeName: '#text',
-			nodeType: 3,
-			textContent: ''
-		});
-		node.children.push(child);
+		node.children = [];
 
 		var results = axe.runVirtualRule('object-alt', node);
 
