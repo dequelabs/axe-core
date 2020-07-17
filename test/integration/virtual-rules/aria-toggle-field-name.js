69e64cedcf69f1fbe101b435d057b532caae3b53
Steven Lambert <steven.lambert@deque.com>
2020-07-17 12:33:41 -0600

    tests: add serial tests for button-name, input-button-name, aria-toggle-field-name, and aria-input-field-name

--- test/integration/virtual-rules/aria-toggle-field-name.js
+++ test/integration/virtual-rules/aria-toggle-field-name.js
@@ -1,8 +1,9 @@
-describe('object-alt', function() {
+describe('aria-toggle-field-name', function() {
 	it('should pass for aria-label', function() {
-		var results = axe.runVirtualRule('object-alt', {
-			nodeName: 'object',
+		var results = axe.runVirtualRule('aria-toggle-field-name', {
+			nodeName: 'div',
 			attributes: {
+				role: 'menuitemcheckbox',
 				'aria-label': 'foobar'
 			}
 		});
@@ -13,9 +14,10 @@ describe('object-alt', function() {
 	});
 
 	it('should incomplete for aria-labelledby', function() {
-		var results = axe.runVirtualRule('object-alt', {
-			nodeName: 'object',
+		var results = axe.runVirtualRule('aria-toggle-field-name', {
+			nodeName: 'div',
 			attributes: {
+				role: 'menuitemradio',
 				'aria-labelledby': 'foobar'
 			}
 		});
@@ -26,38 +28,18 @@ describe('object-alt', function() {
 	});
 
 	it('should pass for title', function() {
-		var results = axe.runVirtualRule('object-alt', {
-			nodeName: 'object',
+		var node = new axe.SerialVirtualNode({
+			nodeName: 'div',
 			attributes: {
+				role: 'checkbox',
 				title: 'foobar'
 			}
 		});
+		// children are required since titleText comes after subtree text
+		// in accessible name calculation
+		node.children = [];
 
-		assert.lengthOf(results.passes, 1);
-		assert.lengthOf(results.violations, 0);
-		assert.lengthOf(results.incomplete, 0);
-	});
-
-	it('should pass for role=presentation', function() {
-		var results = axe.runVirtualRule('object-alt', {
-			nodeName: 'object',
-			attributes: {
-				role: 'presentation'
-			}
-		});
-
-		assert.lengthOf(results.passes, 1);
-		assert.lengthOf(results.violations, 0);
-		assert.lengthOf(results.incomplete, 0);
-	});
-
-	it('should pass for role=none', function() {
-		var results = axe.runVirtualRule('object-alt', {
-			nodeName: 'object',
-			attributes: {
-				role: 'none'
-			}
-		});
+		var results = axe.runVirtualRule('aria-toggle-field-name', node);
 
 		assert.lengthOf(results.passes, 1);
 		assert.lengthOf(results.violations, 0);
@@ -66,7 +48,10 @@ describe('object-alt', function() {
 
 	it('should pass for visible text content', function() {
 		var node = new axe.SerialVirtualNode({
-			nodeName: 'object'
+			nodeName: 'div',
+			attributes: {
+				role: 'radio'
+			}
 		});
 		var child = new axe.SerialVirtualNode({
 			nodeName: '#text',
@@ -75,17 +60,19 @@ describe('object-alt', function() {
 		});
 		node.children = [child];
 
-		var results = axe.runVirtualRule('object-alt', node);
+		var results = axe.runVirtualRule('aria-toggle-field-name', node);
 
 		assert.lengthOf(results.passes, 1);
 		assert.lengthOf(results.violations, 0);
 		assert.lengthOf(results.incomplete, 0);
 	});
 
-	it('should incomplete when alt and children are missing', function() {
-		var results = axe.runVirtualRule('object-alt', {
-			nodeName: 'object',
-			attributes: {}
+	it('should incomplete when children are missing', function() {
+		var results = axe.runVirtualRule('aria-toggle-field-name', {
+			nodeName: 'div',
+			attributes: {
+				role: 'switch'
+			}
 		});
 
 		assert.lengthOf(results.passes, 0);
@@ -95,27 +82,31 @@ describe('object-alt', function() {
 
 	it('should fail children contain no visible text', function() {
 		var node = new axe.SerialVirtualNode({
-			nodeName: 'object'
+			nodeName: 'div',
+			attributes: {
+				role: 'menuitemcheckbox'
+			}
 		});
 		node.children = [];
 
-		var results = axe.runVirtualRule('object-alt', node);
+		var results = axe.runVirtualRule('aria-toggle-field-name', node);
 
 		assert.lengthOf(results.passes, 0);
 		assert.lengthOf(results.violations, 1);
 		assert.lengthOf(results.incomplete, 0);
 	});
 
-	it('should fail when alt contains only whitespace', function() {
+	it('should fail when aria-label contains only whitespace', function() {
 		var node = new axe.SerialVirtualNode({
-			nodeName: 'object',
+			nodeName: 'div',
 			attributes: {
-				alt: ' \t   \n   '
+				role: 'menuitemcheckbox',
+				'aria-label': ' \t   \n   '
 			}
 		});
 		node.children = [];
 
-		var results = axe.runVirtualRule('object-alt', node);
+		var results = axe.runVirtualRule('aria-toggle-field-name', node);
 
 		assert.lengthOf(results.passes, 0);
 		assert.lengthOf(results.violations, 1);
@@ -124,14 +115,15 @@ describe('object-alt', function() {
 
 	it('should fail when aria-label is empty', function() {
 		var node = new axe.SerialVirtualNode({
-			nodeName: 'object',
+			nodeName: 'div',
 			attributes: {
+				role: 'menuitemcheckbox',
 				'aria-label': ''
 			}
 		});
 		node.children = [];
 
-		var results = axe.runVirtualRule('object-alt', node);
+		var results = axe.runVirtualRule('aria-toggle-field-name', node);
 
 		assert.lengthOf(results.passes, 0);
 		assert.lengthOf(results.violations, 1);
@@ -140,17 +132,45 @@ describe('object-alt', function() {
 
 	it('should fail when title is empty', function() {
 		var node = new axe.SerialVirtualNode({
-			nodeName: 'object',
+			nodeName: 'div',
 			attributes: {
+				role: 'menuitemcheckbox',
 				title: ''
 			}
 		});
 		node.children = [];
 
-		var results = axe.runVirtualRule('object-alt', node);
+		var results = axe.runVirtualRule('aria-toggle-field-name', node);
 
 		assert.lengthOf(results.passes, 0);
 		assert.lengthOf(results.violations, 1);
 		assert.lengthOf(results.incomplete, 0);
+	});
+
+	it('should incomplete if has explicit and implicit label', function() {
+		var node = new axe.SerialVirtualNode({
+			nodeName: 'div',
+			attributes: {
+				role: 'menuitemcheckbox',
+				'aria-label': 'name'
+			}
+		});
+		var parent = new axe.SerialVirtualNode({
+			nodeName: 'label'
+		});
+		var child = new axe.SerialVirtualNode({
+			nodeName: '#text',
+			nodeType: 3,
+			nodeValue: 'first name'
+		});
+		node.parent = parent;
+		node.children = [];
+		parent.children = [child, node];
+
+		var results = axe.runVirtualRule('aria-toggle-field-name', node);
+
+		assert.lengthOf(results.passes, 0);
+		assert.lengthOf(results.violations, 0);
+		assert.lengthOf(results.incomplete, 1);
 	});
 });
