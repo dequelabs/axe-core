6e028078901bb265ff636a04ff9cc8fe867eec5e
Wilco Fiers <WilcoFiers@users.noreply.github.com>
2020-07-20 12:30:55 +0200

    revert code-highlighting.js

--- test/integration/full/contrast/code-highlighting.js
+++ test/integration/full/contrast/code-highlighting.js
@@ -1,18 +1,5 @@
 describe('color-contrast code highlighting test', function() {
 	'use strict';
-	var results;
-
-	before(function(done) {
-		var options = { runOnly: { type: 'rule', values: ['color-contrast'] } };
-		axe.run('#fixture', options, function(err, res) {
-			if (err) {
-				done(err);
-			} else {
-				results = res;
-				done();
-			}
-		});
-	});
 
 	before(function(done) {
 		// wait for window load event (or if the window has already loaded) so the
@@ -28,26 +15,50 @@ describe('color-contrast code highlighting test', function() {
 	});
 
 	describe('violations', function() {
-		it('should find issues', function() {
-			assert.lengthOf(results.violations, 1);
-			assert.lengthOf(results.violations[0].nodes, 32);
+		it('should find issues', function(done) {
+			axe.run(
+				'#fixture',
+				{ runOnly: { type: 'rule', values: ['color-contrast'] } },
+				function(err, results) {
+					assert.isNull(err);
+					assert.lengthOf(results.violations, 1);
+					assert.lengthOf(results.violations[0].nodes, 32);
+					done();
+				}
+			);
 		});
 	});
 
 	describe('passes', function() {
-		it('should find passes', function() {
-			assert.lengthOf(results.passes, 1);
-			assert.lengthOf(results.passes[0].nodes, 27);
+		it('should find passes', function(done) {
+			axe.run(
+				'#fixture',
+				{ runOnly: { type: 'rule', values: ['color-contrast'] } },
+				function(err, results) {
+					assert.isNull(err);
+					assert.lengthOf(results.passes, 1);
+					assert.lengthOf(results.passes[0].nodes, 27);
+					done();
+				}
+			);
 		});
 	});
 
 	describe('incomplete', function() {
-		it('should find just the code block', function() {
-			assert.lengthOf(results.incomplete, 1);
-			assert.lengthOf(results.incomplete[0].nodes, 1);
-			assert.equal(
-				results.incomplete[0].nodes[0].html,
-				'<code class=" language-html">'
+		it('should find just the code block', function(done) {
+			axe.run(
+				'#fixture',
+				{ runOnly: { type: 'rule', values: ['color-contrast'] } },
+				function(err, results) {
+					assert.isNull(err);
+					assert.lengthOf(results.incomplete, 1);
+					assert.lengthOf(results.incomplete[0].nodes, 1);
+					assert.equal(
+						results.incomplete[0].nodes[0].html,
+						'<code class=" language-html">'
+					);
+					done();
+				}
 			);
 		});
 	});
