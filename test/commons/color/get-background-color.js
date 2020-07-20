3eb6d2ccde9dc731450c90ebe497580cb98de201
Wilco Fiers <WilcoFiers@users.noreply.github.com>
2020-07-20 15:48:17 +0200

    fix(color-contrast): account for text-shadow (#2334)
    * fix(color-contrast): account for text-shadow
    
    * chore: IE text-shadow issues
    
    * chore: more IE fixes
    
    * chore: more IE tests
    
    * chore: fix failing tests
    
    * chore: fix IE stuff
    
    * chore: ci fix
    
    * Update lib/commons/color/get-text-shadow-colors.js
    
    Co-authored-by: Steven Lambert <2433219+straker@users.noreply.github.com>
    
    * chore: support hsl colors
    
    * Apply suggestions from code review
    
    Co-authored-by: Steven Lambert <2433219+straker@users.noreply.github.com>
    
    * revert code-highlighting.js
    
    Co-authored-by: Steven Lambert <2433219+straker@users.noreply.github.com>

--- test/commons/color/get-background-color.js
+++ test/commons/color/get-background-color.js
@@ -974,4 +974,23 @@ describe('color.getBackgroundColor', function() {
 			assert.equal(actual.alpha, 1);
 		}
 	);
+
+	it('should return the text-shadow mixed in with the background', function() {
+		fixture.innerHTML =
+			'<div id="parent" style="height: 40px; width: 30px; background-color: #800000;">' +
+			'<div id="target" style="height: 20px; width: 15px; text-shadow: red 0 0 2px">foo' +
+			'</div></div>';
+		var target = fixture.querySelector('#target');
+		var parent = fixture.querySelector('#parent');
+		var bgNodes = [];
+		axe.testUtils.flatTreeSetup(fixture);
+		var actual = axe.commons.color.getBackgroundColor(target, bgNodes);
+		// is 128 without the shadow
+		var expected = new axe.commons.color.Color(175, 0, 0, 1);
+		assert.closeTo(actual.red, expected.red, 0.5);
+		assert.closeTo(actual.green, expected.green, 0.5);
+		assert.closeTo(actual.blue, expected.blue, 0.5);
+		assert.closeTo(actual.alpha, expected.alpha, 0.1);
+		assert.deepEqual(bgNodes, [parent]);
+	});
 });
