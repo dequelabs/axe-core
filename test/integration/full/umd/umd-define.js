401094e2372cc02d4f6cf37d9bb8731aaf51b652
Steven Lambert <steven.lambert@deque.com>
2020-07-24 11:30:40 -0600

    fix tests

--- test/integration/full/umd/umd-define.js
+++ test/integration/full/umd/umd-define.js
@@ -9,7 +9,13 @@ describe('UMD define', function() {
 	it('calls define and passes it axe', function() {
 		var call = defineCalls[defineCalls.length - 1];
 		assert.isFunction(call[2]);
-		assert.strictEqual(call[2](), axe);
+
+		var axe = call[2]();
+
+		// test some public apis
+		assert.typeOf(axe.run, 'function');
+		assert.typeOf(axe.configure, 'function');
+		assert.typeOf(axe.reset, 'function');
 	});
 
 	it('defines module name as axe-core', function() {
