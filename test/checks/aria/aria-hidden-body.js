d4a958a8410387e5bded7ed8a29eebf6f32f0395
ShankarShastri <shastri.shankar9@gmail.com>
2020-08-27 00:23:50 +0530

    Use flatTreeSetup for setting the vNode

--- test/checks/aria/aria-hidden-body.js
+++ test/checks/aria/aria-hidden-body.js
@@ -2,36 +2,38 @@ describe('aria-hidden', function() {
 	'use strict';
 
 	var checkContext = axe.testUtils.MockCheckContext();
-	var queryFixture = axe.testUtils.queryFixture;
-
+	var body = document.body;
 	afterEach(function() {
 		checkContext.reset();
+		body.removeAttribute('aria-hidden');
 	});
 
 	it('should not be present on document.body', function() {
-		var vNode = queryFixture('<body id="target"></body>');
+		var tree = axe.testUtils.flatTreeSetup(body);
 		assert.isTrue(
 			axe.testUtils
 				.getCheckEvaluate('aria-hidden-body')
-				.call(checkContext, null, {}, vNode)
+				.call(checkContext, null, {}, tree[0])
 		);
 	});
 
 	it('fails appropriately if aria-hidden=true on document.body', function() {
-		var vNode = queryFixture('<body id="target" aria-hidden="true"></body>');
+		body.setAttribute('aria-hidden', true);
+		var tree = axe.testUtils.flatTreeSetup(body);
 		assert.isFalse(
 			axe.testUtils
 				.getCheckEvaluate('aria-hidden-body')
-				.call(checkContext, null, {}, vNode)
+				.call(checkContext, null, {}, tree[0])
 		);
 	});
 
 	it('passes if aria-hidden=false on document.body', function() {
-		var vNode = queryFixture('<body id="target" aria-hidden="false"></body>');
+		body.setAttribute('aria-hidden', 'false');
+		var tree = axe.testUtils.flatTreeSetup(body);
 		assert.isTrue(
 			axe.testUtils
 				.getCheckEvaluate('aria-hidden-body')
-				.call(checkContext, null, {}, vNode)
+				.call(checkContext, null, {}, tree[0])
 		);
 	});
 });
