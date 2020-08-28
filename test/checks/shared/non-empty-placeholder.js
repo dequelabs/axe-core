5f822f4f32ce5597e78dd7db77b8412aea1154a5
Xiaoyi Chen <xchen@salesforce.com>
2020-08-28 10:50:09 -0700

    fix(attr-non-space-content-evaluate): Split no attribute and empty attribute message (#2495)

--- test/checks/shared/non-empty-placeholder.js
+++ test/checks/shared/non-empty-placeholder.js
@@ -4,27 +4,31 @@ describe('non-empty-placeholder', function() {
 	var fixture = document.getElementById('fixture');
 	var checkSetup = axe.testUtils.checkSetup;
 	var checkEvaluate = axe.testUtils.getCheckEvaluate('non-empty-placeholder');
+	var checkContext = axe.testUtils.MockCheckContext();
 
 	afterEach(function() {
 		fixture.innerHTML = '';
+		checkContext.reset();
 	});
 
 	it('should return true if a placeholder is present', function() {
 		var params = checkSetup('<input id="target" placeholder="woohoo" />');
 
-		assert.isTrue(checkEvaluate.apply(null, params));
+		assert.isTrue(checkEvaluate.apply(checkContext, params));
 	});
 
 	it('should return false if a placeholder is not present', function() {
 		var params = checkSetup('<input id="target" />');
 
-		assert.isFalse(checkEvaluate.apply(null, params));
+		assert.isFalse(checkEvaluate.apply(checkContext, params));
+		assert.equal(checkContext._data.messageKey, 'noAttr');
 	});
 
 	it('should return false if a placeholder is present, but empty', function() {
 		var params = checkSetup('<input id="target" placeholder=" " />');
 
-		assert.isFalse(checkEvaluate.apply(null, params));
+		assert.isFalse(checkEvaluate.apply(checkContext, params));
+		assert.equal(checkContext._data.messageKey, 'emptyAttr');
 	});
 
 	it('should collapse whitespace', function() {
@@ -32,6 +36,7 @@ describe('non-empty-placeholder', function() {
 			'<input id="target" placeholder=" \t \n \r \t  \t\r\n " />'
 		);
 
-		assert.isFalse(checkEvaluate.apply(null, params));
+		assert.isFalse(checkEvaluate.apply(checkContext, params));
+		assert.equal(checkContext._data.messageKey, 'emptyAttr');
 	});
 });
