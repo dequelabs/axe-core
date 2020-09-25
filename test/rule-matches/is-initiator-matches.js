8303ba3157b72f1f8f25c975ac55d166cdc06c8d
Steven Lambert <steven.lambert@deque.com>
2020-09-25 10:47:44 -0600

    is-initiator-matches

---
+++ test/rule-matches/is-initiator-matches.js
@@ -0,0 +1,24 @@
+describe('is-initiator-matches', function() {
+	'use strict';
+
+	var rule;
+
+	beforeEach(function() {
+		rule = axe._audit.rules.find(function(rule) {
+			return rule.id === 'html-has-lang';
+		});
+	});
+
+	afterEach(function() {
+		var fixture = document.getElementById('fixture');
+		fixture.innerHTML = '';
+	});
+
+	it('should return true if the context is the initiator', function() {
+		assert.isTrue(rule.matches(null, null, { initiator: true }));
+	});
+
+	it('should return false if the context is not the initiator', function() {
+		assert.isFalse(rule.matches(null, null, { initiator: false }));
+	});
+});
