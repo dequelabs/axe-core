3585b01d497f5fa7aed52baf9207bbf368613fae
Thomas Braccia <bracciata@gmail.com>
2020-10-01 19:43:42 -0500

    Added the tests for brand and application.

--- test/core/base/audit.js
+++ test/core/base/audit.js
@@ -394,6 +394,33 @@ describe('Audit', function() {
 			audit.resetRulesAndChecks();
 			assert.equal(audit.lang, 'en');
 		});
+		it('should reset brand', function() {
+			var audit = new Audit();
+			assert.equal(audit.brand, 'axe');
+			audit.setBranding({
+				brand: 'test'
+			});
+			assert.equal(audit.brand, 'test');
+			audit.resetRulesAndChecks();
+			assert.equal(audit.brand, 'axe');
+		});
+		it('should reset brand application', function() {
+			var audit = new Audit();
+			assert.equal(audit.application, 'axeAPI');
+			audit.setBranding({
+				application: 'test'
+			});
+			assert.equal(audit.application, 'test');
+			audit.resetRulesAndChecks();
+			assert.equal(audit.application, 'axeAPI');
+		});
+		it('should reset brand tagExlcude', function() {
+			var audit = new Audit();
+			assert.equal(audit.tagExclude, ['experimental']);
+			// TODO: Find where tagExclude is set
+			audit.resetRulesAndChecks();
+			assert.equal(audit.application, ['experimental']);
+		});
 	});
 
 	describe('Audit#addCheck', function() {
