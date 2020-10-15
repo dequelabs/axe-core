3f02d14cbe3562e22762664843f8852f779be2a3
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-10-15 16:30:01 -0600

    fix(get-headers): fix for rowspan and colspan (#2545)
    * fix(get-headers): fix for rowspan and colspan
    
    * fix for invalid row/colspan values

--- test/commons/table/get-headers.js
+++ test/commons/table/get-headers.js
@@ -174,4 +174,88 @@ describe('table.getHeaders', function() {
 		axe.testUtils.flatTreeSetup(fixture.firstChild);
 		assert.deepEqual(axe.commons.table.getHeaders(target), []);
 	});
+
+	it('should handle multiple headers with colspan', function() {
+		fixture.innerHTML =
+			'<table>' +
+			'<thead>' +
+			'<tr>' +
+			'<th id="t1">Product</th>' +
+			'<th id="t2">Amount</th>' +
+			'<th id="t3">Price</th>' +
+			'</tr>' +
+			'</thead>' +
+			'<tbody>' +
+			'<tr><td colspan="2" id="target">No available products to display</td></tr>' +
+			'</thead>' +
+			'</table>';
+
+		var target = $id('target');
+
+		axe.testUtils.flatTreeSetup(fixture.firstChild);
+		assert.deepEqual(axe.commons.table.getHeaders(target), [
+			$id('t1'),
+			$id('t2')
+		]);
+	});
+
+	it('should handle multiple headers with rowspan', function() {
+		fixture.innerHTML =
+			'<table>' +
+			'<tbody>' +
+			'<tr>' +
+			'<th id="t1"><td>Projects</td><td rowspan="2" id="target">Foo</td>' +
+			'</tr>' +
+			'<tr><th id="t2"><td>Projects</td></tr>' +
+			'<tr><th id="t3"><td>Projects</td></tr>' +
+			'</thead>' +
+			'</table>';
+
+		var target = $id('target');
+
+		axe.testUtils.flatTreeSetup(fixture.firstChild);
+		assert.deepEqual(axe.commons.table.getHeaders(target), [
+			$id('t1'),
+			$id('t2')
+		]);
+	});
+
+	it('should handle negative colspan', function() {
+		fixture.innerHTML =
+			'<table>' +
+			'<thead>' +
+			'<tr>' +
+			'<th id="t1">Product</th>' +
+			'<th id="t2">Amount</th>' +
+			'<th id="t3">Price</th>' +
+			'</tr>' +
+			'</thead>' +
+			'<tbody>' +
+			'<tr><td colspan="-3" id="target">No available products to display</td></tr>' +
+			'</thead>' +
+			'</table>';
+
+		var target = $id('target');
+
+		axe.testUtils.flatTreeSetup(fixture.firstChild);
+		assert.deepEqual(axe.commons.table.getHeaders(target), [$id('t1')]);
+	});
+
+	it('should handle negative rowspan', function() {
+		fixture.innerHTML =
+			'<table>' +
+			'<tbody>' +
+			'<tr>' +
+			'<th id="t1"><td>Projects</td><td rowspan="-3" id="target">Foo</td>' +
+			'</tr>' +
+			'<tr><th id="t2"><td>Projects</td></tr>' +
+			'<tr><th id="t3"><td>Projects</td></tr>' +
+			'</thead>' +
+			'</table>';
+
+		var target = $id('target');
+
+		axe.testUtils.flatTreeSetup(fixture.firstChild);
+		assert.deepEqual(axe.commons.table.getHeaders(target), [$id('t1')]);
+	});
 });
