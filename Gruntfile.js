476eb74f84a30b5da8360a6992dd2be0aab8b132
Steven Lambert <steven.lambert@deque.com>
2020-10-21 11:24:42 -0600

    fix merge

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,18 +133,6 @@ module.exports = function(grunt) {
 				]
 			}
 		},
-		esbuild: {
-			core: {
-				files: [
-					{
-						expand: true,
-						cwd: 'lib/core',
-						src: ['core.js'],
-						dest: 'tmp/core'
-					}
-				]
-			}
-		},
 		'aria-supported': {
 			data: {
 				entry: 'lib/commons/aria/index.js',
