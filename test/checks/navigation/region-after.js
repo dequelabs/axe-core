8099c665e74f45c3e3de67696ada7137c1027656
Steven Lambert <steven.lambert@deque.com>
2020-10-29 11:31:41 -0600

    fix(region): dont fail contents of iframe if iframe is in a region

---
+++ test/checks/navigation/region-after.js
@@ -0,0 +1,67 @@
+describe('region-after', function() {
+	'use strict';
+
+	function mockResult(result, element, fromFrame) {
+		return {
+			result: result,
+			node: {
+				_element: element,
+				_fromFrame: fromFrame || false
+			}
+		};
+	}
+
+	var queryFixture = axe.testUtils.queryFixture;
+	var checkContext = axe.testUtils.MockCheckContext();
+	var cache = axe._cache;
+	afterEach(function() {
+		axe._tree = undefined;
+		checkContext.reset();
+		cache.clear();
+	});
+
+	it('should pass the content of an iframe that is within a region', function() {
+		var vNode = queryFixture('<iframe id="target"></iframe>');
+		cache.set('regionlessNodes', []);
+		var results = checks.region.after([
+			mockResult(true, vNode.actualNode), // iframe
+			mockResult(false, vNode.actualNode, true) // iframe content
+		]);
+
+		assert.isTrue(results[1].result);
+	});
+
+	it('should pass the iframe if all its content is within a region', function() {
+		var vNode = queryFixture('<iframe id="target"></iframe>');
+		cache.set('regionlessNodes', [vNode]);
+		var results = checks.region.after([
+			mockResult(false, vNode.actualNode), // iframe
+			mockResult(true, vNode.actualNode, true) // iframe content
+		]);
+
+		assert.isTrue(results[0].result);
+	});
+
+	it('should pass the iframe if it has mixed content', function() {
+		var vNode = queryFixture('<iframe id="target"></iframe>');
+		cache.set('regionlessNodes', [vNode]);
+		var results = checks.region.after([
+			mockResult(false, vNode.actualNode), // iframe
+			mockResult(false, vNode.actualNode, true), // iframe content
+			mockResult(true, vNode.actualNode, true) // iframe content
+		]);
+
+		assert.isTrue(results[0].result);
+	});
+
+	it('should fail the iframe if all its content is outside of a region', function() {
+		var vNode = queryFixture('<iframe id="target"></iframe>');
+		cache.set('regionlessNodes', [vNode]);
+		var results = checks.region.after([
+			mockResult(false, vNode.actualNode), // iframe
+			mockResult(false, vNode.actualNode, true) // iframe content
+		]);
+
+		assert.isFalse(results[0].result);
+	});
+});
--- test/integration/full/region/region-fail.js
+++ test/integration/full/region/region-fail.js
@@ -2,23 +2,36 @@ describe('region fail test', function() {
 	'use strict';
 	var results;
 	before(function(done) {
-		axe.run({ runOnly: { type: 'rule', values: ['region'] } }, function(
-			err,
-			r
-		) {
-			assert.isNull(err);
-			results = r;
-			done();
+		axe.testUtils.awaitNestedLoad(function() {
+			axe.run({ runOnly: { type: 'rule', values: ['region'] } }, function(
+				err,
+				r
+			) {
+				assert.isNull(err);
+				results = r;
+				done();
+			});
 		});
 	});
 
 	describe('violations', function() {
-		it('should find one', function() {
-			assert.lengthOf(results.violations[0].nodes, 1);
+		it('should find three', function() {
+			assert.lengthOf(results.violations[0].nodes, 3);
 		});
 
 		it('should find wrapper', function() {
 			assert.deepEqual(results.violations[0].nodes[0].target, ['#wrapper']);
+		});
+
+		it('should find iframe without a region', function() {
+			assert.deepEqual(results.violations[0].nodes[1].target, ['#no-region']);
+		});
+
+		it('should find failed iframe content', function() {
+			assert.deepEqual(results.violations[0].nodes[2].target, [
+				'#partial-region',
+				'#outside'
+			]);
 		});
 	});
 });
