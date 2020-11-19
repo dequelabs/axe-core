0cf9fd59dbe2b59308946aea7f448509cc06f2d1
michael-siek <michaelsiek094@gmail.com>
2020-11-19 12:14:00 -0500

    remove axios dep

--- test/integration/full/umd/umd-window.js
+++ test/integration/full/umd/umd-window.js
@@ -49,16 +49,4 @@ describe('UMD window', function() {
 	it('should ensure axe has prototype chained keys', function() {
 		assert.hasAnyKeys(axe, ['utils', 'commons', 'core']);
 	});
-
-	it('should expose not expose axios as a property of window', function() {
-		assert.notProperty(window, 'axios');
-	});
-
-	it('should ensure axios is a mounted to axe.imports', function() {
-		assert.hasAnyKeys(axe.imports, ['axios']);
-	});
-
-	it('should ensure axios has prototype chained keys', function() {
-		assert.hasAnyKeys(axe.imports.axios, ['get', 'request', 'options', 'post']);
-	});
 });
