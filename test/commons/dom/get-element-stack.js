a4e4a344cea70308d0a59a04411f6da88b80b00b
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-12-18 10:29:54 -0700

    fix(color-contrast): account for text client rects that start outside the parent container (#2682)
    * fix(color-contrast): account for text client rects that start otuside the parent container
    
    * fix?
    
    * Update lib/commons/dom/get-text-element-stack.js
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>

--- test/commons/dom/get-element-stack.js
+++ test/commons/dom/get-element-stack.js
@@ -3,7 +3,6 @@ describe('dom.getElementStack', function() {
 
   var fixture = document.getElementById('fixture');
   var getElementStack = axe.commons.dom.getElementStack;
-  var getTextElementStack = axe.commons.dom.getTextElementStack;
   var isIE11 = axe.testUtils.isIE11;
   var shadowSupported = axe.testUtils.shadowSupport.v1;
 
@@ -532,62 +531,6 @@ describe('dom.getElementStack', function() {
       var target = fixture.querySelector('#target');
       var stack = mapToIDs(getElementStack(target));
       assert.deepEqual(stack, ['target', '3', '2', '1', 'fixture']);
-    });
-  });
-
-  describe('dom.getTextElementStack', function() {
-    it('should return array of client text rects', function() {
-      fixture.innerHTML =
-        '<main id="1">' +
-        '<div id="target">' +
-        '<span id="2">Hello</span><br/>World' +
-        '</div>' +
-        '</main>';
-      axe.testUtils.flatTreeSetup(fixture);
-      var target = fixture.querySelector('#target');
-      var stacks = getTextElementStack(target).map(mapToIDs);
-      assert.deepEqual(stacks, [['target', '1', 'fixture']]);
-    });
-
-    it('should ignore newline characters', function() {
-      fixture.innerHTML =
-        '<main id="1">' +
-        '<div id="target">' +
-        '<span id="2">Hello</span><br/>\n' +
-        'World' +
-        '</div>' +
-        '</main>';
-      axe.testUtils.flatTreeSetup(fixture);
-      var target = fixture.querySelector('#target');
-      var stacks = getTextElementStack(target).map(mapToIDs);
-      assert.deepEqual(stacks, [['target', '1', 'fixture']]);
-    });
-
-    it('should handle truncated text', function() {
-      fixture.innerHTML =
-        '<main id="1">' +
-        '<div id="target" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap; width: 100px;">' +
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et sollicitudin quam. Fusce mi odio, egestas pulvinar erat eget, vehicula tempus est. Proin vitae ullamcorper velit. Donec sagittis est justo, mattis iaculis arcu facilisis id. Proin pulvinar ornare arcu a fermentum. Quisque et dignissim nulla, sit amet consectetur ipsum. Donec in libero porttitor, dapibus neque imperdiet, aliquam est. Vivamus blandit volutpat fringilla. In mi magna, mollis sit amet imperdiet eu, rutrum ut tellus. Mauris vel condimentum nibh, quis ultricies nisi. Vivamus accumsan quam mauris, id iaculis quam fringilla ac. Curabitur pulvinar dolor ac magna vehicula, non auctor ligula dignissim. Nam ac nibh porttitor, malesuada tortor varius, feugiat turpis. Mauris dapibus, tellus ut viverra porta, ipsum turpis bibendum ligula, at tempor felis ante non libero. Donec dapibus, diam sit amet posuere commodo, magna orci hendrerit ipsum, eu egestas mauris nulla ut ipsum. Sed luctus, orci in fringilla finibus, odio leo porta dolor, eu dignissim risus eros eget erat.' +
-        'World' +
-        '</div>' +
-        '</main>';
-      axe.testUtils.flatTreeSetup(fixture);
-      var target = fixture.querySelector('#target');
-      var stacks = getTextElementStack(target).map(mapToIDs);
-      assert.deepEqual(stacks, [['target', '1', 'fixture']]);
-    });
-
-    it('should handle text that is too large for the container', function() {
-      fixture.innerHTML =
-        '<pre id="1" style="width: 400px; overflow: auto;">' +
-        '<span id="target" style="display: flex; with: 400px;">\n\n' +
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et sollicitudin quam. Fusce mi odio, egestas pulvinar erat eget, vehicula tempus est. Proin vitae ullamcorper velit. Donec sagittis est justo, mattis iaculis arcu facilisis id. Proin pulvinar ornare arcu a fermentum. Quisque et dignissim nulla, sit amet consectetur ipsum. Donec in libero porttitor, dapibus neque imperdiet, aliquam est. Vivamus blandit volutpat fringilla. In mi magna, mollis sit amet imperdiet eu, rutrum ut tellus. Mauris vel condimentum nibh, quis ultricies nisi. Vivamus accumsan quam mauris, id iaculis quam fringilla ac. Curabitur pulvinar dolor ac magna vehicula, non auctor ligula dignissim. Nam ac nibh porttitor, malesuada tortor varius, feugiat turpis. Mauris dapibus, tellus ut viverra porta, ipsum turpis bibendum ligula, at tempor felis ante non libero. Donec dapibus, diam sit amet posuere commodo, magna orci hendrerit ipsum, eu egestas mauris nulla ut ipsum. Sed luctus, orci in fringilla finibus, odio leo porta dolor, eu dignissim risus eros eget erat.' +
-        '</span>' +
-        '</pre>';
-      axe.testUtils.flatTreeSetup(fixture);
-      var target = fixture.querySelector('#target');
-      var stacks = getTextElementStack(target).map(mapToIDs);
-      assert.deepEqual(stacks, [['target', '1', 'fixture']]);
     });
   });
 });
