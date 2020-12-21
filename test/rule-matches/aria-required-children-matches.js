16a06e35bce02f42e5d5234983088078c793843c
Steven Lambert <steven.lambert@deque.com>
2020-12-21 09:41:38 -0700

    fix(aria-required-children): only match for roles that require children

---
+++ test/rule-matches/aria-required-children-matches.js
@@ -0,0 +1,27 @@
+describe('aria-required-children-matches', function() {
+  'use strict';
+
+  var fixture = document.getElementById('fixture');
+  var queryFixture = axe.testUtils.queryFixture;
+  var rule;
+
+  beforeEach(function() {
+    rule = axe._audit.rules.find(function(rule) {
+      return rule.id === 'aria-required-children';
+    });
+  });
+
+  afterEach(function() {
+    fixture.innerHTML = '';
+  });
+
+  it('should return true for a role that requires children', function() {
+    var vNode = queryFixture('<div id="target" role="list"></div>');
+    assert.isTrue(rule.matches(null, vNode));
+  });
+
+  it('should return false for a role that does not require children', function() {
+    var vNode = queryFixture('<div id="target" role="alert"></div>');
+    assert.isFalse(rule.matches(null, vNode));
+  });
+});
