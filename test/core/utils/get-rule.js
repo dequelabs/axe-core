1dcb5bcd9c81680ba9786dd6940895a48e841a03
Steven Lambert <steven.lambert@deque.com>
2020-12-24 11:28:27 -0700

    feat(utils.getRule): add function to get rule by id

---
+++ test/core/utils/get-rule.js
@@ -0,0 +1,24 @@
+describe('axe.utils.getRule', function() {
+  beforeEach(function() {
+    axe._load({
+      rules: [
+        {
+          id: 'rule1'
+        },
+        {
+          id: 'rule2'
+        }
+      ]
+    });
+  });
+
+  it('should return the rule by the id', function() {
+    var rule = axe.utils.getRule('rule1');
+    assert.isTrue(rule.id === 'rule1');
+  });
+
+  it("should return undefined if the rule doesn't exist", function() {
+    var rule = axe.utils.getRule('no-id');
+    assert.isUndefined(rule);
+  });
+});
