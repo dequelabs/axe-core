ce8281e6d45c6888d238ea33c2d39f4c67e8b267
Steven Lambert <2433219+straker@users.noreply.github.com>
2021-01-04 08:23:53 -0700

    fix(aria-required-parent): only match for roles that require parents (#2707)
    * fix(aria-required-parent): only match for roles that require parents
    
    * fix

---
+++ test/rule-matches/aria-required-parent-matches.js
@@ -0,0 +1,27 @@
+describe('aria-required-parent-matches', function() {
+  'use strict';
+
+  var fixture = document.getElementById('fixture');
+  var queryFixture = axe.testUtils.queryFixture;
+  var rule;
+
+  beforeEach(function() {
+    rule = axe._audit.rules.find(function(rule) {
+      return rule.id === 'aria-required-parent';
+    });
+  });
+
+  afterEach(function() {
+    fixture.innerHTML = '';
+  });
+
+  it('should return true for a role that requires parent', function() {
+    var vNode = queryFixture('<div id="target" role="listitem"></div>');
+    assert.isTrue(rule.matches(null, vNode));
+  });
+
+  it('should return false for a role that does not require parent', function() {
+    var vNode = queryFixture('<div id="target" role="alert"></div>');
+    assert.isFalse(rule.matches(null, vNode));
+  });
+});
