4b299fb2652efdd8735ec6d20f517e0192ef1446
Steven Lambert <steven.lambert@deque.com>
2021-01-07 14:01:27 -0700

    feat(setup/teardown): add functions to setup and teardown axe-core internal data

---
+++ test/core/public/setup.js
@@ -0,0 +1,29 @@
+describe('axe.setup', function() {
+  'use strict';
+
+  afterEach(function() {
+    axe.teardown();
+  });
+
+  it('should setup the tree', function() {
+    axe._tree = undefined;
+    axe.setup();
+    assert.exists(axe._tree);
+  });
+
+  it('should default the tree to use html element', function() {
+    axe.setup();
+    assert.equal(axe._tree[0].actualNode, document.documentElement);
+  });
+
+  it('should use the passed in node as the root of the tree', function() {
+    axe.setup(document.body);
+    assert.equal(axe._tree[0].actualNode, document.body);
+  });
+
+  it('should setup selector data', function() {
+    axe._selectorData = undefined;
+    axe.setup();
+    assert.exists(axe._selectorData);
+  });
+});
