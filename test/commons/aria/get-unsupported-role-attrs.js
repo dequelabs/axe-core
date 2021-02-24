453dbe140f3586c18e8073a2dfae83ec44fd64dd
Steven Lambert <steven.lambert@deque.com>
2021-02-24 10:27:38 -0700

    fix for radiogroup

--- test/commons/aria/get-unsupported-role-attrs.js
+++ test/commons/aria/get-unsupported-role-attrs.js
@@ -20,30 +20,30 @@ describe('aria.getUnsupportedRoleAttrs', function() {
     assert.deepEqual(getUnsupportedRoleAttrs(vNode), ['aria-selected']);
   });
 
-  it('should return "aria-required" for a fieldset with aria-required=true but no descendants are required', function() {
+  it('should return "aria-required" for a radiogroup with aria-required=true but no descendants are required', function() {
     var vNode = queryFixture(
-      '<fieldset id="target" aria-required="true"><input type="radio"></fieldset>'
+      '<fieldset id="target" role="radiogroup" aria-required="true"><input type="radio"></fieldset>'
     );
     assert.deepEqual(getUnsupportedRoleAttrs(vNode), ['aria-required']);
   });
 
-  it('should return empty array for a fieldset with aria-required=false', function() {
+  it('should return empty array for a radiogroup with aria-required=false', function() {
     var vNode = queryFixture(
-      '<fieldset id="target" aria-required="false"><input type="radio"></fieldset>'
+      '<fieldset id="target" role="radiogroup" aria-required="false"><input type="radio"></fieldset>'
     );
     assert.deepEqual(getUnsupportedRoleAttrs(vNode), []);
   });
 
-  it('should empty array for a fieldset with aria-required=true with required descendant', function() {
+  it('should empty array for a radiogroup with aria-required=true with required descendant', function() {
     var vNode = queryFixture(
-      '<fieldset id="target" aria-required="true"><input type="radio" required></fieldset>'
+      '<fieldset id="target" role="radiogroup" aria-required="true"><input type="radio" required></fieldset>'
     );
     assert.deepEqual(getUnsupportedRoleAttrs(vNode), []);
   });
 
-  it('should empty array for a fieldset with aria-required=true with aria-required descendant', function() {
+  it('should empty array for a radiogroup with aria-required=true with aria-required descendant', function() {
     var vNode = queryFixture(
-      '<fieldset id="target" aria-required="true"><input type="radio" aria-required="true"></fieldset>'
+      '<fieldset id="target" role="radiogroup" aria-required="true"><input type="radio" aria-required="true"></fieldset>'
     );
     assert.deepEqual(getUnsupportedRoleAttrs(vNode), []);
   });
