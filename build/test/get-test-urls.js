2b5bb2d1f990644d731109f37a921cc3bba449c1
Daniel Weck <daniel.weck@gmail.com>
2021-03-10 18:26:17 +0000

    intermediary commit (Axe hook seems to stash things in the working copy ... so I need to test a full commit)

--- build/test/get-test-urls.js
+++ build/test/get-test-urls.js
@@ -2,33 +2,35 @@ const globby = require('globby');
 
 const getTestUrls = async (host = `localhost`, port = `9876`) => {
 	const urls = [
-		/**
-		 * Unit tests -> Core
-		 */
-		`http://${host}:${port}/test/core/`,
-		/**
-		 * Unit tests -> Checks
-		 */
-		`http://${host}:${port}/test/checks/`,
-		/**
-		 * Unit tests -> Matches
-		 */
-		`http://${host}:${port}/test/rule-matches/`,
-		/**
-		 * Unit tests -> Commons
-		 */
-		`http://${host}:${port}/test/commons/`,
-		/**
-		 * Integration tests -> rules
-		 */
-		`http://${host}:${port}/test/integration/rules`,
+		// /**
+		//  * Unit tests -> Core
+		//  */
+		// `http://${host}:${port}/test/core/`,
+		// /**
+		//  * Unit tests -> Checks
+		//  */
+		// `http://${host}:${port}/test/checks/`,
+		// /**
+		//  * Unit tests -> Matches
+		//  */
+		// `http://${host}:${port}/test/rule-matches/`,
+		// /**
+		//  * Unit tests -> Commons
+		//  */
+		// `http://${host}:${port}/test/commons/`,
+		// /**
+		//  * Integration tests -> rules
+		//  */
+		// `http://${host}:${port}/test/integration/rules`,
 		/**
 		 * Integration tests -> full
 		 */
 		...(
 			await globby([
-				'test/integration/full/**/*.html',
-				'!test/integration/full/**/frames/**/*.html'
+				'test/integration/full/landmark-one-main/**/*.html',
+				'!test/integration/full/landmark-one-main/**/frames/**/*.html'
+				// 'test/integration/full/**/*.html',
+				// '!test/integration/full/**/frames/**/*.html'
 			])
 		).map(file => `http://${host}:${port}/${file}`)
 	];
