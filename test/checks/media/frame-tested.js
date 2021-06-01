a3e304494722ca4bc6029f3bd35e682f4f97e4bb
Steven Lambert <steven.lambert@deque.com>
2021-06-01 15:29:46 -0600

    add after tests

--- test/checks/media/frame-tested.js
+++ test/checks/media/frame-tested.js
@@ -2,12 +2,91 @@ describe('frame-tested', function() {
   'use strict';
 
   var checkEvaluate = axe.testUtils.getCheckEvaluate('frame-tested');
+  var frameTestedAfter = checks['frame-tested'].after;
 
-  it('returns undefined', function() {
-    assert.isUndefined(checkEvaluate());
+  describe('evaluate', function() {
+    it('returns undefined', function() {
+      assert.isUndefined(checkEvaluate());
+    });
+
+    it('returns false if passed isViolation:true', function() {
+      assert.isFalse(checkEvaluate(null, { isViolation: true }));
+    });
   });
 
-  it('returns false if passed isViolation:true', function() {
-    assert.isFalse(checkEvaluate(null, { isViolation: true }));
+  describe('after', function() {
+    it('changes result to true if frame has been tested', function() {
+      var results = [
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#2']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1', 'html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#2', 'html']
+          }
+        }
+      ];
+
+      var afterResults = frameTestedAfter(results);
+      assert.lengthOf(afterResults, 2);
+      assert.isTrue(afterResults[0].result);
+      assert.isTrue(afterResults[1].result);
+    });
+
+    it('does not change result when iframe has not been tested', function() {
+      var results = [
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#2']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1', 'html']
+          }
+        }
+      ];
+
+      var afterResults = frameTestedAfter(results);
+      assert.lengthOf(afterResults, 2);
+      assert.isTrue(afterResults[0].result);
+      assert.isUndefined(afterResults[1].result);
+    });
   });
 });
