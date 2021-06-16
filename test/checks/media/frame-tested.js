c046114e9740e3e130ad0cb98288eb77331fbb93
Steven Lambert <2433219+straker@users.noreply.github.com>
2021-06-16 08:47:41 -0600

    fix(frame-tested): run without respondable (#2942)
    * fix(frame-tested): run without respondable
    
    * revert playground and files
    
    * add after tests
    
    * work with shadow dom
    
    * fixes
    
    * show undefined for all tests

--- test/checks/media/frame-tested.js
+++ test/checks/media/frame-tested.js
@@ -1,52 +1,249 @@
 describe('frame-tested', function() {
   'use strict';
 
-  var checkContext, iframe;
   var checkEvaluate = axe.testUtils.getCheckEvaluate('frame-tested');
-  var fixture = document.querySelector('#fixture');
+  var frameTestedAfter = checks['frame-tested'].after;
 
-  beforeEach(function() {
-    iframe = document.createElement('iframe');
-    fixture.appendChild(iframe);
-    checkContext = axe.testUtils.MockCheckContext();
-    // Don't throw on async
-    checkContext._onAsync = function() {};
-  });
+  describe('evaluate', function() {
+    it('returns undefined', function() {
+      assert.isUndefined(checkEvaluate());
+    });
 
-  after(function() {
-    fixture = '';
-  });
-
-  it('passes if the iframe contains axe-core', function(done) {
-    iframe.src = '/test/mock/frames/test.html';
-    iframe.addEventListener('load', function() {
-      checkContext._onAsync = function(result) {
-        assert.isTrue(result);
-        done();
-      };
-
-      checkEvaluate.call(checkContext, iframe);
+    it('returns false if passed isViolation:true', function() {
+      assert.isFalse(checkEvaluate(null, { isViolation: true }));
     });
   });
 
-  it('fails if the iframe does not contain axe-core, and isViolation is true', function(done) {
-    checkContext._onAsync = function(result) {
-      assert.isFalse(result);
-      done();
-    };
-    // Timeout after 10ms
-    checkEvaluate.call(checkContext, iframe, {
-      timeout: 10,
-      isViolation: true
-    });
-  });
+  describe('after', function() {
+    it('changes result to true if frame has been tested', function() {
+      var results = [
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#2']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1', 'html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#2', 'html']
+          }
+        }
+      ];
 
-  it('is incomplete if the iframe does not contain axe-core', function(done) {
-    checkContext._onAsync = function(result) {
-      assert.isUndefined(result);
-      done();
-    };
-    // Timeout after 10ms
-    checkEvaluate.call(checkContext, iframe, { timeout: 10 });
+      var afterResults = frameTestedAfter(results);
+      assert.lengthOf(afterResults, 2);
+
+      assert.isTrue(afterResults[0].result);
+      assert.deepEqual(afterResults[0].node.ancestry, [
+        'html > body > iframe#1'
+      ]);
+
+      assert.isTrue(afterResults[1].result);
+      assert.deepEqual(afterResults[1].node.ancestry, [
+        'html > body > iframe#2'
+      ]);
+    });
+
+    it('does not change result when iframe has not been tested', function() {
+      var results = [
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#2']
+          }
+        },
+        {
+          result: false,
+          node: {
+            ancestry: ['html > body > iframe#3']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1', 'html']
+          }
+        }
+      ];
+
+      var afterResults = frameTestedAfter(results);
+      assert.lengthOf(afterResults, 3);
+
+      assert.isTrue(afterResults[0].result);
+      assert.deepEqual(afterResults[0].node.ancestry, [
+        'html > body > iframe#1'
+      ]);
+
+      assert.isUndefined(afterResults[1].result);
+      assert.deepEqual(afterResults[1].node.ancestry, [
+        'html > body > iframe#2'
+      ]);
+
+      assert.isFalse(afterResults[2].result);
+      assert.deepEqual(afterResults[2].node.ancestry, [
+        'html > body > iframe#3'
+      ]);
+    });
+
+    it('works with shadow DOM', function() {
+      var results = [
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [['html > body > custom-elm1', 'iframe#1']]
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [['html > body > custom-elm1', 'iframe#2']]
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [['html > body > custom-elm1', 'iframe#1'], 'html']
+          }
+        }
+      ];
+
+      var afterResults = frameTestedAfter(results);
+      assert.lengthOf(afterResults, 2);
+
+      assert.isTrue(afterResults[0].result);
+      assert.deepEqual(afterResults[0].node.ancestry, [
+        ['html > body > custom-elm1', 'iframe#1']
+      ]);
+
+      assert.isUndefined(afterResults[1].result);
+      assert.deepEqual(afterResults[1].node.ancestry, [
+        ['html > body > custom-elm1', 'iframe#2']
+      ]);
+    });
+
+    it('works with nested shadow DOM and iframes', function() {
+      var results = [
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [['html > body > custom-elm1', 'iframe#2']]
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: ['html > body > iframe#1', 'html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [['html > body > custom-elm1', 'iframe#2'], 'html']
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [
+              ['html > body > custom-elm1', 'iframe#2'],
+              ['html > body > other-element', 'iframe#3']
+            ]
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [
+              ['html > body > custom-elm1', 'iframe#2'],
+              ['html > body > other-element', 'iframe#4']
+            ]
+          }
+        },
+        {
+          result: undefined,
+          node: {
+            ancestry: [
+              ['html > body > custom-elm1', 'iframe#2'],
+              ['html > body > other-element', 'iframe#3'],
+              'html'
+            ]
+          }
+        }
+      ];
+
+      var afterResults = frameTestedAfter(results);
+      assert.lengthOf(afterResults, 4);
+
+      assert.isTrue(afterResults[0].result);
+      assert.deepEqual(afterResults[0].node.ancestry, [
+        'html > body > iframe#1'
+      ]);
+
+      assert.isTrue(afterResults[1].result);
+      assert.deepEqual(afterResults[1].node.ancestry, [
+        ['html > body > custom-elm1', 'iframe#2']
+      ]);
+
+      assert.isTrue(afterResults[2].result);
+      assert.deepEqual(afterResults[2].node.ancestry, [
+        ['html > body > custom-elm1', 'iframe#2'],
+        ['html > body > other-element', 'iframe#3']
+      ]);
+
+      assert.isUndefined(afterResults[3].result);
+      assert.deepEqual(afterResults[3].node.ancestry, [
+        ['html > body > custom-elm1', 'iframe#2'],
+        ['html > body > other-element', 'iframe#4']
+      ]);
+    });
   });
 });
