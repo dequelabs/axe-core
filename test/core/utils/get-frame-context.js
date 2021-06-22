2669cd679df1335f6ac4a371e65348471966d79a
Steven Lambert <steven.lambert@deque.com>
2021-06-22 10:28:56 -0600

    finish tests

--- test/core/utils/get-frame-context.js
+++ test/core/utils/get-frame-context.js
@@ -17,10 +17,10 @@ describe('utils.getFrameContexts', function() {
       return frameData.frameContext;
     });
 
+    assert.lengthOf(contexts, 3);
     assert.isFalse(contexts[0].initiator);
     assert.isFalse(contexts[1].initiator);
     assert.isFalse(contexts[2].initiator);
-    assert.lengthOf(contexts, 3);
   });
 
   it('returns a `frameSelector` for each included frame', function() {
@@ -30,10 +30,10 @@ describe('utils.getFrameContexts', function() {
     var selectors = getFrameContexts().map(function(frameData) {
       return frameData.frameSelector;
     });
+    assert.lengthOf(selectors, 3);
     assert.include(selectors[0], 'iframe:nth-child(1)');
     assert.include(selectors[1], 'iframe:nth-child(2)');
     assert.include(selectors[2], 'iframe:nth-child(3)');
-    assert.lengthOf(selectors, 3);
   });
 
   it('returns a `frameContext` for each included frame', function() {
@@ -52,39 +52,7 @@ describe('utils.getFrameContexts', function() {
       return frameData.frameContext;
     });
 
-    assert.deepEqual(contexts[0], {
-      initiator: false,
-      include: [['header']],
-      exclude: []
-    });
-    assert.deepEqual(contexts[1], {
-      initiator: false,
-      include: [['main']],
-      exclude: []
-    });
-    assert.deepEqual(contexts[2], {
-      initiator: false,
-      include: [],
-      exclude: [['footer']]
-    });
-  });
-
-  it('returns a `frameContext` for each included frame', function() {
-    fixture.innerHTML =
-      '<iframe id="f1"></iframe>' +
-      '<iframe id="f2"></iframe>' +
-      '<iframe id="f3"></iframe>';
-    var context = {
-      include: [
-        ['#f1', 'header'],
-        ['#f2', 'main']
-      ],
-      exclude: [['#f3', 'footer']]
-    };
-    var contexts = getFrameContexts(context).map(function(frameData) {
-      return frameData.frameContext;
-    });
-
+    assert.lengthOf(contexts, 3);
     assert.deepEqual(contexts[0], {
       initiator: false,
       include: [['header']],
@@ -111,6 +79,7 @@ describe('utils.getFrameContexts', function() {
       return frameData.frameContext;
     });
 
+    assert.lengthOf(contexts, 1);
     assert.deepEqual(contexts[0], {
       initiator: false,
       include: [['header']],
@@ -131,6 +100,7 @@ describe('utils.getFrameContexts', function() {
     var contexts = getFrameContexts(context).map(function(frameData) {
       return frameData.frameContext;
     });
+    assert.lengthOf(contexts, 2);
     assert.deepEqual(contexts[0], {
       initiator: false,
       include: [['header']],
@@ -159,6 +129,7 @@ describe('utils.getFrameContexts', function() {
       return frameData.frameContext;
     });
 
+    assert.lengthOf(contexts, 1);
     assert.deepEqual(contexts[0], {
       initiator: false,
       include: [['header'], ['main']],
@@ -177,9 +148,9 @@ describe('utils.getFrameContexts', function() {
     var selectors = getFrameContexts(context).map(function(frameData) {
       return frameData.frameSelector;
     });
+    assert.lengthOf(selectors, 2);
     assert.include(selectors[0], 'iframe:nth-child(1)');
     assert.include(selectors[1], 'iframe:nth-child(3)');
-    assert.lengthOf(selectors, 2);
   });
 
   it('skips frames excluded by a parent', function() {
@@ -191,20 +162,76 @@ describe('utils.getFrameContexts', function() {
   });
 
   it('normalizes the context', function() {
-    var frameContexts;
+    var contexts;
     fixture.innerHTML =
       '<iframe id="f1"></iframe>' + '<iframe id="f2"></iframe>';
-    frameContexts = getFrameContexts('#f1');
-    assert.lengthOf(frameContexts, 1);
+    contexts = getFrameContexts('#f1').map(function(frameData) {
+      return frameData.frameContext;
+    });
+    assert.lengthOf(contexts, 1);
+    assert.deepEqual(contexts[0], {
+      initiator: false,
+      include: [],
+      exclude: []
+    });
 
-    frameContexts = getFrameContexts({ include: ['#f1'] });
-    assert.lengthOf(frameContexts, 1);
+    contexts = getFrameContexts({ include: ['#f1'] }).map(function(frameData) {
+      return frameData.frameContext;
+    });
+    assert.lengthOf(contexts, 1);
+    assert.deepEqual(contexts[0], {
+      initiator: false,
+      include: [],
+      exclude: []
+    });
 
-    frameContexts = getFrameContexts({ exclude: ['#f2'] });
-    assert.lengthOf(frameContexts, 1);
+    contexts = getFrameContexts({ exclude: ['#f2'] }).map(function(frameData) {
+      return frameData.frameContext;
+    });
+    assert.lengthOf(contexts, 1);
+    assert.deepEqual(contexts[0], {
+      initiator: false,
+      include: [],
+      exclude: []
+    });
   });
 
-  it('accepts elements', function() {});
+  it('accepts elements', function() {
+    var contexts;
+    fixture.innerHTML =
+      '<iframe id="f1"></iframe>' + '<iframe id="f2"></iframe>';
+    var f1 = fixture.querySelector('#f1');
+    var f2 = fixture.querySelector('#f2');
+    contexts = getFrameContexts(f1).map(function(frameData) {
+      return frameData.frameContext;
+    });
+    assert.lengthOf(contexts, 1);
+    assert.deepEqual(contexts[0], {
+      initiator: false,
+      include: [],
+      exclude: []
+    });
+
+    contexts = getFrameContexts({ include: [f1] }).map(function(frameData) {
+      return frameData.frameContext;
+    });
+    assert.lengthOf(contexts, 1);
+    assert.deepEqual(contexts[0], {
+      initiator: false,
+      include: [],
+      exclude: []
+    });
+
+    contexts = getFrameContexts({ exclude: [f2] }).map(function(frameData) {
+      return frameData.frameContext;
+    });
+    assert.lengthOf(contexts, 1);
+    assert.deepEqual(contexts[0], {
+      initiator: false,
+      include: [],
+      exclude: []
+    });
+  });
 
   it('works with nested frames', function() {
     fixture.innerHTML =
@@ -220,6 +247,7 @@ describe('utils.getFrameContexts', function() {
       return frameData.frameContext;
     });
 
+    assert.lengthOf(contexts, 2);
     assert.deepEqual(contexts[0], {
       initiator: false,
       include: [['#f3', 'header']],
@@ -236,17 +264,16 @@ describe('utils.getFrameContexts', function() {
     fixture.innerHTML = '<div id="shadow"></div>';
     var div = fixture.querySelector('div');
     var shadowRoot = div.attachShadow({ mode: 'open' });
-    shadowRoot.innerHTML = '<main<iframe id="f1"></iframe></main>';
+    shadowRoot.innerHTML = '<main><iframe id="f1"></iframe></main>';
 
     var context = {
-      include: [[['main']]]
+      include: [['#fixture', 'header']]
     };
     var contexts = getFrameContexts(context).map(function(frameData) {
       return frameData.frameContext;
     });
 
-    console.log(JSON.stringify(contexts, null, 2));
-
+    assert.lengthOf(contexts, 1);
     assert.deepEqual(contexts[0], {
       initiator: false,
       include: [['header']],
