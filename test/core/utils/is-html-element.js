2debf30836a4987d278a8f7b57a249a018ea441a
Steven Lambert <2433219+straker@users.noreply.github.com>
2021-07-09 09:54:48 -0600

    chore: allow aria-allowed-role to run virtually (#3067)
    * chore: allow aria-allowed-role to run virtually
    
    * chore: allow aria-allowed-role to run virtually
    
    * revert is-visible
    
    * finished
    
    * fix
    
    * Update lib/core/utils/is-html-element.js
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>
    
    * Update lib/commons/aria/get-element-unallowed-roles.js
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>
    
    * Update test/checks/aria/aria-allowed-role.js
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>
    
    * add test
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>

--- test/core/utils/is-html-element.js
+++ test/core/utils/is-html-element.js
@@ -1,28 +1,31 @@
 /* global axe */
 describe('axe.utils.isHtmlElement', function() {
+  var queryFixture = axe.testUtils.queryFixture;
+  var isHtmlElement = axe.utils.isHtmlElement;
+
   it('returns true if given ul', function() {
     var node = document.createElement('ul');
-    assert.isTrue(axe.utils.isHtmlElement(node));
+    assert.isTrue(isHtmlElement(node));
   });
 
   it('returns true if given nav', function() {
     var node = document.createElement('nav');
-    assert.isTrue(axe.utils.isHtmlElement(node));
+    assert.isTrue(isHtmlElement(node));
   });
 
   it('returns true if given iframe', function() {
     var node = document.createElement('iframe');
-    assert.isTrue(axe.utils.isHtmlElement(node));
+    assert.isTrue(isHtmlElement(node));
   });
 
   it('returns false if given custom element', function() {
     var node = document.createElement('myElement');
-    assert.isFalse(axe.utils.isHtmlElement(node));
+    assert.isFalse(isHtmlElement(node));
   });
 
   it('returns false if given svg namespace', function() {
     var node = document.createElementNS('http://www.w3.org/2000/svg', 'a');
-    assert.isFalse(axe.utils.isHtmlElement(node));
+    assert.isFalse(isHtmlElement(node));
   });
 
   it('returns false if node has inherited svg namespace', function() {
@@ -34,6 +37,16 @@ describe('axe.utils.isHtmlElement', function() {
     node.appendChild(child);
 
     var childNode = node.querySelector('a');
-    assert.isFalse(axe.utils.isHtmlElement(childNode));
+    assert.isFalse(isHtmlElement(childNode));
+  });
+
+  it('works with VirtualNodes', function() {
+    var vNode = queryFixture('<ul id="target"></ul>');
+    assert.isTrue(isHtmlElement(vNode));
+  });
+
+  it('works with SerialVirtualNode', function() {
+    var vNode = new axe.SerialVirtualNode({ nodeName: 'ul' });
+    assert.isTrue(isHtmlElement(vNode));
   });
 });
