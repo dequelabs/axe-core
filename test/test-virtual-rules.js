404608773abf7b4d069a64931adf4ac7e942b663
Steven Lambert <2433219+straker@users.noreply.github.com>
2021-09-28 11:09:23 -0600

    fix(is-visible): do not error if window.Node does not exist (#3168)
    * fix(is-visible): do not error if window.Node does not exist
    
    * circle
    
    * Update test/test-virtual-rules.js
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>

---
+++ test/test-virtual-rules.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var assert = require('chai').assert;
+var glob = require('glob');
+var axe = require('../axe');
+
+var files = glob.sync(path.join(__dirname, 'integration/virtual-rules/*.js'));
+
+before(function() {
+  global.axe = axe;
+  global.assert = assert;
+});
+
+after(function() {
+  delete global.axe;
+  delete global.assert;
+});
+
+describe('virtual-rule node tests', function() {
+  files.forEach(function(file) {
+    // load the test file and run with global axe and assert now defined
+    require(file);
+  });
+});
