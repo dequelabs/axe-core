c141a585934bc983f2197efe38dfa7ac463a3191
Dan Tripp <daniel.john.tripp@gmail.com>
2021-10-09 09:29:38 -0400

    refactor(check): undo recent split of checks
    Recombine checks "no-focusable-content-for-aria-text" and "no-focusable-content-for-nested-interactive" back into "no-focusable-content".

--- test/checks/keyboard/no-focusable-content.js
+++ test/checks/keyboard/no-focusable-content.js
@@ -1,8 +1,8 @@
-describe('no-focusable-content-for-aria-text tests', function() {
+describe('no-focusable-content tests', function() {
   var fixture = document.querySelector('#fixture');
   var queryFixture = axe.testUtils.queryFixture;
-  var noFocusableContentForAriaText = axe.testUtils.getCheckEvaluate(
-    'no-focusable-content-for-aria-text'
+  var noFocusableContent = axe.testUtils.getCheckEvaluate(
+    'no-focusable-content'
   );
 
   afterEach(function() {
@@ -11,31 +11,31 @@ describe('no-focusable-content-for-aria-text tests', function() {
 
   it('should return true if element has no focusable content', function() {
     var vNode = queryFixture('<button id="target"><span>Hello</span></button>');
-    assert.isTrue(noFocusableContentForAriaText(null, null, vNode));
+    assert.isTrue(noFocusableContent(null, null, vNode));
   });
 
   it('should return true if element is empty', function() {
     var vNode = queryFixture('<button id="target"></button>');
-    assert.isTrue(noFocusableContentForAriaText(null, null, vNode));
+    assert.isTrue(noFocusableContent(null, null, vNode));
   });
 
   it('should return true if element only has text content', function() {
     var vNode = queryFixture('<button id="target">Hello</button>');
-    assert.isTrue(noFocusableContentForAriaText(null, null, vNode));
+    assert.isTrue(noFocusableContent(null, null, vNode));
   });
 
   it('should return false if element has focusable content', function() {
     var vNode = queryFixture(
       '<button id="target"><span tabindex="0">Hello</span></button>'
     );
-    assert.isFalse(noFocusableContentForAriaText(null, null, vNode));
+    assert.isFalse(noFocusableContent(null, null, vNode));
   });
 
   it('should return false if element has natively focusable content', function() {
     var vNode = queryFixture(
       '<button id="target"><a href="foo.html">Hello</a></button>'
     );
-    assert.isFalse(noFocusableContentForAriaText(null, null, vNode));
+    assert.isFalse(noFocusableContent(null, null, vNode));
   });
   
 });
