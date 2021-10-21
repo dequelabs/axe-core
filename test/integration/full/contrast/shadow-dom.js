db71e4356edb67251a428b830684c19f961489cb
Dan Tripp <daniel.john.tripp@gmail.com>
2021-10-20 21:03:14 -0400

    Rearranging shadow-dom test.

--- test/integration/full/contrast/shadow-dom.js
+++ test/integration/full/contrast/shadow-dom.js
@@ -19,18 +19,23 @@ describe('color-contrast shadow dom test', function() {
   });
   
   describe('violations', function() {
-    (shadowSupported ? it : xit)('should find WCAG AA issues in shadow tree', function(
+    (shadowSupported ? it : xit)('should find issues in shadow tree', function(
       done
     ) {
       axe.run(
         '#fixture',
-        { runOnly: { type: 'rule', values: ['color-contrast'] } },
+        { runOnly: { type: 'rule', values: ['color-contrast-enhanced', 'color-contrast'] } },
         function(err, results) {
           assert.isNull(err);
-          assert.lengthOf(results.violations, 1);
-          assert.lengthOf(results.violations[0].nodes, 2);
+          assert.lengthOf(results.violations, 2);
+          assert.lengthOf(results.violations[0].nodes, 3);
+          assert.lengthOf(results.violations[1].nodes, 2);
           assert.equal(
-            results.violations[0].nodes[1].any[0].data.bgColor,
+            results.violations[0].nodes[2].any[0].data.fgColor,
+            '#888888'
+          );
+          assert.equal(
+            results.violations[1].nodes[1].any[0].data.bgColor,
             '#000000'
           );
           assert.lengthOf(results.incomplete, 0);
@@ -40,29 +45,4 @@ describe('color-contrast shadow dom test', function() {
     });
   });
   
-  describe('violations', function() {
-    (shadowSupported ? it : xit)('should find WCAG AAA issues in shadow tree', function(
-      done
-    ) {
-      axe.run(
-        '#fixture',
-        { runOnly: { type: 'rule', values: ['color-contrast-enhanced'] } },
-        function(err, results) {
-          assert.isNull(err);
-          assert.lengthOf(results.violations, 1);
-          assert.lengthOf(results.violations[0].nodes, 5);
-          assert.equal(
-            results.violations[0].nodes[3].any[0].data.bgColor,
-            '#000000'
-          );
-          assert.equal(
-            results.violations[0].nodes[4].any[0].data.fgColor,
-            '#888888'
-          );
-          assert.lengthOf(results.incomplete, 0);
-          done();
-        }
-      );
-    });
-  });
 });
