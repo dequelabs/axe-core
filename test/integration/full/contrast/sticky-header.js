06b38a4fbe1380ade44e0d6af0ca331aeecdcdce
Dan Tripp <daniel.john.tripp@gmail.com>
2021-10-21 20:23:09 -0400

    adding new 'color-contrast-enhanced' rule to sticky-header test.

--- test/integration/full/contrast/sticky-header.js
+++ test/integration/full/contrast/sticky-header.js
@@ -5,10 +5,10 @@ describe('color-contrast sticky header test', function() {
     it('should find none', function(done) {
       axe.run(
         '#fixture',
-        { runOnly: { type: 'rule', values: ['color-contrast'] } },
+        { runOnly: { type: 'rule', values: ['color-contrast-enhanced', 'color-contrast'] } },
         function(err, results) {
           assert.isNull(err);
-          assert.lengthOf(results.violations, 0);
+          assert.lengthOf(results.violations, 0); 
           done();
         }
       );
