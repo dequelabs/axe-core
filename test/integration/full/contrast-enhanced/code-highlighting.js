45721bdeda76c98882acff28fcbc42a8cb3ede53
Dan Tripp <daniel.john.tripp@gmail.com>
2021-11-02 07:26:59 -0400

    work in progress: splitting tests for contrast and the new contrast-enhanced.

--- test/integration/full/contrast-enhanced/code-highlighting.js
+++ test/integration/full/contrast-enhanced/code-highlighting.js
@@ -5,7 +5,7 @@ describe('color-contrast code highlighting test', function() {
   function run(done) {
     axe.run(
       '#fixture',
-      { runOnly: { type: 'rule', values: ['color-contrast'] } },
+      { runOnly: { type: 'rule', values: ['color-contrast-enhanced'] } },
       function(err, r) {
         assert.isNull(err);
         results = r;
@@ -29,21 +29,23 @@ describe('color-contrast code highlighting test', function() {
 
   describe('violations', function() {
     it('should find issues', function() {
-      assert.lengthOf(results.violations, 1);
-      assert.lengthOf(results.violations[0].nodes, 32);
+      assert.lengthOf(results.violations, 2);
+      assert.lengthOf(results.violations[0].nodes, 33);
+      assert.lengthOf(results.violations[1].nodes, 32);
     });
   });
 
   describe('passes', function() {
     it('should find passes', function() {
-      assert.lengthOf(results.passes, 1);
-      assert.lengthOf(results.passes[0].nodes, 27);
+      assert.lengthOf(results.passes, 2);
+      assert.lengthOf(results.passes[0].nodes, 26);
+      assert.lengthOf(results.passes[1].nodes, 27);
     });
   });
 
   describe('incomplete', function() {
     it('should find just the code block', function() {
-      assert.lengthOf(results.incomplete, 1);
+      assert.lengthOf(results.incomplete, 2);
       assert.lengthOf(results.incomplete[0].nodes, 1);
       assert.equal(
         results.incomplete[0].nodes[0].html,
--- test/integration/full/contrast-enhanced/sticky-header.js
+++ test/integration/full/contrast-enhanced/sticky-header.js
@@ -5,7 +5,7 @@ describe('color-contrast sticky header test', function() {
     it('should find none', function(done) {
       axe.run(
         '#fixture',
-        { runOnly: { type: 'rule', values: ['color-contrast'] } },
+        { runOnly: { type: 'rule', values: ['color-contrast-enhanced'] } },
         function(err, results) {
           assert.isNull(err);
           assert.lengthOf(results.violations, 0);
