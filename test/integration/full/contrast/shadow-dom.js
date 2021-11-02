88d46309b08f6235b533ef4833a3bce22b7815a2
Dan Tripp <daniel.john.tripp@gmail.com>
2021-11-02 07:58:10 -0400

    work in progress: tests

--- test/integration/full/contrast/shadow-dom.js
+++ test/integration/full/contrast/shadow-dom.js
@@ -13,29 +13,23 @@ describe('color-contrast shadow dom test', function() {
         '<div><label style="color:#ccc;">Text<input type="text"></label></div>' +
         '<div style="background-color:black; height:20px;">' +
         '<div style="color:#666; position:absolute;">Text</div>' +
-        '</div>' +
-        '<div style="background-color:black; color:#888888">Passes WCAG AA, fails WCAG AAA.</div>';
+        '</div>';
     }
   });
-  
+
   describe('violations', function() {
     (shadowSupported ? it : xit)('should find issues in shadow tree', function(
       done
     ) {
       axe.run(
         '#fixture',
-        { runOnly: { type: 'rule', values: ['color-contrast-enhanced', 'color-contrast'] } },
+        { runOnly: { type: 'rule', values: ['color-contrast'] } },
         function(err, results) {
           assert.isNull(err);
-          assert.lengthOf(results.violations, 2);
-          assert.lengthOf(results.violations[0].nodes, 3);
-          assert.lengthOf(results.violations[1].nodes, 2);
+          assert.lengthOf(results.violations, 1);
+          assert.lengthOf(results.violations[0].nodes, 2);
           assert.equal(
-            results.violations[0].nodes[2].any[0].data.fgColor,
-            '#888888'
-          );
-          assert.equal(
-            results.violations[1].nodes[1].any[0].data.bgColor,
+            results.violations[0].nodes[1].any[0].data.bgColor,
             '#000000'
           );
           assert.lengthOf(results.incomplete, 0);
@@ -44,5 +38,4 @@ describe('color-contrast shadow dom test', function() {
       );
     });
   });
-  
 });
