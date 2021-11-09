c6053634bcf1c4fbfd595a1f0a22bcc471f8099e
Wilco Fiers <wilco.fiers@deque.com>
2021-11-09 12:00:30 +0100

    chore: remove express

--- test/aria-practices/apg.spec.js
+++ test/aria-practices/apg.spec.js
@@ -1,10 +1,7 @@
 const path = require('path');
 const fs = require('fs');
-const { createServer } = require('http');
-const express = require('express');
 const chromedriver = require('chromedriver');
 const AxeBuilder = require('@axe-core/webdriverjs');
-const testListen = require('test-listen');
 const { getWebdriver, connectToChromeDriver } = require('./run-server');
 const { assert } = require('chai');
 const globby = require('globby');
@@ -15,7 +12,8 @@ describe('aria-practices', function () {
   const filePaths = globby.sync(`${apgPath}/examples/**/*.html`)
   const festFiles = filePaths.map(fileName => fileName.split('/aria-practices/examples/')[1])
   const port = 9515;
-  let driver, server, addr, axeSource;
+  const addr = `http://localhost:9876/node_modules/aria-practices/`;
+  let driver, axeSource;
   this.timeout(50000);
   this.retries(3);
 
@@ -25,17 +23,11 @@ describe('aria-practices', function () {
     chromedriver.start([`--port=${port}`]);
     await new Promise(r => setTimeout(r, 500));
     await connectToChromeDriver(port);
-
-    const app = express();
-    app.use(express.static(apgPath));
-    server = createServer(app);
-    addr = await testListen(server);
     driver = getWebdriver();
   });
 
   after(async () => {
     await driver.close();
-    server.close();
     chromedriver.stop();
   });
 
@@ -43,11 +35,11 @@ describe('aria-practices', function () {
     '*': [
       'color-contrast',
       'region', // dequelabs/axe-core#3260
-      'heading-order', // APG issues
-      'list', // APG issues
+      'heading-order', // w3c/aria-practices#2119
+      'list', // w3c/aria-practices#2118
       'scrollable-region-focusable', // w3c/aria-practices#2114
     ],
-    'feed/feedDisplay.html': ['page-has-heading-one'], // APG issue
+    'feed/feedDisplay.html': ['page-has-heading-one'], // w3c/aria-practices#2120
     // "page within a page" type thing going on
     'menubar/menubar-navigation.html': [
       'aria-allowed-role',
