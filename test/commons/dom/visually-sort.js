04c5434791d5dd906259dd46cf184aebbb88db59
Wilco Fiers <wilco.fiers@deque.com>
2022-02-03 13:14:51 +0100

    Improve hasVisualOverlap

--- test/commons/dom/visually-sort.js
+++ test/commons/dom/visually-sort.js
@@ -1,5 +1,28 @@
+// This method is mostly tested through color-contrast integrations
 describe('visually-sort', function () {
-  // This method is mostly tested through color-contrast integrations
+  'use strict';
 
-  // TODO: Add tests here
+  var fixtureSetup = axe.testUtils.fixtureSetup;
+  var visuallySort = axe.commons.dom.visuallySort;
+
+  // visuallySort requires the grid to be set up
+  // For performance reasons axe-core does not do this in setup,
+  // so we'll need to call it ourselves.
+  var createGrid = axe.commons.dom.createGrid;
+
+  it('returns 1 if B overlaps A', function () {
+    var rootNode = fixtureSetup('<a><b>bar</b></a>');
+    createGrid();
+    var vNodeA = rootNode.children[0];
+    var vNodeB = vNodeA.children[0];
+    assert.equal(visuallySort(vNodeA, vNodeB), 1);
+  });
+
+  it('returns -1 if A overlaps B', function () {
+    var rootNode = fixtureSetup('<b><a>bar</a></b>');
+    createGrid();
+    var vNodeB = rootNode.children[0];
+    var vNodeA = vNodeB.children[0];
+    assert.equal(visuallySort(vNodeA, vNodeB), -1);
+  });
 });
--- test/commons/math/has-visual-overlap.js
+++ test/commons/math/has-visual-overlap.js
@@ -8,27 +8,27 @@ describe('hasVisualOverlap', function () {
   // so we'll need to call it ourselves.
   var createGrid = axe.commons.dom.createGrid;
 
-  it('returns 0 if there is no overlap', function () {
+  it('returns false if there is no overlap', function () {
     var rootNode = fixtureSetup('<a>foo</a><b>bar</b>');
     createGrid();
     var vNodeA = rootNode.children[0];
     var vNodeB = rootNode.children[1];
-    assert.equal(hasVisualOverlap(vNodeA, vNodeB), 0);
+    assert.isFalse(hasVisualOverlap(vNodeA, vNodeB));
   });
 
-  it('returns 1 if B overlaps A', function () {
+  it('returns true if B overlaps A', function () {
     var rootNode = fixtureSetup('<a><b>bar</b></a>');
     createGrid();
     var vNodeA = rootNode.children[0];
     var vNodeB = vNodeA.children[0];
-    assert.equal(hasVisualOverlap(vNodeA, vNodeB), 1);
+    assert.isTrue(hasVisualOverlap(vNodeA, vNodeB));
   });
 
-  it('returns -1 if A overlaps B', function () {
+  it('returns true A overlaps B', function () {
     var rootNode = fixtureSetup('<b><a>bar</a></b>');
     createGrid();
     var vNodeB = rootNode.children[0];
     var vNodeA = vNodeB.children[0];
-    assert.equal(hasVisualOverlap(vNodeA, vNodeB), -1);
+    assert.isFalse(hasVisualOverlap(vNodeA, vNodeB));
   });
 });
