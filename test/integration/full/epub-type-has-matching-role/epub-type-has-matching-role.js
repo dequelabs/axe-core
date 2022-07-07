c6c697eb340017fbd6264f570ad073269ec792d6
Daniel Weck <daniel.weck@gmail.com>
2022-07-07 18:01:49 +0100

    fix: epub:type with deprecated ARIA role does not trigger matching rule fail

--- test/integration/full/epub-type-has-matching-role/epub-type-has-matching-role.js
+++ test/integration/full/epub-type-has-matching-role/epub-type-has-matching-role.js
@@ -40,7 +40,7 @@ describe('epub-type-has-matching-role test fail', function() {
     });
 
     it('should find section #pass1-6', function() {
-      assert.deepEqual(results.passes[0].nodes[0].target, ['section']);
+      assert.deepEqual(results.passes[0].nodes[0].target, ['#sec']);
       assert.deepEqual(results.passes[0].nodes[1].target, ['#pass2']);
       assert.deepEqual(results.passes[0].nodes[2].target, ['#pass3']);
       assert.deepEqual(results.passes[0].nodes[3].target, ['#pass4']);
@@ -48,6 +48,14 @@ describe('epub-type-has-matching-role test fail', function() {
       assert.deepEqual(results.passes[0].nodes[5].target, ['#pass5']);
       assert.deepEqual(results.passes[0].nodes[6].target, ['#id-landmarks']);
       assert.deepEqual(results.passes[0].nodes[7].target, ['#pass0']);
+      assert.deepEqual(results.passes[0].nodes[8].target, ['#ok1']);
+      assert.deepEqual(results.passes[0].nodes[9].target, ['#deprecated1_']);
+      assert.deepEqual(results.passes[0].nodes[10].target, ['#ok2']);
+      assert.deepEqual(results.passes[0].nodes[11].target, ['#deprecated2_']);
+      assert.deepEqual(results.passes[0].nodes[12].target, ['#ok3']);
+      assert.deepEqual(results.passes[0].nodes[13].target, ['#deprecated3_']);
+      assert.deepEqual(results.passes[0].nodes[14].target, ['#ok4']);
+      assert.deepEqual(results.passes[0].nodes[15].target, ['#deprecated4_']);
     });
   });
 
