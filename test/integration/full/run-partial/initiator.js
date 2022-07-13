7f3d5a93ad0dbc35104f29acc6f7d4bc6037f543
Dan Bowling <dan.bowling@deque.com>
2022-07-13 15:53:59 -0600

    fix deepEqual due to webdriver issue

--- test/integration/full/run-partial/initiator.js
+++ test/integration/full/run-partial/initiator.js
@@ -1,27 +1,32 @@
-describe('run-partial, initiator', function() {
+describe('run-partial, initiator', function () {
   'use strict';
   var ruleName = 'document-title';
   var runPartialRecursive = axe.testUtils.runPartialRecursive;
   var clone = axe.utils.clone;
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     axe.testUtils.awaitNestedLoad(done);
   });
 
-  it('gives the same results as axe.run when context.initiator is used', function(done) {
+  it('gives the same results as axe.run when context.initiator is used', function (done) {
     var options = { runOnly: ruleName };
     var context = { exclude: [] };
     Promise.all(runPartialRecursive(clone(context), options))
-      .then(function(partialResults) {
+      .then(function (partialResults) {
         return Promise.all([
           axe.finishRun(partialResults, options),
           axe.run(clone(context), options)
         ]);
       })
-      .then(function(results) {
+      .then(function (results) {
         var axeRunPartialResult = results[0];
         var axeRunResult = results[1];
         assert.lengthOf(axeRunPartialResult.violations, 0);
+
+        // it appears the selenium webdriver browser message "Chrome is being controlled by
+        // automated software" changes the window height when it appears
+        // between running the two axe runs
+        axeRunPartialResult.testEnvironment = axeRunResult.testEnvironment;
 
         axeRunPartialResult.timestamp = axeRunResult.timestamp;
         assert.deepEqual(axeRunPartialResult, axeRunResult);
