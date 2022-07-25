d01532c243646adb656058f6ba7bf47802290636
Dan Bowling <dbowling@users.noreply.github.com>
2022-07-25 14:07:04 -0600

    chore: update dependencies (#3539)
    * update esbuild to support m1
    
    * update webdriverjs
    
    * update babel
    
    * update act rules
    
    * update aria practices
    
    * aria-query bump
    
    * update babel
    
    * husky 3.x
    
    # Conflicts:
    #	package-lock.json
    #	package.json
    
    * chore: update dependencies
    
    depenencies with a .x cannot be upgraded further
    
    * Pin execa due to ESM
    
    * bump selenium-webdriver
    
    requested by Michael
    
    * partial fix for selenium-webdriver 3.4.0
    
    Flakey test locally; checking CI
    
    * fix selenium browser name
    
    Requires “internet explorer”
    
    * Fix deepEqual assert
    
    deepEqual() assert failed because selenium now places a banner that throws off window height
    
    * Fix deepEqual assert
    
    deepEqual() assert failed because selenium now places a banner that throws off window height
    
    * fix deepEqual due to webdriver issue
    
    * update globby
    
    * update sinon to 10.x
    
    v11 introduces ESM
    
    * roll back sinon to v9.x
    confirmed v10 sinon does not support IE11 for CI
    
    * break up &&
    
    * linting
    
    * remove comments
    
    * require dev dependencies to be installed
    
    Co-authored-by: Steven Lambert <2433219+straker@users.noreply.github.com>
    
    * regenerate package-lock after integration
    
    * jsdoc patch update
    
    * patch core-js
    
    * downgrade esbuild
    
    * update esbuild to 0.13.1
    
    * upgrade esbuild to 0.13.2
    
    * esbuild 0.13.15
    
    * update esbuild to 0.14.0
    
    * update esbuild to 0.14.1
    
    * update esbuild to 0.14.2
    
    * update esbuild to 0.14.3
    
    * update esbuild to 0.14.5
    
    * update esbuild to 0.14.6
    
    * update esbuild to 0.14.7
    
    Co-authored-by: Steven Lambert <2433219+straker@users.noreply.github.com>

--- test/integration/full/run-partial/initiator.js
+++ test/integration/full/run-partial/initiator.js
@@ -1,28 +1,28 @@
-describe('run-partial, initiator', function() {
+describe('run-partial, initiator', function () {
   'use strict';
   var ruleName = 'document-title';
   var runPartialRecursive = axe.testUtils.runPartialRecursive;
   var clone = axe.utils.clone;
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     axe.testUtils.awaitNestedLoad(done);
   });
 
-  it('gives the same results as axe.run when context.initiator is used', function(done) {
+  it('gives the same results as axe.run when context.initiator is used', function (done) {
     var options = { runOnly: ruleName };
     var context = { exclude: [] };
     Promise.all(runPartialRecursive(clone(context), options))
-      .then(function(partialResults) {
+      .then(function (partialResults) {
         return Promise.all([
           axe.finishRun(partialResults, options),
           axe.run(clone(context), options)
         ]);
       })
-      .then(function(results) {
+      .then(function (results) {
         var axeRunPartialResult = results[0];
         var axeRunResult = results[1];
         assert.lengthOf(axeRunPartialResult.violations, 0);
-
+        axeRunPartialResult.testEnvironment = axeRunResult.testEnvironment;
         axeRunPartialResult.timestamp = axeRunResult.timestamp;
         assert.deepEqual(axeRunPartialResult, axeRunResult);
         done();
