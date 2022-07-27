0cee3834472efe5f53c231b6320ba26a1cc18b18
Dan Bowling <dan.bowling@deque.com>
2022-07-27 14:52:16 -0600

    revert

--- test/checks/aria/aria-roledescription.js
+++ test/checks/aria/aria-roledescription.js
@@ -1,22 +1,22 @@
-describe.only('aria-roledescription', function () {
+describe('aria-roledescription', function () {
   'use strict';
 
-  var checkContext = axe.testUtils.MockCheckContext();
-  var queryFixture = axe.testUtils.queryFixture;
+  var fixture = document.getElementById('fixture');
   var flatTreeSetup = axe.testUtils.flatTreeSetup;
+  var checkContext = axe.testUtils.MockCheckContext();
 
   afterEach(function () {
+    fixture.innerHTML = '';
     checkContext.reset();
   });
 
   it('returns true for elements with an implicit supported role', function () {
-    var virtualNode = queryFixture(
-      '<button aria-roledescription="Awesome Button" id="target">Click</button>'
-    );
-    flatTreeSetup(virtualNode);
+    fixture.innerHTML =
+      '<button aria-roledescription="Awesome Button">Click</button>';
+    flatTreeSetup(fixture);
     var actual = axe.testUtils
       .getCheckEvaluate('aria-roledescription')
-      .call(checkContext, virtualNode.firstChild, {
+      .call(checkContext, fixture.firstChild, {
         supportedRoles: ['button']
       });
     assert.equal(actual, true);
@@ -24,13 +24,12 @@ describe.only('aria-roledescription', function () {
   });
 
   it('returns true for elements with an explicit supported role', function () {
-    var virtualNode = queryFixture(
-      '<div role="radio" aria-roledescription="Awesome Radio"  id="target">Click</div>'
-    );
-    flatTreeSetup(virtualNode);
+    fixture.innerHTML =
+      '<div role="radio" aria-roledescription="Awesome Radio">Click</div>';
+    flatTreeSetup(fixture);
     var actual = axe.testUtils
       .getCheckEvaluate('aria-roledescription')
-      .call(checkContext, virtualNode.firstChild, {
+      .call(checkContext, fixture.firstChild, {
         supportedRoles: ['radio']
       });
     assert.equal(actual, true);
@@ -38,49 +37,45 @@ describe.only('aria-roledescription', function () {
   });
 
   it('returns undefined for elements with an unsupported role', function () {
-    var virtualNode = queryFixture(
-      '<div role="main" aria-roledescription="Awesome Main"  id="target">The main element</div>'
-    );
-    flatTreeSetup(virtualNode);
+    fixture.innerHTML =
+      '<div role="main" aria-roledescription="Awesome Main">The main element</div>';
+    flatTreeSetup(fixture);
     var actual = axe.testUtils
       .getCheckEvaluate('aria-roledescription')
-      .call(checkContext, virtualNode.firstChild);
+      .call(checkContext, fixture.firstChild);
     assert.equal(actual, undefined);
     assert.isNull(checkContext._data, null);
   });
 
   it('returns false for elements without role', function () {
-    var virtualNode = queryFixture(
-      '<div aria-roledescription="Awesome Main">The main element</div>'
-    );
-    flatTreeSetup(virtualNode);
+    fixture.innerHTML =
+      '<div aria-roledescription="Awesome Main">The main element</div>';
+    flatTreeSetup(fixture);
     var actual = axe.testUtils
       .getCheckEvaluate('aria-roledescription')
-      .call(checkContext, virtualNode.firstChild);
+      .call(checkContext, fixture.firstChild);
     assert.equal(actual, false);
     assert.isNull(checkContext._data, null);
   });
 
   it('returns false for elements with role=presentation', function () {
-    var virtualNode = queryFixture(
-      '<div role="presentation" aria-roledescription="Awesome Main" id="target">The main element</div>'
-    );
-    flatTreeSetup(virtualNode);
+    fixture.innerHTML =
+      '<div role="presentation" aria-roledescription="Awesome Main">The main element</div>';
+    flatTreeSetup(fixture);
     var actual = axe.testUtils
       .getCheckEvaluate('aria-roledescription')
-      .call(checkContext, virtualNode.firstChild);
+      .call(checkContext, fixture.firstChild);
     assert.equal(actual, false);
     assert.isNull(checkContext._data, null);
   });
 
   it('returns false for elements with role=none', function () {
-    var virtualNode = queryFixture(
-      '<div role="none" aria-roledescription="Awesome Main" id="target">The main element</div>'
-    );
-    flatTreeSetup(virtualNode);
+    fixture.innerHTML =
+      '<div role="none" aria-roledescription="Awesome Main">The main element</div>';
+    flatTreeSetup(fixture);
     var actual = axe.testUtils
       .getCheckEvaluate('aria-roledescription')
-      .call(checkContext, virtualNode.firstChild);
+      .call(checkContext, fixture.firstChild);
     assert.equal(actual, false);
     assert.isNull(checkContext._data, null);
   });
