9aedd6cf600a4f4216b8d2ace7425360068a3f11
Dan Bowling <dan.bowling@deque.com>
2022-07-29 15:30:28 -0600

    wip

--- test/integration/virtual-rules/no-focusable-content.js
+++ test/integration/virtual-rules/no-focusable-content.js
@@ -1,39 +1,39 @@
 describe('no-focusable-content virtual-rule', function () {
-  // Test cases:
-  // no children
-  //   "\"role=text\" should have no focusable descendants"
-  it('should pass for element without focusable content', function () {
+  it('should incomplete for element with undefined children', function () {
     var node = new axe.SerialVirtualNode({
-      nodeName: 'button'
+      nodeName: 'div',
+      attributes: {
+        role: 'text'
+      }
     });
-    var child = new axe.SerialVirtualNode({
-      nodeName: '#text',
-      nodeType: 3,
-      nodeValue: 'Hello World'
-    });
-    node.children = [child];
+    node.children = null;
 
-    var results = axe.runVirtualRule('nested-interactive', node);
+    var results = axe.runVirtualRule('aria-text', node);
 
-    assert.lengthOf(results.passes, 1);
+    assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 0);
-    assert.lengthOf(results.incomplete, 0);
+    assert.lengthOf(results.incomplete, 1);
   });
 
   it('should pass if element has no focusable content', function () {
     var node = new axe.SerialVirtualNode({
-      nodeName: 'button'
+      nodeName: 'div',
+      attributes: {
+        role: 'text'
+      }
     });
     var child = new axe.SerialVirtualNode({
       nodeName: 'span'
     });
     var grandchild = new axe.SerialVirtualNode({
       nodeName: '#text',
-      nodeType: 3, // what is this????
+      nodeType: 3,
       nodeValue: 'hello'
     });
 
+    // grandchild.parent = child;
     child.children = [grandchild];
+    // child.parent = node;
     node.children = [child];
     node.parent = null;
 
@@ -44,36 +44,36 @@ describe('no-focusable-content virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass if the element is empty', function () {
-    var node = new axe.SerialVirtualNode({
-      nodeName: 'button'
-    });
+  //   xit('should pass if the element is empty', function () {
+  //     var node = new axe.SerialVirtualNode({
+  //       nodeName: 'button'
+  //     });
 
-    node.children = [];
-    node.parent = null;
+  //     node.children = [];
+  //     node.parent = null;
 
-    var results = axe.runVirtualRule('aria-text', node);
+  //     var results = axe.runVirtualRule('aria-text', node);
 
-    assert.lengthOf(results.passes, 1);
-    assert.lengthOf(results.violations, 0);
-    assert.lengthOf(results.incomplete, 0);
-  });
+  //     assert.lengthOf(results.passes, 1);
+  //     assert.lengthOf(results.violations, 0);
+  //     assert.lengthOf(results.incomplete, 0);
+  //   });
 
-  it('should pass if element only has text content', function () {
-    var node = new axe.SerialVirtualNode({
-      nodeName: 'button'
-    });
-    var child = new axe.SerialVirtualNode({
-      nodeName: '#text',
-      nodeType: 3,
-      nodeValue: 'Hello World'
-    });
-    node.children = [child];
+  //   xit('should pass if element only has text content', function () {
+  //     var node = new axe.SerialVirtualNode({
+  //       nodeName: 'button'
+  //     });
+  //     var child = new axe.SerialVirtualNode({
+  //       nodeName: '#text',
+  //       nodeType: 3,
+  //       nodeValue: 'Hello World'
+  //     });
+  //     node.children = [child];
 
-    var results = axe.runVirtualRule('aria-text', node);
+  //     var results = axe.runVirtualRule('aria-text', node);
 
-    assert.lengthOf(results.passes, 1);
-    assert.lengthOf(results.violations, 0);
-    assert.lengthOf(results.incomplete, 0);
-  });
+  //     assert.lengthOf(results.passes, 1);
+  //     assert.lengthOf(results.violations, 0);
+  //     assert.lengthOf(results.incomplete, 0);
+  //   });
 });
