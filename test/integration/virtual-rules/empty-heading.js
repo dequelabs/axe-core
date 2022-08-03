89a20fe69c46a7353e7b11af5f412f5e340b8e35
Dan Bowling <dan.bowling@deque.com>
2022-08-03 17:35:55 -0600

    add virtual rule tests

--- test/integration/virtual-rules/empty-heading.js
+++ test/integration/virtual-rules/empty-heading.js
@@ -1,45 +1,53 @@
 describe('empty-heading virtual-rule', function () {
-  it('should pass with text', function () {
-    var node = new axe.SerialVirtualNode({
-      nodeName: 'h1',
-      attributes: {}
+  describe('has-visible-text', function () {
+    it('should pass with text', function () {
+      var node = new axe.SerialVirtualNode({
+        nodeName: 'h1',
+        attributes: {}
+      });
+      var child = new axe.SerialVirtualNode({
+        nodeName: '#text',
+        nodeType: 3,
+        nodeValue: 'OK',
+        attributes: {}
+      });
+
+      node.children = [child];
+
+      var results = axe.runVirtualRule('empty-heading', node);
+
+      assert.lengthOf(results.violations, 0);
+      assert.lengthOf(results.incomplete, 0);
+      assert.lengthOf(results.passes, 1);
     });
-    var child = new axe.SerialVirtualNode({
-      nodeName: '#text',
-      nodeType: 3,
-      nodeValue: 'OK',
-      attributes: {}
+
+    xit('should fail with no visible text', function () {
+      var node = new axe.SerialVirtualNode({
+        nodeName: 'h1',
+        attributes: {}
+      });
+      var child = new axe.SerialVirtualNode({
+        nodeName: '#text',
+        nodeType: 3,
+        nodeValue: 'OK'
+      });
+
+      child.parent = node;
+      node.children = [child];
+
+      var results = axe.runVirtualRule('empty-heading', node);
+
+      assert.lengthOf(results.violations, 0);
+      assert.lengthOf(results.incomplete, 0);
+      assert.lengthOf(results.passes, 1);
     });
-
-    node.children = [child];
-
-    var results = axe.runVirtualRule('empty-heading', node);
-
-    assert.lengthOf(results.violations, 0);
-    assert.lengthOf(results.incomplete, 0);
-    assert.lengthOf(results.passes, 1);
   });
 
-  xit('should fail with no visible text', function () {
-    var node = new axe.SerialVirtualNode({
-      nodeName: 'h1',
-      attributes: {}
-    });
-    var child = new axe.SerialVirtualNode({
-      nodeName: '#text',
-      nodeType: 3,
-      nodeValue: 'OK'
-    });
-
-    child.parent = node;
-    node.children = [child];
-
-    var results = axe.runVirtualRule('empty-heading', node);
-
-    assert.lengthOf(results.violations, 0);
-    assert.lengthOf(results.incomplete, 0);
-    assert.lengthOf(results.passes, 1);
+  xit('should pass on explicit role', function () {
+    //TODO
   });
 
-  xit('should incomplete on error', function () {});
+  xit('should pass on implicit role', function () {
+    //TODO
+  });
 });
