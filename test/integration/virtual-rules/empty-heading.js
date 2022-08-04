bdc9a2b12e4460f3394ee0c19b565c85609bcf5a
Dan Bowling <dan.bowling@deque.com>
2022-08-04 11:36:43 -0600

    add failing/stubbed out tests

--- test/integration/virtual-rules/empty-heading.js
+++ test/integration/virtual-rules/empty-heading.js
@@ -1,6 +1,6 @@
 describe('empty-heading virtual-rule', function () {
-  describe('has-visible-text', function () {
-    it('should pass with text', function () {
+  describe('has-visible-text checks', function () {
+    it('should pass with visible text', function () {
       var node = new axe.SerialVirtualNode({
         nodeName: 'h1',
         attributes: {}
@@ -21,33 +21,118 @@ describe('empty-heading virtual-rule', function () {
       assert.lengthOf(results.passes, 1);
     });
 
-    xit('should fail with no visible text', function () {
+    it('should incomplete if no other properties are set', function () {
       var node = new axe.SerialVirtualNode({
-        nodeName: 'h1',
-        attributes: {}
+        nodeName: 'h1'
       });
-      var child = new axe.SerialVirtualNode({
-        nodeName: '#text',
-        nodeType: 3,
-        nodeValue: 'OK'
-      });
-
-      child.parent = node;
-      node.children = [child];
 
       var results = axe.runVirtualRule('empty-heading', node);
 
       assert.lengthOf(results.violations, 0);
-      assert.lengthOf(results.incomplete, 0);
-      assert.lengthOf(results.passes, 1);
+      assert.lengthOf(results.incomplete, 1);
+      assert.lengthOf(results.passes, 0);
+    });
+
+    it('should fail with no visible text', function () {
+      var heading = new axe.SerialVirtualNode({
+        nodeName: 'h1',
+        attributes: {}
+      });
+      var span = new axe.SerialVirtualNode({
+        nodeName: 'span',
+        attributes: {
+          hidden: true
+        },
+        hidden: true
+      });
+      var text = new axe.SerialVirtualNode({
+        nodeName: '#text',
+        nodeType: 3,
+        nodeValue: 'Hidden Text',
+        attributes: {
+          hidden: true
+        },
+        hidden: true
+      });
+      span.children = [text];
+      heading.children = [span];
+
+      debugNode(heading);
+      var results = axe.runVirtualRule('empty-heading', heading);
+
+      assert.lengthOf(results.violations, 1, 'should have 1 violation');
+      assert.lengthOf(results.incomplete, 0, 'should have 0 incompletes');
+      assert.lengthOf(results.passes, 0, 'should have 0 passed');
+    });
+
+    xit('should pass on explicit role', function () {
+      //TODO
+    });
+
+    xit('should pass on implicit role', function () {
+      //TODO
     });
   });
 
-  xit('should pass on explicit role', function () {
-    //TODO
+  describe('aria-label checks', function () {
+    it('should pass for aria-label', function () {
+      var results = axe.runVirtualRule('empty-heading', {
+        nodeName: 'h1',
+        attributes: {
+          'aria-label': 'foobar'
+        }
+      });
+
+      assert.lengthOf(results.passes, 1);
+      assert.lengthOf(results.violations, 0);
+      assert.lengthOf(results.incomplete, 0);
+    });
+
+    it('should fail when aria-label is empty', function () {
+      var results = axe.runVirtualRule('empty-heading', {
+        nodeName: 'h1',
+        attributes: {
+          'aria-label': ''
+        }
+      });
+
+      assert.lengthOf(results.passes, 0);
+      assert.lengthOf(results.violations, 1);
+      assert.lengthOf(results.incomplete, 0);
+    });
   });
 
-  xit('should pass on implicit role', function () {
-    //TODO
+  describe('aria-labelledby checks', function () {
+    it('should incomplete for aria-labelledby', function () {
+      var results = axe.runVirtualRule('empty-heading', {
+        nodeName: 'h1',
+        attributes: {
+          'aria-labelledby': 'foobar'
+        }
+      });
+
+      assert.lengthOf(results.passes, 0);
+      assert.lengthOf(results.violations, 0);
+      assert.lengthOf(results.incomplete, 1);
+    });
+  });
+  describe('non-empty-title checks', function () {
+    it('should fail when title is empty', function () {
+      var results = axe.runVirtualRule('empty-heading', {
+        nodeName: 'h1',
+        attributes: {
+          type: 'button',
+          title: ''
+        }
+      });
+
+      assert.lengthOf(results.passes, 0);
+      assert.lengthOf(results.violations, 1);
+      assert.lengthOf(results.incomplete, 0);
+    });
   });
 });
+
+var debugNode = function (node) {
+  console.debug(JSON.stringify(node, null, 2));
+};
