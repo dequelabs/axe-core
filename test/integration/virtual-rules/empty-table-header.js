7639ef6ac2c73ba0eacc78263fbf80f26d70ac72
Dan Bowling <dan.bowling@deque.com>
2022-08-10 15:13:53 -0600

    stub out integration tests

---
+++ test/integration/virtual-rules/empty-table-header.js
@@ -0,0 +1,13 @@
+describe('empty-table-header virtual-rule', function () {
+  it('should pass with a table header');
+  it('should pass with a table definition of role rowheader');
+  it('should pass with a table definition of role rowheader');
+  it('should fail if table header has no child nodes');
+  it('should fail with a table header that only has hidden text');
+  it(
+    'should fail with a table definition of role rowheader that only has hidden text'
+  );
+  it(
+    'should fail with a table definition of role rowheader that only has hidden text'
+  );
+});
