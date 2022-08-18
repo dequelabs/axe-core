a89abfa6953dfd3f6b1c8a7811cdae7c98e912f5
Dan Bowling <dan.bowling@deque.com>
2022-08-18 16:21:36 -0600

    pairing

--- test/core/base/cache.js
+++ test/core/base/cache.js
@@ -34,80 +34,64 @@ describe('axe._cache', function () {
     assert.isUndefined(value);
   });
 
-  it('should set a default value when using `get()` with a second param', function () {
-    assert.strictEqual(axe._cache.get('foo', 'bar'), 'bar');
+  it('should return undefined for key lookup miss', function () {
+    assert.isUndefined(axe._cache.get('foo'));
   });
 
-  it('should accept callbacks that return primitive types as default value', function () {
-    var primitives = [12345, 'foo', false, null, undefined];
+  describe('`get()` with default value', function () {
+    it('should set a default value', function () {
+      assert.equal(axe._cache.get('foo', 'bar'), 'bar');
+    });
 
-    for (var i = 0; i < primitives.length; i++) {
-      var testCase = primitives[i];
-      assert.strictEqual(
-        axe._cache.get(i, function () {
-          return testCase;
-        }),
-        testCase,
-        typeof testCase
+    it('should accept callbacks that return primitive types', function () {
+      var primitives = [12345, 'foo', false, null, undefined, {}, []];
+
+      for (var i = 0; i < primitives.length; i++) {
+        var testCase = primitives[i];
+        assert.strictEqual(
+          axe._cache.get(i, function () {
+            return testCase;
+          }),
+          testCase,
+          typeof testCase
+        );
+      }
+    });
+
+    it('default function should only be called once', function () {
+      var spy = sinon.spy();
+      axe._cache.get('foo', spy);
+      axe._cache.get('foo', spy);
+      axe._cache.get('foo', spy);
+      assert.isTrue(spy.calledOnce);
+    });
+
+    it('should not re-calculate for second `get()` call', function () {
+      axe._cache.get('Foo', 12345);
+
+      assert.equal(
+        axe._cache.get('Foo', 'should not be set to a string'),
+        12345
       );
-    }
-  });
+    });
 
-  it('should accept callbacks who return objects as default value', function () {
-    assert.deepEqual(
-      axe._cache.get('object callback', function () {
-        return {
-          foo: 'bar'
-        };
-      }),
-      { foo: 'bar' }
-    );
-  });
+    it('should not re-calculate when set to `undefined`', function () {
+      axe._cache.get('Foo', undefined);
+      assert.isUndefined(
+        axe._cache.get('Foo', 'should not be set to a string')
+      );
+    });
 
-  it('should not re-calculate the default value for second `get()` call', function () {
-    axe._cache.get('Foo', 12345);
+    it('should not override a value from `set()`', function () {
+      axe._cache.set('foo', 'bar');
+      axe._cache.get('foo', 'baz');
+      assert.equal(axe._cache.get('foo'), 'bar');
+    });
 
-    assert.strictEqual(
-      typeof axe._cache.get('Foo', 'should not be set to a string'),
-      'number'
-    );
-  });
-
-  it('should not re-calculate the default value when it is set to `undefined`', function () {
-    axe._cache.get('Foo', undefined);
-    assert.strictEqual(
-      typeof axe._cache.get('Foo', 'should not be set to a string'),
-      'undefined'
-    );
-  });
-
-  it('should not override a value from `set()` with a default `get()` value', function () {
-    axe._cache.set('foo', 'bar');
-    axe._cache.get('foo', 'baz');
-    assert.equal(axe._cache.get('foo'), 'bar');
-  });
-
-  it('should override a value from `get()` default with a `set()` value', function () {
-    axe._cache.get('foo', 'baz');
-    axe._cache.set('foo', 'bar');
-    assert.equal(axe._cache.get('foo'), 'bar');
-  });
-
-  it('should return undefined for key lookup miss when no default value is provided', function () {
-    assert.strictEqual(typeof axe._cache.get('foo'), 'undefined');
-  });
-
-  it('should throw if there are too many arguments', function () {
-    function fn() {
-      axe._cache.get('foo', 'bar', 'baz');
-    }
-    assert.throws(fn);
-  });
-
-  it('should throw if there are no arguments', function () {
-    function fn() {
-      axe._cache.get();
-    }
-    assert.throws(fn);
+    it('should override a value from `get()` with a `set()` value', function () {
+      axe._cache.get('foo', 'baz');
+      axe._cache.set('foo', 'bar');
+      assert.equal(axe._cache.get('foo'), 'bar');
+    });
   });
 });
