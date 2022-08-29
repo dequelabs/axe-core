6806ad0da3911d4947aefe6f63e9bd82010abe15
Dan Bowling <dan.bowling@deque.com>
2022-08-28 22:43:53 -0600

    major refactor

--- test/core/base/cache.js
+++ test/core/base/cache.js
@@ -2,14 +2,22 @@ describe('axe._cache', function () {
   'use strict';
 
   describe('set', function () {
-    it('should set items from the cache without error', function () {
+    it('should set items without error', function () {
       function fn() {
         axe._cache.set('foo', 'bar');
       }
       assert.doesNotThrow(fn);
     });
 
-    it('should throw for non-string keys', function () {
+    it('should set `undefined` without error', function () {
+      function fn() {
+        axe._cache.set('foo', undefined);
+        assert.isUndefined(axe._cache.get('foo'));
+      }
+      assert.doesNotThrow(fn);
+    });
+
+    it('should throw TypeError for non-string keys', function () {
       assert.throws(function () {
         axe._cache.set(1, 'bar');
       }, TypeError);
@@ -25,6 +33,12 @@ describe('axe._cache', function () {
         }, 'bar');
       }, TypeError);
     });
+
+    it('should throw TypeError for empty string keys', function () {
+      assert.throws(function () {
+        axe._cache.set('', 'bar');
+      }, TypeError);
+    });
   });
 
   describe('get', function () {
@@ -34,32 +48,74 @@ describe('axe._cache', function () {
       assert.equal(value, 'bar');
     });
 
-    it('should return undefined for key lookup miss', function () {
+    it('should return `undefined` for key lookup miss', function () {
       assert.isUndefined(axe._cache.get('foo'));
     });
 
     describe('with default value', function () {
-      it('should set a default value', function () {
-        assert.equal(axe._cache.get('foo', 'bar'), 'bar');
+      it('should set and return primitive types', function () {
+        assert.equal(
+          axe._cache.get('integer', function () {
+            return 1;
+          }),
+          1
+        );
+        assert.equal(
+          axe._cache.get('float', function () {
+            return 1.1;
+          }),
+          1.1
+        );
+        assert.equal(
+          axe._cache.get('string', function () {
+            return 'foo';
+          }),
+          'foo'
+        );
+        assert.isTrue(
+          axe._cache.get('boolean', function () {
+            return true;
+          })
+        );
+        assert.isNull(
+          axe._cache.get('null', function () {
+            return null;
+          })
+        );
+        assert.deepEqual(
+          axe._cache.get('object', function () {
+            return { foo: 'bar' };
+          }),
+          { foo: 'bar' }
+        );
+        assert.sameOrderedMembers(
+          axe._cache.get('array', function () {
+            return [1, 2, 3];
+          }),
+          [1, 2, 3]
+        );
       });
 
-      it('should accept callbacks that return primitive types', function () {
-        var primitives = [12345, 'foo', false, null, undefined, {}, []];
-
-        for (var i = 0; i < primitives.length; i++) {
-          var testCase = primitives[i];
-          assert.strictEqual(
-            axe._cache.get(i.toString(), function () {
-              return testCase;
-            }),
-            testCase,
-            typeof testCase
-          );
+      it('should throw TypeError when creator is not a function', function () {
+        function fn() {
+          axe._cache.get('foo', 'bar');
         }
+        assert.throws(fn, TypeError);
       });
 
-      it('default function should only be called once', function () {
-        var spy = sinon.spy();
+      it('should throw TypeError when creator returns `undefined`', function () {
+        function fn() {
+          axe._cache.get('foo', function () {
+            return undefined;
+          });
+        }
+        assert.throw(fn);
+      });
+
+      it('creator should evaluate once', function () {
+        var spy = sinon.spy(function () {
+          return true;
+        });
         axe._cache.get('foo', spy);
         axe._cache.get('foo', spy);
         axe._cache.get('foo', spy);
@@ -67,9 +123,13 @@ describe('axe._cache', function () {
       });
 
       it('should not re-calculate when set to `undefined`', function () {
-        axe._cache.get('Foo', undefined);
-        assert.isUndefined(
-          axe._cache.get('Foo', 'should not be set to a string')
+        axe._cache.get('Foo', function () {
+          return false;
+        });
+        assert.isBoolean(
+          axe._cache.get('Foo', function () {
+            return 'should not be set to a string because Foo is in the cache';
+          })
         );
       });
 
@@ -80,7 +140,9 @@ describe('axe._cache', function () {
       });
 
       it('should override a value from `get()` with a `set()` value', function () {
-        axe._cache.get('foo', 'baz');
+        axe._cache.get('foo', function () {
+          return 'baz';
+        });
         axe._cache.set('foo', 'bar');
         assert.equal(axe._cache.get('foo'), 'bar');
       });
