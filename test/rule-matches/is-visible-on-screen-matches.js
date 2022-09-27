15e950cde2107069b3c5aea2f03b9255efc91381
Steven Lambert <steven.lambert@deque.com>
2022-09-27 08:58:03 -0600

    use virtual node

--- test/rule-matches/is-visible-on-screen-matches.js
+++ test/rule-matches/is-visible-on-screen-matches.js
@@ -1,32 +1,30 @@
-describe('is-visible-on-screen-matches', function () {
+describe('is-visible-on-screen-matches', () => {
   'use strict';
 
-  var rule;
-  var fixture = document.getElementById('fixture');
-  var fixtureSetup = axe.testUtils.fixtureSetup;
+  const rule = axe.utils.getRule('avoid-inline-spacing');
+  const queryFixture = axe.testUtils.queryFixture;
 
-  beforeEach(function () {
-    fixture.innerHTML = '';
-    rule = axe.utils.getRule('avoid-inline-spacing');
+  it('returns true for visible elements', () => {
+    const vNode = queryFixture('<p id="target">Hello world</p>');
+    assert.isTrue(rule.matches(null, vNode));
   });
 
-  it('returns true for visible elements', function () {
-    fixtureSetup('<p id="target">Hello world</p>');
-    assert.isTrue(rule.matches(fixture.firstChild));
+  it('returns false for elements with hidden', () => {
+    const vNode = queryFixture('<p id="target" hidden>Hello world</p>');
+    assert.isFalse(rule.matches(null, vNode));
   });
 
-  it('returns false for elements with hidden', function () {
-    fixtureSetup('<p id="target" hidden>Hello world</p>');
-    assert.isFalse(rule.matches(fixture.firstChild));
+  it('returns true for visible elements with aria-hidden="true"', () => {
+    const vNode = queryFixture(
+      '<p id="target" aria-hidden="true">Hello world</p>'
+    );
+    assert.isTrue(rule.matches(null, vNode));
   });
 
-  it('returns true for visible elements with aria-hidden="true"', function () {
-    fixtureSetup('<p id="target" aria-hidden="true">Hello world</p>');
-    assert.isTrue(rule.matches(fixture.firstChild));
-  });
-
-  it('returns false for opacity:0 elements with accessible text', function () {
-    fixtureSetup('<p id="target" style="opacity:0">Hello world</p>');
-    assert.isFalse(rule.matches(fixture.firstChild));
+  it('returns false for opacity:0 elements with accessible text', () => {
+    const vNode = queryFixture(
+      '<p id="target" style="opacity:0">Hello world</p>'
+    );
+    assert.isFalse(rule.matches(null, vNode));
   });
 });
