54a8116546b056569d1551b8ea3c8b01fa7d8df5
Wilco Fiers <WilcoFiers@users.noreply.github.com>
2022-10-04 10:47:43 +0200

    fix(label): avoid passing labels because of an input[value] (#3688)
    * fix(label): avoid passing labels because of an input[value]
    
    * correct label examples
    
    * Add textarea integration test
    
    * Fix failing test
    
    * Use sanitize
    
    * Add addition test for explicit-evaluate

--- test/checks/label/implicit.js
+++ test/checks/label/implicit.js
@@ -1,79 +1,109 @@
-describe('implicit-label', function() {
-  'use strict';
+describe('implicit-label', () => {
+  const fixtureSetup = axe.testUtils.fixtureSetup;
+  const checkSetup = axe.testUtils.checkSetup;
+  const checkEvaluate = axe.testUtils.getCheckEvaluate('implicit-label');
+  const checkContext = axe.testUtils.MockCheckContext();
 
-  var fixture = document.getElementById('fixture');
-  var fixtureSetup = axe.testUtils.fixtureSetup;
-
-  afterEach(function() {
-    fixture.innerHTML = '';
-    axe._tree = undefined;
+  afterEach(() => {
+    checkContext.reset();
   });
 
-  it('should return false if an empty label is present', function() {
-    fixtureSetup('<label><input type="text" id="target"></label>');
-    var node = fixture.querySelector('#target');
-    var virtualNode = axe.utils.getNodeFromTree(node);
-    assert.isFalse(
-      axe.testUtils.getCheckEvaluate('implicit-label')(null, {}, virtualNode)
+  it('returns false if an empty label is present', () => {
+    const params = checkSetup('<label><input type="text" id="target"></label>');
+    assert.isFalse(checkEvaluate.apply(checkContext, params));
+  });
+
+  it('returns false on an empty label when then control has a value', () => {
+    const params = checkSetup(
+      '<label><input type="text" id="target" value="snacks"></label>'
     );
+    assert.isFalse(checkEvaluate.apply(checkContext, params));
   });
 
-  it('should return false if an invisible non-empty label is present', function() {
-    fixtureSetup(
+  it('returns false if an invisible non-empty label is present', () => {
+    const params = checkSetup(
       '<label><span style="display: none">Text</span> <input type="text" id="target"></label>'
     );
-    var node = fixture.querySelector('#target');
-    var virtualNode = axe.utils.getNodeFromTree(node);
-    assert.isFalse(
-      axe.testUtils.getCheckEvaluate('implicit-label')(null, {}, virtualNode)
-    );
+    assert.isFalse(checkEvaluate.apply(checkContext, params));
   });
 
-  it('should return true if a non-empty label is present', function() {
-    fixtureSetup('<label>Text <input type="text" id="target"></label>');
-    var node = fixture.querySelector('#target');
-    var virtualNode = axe.utils.getNodeFromTree(node);
-    assert.isTrue(
-      axe.testUtils.getCheckEvaluate('implicit-label')(null, {}, virtualNode)
+  it('returns true if a non-empty label is present', () => {
+    const params = checkSetup(
+      '<label>Text <input type="text" id="target"></label>'
     );
+    assert.isTrue(checkEvaluate.apply(checkContext, params));
   });
 
-  it('should return false if a label is not present', function() {
-    var node = document.createElement('input');
+  it('returns false if a label is not present', () => {
+    const node = document.createElement('input');
     node.type = 'text';
     fixtureSetup(node);
-
-    var virtualNode = axe.utils.getNodeFromTree(node);
-    assert.isFalse(
-      axe.testUtils.getCheckEvaluate('implicit-label')(null, {}, virtualNode)
-    );
+    const virtualNode = axe.utils.getNodeFromTree(node);
+    assert.isFalse(checkEvaluate.call(checkContext, null, {}, virtualNode));
   });
 
-  describe('SerialVirtualNode', function() {
-    it('should return false if no implicit label', function() {
-      var virtualNode = new axe.SerialVirtualNode({
+  describe('data', () => {
+    it('is null if there is no label', () => {
+      const params = checkSetup('<input type="text" id="target">');
+      checkEvaluate.apply(checkContext, params);
+      assert.isNull(checkContext._data);
+    });
+
+    it('includes the implicit label if one is set', () => {
+      const params = checkSetup(
+        '<label>Some <input type="text" id="target"> text</label>'
+      );
+      checkEvaluate.apply(checkContext, params);
+      assert.deepEqual(checkContext._data, { implicitLabel: 'Some text' });
+    });
+
+    it('has { implicitLabel: "" } when the label is empty', () => {
+      const params = checkSetup(
+        '<label> <input type="text" id="target"> </label>'
+      );
+      checkEvaluate.apply(checkContext, params);
+      assert.deepEqual(checkContext._data, { implicitLabel: '' });
+    });
+  });
+
+  describe('relatedNodes', () => {
+    it('is null if there is no label', () => {
+      const params = checkSetup('<input type="text" id="target">');
+      checkEvaluate.apply(checkContext, params);
+      assert.isEmpty(checkContext._relatedNodes);
+    });
+
+    it('includes the nearest label as its related node', () => {
+      const params = checkSetup(
+        '<label id="lbl"> <input type="text" id="target"> </label>'
+      );
+      checkEvaluate.apply(checkContext, params);
+      const ids = checkContext._relatedNodes.map(node => '#' + node.id);
+      assert.deepEqual(ids, ['#lbl']);
+    });
+  });
+
+  describe('SerialVirtualNode', () => {
+    it('returns false if no implicit label', () => {
+      const virtualNode = new axe.SerialVirtualNode({
         nodeName: 'input',
         attributes: {
           type: 'text'
         }
       });
       virtualNode.parent = null;
-
-      assert.isFalse(
-        axe.testUtils.getCheckEvaluate('implicit-label')(null, {}, virtualNode)
-      );
+      assert.isFalse(checkEvaluate.call(checkContext, null, {}, virtualNode));
     });
 
-    it('should return undefined if tree is not complete', function() {
-      var virtualNode = new axe.SerialVirtualNode({
+    it('returns undefined if tree is not complete', () => {
+      const virtualNode = new axe.SerialVirtualNode({
         nodeName: 'input',
         attributes: {
           type: 'text'
         }
       });
-
       assert.isUndefined(
-        axe.testUtils.getCheckEvaluate('implicit-label')(null, {}, virtualNode)
+        checkEvaluate.call(checkContext, null, {}, virtualNode)
       );
     });
   });
