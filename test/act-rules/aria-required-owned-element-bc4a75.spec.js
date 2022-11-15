98ab2e6b177aacffb6faa7d55eaedf92222c851d
Wilco Fiers <wilco.fiers@deque.com>
2022-11-15 14:51:17 +0100

    Update aria-required-attr

--- test/act-rules/aria-required-owned-element-bc4a75.spec.js
+++ test/act-rules/aria-required-owned-element-bc4a75.spec.js
@@ -1,7 +1,5 @@
 require('./act-runner.js')({
   id: 'bc4a75',
   title: 'ARIA required owned elements',
-  axeRules: ['aria-required-children'],
-  // Awaiting https://github.com/act-rules/act-rules.github.io/pull/1950
-  skipTests: ['52c725e462af074a3559cf4bf4d4dd2386168938']
+  axeRules: ['aria-required-children']
 });
