df5c9302ed10698329f958c74ef33992c085b647
Steven Lambert <steven.lambert@deque.com>
2023-01-16 14:26:46 -0700

    typos

--- test/commons/dom/is-inert.js
+++ test/commons/dom/is-inert.js
@@ -1,26 +1,26 @@
-describe('dom.is-inert', () => {
+describe('dom.isInert', () => {
   const isInert = axe.commons.dom.isInert;
   const { queryFixture } = axe.testUtils;
 
-  it('should return true for element with "inert=false`', () => {
+  it('returns true for element with "inert=false`', () => {
     const vNode = queryFixture('<div id="target" inert="false"></div>');
 
     assert.isTrue(isInert(vNode));
   });
 
-  it('should return true for element with "inert`', () => {
+  it('returns true for element with "inert`', () => {
     const vNode = queryFixture('<div id="target" inert></div>');
 
     assert.isTrue(isInert(vNode));
   });
 
-  it('should return false for element without inert', () => {
+  it('returns false for element without inert', () => {
     const vNode = queryFixture('<div id="target"></div>');
 
     assert.isFalse(isInert(vNode));
   });
 
-  it('should return true for ancestor with inert', () => {
+  it('returns true for ancestor with inert', () => {
     const vNode = queryFixture(
       '<div inert><div><div id="target"></div></div></div>'
     );
@@ -29,7 +29,7 @@ describe('dom.is-inert', () => {
   });
 
   describe('options.skipAncestors', () => {
-    it('should return false for ancestor with inert', () => {
+    it('returns false for ancestor with inert', () => {
       const vNode = queryFixture(
         '<div inert><div><div id="target"></div></div></div>'
       );
