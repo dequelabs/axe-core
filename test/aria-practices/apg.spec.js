ce7ad1b8cadc91240da77c499a2c6dcb217f09ce
Steven Lambert <steven.lambert@deque.com>
2023-04-05 09:11:24 -0600

    tests: fix nightly apg tests

--- test/aria-practices/apg.spec.js
+++ test/aria-practices/apg.spec.js
@@ -6,12 +6,15 @@ const { assert } = require('chai');
 const globby = require('globby');
 
 describe('aria-practices', function () {
-  // Use path.resolve rather than require.resolve because APG has no package.json
+  // Use path.resolve rather than require.resolve because APG package.json main file does not exist
   const apgPath = path.resolve(__dirname, '../../node_modules/aria-practices/');
-  const filePaths = globby.sync(`${apgPath}/examples/**/*.html`);
-  const testFiles = filePaths.map(
-    fileName => fileName.split('/aria-practices/examples/')[1]
+  const filePaths = globby.sync(
+    `${apgPath}/content/patterns/*/**/examples/*.html`
   );
+  const testFiles = filePaths.map(
+    fileName => fileName.split('/aria-practices/content/patterns/')[1]
+  );
+
   const addr = `http://localhost:9876/node_modules/aria-practices/`;
   let driver, axeSource;
   this.timeout(50000);
@@ -32,56 +35,40 @@ describe('aria-practices', function () {
       'color-contrast',
       'target-size',
       'heading-order', // w3c/aria-practices#2119
-      'list', // w3c/aria-practices#2118
       'scrollable-region-focusable' // w3c/aria-practices#2114
-    ],
-    'feed/feedDisplay.html': ['page-has-heading-one'], // w3c/aria-practices#2120
-    // "page within a page" type thing going on
-    'menubar/menubar-navigation.html': [
-      'aria-allowed-role',
-      'landmark-banner-is-top-level',
-      'landmark-contentinfo-is-top-level'
-    ],
-    // "page within a page" type thing going on
-    'treeview/treeview-navigation.html': [
-      'aria-allowed-role',
-      'landmark-banner-is-top-level',
-      'landmark-contentinfo-is-top-level'
-    ],
-    // https://github.com/w3c/aria-practices/issues/2199
-    'button/button_idl.html': ['aria-allowed-attr'],
-    // https://github.com/w3c/aria-practices/issues/2285
-    'checkbox/checkbox.html': ['empty-table-header'],
-    'dialog-modal/datepicker-dialog.html': ['empty-table-header'],
-    // https://github.com/w3c/aria-practices/issues/2505
-    'landmarks/search.html': ['link-in-text-block']
+    ]
   };
 
   // Not an actual content file
   const skippedPages = [
-    'index.html', // Not an example, just an index file
-    'js/notice.html', // Embedded into another page
-    'toolbar/help.html' // Embedded into another page
+    'toolbar/examples/help.html' // Embedded into another page
   ];
+
+  it('finds examples', () => {
+    assert.isTrue(testFiles.length > 0);
+  });
 
   testFiles
     .filter(filePath => !skippedPages.includes(filePath))
     .forEach(filePath => {
       it(`finds no issue in "${filePath}"`, async () => {
-        await driver.get(`${addr}/examples/${filePath}`);
+        await driver.get(`${addr}content/patterns/${filePath}`);
 
-        const builder = new AxeBuilder(driver, axeSource);
-        builder.disableRules([
-          ...disabledRules['*'],
-          ...(disabledRules[filePath] || [])
-        ]);
+        const builder = new AxeBuilder(driver, axeSource)
+          // Support table has no title and has duplicate ids
+          .exclude('#at-support')
+          .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+          .disableRules([
+            ...disabledRules['*'],
+            ...(disabledRules[filePath] || [])
+          ]);
 
         const { violations } = await builder.analyze();
         const issues = violations.map(({ id, nodes }) => ({
           id,
           issues: nodes.length
         }));
-        assert.lengthOf(issues, 0);
+        assert.lengthOf(issues, 0, issues.map(({ id }) => id).join(', '));
       });
     });
 });
