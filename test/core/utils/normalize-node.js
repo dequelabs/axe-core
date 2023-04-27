ce339ca50f1e6b05fe1864877a0875e7b8adf334
Steven Lambert <steven.lambert@deque.com>
2023-04-27 15:26:39 -0600

    refactor code

--- test/core/utils/normalize-node.js
+++ test/core/utils/normalize-node.js
@@ -2,19 +2,35 @@ describe('axe.utils.normalizeNode', () => {
   const normalizeNode = axe.utils.normalizeNode;
   const queryFixture = axe.testUtils.queryFixture;
 
-  it('returns the virtual node and DOM node when passed a virtual node', () => {
-    const expected = queryFixture('<div id="target">Hello</div>');
+  it('works with virtual nodes', () => {
+    const element = queryFixture('<div id="target">Hello</div>');
 
-    const { vNode, node } = normalizeNode(expected);
-    assert.equal(expected, vNode);
-    assert.equal(expected.actualNode, node);
+    const { vNode, node } = normalizeNode(element);
+    assert.equal(element, vNode);
+    assert.equal(element.actualNode, node);
   });
 
-  it('returns the virtual node and DOM node when passed a virtual node', () => {
-    const expected = queryFixture('<div id="target">Hello</div>');
+  it('works with serial virtual nodes', () => {
+    const element = new axe.SerialVirtualNode({ nodeName: 'div' });
 
-    const { vNode, node } = normalizeNode(expected.actualNode);
-    assert.equal(expected, vNode);
-    assert.equal(expected.actualNode, node);
+    const { vNode, node } = normalizeNode(element);
+    assert.equal(element, vNode);
+    assert.isUndefined(node);
+  });
+
+  it('works with elements', () => {
+    const element = queryFixture('<div id="target">Hello</div>');
+
+    const { vNode, node } = normalizeNode(element.actualNode);
+    assert.equal(element, vNode);
+    assert.equal(element.actualNode, node);
+  });
+
+  it('works with non-elements', () => {
+    const element = document.createTextNode('my text node');
+
+    const { vNode, node } = normalizeNode(element);
+    assert.isNull(vNode);
+    assert.equal(element, node);
   });
 });
