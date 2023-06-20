bda383886a55ab1b53ce88923db60797208edca6
Wilco Fiers <wilco.fiers@deque.com>
2023-06-20 13:01:55 +0200

    fix: avoid problems from element IDs that exist on object prototype

--- test/core/utils/find-by.js
+++ test/core/utils/find-by.js
@@ -40,4 +40,36 @@ describe('axe.utils.findBy', function () {
   it('should not throw if passed falsey first parameter', function () {
     assert.isUndefined(axe.utils.findBy(null, 'id', 'macaque'));
   });
+
+  it('ignores any non-object elements in the array', function () {
+    var array = [
+      {
+        id: 'monkeys',
+        foo: 'bar'
+      },
+      'bananas',
+      true,
+      null,
+      123
+    ];
+
+    assert.equal(axe.utils.findBy(array, 'id', 'monkeys'), array[0]);
+  });
+
+  it('only looks at owned properties', function () {
+    var array = [
+      {
+        id: 'monkeys',
+        Constructor: 'monkeys'
+      },
+      {
+        id: 'bananas'
+      }
+    ];
+
+    assert.deepEqual(axe.utils.findBy(array, 'Constructor', 'monkeys'), {
+      id: 'monkeys',
+      Constructor: 'monkeys'
+    });
+  });
 });
