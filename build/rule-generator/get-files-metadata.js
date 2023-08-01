2cc5547634ee783701675631ee3978129707e6f0
Wilco Fiers <WilcoFiers@users.noreply.github.com>
2023-08-01 16:28:08 +0200

    feat: Deprecate impact on checks, use rules instead (#4114)
    * feat: Deprecate impact on checks, use rules instead
    
    * Update docs

--- build/rule-generator/get-files-metadata.js
+++ build/rule-generator/get-files-metadata.js
@@ -25,6 +25,7 @@ const getRuleSpecFileMeta = (ruleName, ruleHasMatches, ruleChecks) => {
     content: JSON.stringify(
       {
         id: ruleName,
+        impact: '',
         selector: '*',
         ...(ruleHasMatches && {
           matches: `${ruleName}-matches`
@@ -121,7 +122,6 @@ const getCheckSpecFileMeta = (name, dir) => {
         id: `${name}`,
         evaluate: `${name}-evaluate`,
         metadata: {
-          impact: '',
           messages: {
             pass: '',
             fail: '',
