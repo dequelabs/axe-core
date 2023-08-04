7ace8bbcbdbb4b093e85d0d1266fec2548154a81
Steven Lambert <steven.lambert@deque.com>
2023-08-04 08:13:56 -0600

    udpate tests

--- test/commons/math/get-offset.js
+++ test/commons/math/get-offset.js
@@ -5,8 +5,8 @@ describe('getOffset', () => {
 
   it('returns center to edge of circle when both are undersized', () => {
     const fixture = fixtureSetup(`
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 50px; left: 0">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 50px; left: 0">&nbsp;</button>
     `);
     const nodeA = fixture.children[1];
     const nodeB = fixture.children[3];
@@ -15,8 +15,8 @@ describe('getOffset', () => {
 
   it('returns center to edge of square when one is undersized', () => {
     const fixture = fixtureSetup(`
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
-      <button style="width: 30px; height: 30px; margin: 0; position: absolute; top: 50px; left: 0">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
+      <button style="width: 30px; height: 30px; margin: 0; padding: 0; position: absolute; top: 50px; left: 0">&nbsp;</button>
     `);
     const nodeA = fixture.children[1];
     const nodeB = fixture.children[3];
@@ -25,18 +25,18 @@ describe('getOffset', () => {
 
   it('returns center to corner of square when at a diagonal', () => {
     const fixture = fixtureSetup(`
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
-      <button style="width: 30px; height: 30px; margin: 0; position: absolute; top: 50px; left: 50px">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
+      <button style="width: 30px; height: 30px; margin: 0; padding: 0; position: absolute; top: 50px; left: 50px">&nbsp;</button>
     `);
     const nodeA = fixture.children[1];
     const nodeB = fixture.children[3];
-    assert.closeTo(getOffset(nodeA, nodeB), 61.5, round);
+    assert.closeTo(getOffset(nodeA, nodeB), 63.6, round);
   });
 
   it('returns 0 if nodeA is overlapped by nodeB', () => {
     const fixture = fixtureSetup(`
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
-      <button style="width: 30px; height: 30px; margin: 0; position: absolute; top: -10px; left: -10px">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
+      <button style="width: 30px; height: 30px; margin: 0; padding: 0; position: absolute; top: -10px; left: -10px">&nbsp;</button>
     `);
     const nodeA = fixture.children[1];
     const nodeB = fixture.children[3];
@@ -45,8 +45,8 @@ describe('getOffset', () => {
 
   it('returns 0 if nodeB is overlapped by nodeA', () => {
     const fixture = fixtureSetup(`
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
-      <button style="width: 30px; height: 30px; margin: 0; position: absolute; top: -10px; left: -10px">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
+      <button style="width: 30px; height: 30px; margin: 0; padding: 0; position: absolute; top: -10px; left: -10px">&nbsp;</button>
     `);
     const nodeA = fixture.children[3];
     const nodeB = fixture.children[1];
@@ -55,8 +55,8 @@ describe('getOffset', () => {
 
   it('subtracts minNeighbourRadius from center-to-center calculations', () => {
     const fixture = fixtureSetup(`
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
-      <button style="width: 10px; height: 10px; margin: 0; position: absolute; top: 50px; left: 0">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 0; left: 0">&nbsp;</button>
+      <button style="width: 10px; height: 10px; margin: 0; padding: 0; position: absolute; top: 50px; left: 0">&nbsp;</button>
     `);
     const nodeA = fixture.children[1];
     const nodeB = fixture.children[3];
--- test/commons/math/split-rects.js
+++ test/commons/math/split-rects.js
@@ -1,19 +1,16 @@
 describe('splitRects', () => {
   const splitRects = axe.commons.math.splitRects;
-  function createRect(x, y, width, height) {
-    return new DOMRect(x, y, width, height);
-  }
 
   it('returns the original rect if there is no clipping rect', () => {
-    const rectA = createRect(0, 0, 100, 50);
+    const rectA = new DOMRect(0, 0, 100, 50);
     const rects = splitRects(rectA, []);
     assert.lengthOf(rects, 1);
     assert.deepEqual(rects[0], rectA);
   });
 
   it('returns the original rect if there is no overlap', () => {
-    const rectA = createRect(0, 0, 100, 50);
-    const rectB = createRect(0, 50, 50, 50);
+    const rectA = new DOMRect(0, 0, 100, 50);
+    const rectB = new DOMRect(0, 50, 50, 50);
     const rects = splitRects(rectA, [rectB]);
     assert.lengthOf(rects, 1);
     assert.deepEqual(rects[0], rectA);
@@ -21,46 +18,46 @@ describe('splitRects', () => {
 
   describe('with one overlapping rect', () => {
     it('returns one rect if overlaps covers two corners', () => {
-      const rectA = createRect(0, 0, 100, 50);
-      const rectB = createRect(40, 0, 100, 50);
+      const rectA = new DOMRect(0, 0, 100, 50);
+      const rectB = new DOMRect(40, 0, 100, 50);
       const rects = splitRects(rectA, [rectB]);
       assert.lengthOf(rects, 1);
-      assert.deepEqual(rects[0], createRect(0, 0, 40, 50));
+      assert.deepEqual(rects[0], new DOMRect(0, 0, 40, 50));
     });
 
     it('returns two rects if overlap covers one corner', () => {
-      const rectA = createRect(0, 0, 100, 100);
-      const rectB = createRect(50, 50, 50, 50);
+      const rectA = new DOMRect(0, 0, 100, 100);
+      const rectB = new DOMRect(50, 50, 50, 50);
       const rects = splitRects(rectA, [rectB]);
       assert.lengthOf(rects, 2);
-      assert.deepEqual(rects[0], createRect(0, 0, 100, 50));
-      assert.deepEqual(rects[1], createRect(0, 0, 50, 100));
+      assert.deepEqual(rects[0], new DOMRect(0, 0, 100, 50));
+      assert.deepEqual(rects[1], new DOMRect(0, 0, 50, 100));
     });
 
     it('returns three rects if overlap covers an edge, but no corner', () => {
-      const rectA = createRect(0, 0, 100, 150);
-      const rectB = createRect(50, 50, 50, 50);
+      const rectA = new DOMRect(0, 0, 100, 150);
+      const rectB = new DOMRect(50, 50, 50, 50);
       const rects = splitRects(rectA, [rectB]);
       assert.lengthOf(rects, 3);
-      assert.deepEqual(rects[0], createRect(0, 0, 100, 50));
-      assert.deepEqual(rects[1], createRect(0, 100, 100, 50));
-      assert.deepEqual(rects[2], createRect(0, 0, 50, 150));
+      assert.deepEqual(rects[0], new DOMRect(0, 0, 100, 50));
+      assert.deepEqual(rects[1], new DOMRect(0, 100, 100, 50));
+      assert.deepEqual(rects[2], new DOMRect(0, 0, 50, 150));
     });
 
     it('returns four rects if overlap sits in the middle, touching no corner', () => {
-      const rectA = createRect(0, 0, 150, 150);
-      const rectB = createRect(50, 50, 50, 50);
+      const rectA = new DOMRect(0, 0, 150, 150);
+      const rectB = new DOMRect(50, 50, 50, 50);
       const rects = splitRects(rectA, [rectB]);
       assert.lengthOf(rects, 4);
-      assert.deepEqual(rects[0], createRect(0, 0, 150, 50));
-      assert.deepEqual(rects[1], createRect(100, 0, 50, 150));
-      assert.deepEqual(rects[2], createRect(0, 100, 150, 50));
-      assert.deepEqual(rects[3], createRect(0, 0, 50, 150));
+      assert.deepEqual(rects[0], new DOMRect(0, 0, 150, 50));
+      assert.deepEqual(rects[1], new DOMRect(100, 0, 50, 150));
+      assert.deepEqual(rects[2], new DOMRect(0, 100, 150, 50));
+      assert.deepEqual(rects[3], new DOMRect(0, 0, 50, 150));
     });
 
     it('returns no rects if overlap covers the entire input rect', () => {
-      const rectA = createRect(0, 0, 100, 50);
-      const rectB = createRect(-50, -50, 400, 400);
+      const rectA = new DOMRect(0, 0, 100, 50);
+      const rectB = new DOMRect(-50, -50, 400, 400);
       const rects = splitRects(rectA, [rectB]);
       assert.lengthOf(rects, 0);
     });
@@ -68,29 +65,29 @@ describe('splitRects', () => {
 
   describe('with multiple overlaps', () => {
     it('can return a single rect two overlaps each cover an edge', () => {
-      const rectA = createRect(0, 0, 150, 50);
-      const rectB = createRect(0, 0, 50, 50);
-      const rectC = createRect(100, 0, 50, 50);
+      const rectA = new DOMRect(0, 0, 150, 50);
+      const rectB = new DOMRect(0, 0, 50, 50);
+      const rectC = new DOMRect(100, 0, 50, 50);
       const rects = splitRects(rectA, [rectB, rectC]);
       assert.lengthOf(rects, 1);
-      assert.deepEqual(rects[0], createRect(50, 0, 50, 50));
+      assert.deepEqual(rects[0], new DOMRect(50, 0, 50, 50));
     });
 
     it('can recursively clips regions', () => {
-      const rectA = createRect(0, 0, 150, 100);
-      const rectB = createRect(0, 50, 50, 50);
-      const rectC = createRect(100, 50, 50, 50);
+      const rectA = new DOMRect(0, 0, 150, 100);
+      const rectB = new DOMRect(0, 50, 50, 50);
+      const rectC = new DOMRect(100, 50, 50, 50);
       const rects = splitRects(rectA, [rectB, rectC]);
       assert.lengthOf(rects, 3);
-      assert.deepEqual(rects[0], createRect(0, 0, 150, 50));
-      assert.deepEqual(rects[1], createRect(50, 0, 100, 50));
-      assert.deepEqual(rects[2], createRect(50, 0, 50, 100));
+      assert.deepEqual(rects[0], new DOMRect(0, 0, 150, 50));
+      assert.deepEqual(rects[1], new DOMRect(50, 0, 100, 50));
+      assert.deepEqual(rects[2], new DOMRect(50, 0, 50, 100));
     });
 
     it('returns no rects if overlap covers the entire input rect', () => {
-      const rectA = createRect(0, 0, 100, 50);
-      const rectB = createRect(50, 50, 200, 200);
-      const rectC = createRect(-50, -50, 200, 200);
+      const rectA = new DOMRect(0, 0, 100, 50);
+      const rectB = new DOMRect(50, 50, 200, 200);
+      const rectC = new DOMRect(-50, -50, 200, 200);
       const rects = splitRects(rectA, [rectB, rectC]);
       assert.lengthOf(rects, 0);
     });
