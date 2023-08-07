1bdd71ea1e8634ff88732ccf8ce86030ad4d1faa
Steven Lambert <steven.lambert@deque.com>
2023-08-07 13:46:55 -0600

    fix test

--- test/commons/dom/get-target-size.js
+++ test/commons/dom/get-target-size.js
@@ -4,7 +4,7 @@ describe('get-target-size', () => {
 
   it('returns the bounding rect when unobscured', () => {
     const vNode = queryFixture('<button id="target">x</button>');
-    const { rect } = getTargetSize(vNode);
+    const rect = getTargetSize(vNode);
     assert.deepEqual(rect, vNode.actualNode.getBoundingClientRect());
   });
 
@@ -13,7 +13,7 @@ describe('get-target-size', () => {
       <button id="target" style="width: 30px; height: 40px; position: absolute; left: 10px; top: 5px">x</button>
       <div style="position: absolute; left: 30px; top: 0; width: 50px; height: 50px;"></div>
     `);
-    const { rect } = getTargetSize(vNode);
+    const rect = getTargetSize(vNode);
     assert.deepEqual(rect, new DOMRect(10, 5, 20, 40));
   });
 
@@ -22,7 +22,7 @@ describe('get-target-size', () => {
       <button id="target" style="width: 30px; height: 40px; position: absolute; left: 10px; top: 5px">x</button>
       <div style="position: absolute; left: 30px; top: 0; width: 50px; height: 50px; pointer-events: none"></div>
     `);
-    const { rect } = getTargetSize(vNode);
+    const rect = getTargetSize(vNode);
     assert.deepEqual(rect, new DOMRect(10, 5, 30, 40));
   });
 
@@ -31,7 +31,7 @@ describe('get-target-size', () => {
       <button id="target" style="width: 30px; height: 40px; position: absolute; left: 10px; top: 5px">x</button>
       <div style="position: absolute; left: 60px; top: 0; width: 50px; height: 50px;"></div>
     `);
-    const { rect } = getTargetSize(vNode);
+    const rect = getTargetSize(vNode);
     assert.deepEqual(rect, new DOMRect(10, 5, 30, 40));
   });
 
@@ -41,7 +41,7 @@ describe('get-target-size', () => {
       <div style="position: absolute; left: 30px; top: 0; width: 50px; height: 50px;"></div>
       <div style="position: absolute; left: 0px; top: 0px; width: 50px; height: 10px"></div>
     `);
-    const { rect } = getTargetSize(vNode);
+    const rect = getTargetSize(vNode);
     assert.deepEqual(rect, new DOMRect(10, 10, 20, 35));
   });
 });
