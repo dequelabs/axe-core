f1af598cf3a9d0d99c05ae2e06fd203ae4936b00
Steven Lambert <2433219+straker@users.noreply.github.com>
2023-11-28 11:51:20 -0700

    chore: fix prettier format for built files (#4258)
    * chore: fix prettier format for built files
    
    * catch

--- build/tasks/configure.js
+++ build/tasks/configure.js
@@ -1,15 +1,15 @@
 /*eslint-env node */
 'use strict';
-var buildRules = require('../configure');
-var format = require('../shared/format');
+const buildRules = require('../configure');
+const format = require('../shared/format');
 
 module.exports = function (grunt) {
   grunt.registerMultiTask(
     'configure',
     'Task for configuring rules and checks',
     function () {
-      var done = this.async();
-      var options = this.options({
+      const done = this.async();
+      const options = this.options({
         rules: ['lib/rules/**/*.json'],
         checks: ['lib/checks/**/*.json'],
         misc: ['lib/misc/**/*.json'],
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
       });
 
       this.files.forEach(function (file) {
-        var match = file.dest.auto.match(/\.([a-z]{2,3})\.js/);
+        const match = file.dest.auto.match(/\.([a-z]{2,3})\.js/);
         if (match) {
           options.locale = match[1];
         }
@@ -28,12 +28,15 @@ module.exports = function (grunt) {
 
           // Format the content so Prettier doesn't create a diff after running.
           // See https://github.com/dequelabs/axe-core/issues/1310.
-          const descriptionsContent = format(
-            result.descriptions,
-            file.dest.descriptions
-          );
-          grunt.file.write(file.dest.descriptions, descriptionsContent);
-          done();
+          format(result.descriptions, file.dest.descriptions)
+            .then(descriptionsContent => {
+              grunt.file.write(file.dest.descriptions, descriptionsContent);
+              done();
+            })
+            .catch(err => {
+              console.error(err.message);
+              done(false);
+            });
         });
       });
     }
