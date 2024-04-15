b0f7bea73a8c985ee513961cd4ecc9c955aa9add
Steven Lambert <2433219+straker@users.noreply.github.com>
2024-04-15 14:10:51 -0600

    fix(label-content-name-mismatch): use only the visible text for the check

--- test/commons/text/visible-virtual.js
+++ test/commons/text/visible-virtual.js
@@ -1,159 +1,199 @@
-describe('text.visible', function () {
+describe('text.visible', () => {
   'use strict';
 
-  var fixture = document.getElementById('fixture');
-  var shadowSupported = axe.testUtils.shadowSupport.v1;
-  var visibleVirtual = axe.commons.text.visibleVirtual;
+  const fixture = document.getElementById('fixture');
+  const shadowSupported = axe.testUtils.shadowSupport.v1;
+  const visibleVirtual = axe.commons.text.visibleVirtual;
 
-  afterEach(function () {
-    document.getElementById('fixture').innerHTML = '';
+  const fontApiSupport = !!document.fonts;
+
+  before(done => {
+    if (!fontApiSupport) {
+      done();
+    }
+
+    const materialFont = new FontFace(
+      'Material Icons',
+      'url(https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2)'
+    );
+    materialFont.load().then(() => {
+      document.fonts.add(materialFont);
+      done();
+    });
   });
 
-  describe('non-screen-reader', function () {
-    it('should not return elements with visibility: hidden', function () {
+  describe('non-screen-reader', () => {
+    it('should not return elements with visibility: hidden', () => {
       fixture.innerHTML = 'Hello<span style="visibility: hidden;">Hi</span>';
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0]), 'Hello');
     });
 
-    it('should handle implicitly recursive calls', function () {
+    it('should handle implicitly recursive calls', () => {
       fixture.innerHTML = 'Hello<span><span>Hi</span></span>';
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0]), 'HelloHi');
     });
 
-    it('should handle explicitly recursive calls', function () {
+    it('should handle explicitly recursive calls', () => {
       fixture.innerHTML = 'Hello<span><span>Hi</span></span>';
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], null, false), 'HelloHi');
     });
 
-    it('should handle non-recursive calls', function () {
+    it('should handle non-recursive calls', () => {
       fixture.innerHTML = 'Hello<span><span>Hi</span></span>';
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], null, true), 'Hello');
     });
 
-    it('should know how visibility works', function () {
+    it('should know how visibility works', () => {
       fixture.innerHTML =
         'Hello <span style="visibility: hidden;">' +
         '<span style="visibility: visible;">Hi</span>' +
         '</span>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0]), 'Hello Hi');
     });
 
-    it('should not return elements with display: none', function () {
+    it('should not return elements with display: none', () => {
       fixture.innerHTML =
         'Hello<span style="display: none;"><span>Hi</span></span>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0]), 'Hello');
     });
 
-    it('should trim the result', function () {
+    it('should trim the result', () => {
       fixture.innerHTML =
         '   &nbsp;\u00A0    Hello  &nbsp;\r\n   Hi     \n \n &nbsp; \n   ';
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0]), 'Hello Hi');
     });
 
-    it('should ignore script and style tags', function () {
+    it('should ignore script and style tags', () => {
       fixture.innerHTML =
         '<script> // hello </script><style> /*hello */</style>' + 'Hello';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0]), 'Hello');
     });
 
-    it('should not take into account position of parents', function () {
+    it('should not take into account position of parents', () => {
       fixture.innerHTML =
         '<div style="position: absolute; top: -9999px;">' +
         '<div style="position: absolute; top: 10000px;">Hello</div>' +
         '</div>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0]), 'Hello');
     });
 
+    (fontApiSupport ? it : it.skip)(
+      'returns visible text including ligature icon',
+      () => {
+        fixture.innerHTML =
+          'next page <span style="font-family: \'Material Icons\'">delete</span>';
+
+        const tree = axe.utils.getFlattenedTree(fixture);
+        assert.equal(visibleVirtual(tree[0]), 'next page delete');
+      }
+    );
+
+    (fontApiSupport ? it : it.skip)(
+      'returns visible text excluding ligature icon when passed ignoreIconLigature option',
+      () => {
+        fixture.innerHTML =
+          'next page <span style="font-family: \'Material Icons\'">delete</span>';
+
+        const tree = axe.utils.getFlattenedTree(fixture);
+        assert.equal(
+          visibleVirtual(tree[0], false, false, {
+            ignoreIconLigature: true
+          }),
+          'next page'
+        );
+      }
+    );
+
     (shadowSupported ? it : xit)(
       'should correctly handle slotted elements',
-      function () {
+      () => {
         function createContentSlotted() {
-          var group = document.createElement('div');
+          const group = document.createElement('div');
           group.innerHTML = '<div id="target">Stuff<slot></slot></div>';
           return group;
         }
         function makeShadowTree(node) {
-          var root = node.attachShadow({ mode: 'open' });
-          var div = document.createElement('div');
+          const root = node.attachShadow({ mode: 'open' });
+          const div = document.createElement('div');
           root.appendChild(div);
           div.appendChild(createContentSlotted());
         }
         fixture.innerHTML = '<div><a>hello</a></div>';
         makeShadowTree(fixture.firstChild);
-        var tree = axe.utils.getFlattenedTree(fixture.firstChild);
+        const tree = axe.utils.getFlattenedTree(fixture.firstChild);
         assert.equal(visibleVirtual(tree[0]), 'Stuffhello');
       }
     );
   });
 
-  describe('screen reader', function () {
-    it('should not return elements with visibility: hidden', function () {
+  describe('screen reader', () => {
+    it('should not return elements with visibility: hidden', () => {
       fixture.innerHTML = 'Hello<span style="visibility: hidden;">Hi</span>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], true), 'Hello');
     });
 
-    it('should know how visibility works', function () {
+    it('should know how visibility works', () => {
       fixture.innerHTML =
         'Hello <span style="visibility: hidden;">' +
         '<span style="visibility: visible;">Hi</span>' +
         '</span>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], true), 'Hello Hi');
     });
 
-    it('should not return elements with display: none', function () {
+    it('should not return elements with display: none', () => {
       fixture.innerHTML =
         'Hello<span style="display: none;"><span>Hi</span></span>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], true), 'Hello');
     });
 
-    it('should trim the result', function () {
+    it('should trim the result', () => {
       fixture.innerHTML =
         '   &nbsp;\u00A0    Hello  &nbsp;\r\n   Hi     \n \n &nbsp; \n   ';
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], true), 'Hello Hi');
     });
 
-    it('should ignore script and style tags', function () {
+    it('should ignore script and style tags', () => {
       fixture.innerHTML =
         '<script> // hello </script><style> /*hello */</style>' + 'Hello';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], true), 'Hello');
     });
 
-    it('should not consider offscreen text as hidden (position)', function () {
+    it('should not consider offscreen text as hidden (position)', () => {
       fixture.innerHTML =
         '<div style="position: absolute; top: -9999px;">' +
         '<div>Hello</div>' +
         '</div>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], true), 'Hello');
     });
 
-    it('should not consider offscreen text as hidden (text-indent)', function () {
+    it('should not consider offscreen text as hidden (text-indent)', () => {
       fixture.innerHTML = '<div style="text-indent: -9999px;">' + 'Hello</div>';
 
-      var tree = axe.utils.getFlattenedTree(fixture);
+      const tree = axe.utils.getFlattenedTree(fixture);
       assert.equal(visibleVirtual(tree[0], true), 'Hello');
     });
   });
