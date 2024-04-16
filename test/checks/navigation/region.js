ace25cd51fab327a9a5239608bdfba3bdad2f0ee
Ava Gaiety W <ava.wroten@deque.com>
2024-04-16 13:06:29 -0500

    fix(region): img alt='' ignored by region rule
    added isPresentationGraphic check which currently only handles alt='' and ignores them for the region rule
    svg I believe is handled differently already
    role='presentation' test added as well, use-case already handled
    
    fix: #4145

--- test/checks/navigation/region.js
+++ test/checks/navigation/region.js
@@ -47,11 +47,30 @@ describe('region', function () {
   });
 
   it('should return false when img content is outside the region', function () {
-    var checkArgs = checkSetup(
-      '<img id="target" src="data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7"><div role="main"><h1 id="mainheader" tabindex="0">Introduction</h1></div>'
-    );
+    var checkArgs = checkSetup(`
+      <img id="target" src="data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7">
+			<div role="main">Content</div>
+    `);
 
     assert.isFalse(checkEvaluate.apply(checkContext, checkArgs));
+  });
+
+  it('should return true when img content outside of the region is decorative, via an empty alt attr', function () {
+    var checkArgs = checkSetup(`
+      <img id="target" src="#" width="1" height="1" alt="" />
+			<div role="main">Content</div>
+    `);
+
+    assert.isTrue(checkEvaluate.apply(checkContext, checkArgs));
+  });
+
+  it('should return true when img content outside of the region is decorative, via a presentation role', function () {
+    var checkArgs = checkSetup(`
+      <img id="target" src="#" width="1" height="1" role="presentation />
+			<div role="main">Content</div>
+    `);
+
+    assert.isTrue(checkEvaluate.apply(checkContext, checkArgs));
   });
 
   it('should return true when textless text content is outside the region', function () {
