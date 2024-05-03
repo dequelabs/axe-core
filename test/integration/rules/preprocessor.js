393bc6f977a7b8b91c33f611765d3ad100ba5ce3
Ava Gaiety W <ava.wroten@deque.com>
2024-05-03 10:26:39 -0500

    keep var for integration preprocessor

--- test/integration/rules/preprocessor.js
+++ test/integration/rules/preprocessor.js
@@ -1,15 +1,16 @@
-let path = require('path');
-let fs = require('fs');
-let extRegex = /\.json$/;
-let template = fs.readFileSync(path.join(__dirname, 'runner.js'), 'utf-8');
+/* eslint-disable no-var */
+var path = require('path');
+var fs = require('fs');
+var extRegex = /\.json$/;
+var template = fs.readFileSync(path.join(__dirname, 'runner.js'), 'utf-8');
 
 /**
  * Turn each rule.json integration test JSON into a js file using
  * the runner.js script. This allow us to load the JSON files in
  * the karma config and they'll run as js files.
  */
-let createIntegrationPreprocessor = function (logger) {
-  let log = logger.create('preprocessor.integration');
+var createIntegrationPreprocessor = function (logger) {
+  var log = logger.create('preprocessor.integration');
 
   return function (content, file, done) {
     try {
@@ -18,16 +19,16 @@ let createIntegrationPreprocessor = function (logger) {
 
       // turn the json file into the a test file using the js test template
       // and add the test data to it
-      let htmlpath = file.originalPath.replace(extRegex, '.html');
-      let html = fs.readFileSync(htmlpath, 'utf-8');
+      var htmlpath = file.originalPath.replace(extRegex, '.html');
+      var html = fs.readFileSync(htmlpath, 'utf-8');
       try {
-        JSON.parse(content);
+        var test = JSON.parse(content);
       } catch (e) {
         throw new Error('Unable to parse content of ' + file.originalPath);
       }
       test.content = html;
 
-      let result = template.replace('{}; /*tests*/', JSON.stringify(test));
+      var result = template.replace('{}; /*tests*/', JSON.stringify(test));
 
       done(null, result);
     } catch (e) {
