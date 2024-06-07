743775d7078bd2faea7e1767fbf6704253ab22b5
Ava Gaiety W <ava.wroten@deque.com>
2024-06-07 10:20:02 -0500

    style(test/rule-matches): var -> const & let
    related to #4444

--- test/rule-matches/no-naming-method-matches.js
+++ test/rule-matches/no-naming-method-matches.js
@@ -1,95 +1,95 @@
 describe('no-naming-method-matches', function () {
   'use strict';
 
-  var fixture = document.getElementById('fixture');
-  var queryFixture = axe.testUtils.queryFixture;
-  var rule = axe.utils.getRule('aria-toggle-field-name');
+  let fixture = document.getElementById('fixture');
+  let queryFixture = axe.testUtils.queryFixture;
+  let rule = axe.utils.getRule('aria-toggle-field-name');
 
   afterEach(function () {
     fixture.innerHTML = '';
   });
 
   it('returns false for node `a[href]`', function () {
-    var vNode = queryFixture('<a href="# role="checkbox" id="target"></a>');
-    var actual = rule.matches(null, vNode);
+    let vNode = queryFixture('<a href="# role="checkbox" id="target"></a>');
+    let actual = rule.matches(null, vNode);
     assert.isFalse(actual);
   });
 
   it('returns false for node `map area[href]`', function () {
-    var vNode = queryFixture(
+    let vNode = queryFixture(
       '<map><area id="target" href="#" role="checkbox"></map>'
     );
-    var actual = rule.matches(null, vNode);
+    let actual = rule.matches(null, vNode);
     assert.isFalse(actual);
   });
 
   it('returns false when node is either INPUT, SELECT or TEXTAREA', function () {
     ['input', 'select', 'textarea'].forEach(function (node) {
-      var vNode = queryFixture(
+      let vNode = queryFixture(
         '<' + node + ' role="menuitemcheckbox" id="target"><' + node + '>'
       );
-      var actual = rule.matches(null, vNode);
+      let actual = rule.matches(null, vNode);
       assert.isFalse(actual);
     });
   });
 
   it('returns false when node is IMG', function () {
-    var vNode = queryFixture('<img id="target" role="menuitemradio">');
-    var actual = rule.matches(null, vNode);
+    let vNode = queryFixture('<img id="target" role="menuitemradio">');
+    let actual = rule.matches(null, vNode);
     assert.isFalse(actual);
   });
 
   it('returns false when node is not SVG', function () {
-    var vNode = queryFixture('<svg id="target"></svg>');
-    var actual = rule.matches(null, vNode);
+    let vNode = queryFixture('<svg id="target"></svg>');
+    let actual = rule.matches(null, vNode);
     assert.isFalse(actual);
   });
 
   it('returns false when node is BUTTON', function () {
-    var vNode = queryFixture('<button id="target"></button>');
-    var actual = rule.matches(null, vNode);
+    let vNode = queryFixture('<button id="target"></button>');
+    let actual = rule.matches(null, vNode);
     assert.isFalse(actual);
   });
 
   it('returns false for INPUT of type `BUTTON`, `SUBMIT` or `RESET`', function () {
     ['button', 'submit', 'reset'].forEach(function (type) {
-      var vNode = queryFixture(
+      let vNode = queryFixture(
         '<input id="target" role="radio" type="' + type + '">'
       );
-      var actual = rule.matches(null, vNode);
+      let actual = rule.matches(null, vNode);
       assert.isFalse(actual);
     });
   });
 
   it('returns false when role=`combobox` has a child input', function () {
-    var vNode = queryFixture(
+    let vNode = queryFixture(
       '<div id="target" role="combobox"><input type="text"/></div>'
     );
-    var actual = rule.matches(null, vNode);
+    let actual = rule.matches(null, vNode);
     assert.isFalse(actual);
   });
 
   it('returns false for the listbox popup of a role=`combobox`', function () {
-    var vNode = queryFixture(
+    let vNode = queryFixture(
       '<div role="combobox" aria-controls="target"></div>' +
         '<div id="target" role="listbox"></div>'
     );
-    var actual = rule.matches(null, vNode);
+    let actual = rule.matches(null, vNode);
     assert.isFalse(actual);
   });
 
   it('returns true for the dialog popup of a role=`combobox`', function () {
-    var vNode = queryFixture(
+    let vNode = queryFixture(
       '<div role="combobox" aria-controls="target"></div>' +
         '<div id="target" role="dialog"></div>'
     );
-    var actual = rule.matches(null, vNode);
+    let actual = rule.matches(null, vNode);
     assert.isTrue(actual);
   });
 
   it('returns true for a div with role=`button`', function () {
-    var vNode = queryFixture('<div id="target" role="button"></div>');
-    var actual = rule.matches(null, vNode);
+    let vNode = queryFixture('<div id="target" role="button"></div>');
+    let actual = rule.matches(null, vNode);
     assert.isTrue(actual);
   });
 });
