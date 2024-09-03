dcf03d9318925f44dff149bb5795d4fd12ef1bc0
Steven Lambert <2433219+straker@users.noreply.github.com>
2024-09-03 09:21:10 -0600

    test: add deep axe-core result compare

--- test/integration/full/run-partial/after-method.js
+++ test/integration/full/run-partial/after-method.js
@@ -22,9 +22,9 @@ describe('run-partial, after-method', function () {
         var axeRunPartialResult = results[0];
         var axeRunResult = results[1];
         assert.lengthOf(axeRunPartialResult.violations, 0);
-        axeRunPartialResult.testEnvironment = axeRunResult.testEnvironment;
-        axeRunPartialResult.timestamp = axeRunResult.timestamp;
-        assert.deepEqual(axeRunPartialResult, axeRunResult);
+        axe.testUtils.resultsDeepEqual(axeRunPartialResult, axeRunResult, [
+          'testEnvironment'
+        ]);
         done();
       })
       .catch(done);
@@ -51,8 +51,9 @@ describe('run-partial, after-method', function () {
         assert.lengthOf(nodes, 1);
         assert.deepEqual(nodes[0].target, ['#frame1', 'h3']);
 
-        axeRunPartialResult.timestamp = axeRunResult.timestamp;
-        assert.deepEqual(axeRunPartialResult, axeRunResult);
+        axe.testUtils.resultsDeepEqual(axeRunPartialResult, axeRunResult, [
+          'testEnvironment'
+        ]);
         done();
       })
       .catch(done);
