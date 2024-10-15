a9710d757c6ca6ee0ce5d26be3427bab54b87a7a
Steven Lambert <2433219+straker@users.noreply.github.com>
2024-10-15 09:10:56 -0600

    fix(button-name,input-button-name,input-img-alt): allow label to give accessible name (#4607)
    I'm not sure if this should close #4472 as that also deals with buttons
    with a different role running `button-name`. For sure this closes #3696
    
    Closes: #3696
    
    ---------
    
    Co-authored-by: Wilco Fiers <WilcoFiers@users.noreply.github.com>

--- test/integration/virtual-rules/button-name.js
+++ test/integration/virtual-rules/button-name.js
@@ -1,6 +1,6 @@
-describe('button-name virtual-rule', function () {
-  it('should pass for aria-label', function () {
-    var results = axe.runVirtualRule('button-name', {
+describe('button-name virtual-rule', () => {
+  it('should pass for aria-label', () => {
+    const results = axe.runVirtualRule('button-name', {
       nodeName: 'button',
       attributes: {
         'aria-label': 'foobar'
@@ -12,21 +12,24 @@ describe('button-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should incomplete for aria-labelledby', function () {
-    var results = axe.runVirtualRule('button-name', {
+  it('should incomplete for aria-labelledby', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button',
       attributes: {
         'aria-labelledby': 'foobar'
       }
     });
+    node.parent = null;
+
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 0);
     assert.lengthOf(results.incomplete, 1);
   });
 
-  it('should pass for title', function () {
-    var results = axe.runVirtualRule('button-name', {
+  it('should pass for title', () => {
+    const results = axe.runVirtualRule('button-name', {
       nodeName: 'button',
       attributes: {
         title: 'foobar'
@@ -38,8 +41,8 @@ describe('button-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass for role=presentation when disabled', function () {
-    var results = axe.runVirtualRule('button-name', {
+  it('should pass for role=presentation when disabled', () => {
+    const results = axe.runVirtualRule('button-name', {
       nodeName: 'button',
       attributes: {
         role: 'presentation',
@@ -52,8 +55,8 @@ describe('button-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass for role=none when disabled', function () {
-    var results = axe.runVirtualRule('button-name', {
+  it('should pass for role=none when disabled', () => {
+    const results = axe.runVirtualRule('button-name', {
       nodeName: 'button',
       attributes: {
         role: 'none',
@@ -66,30 +69,30 @@ describe('button-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass for visible text content', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should pass for visible text content', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button'
     });
-    var child = new axe.SerialVirtualNode({
+    const child = new axe.SerialVirtualNode({
       nodeName: '#text',
       nodeType: 3,
       nodeValue: 'foobar'
     });
     node.children = [child];
 
-    var results = axe.runVirtualRule('button-name', node);
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 1);
     assert.lengthOf(results.violations, 0);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail for hidden text', function () {
-    var button = new axe.SerialVirtualNode({
+  it('should fail for hidden text', () => {
+    const button = new axe.SerialVirtualNode({
       nodeName: 'button'
     });
 
-    var span = new axe.SerialVirtualNode({
+    const span = new axe.SerialVirtualNode({
       nodeName: 'span',
       attributes: {
         'aria-hidden': true
@@ -98,119 +101,168 @@ describe('button-name virtual-rule', function () {
     span.parent = button;
     button.children = [span];
 
-    var text = new axe.SerialVirtualNode({
+    const text = new axe.SerialVirtualNode({
       nodeName: '#text',
       nodeType: 3,
       nodeValue: 'foobar'
     });
     text.parent = span;
     span.children = [text];
+    button.parent = null;
 
-    var results = axe.runVirtualRule('button-name', button);
+    const results = axe.runVirtualRule('button-name', button);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should incomplete when alt and children are missing', function () {
-    var results = axe.runVirtualRule('button-name', {
+  it('should incomplete when alt and children are missing', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button',
       attributes: {}
     });
+    node.parent = null;
+
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 0);
     assert.lengthOf(results.incomplete, 1);
   });
 
-  it('should fail children contain no visible text', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail children contain no visible text', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button'
     });
     node.children = [];
+    node.parent = null;
 
-    var results = axe.runVirtualRule('button-name', node);
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail when alt contains only whitespace', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail when alt contains only whitespace', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button',
       attributes: {
         alt: ' \t   \n   '
       }
     });
     node.children = [];
+    node.parent = null;
 
-    var results = axe.runVirtualRule('button-name', node);
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail when aria-label is empty', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail when aria-label is empty', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button',
       attributes: {
         alt: ''
       }
     });
     node.children = [];
+    node.parent = null;
 
-    var results = axe.runVirtualRule('button-name', node);
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail when title is empty', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail when title is empty', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button',
       attributes: {
         title: ''
       }
     });
     node.children = [];
+    node.parent = null;
 
-    var results = axe.runVirtualRule('button-name', node);
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail for role=presentation', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should pass for implicit label', function () {
+    const node = new axe.SerialVirtualNode({
+      nodeName: 'button'
+    });
+    const parent = new axe.SerialVirtualNode({
+      nodeName: 'label'
+    });
+    const child = new axe.SerialVirtualNode({
+      nodeName: '#text',
+      nodeType: 3,
+      nodeValue: 'foobar'
+    });
+    node.parent = parent;
+    node.children = [];
+    parent.children = [child, node];
+
+    const results = axe.runVirtualRule('button-name', node);
+
+    assert.lengthOf(results.passes, 1);
+    assert.lengthOf(results.violations, 0);
+    assert.lengthOf(results.incomplete, 0);
+  });
+
+  it('should incomplete for explicit label', function () {
+    const node = new axe.SerialVirtualNode({
+      nodeName: 'button',
+      attributes: {
+        id: 'foobar'
+      }
+    });
+    node.parent = null;
+
+    const results = axe.runVirtualRule('button-name', node);
+
+    assert.lengthOf(results.passes, 0);
+    assert.lengthOf(results.violations, 0);
+    assert.lengthOf(results.incomplete, 1);
+  });
+
+  it('should fail for role=presentation', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button',
       attributes: {
         role: 'presentation'
       }
     });
     node.children = [];
+    node.parent = null;
 
-    var results = axe.runVirtualRule('button-name', node);
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail for role=none', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail for role=none', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'button',
       attributes: {
         role: 'none'
       }
     });
     node.children = [];
+    node.parent = null;
 
-    var results = axe.runVirtualRule('button-name', node);
+    const results = axe.runVirtualRule('button-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
