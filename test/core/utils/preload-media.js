b96dd2b64654f9d9a80664faf093983312121fcf
Steven Lambert <2433219+straker@users.noreply.github.com>
2025-01-28 15:14:35 -0700

    fix(no-autoplay-audio): don't timeout for preload=none media elements

--- test/core/utils/preload-media.js
+++ test/core/utils/preload-media.js
@@ -1,54 +1,68 @@
-describe('axe.utils.preloadMedia', function () {
-  'use strict';
+describe('axe.utils.preloadMedia', () => {
+  const fixtureSetup = axe.testUtils.fixtureSetup;
 
-  var fixture = document.getElementById('fixture');
-  var fixtureSetup = axe.testUtils.fixtureSetup;
-
-  afterEach(function () {
-    fixture.innerHTML = '';
-  });
-
-  it('returns empty array when there are no media nodes to be preloaded', function (done) {
+  it('returns empty array when there are no media nodes to be preloaded', async () => {
     axe._tree = axe.utils.getFlattenedTree(document);
 
-    axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function (result) {
-      assert.equal(result.length, 0);
-      done();
-    });
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 0);
   });
 
-  it('returns empty array when <audio> has no source', function (done) {
+  it('returns empty array when <audio> has no source', async () => {
     fixtureSetup('<audio autoplay="true" controls></audio>');
 
-    axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function (result) {
-      assert.equal(result.length, 0);
-      done();
-    });
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 0);
   });
 
-  it('returns empty array when <video> has no source', function (done) {
+  it('returns empty array when <video> has no source', async () => {
     fixtureSetup('<video id="target"><source src=""/></video>');
-    axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function (result) {
-      assert.equal(result.length, 0);
-      done();
-    });
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 0);
   });
 
-  it('returns media node (audio) after their metadata has been preloaded', function (done) {
+  it('returns empty array when media node does not preload', async () => {
+    fixtureSetup(`
+      <video id="target" preload="none">
+        <source src="/test/assets/video.mp4" type="video/mp4" />
+      </video>
+    `);
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 0);
+  });
+
+  it('returns empty array when media node is muted', async () => {
+    fixtureSetup(`
+      <video id="target" muted>
+        <source src="/test/assets/video.mp4" type="video/mp4" />
+      </video>
+    `);
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 0);
+  });
+
+  it('returns empty array when media node is paused', async () => {
+    fixtureSetup(`
+      <video id="target" paused>
+        <source src="/test/assets/video.mp4" type="video/mp4" />
+      </video>
+    `);
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 0);
+  });
+
+  it('returns media node (audio) after their metadata has been preloaded', async () => {
     fixtureSetup(
       '<audio src="/test/assets/moon-speech.mp3" autoplay="true" controls></audio>'
     );
 
-    axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function (result) {
-      assert.equal(result.length, 1);
-      assert.isTrue(result[0].readyState > 0);
-      assert.equal(Math.round(result[0].duration), 27);
-
-      done();
-    });
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 1);
+    assert.isTrue(result[0].readyState > 0);
+    assert.equal(Math.round(result[0].duration), 27);
   });
 
-  it('returns media nodes (audio, video) after their metadata has been preloaded', function (done) {
+  it('returns media nodes (audio, video) after their metadata has been preloaded', async () => {
     fixtureSetup(
       // 1 audio elm
       '<audio src="/test/assets/moon-speech.mp3"></audio>' +
@@ -59,15 +73,12 @@ describe('axe.utils.preloadMedia', function () {
         '</video>'
     );
 
-    axe.utils.preloadMedia({ treeRoot: axe._tree[0] }).then(function (result) {
-      assert.equal(result.length, 2);
-      assert.isTrue(result[0].readyState > 0);
-      assert.equal(Math.round(result[0].duration), 27);
+    const result = await axe.utils.preloadMedia({ treeRoot: axe._tree[0] });
+    assert.equal(result.length, 2);
+    assert.isTrue(result[0].readyState > 0);
+    assert.equal(Math.round(result[0].duration), 27);
 
-      assert.isTrue(result[1].readyState > 0);
-      assert.equal(Math.round(result[1].duration), 14);
-
-      done();
-    });
+    assert.isTrue(result[1].readyState > 0);
+    assert.equal(Math.round(result[1].duration), 14);
   });
 });
