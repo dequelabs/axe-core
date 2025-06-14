9fd9078029fa8be92ab2268dbd9f178777e55836
unknown <udeveer0321@gmail.com>
2025-06-14 19:06:32 +0530

    fixed method to be HTML safe and modified creation of outerHTML logic

--- test/core/utils/dq-element.js
+++ test/core/utils/dq-element.js
@@ -81,7 +81,7 @@ describe('DqElement', () => {
       assert.equal(result.source, '<div class="foo" id="target">');
     });
 
-    it('should truncate a large element and its attributes', () => {
+    it('should truncate an element having a large number of attributes', () => {
       let customElement = '<div id="target"';
       for (let i = 0; i < 10; i++) {
         customElement += ` attribute${Array(i + 1).join('A') + (i + 1)}="value${Array(i + 1).join('B') + (i + 1)}"`;
@@ -91,11 +91,11 @@ describe('DqElement', () => {
       const result = new DqElement(vNode);
       assert.equal(
         result.source,
-        `<div id="target" attribute1="value1" attributea2="valueB2" attributeaa3="valueBB3" attributeaaa4... ...>`
+        `<div id="target" attribute1="value1" attributea2="valueB2" attributeaa3="valueBB3" attributeaaaaaaaa...>`
       );
     });
 
-    it('should truncate a large element with long custom tag', () => {
+    it('should truncate a large element with long custom tag name', () => {
       let longCustomElementTagName = new Array(300).join('b');
       let customElement = `<${longCustomElementTagName} id="target">A</${longCustomElementTagName}>`;
       const vNode = queryFixture(customElement);
