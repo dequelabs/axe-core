73065d12ddbc9678934851226fc3f6ddb9574d63
Steven Lambert <2433219+straker@users.noreply.github.com>
2025-06-27 08:17:26 -0600

    test: skip aria-required-owned failed example 10

--- test/act-rules/aria-required-owned-element-bc4a75.spec.js
+++ test/act-rules/aria-required-owned-element-bc4a75.spec.js
@@ -1,5 +1,7 @@
 require('./act-runner.js')({
   id: 'bc4a75',
   title: 'ARIA required owned elements',
-  axeRules: ['aria-required-children']
+  axeRules: ['aria-required-children'],
+  // we fail failed example 10 with the list rule and not aria-required-children
+  skipTests: ['8b65672c9aefc4957b09a338eb85ad7dff6e53de']
 });
