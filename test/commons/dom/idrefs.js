1617842a1e238bb8fea1c09188d9e14f45cf0536
Steven Lambert <2433219+straker@users.noreply.github.com>
2025-07-30 09:48:34 -0600

    fix(idref): fallback to qsa

--- test/commons/dom/idrefs.js
+++ test/commons/dom/idrefs.js
@@ -1,40 +1,36 @@
 function createContentIDR() {
   'use strict';
-  var group = document.createElement('div');
+  const group = document.createElement('div');
   group.id = 'target';
   return group;
 }
 
 function makeShadowTreeIDR(node) {
   'use strict';
-  var root = node.attachShadow({ mode: 'open' });
-  var div = document.createElement('div');
+  const root = node.attachShadow({ mode: 'open' });
+  const div = document.createElement('div');
   div.className = 'parent';
   div.setAttribute('target', 'target');
   root.appendChild(div);
   div.appendChild(createContentIDR());
 }
 
-describe('dom.idrefs', function () {
+describe('dom.idrefs', () => {
   'use strict';
 
-  var fixture = document.getElementById('fixture');
-  var shadowSupported = axe.testUtils.shadowSupport.v1;
+  const fixture = document.getElementById('fixture');
+  const shadowSupported = axe.testUtils.shadowSupport.v1;
 
-  afterEach(function () {
-    fixture.innerHTML = '';
-  });
-
-  it('should find referenced nodes by ID', function () {
+  it('should find referenced nodes by ID', () => {
     fixture.innerHTML =
       '<div aria-cats="target1 target2" id="start"></div>' +
       '<div id="target1"></div><div id="target2"></div>';
 
-    var start = document.getElementById('start'),
-      expected = [
-        document.getElementById('target1'),
-        document.getElementById('target2')
-      ];
+    const start = document.getElementById('start');
+    const expected = [
+      document.getElementById('target1'),
+      document.getElementById('target2')
+    ];
 
     assert.deepEqual(
       axe.commons.dom.idrefs(start, 'aria-cats'),
@@ -45,13 +41,13 @@ describe('dom.idrefs', function () {
 
   (shadowSupported ? it : xit)(
     'should find only referenced nodes within the current root: shadow DOM',
-    function () {
+    () => {
       // shadow DOM v1 - note: v0 is compatible with this code, so no need
       // to specifically test this
       fixture.innerHTML = '<div target="target"><div id="target"></div></div>';
       makeShadowTreeIDR(fixture.firstChild);
-      var start = fixture.firstChild.shadowRoot.querySelector('.parent');
-      var expected = [fixture.firstChild.shadowRoot.getElementById('target')];
+      const start = fixture.firstChild.shadowRoot.querySelector('.parent');
+      const expected = [fixture.firstChild.shadowRoot.getElementById('target')];
 
       assert.deepEqual(
         axe.commons.dom.idrefs(start, 'target'),
@@ -63,14 +59,14 @@ describe('dom.idrefs', function () {
 
   (shadowSupported ? it : xit)(
     'should find only referenced nodes within the current root: document',
-    function () {
+    () => {
       // shadow DOM v1 - note: v0 is compatible with this code, so no need
       // to specifically test this
       fixture.innerHTML =
         '<div target="target" class="parent"><div id="target"></div></div>';
       makeShadowTreeIDR(fixture.firstChild);
-      var start = fixture.querySelector('.parent');
-      var expected = [document.getElementById('target')];
+      const start = fixture.querySelector('.parent');
+      const expected = [document.getElementById('target')];
 
       assert.deepEqual(
         axe.commons.dom.idrefs(start, 'target'),
@@ -80,17 +76,17 @@ describe('dom.idrefs', function () {
     }
   );
 
-  it('should insert null if a reference is not found', function () {
+  it('should insert null if a reference is not found', () => {
     fixture.innerHTML =
       '<div aria-cats="target1 target2 target3" id="start"></div>' +
       '<div id="target1"></div><div id="target2"></div>';
 
-    var start = document.getElementById('start'),
-      expected = [
-        document.getElementById('target1'),
-        document.getElementById('target2'),
-        null
-      ];
+    const start = document.getElementById('start');
+    const expected = [
+      document.getElementById('target1'),
+      document.getElementById('target2'),
+      null
+    ];
 
     assert.deepEqual(
       axe.commons.dom.idrefs(start, 'aria-cats'),
@@ -99,17 +95,17 @@ describe('dom.idrefs', function () {
     );
   });
 
-  it('should not fail when extra whitespace is used', function () {
+  it('should not fail when extra whitespace is used', () => {
     fixture.innerHTML =
       '<div aria-cats="    \ttarget1 \n  target2  target3 \n\t" id="start"></div>' +
       '<div id="target1"></div><div id="target2"></div>';
 
-    var start = document.getElementById('start'),
-      expected = [
-        document.getElementById('target1'),
-        document.getElementById('target2'),
-        null
-      ];
+    const start = document.getElementById('start');
+    const expected = [
+      document.getElementById('target1'),
+      document.getElementById('target2'),
+      null
+    ];
 
     assert.deepEqual(
       axe.commons.dom.idrefs(start, 'aria-cats'),
@@ -117,4 +113,6 @@ describe('dom.idrefs', function () {
       'Should find it!'
     );
   });
+
+  // virtual-node tests test throwing for non-DOM nodes and working with complete trees
 });
--- test/integration/virtual-rules/select-name.js
+++ test/integration/virtual-rules/select-name.js
@@ -1,6 +1,6 @@
-describe('select-name virtual-rule', function () {
-  it('should pass for aria-label', function () {
-    var results = axe.runVirtualRule('select-name', {
+describe('select-name virtual-rule', () => {
+  it('should pass for aria-label', () => {
+    const results = axe.runVirtualRule('select-name', {
       nodeName: 'select',
       attributes: {
         'aria-label': 'foobar'
@@ -12,8 +12,8 @@ describe('select-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should incomplete for aria-labelledby', function () {
-    var results = axe.runVirtualRule('select-name', {
+  it('should incomplete for aria-labelledby', () => {
+    const results = axe.runVirtualRule('select-name', {
       nodeName: 'select',
       attributes: {
         'aria-labelledby': 'foobar'
@@ -25,14 +25,59 @@ describe('select-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 1);
   });
 
-  it('should pass for implicit label', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should pass for aria-labelledby in complete tree', () => {
+    const html = new axe.SerialVirtualNode({
+      nodeName: 'html'
+    });
+    const body = new axe.SerialVirtualNode({
+      nodeName: 'body'
+    });
+    const div = new axe.SerialVirtualNode({
+      nodeName: 'div',
+      attributes: {
+        id: 'foobar'
+      },
+      id: 'foobar'
+    });
+    const text = new axe.SerialVirtualNode({
+      nodeName: '#text',
+      nodeType: 3,
+      nodeValue: 'foobar'
+    });
+    const select = new axe.SerialVirtualNode({
+      nodeName: 'select',
+      attributes: {
+        'aria-labelledby': 'foobar'
+      }
+    });
+    html.parent = null;
+    html.children = [body];
+
+    body.parent = html;
+    body.children = [div, select];
+
+    div.parent = body;
+    div.children = [text];
+
+    text.parent = div;
+
+    select.parent = body;
+
+    const results = axe.runVirtualRule('select-name', select);
+
+    assert.lengthOf(results.passes, 1);
+    assert.lengthOf(results.violations, 0);
+    assert.lengthOf(results.incomplete, 0);
+  });
+
+  it('should pass for implicit label', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'select'
     });
-    var parent = new axe.SerialVirtualNode({
+    const parent = new axe.SerialVirtualNode({
       nodeName: 'label'
     });
-    var child = new axe.SerialVirtualNode({
+    const child = new axe.SerialVirtualNode({
       nodeName: '#text',
       nodeType: 3,
       nodeValue: 'foobar'
@@ -40,30 +85,30 @@ describe('select-name virtual-rule', function () {
     node.parent = parent;
     parent.children = [child, node];
 
-    var results = axe.runVirtualRule('select-name', node);
+    const results = axe.runVirtualRule('select-name', node);
 
     assert.lengthOf(results.passes, 1);
     assert.lengthOf(results.violations, 0);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should incomplete for explicit label', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should incomplete for explicit label', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'select',
       attributes: {
         id: 'foobar'
       }
     });
 
-    var results = axe.runVirtualRule('select-name', node);
+    const results = axe.runVirtualRule('select-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 0);
     assert.lengthOf(results.incomplete, 1);
   });
 
-  it('should pass for title', function () {
-    var results = axe.runVirtualRule('select-name', {
+  it('should pass for title', () => {
+    const results = axe.runVirtualRule('select-name', {
       nodeName: 'select',
       attributes: {
         title: 'foobar'
@@ -75,8 +120,8 @@ describe('select-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass for role=presentation when disabled', function () {
-    var results = axe.runVirtualRule('select-name', {
+  it('should pass for role=presentation when disabled', () => {
+    const results = axe.runVirtualRule('select-name', {
       nodeName: 'select',
       attributes: {
         role: 'presentation',
@@ -89,8 +134,8 @@ describe('select-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass for role=none when disabled', function () {
-    var results = axe.runVirtualRule('select-name', {
+  it('should pass for role=none when disabled', () => {
+    const results = axe.runVirtualRule('select-name', {
       nodeName: 'select',
       attributes: {
         role: 'none',
@@ -103,8 +148,8 @@ describe('select-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should incomplete for both missing aria-label and implicit label', function () {
-    var results = axe.runVirtualRule('select-name', {
+  it('should incomplete for both missing aria-label and implicit label', () => {
+    const results = axe.runVirtualRule('select-name', {
       nodeName: 'select',
       attributes: {
         'aria-label': ''
@@ -116,8 +161,8 @@ describe('select-name virtual-rule', function () {
     assert.lengthOf(results.incomplete, 1);
   });
 
-  it('should fail when aria-label contains only whitespace and no implicit label', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail when aria-label contains only whitespace and no implicit label', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'select',
       attributes: {
         'aria-label': ' \t   \n   '
@@ -125,15 +170,15 @@ describe('select-name virtual-rule', function () {
     });
     node.parent = null;
 
-    var results = axe.runVirtualRule('select-name', node);
+    const results = axe.runVirtualRule('select-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail when aria-label is empty and no implicit label', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail when aria-label is empty and no implicit label', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'select',
       attributes: {
         'aria-label': ''
@@ -141,15 +186,88 @@ describe('select-name virtual-rule', function () {
     });
     node.parent = null;
 
-    var results = axe.runVirtualRule('select-name', node);
+    const results = axe.runVirtualRule('select-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should fail when title is empty and no implicit label', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should fail when aria-labelledby contains only whitespace and no implicit label in complete tree', () => {
+    const html = new axe.SerialVirtualNode({
+      nodeName: 'html'
+    });
+    const body = new axe.SerialVirtualNode({
+      nodeName: 'body'
+    });
+    const div = new axe.SerialVirtualNode({
+      nodeName: 'div',
+      attributes: {
+        id: 'foobar'
+      },
+      id: 'foobar'
+    });
+    const text = new axe.SerialVirtualNode({
+      nodeName: '#text',
+      nodeType: 3,
+      nodeValue: '    '
+    });
+    const select = new axe.SerialVirtualNode({
+      nodeName: 'select',
+      attributes: {
+        'aria-labelledby': 'foobar'
+      }
+    });
+    html.parent = null;
+    html.children = [body];
+
+    body.parent = html;
+    body.children = [div, select];
+
+    div.parent = body;
+    div.children = [text];
+
+    text.parent = div;
+
+    select.parent = body;
+
+    const results = axe.runVirtualRule('select-name', select);
+
+    assert.lengthOf(results.passes, 0);
+    assert.lengthOf(results.violations, 1);
+    assert.lengthOf(results.incomplete, 0);
+  });
+
+  it('should fail when aria-labelledby is missing and no implicit label in complete tree', () => {
+    const html = new axe.SerialVirtualNode({
+      nodeName: 'html'
+    });
+    const body = new axe.SerialVirtualNode({
+      nodeName: 'body'
+    });
+    const select = new axe.SerialVirtualNode({
+      nodeName: 'select',
+      attributes: {
+        'aria-labelledby': 'foobar'
+      }
+    });
+    html.parent = null;
+    html.children = [body];
+
+    body.parent = html;
+    body.children = [select];
+
+    select.parent = body;
+
+    const results = axe.runVirtualRule('select-name', select);
+
+    assert.lengthOf(results.passes, 0);
+    assert.lengthOf(results.violations, 1);
+    assert.lengthOf(results.incomplete, 0);
+  });
+
+  it('should fail when title is empty and no implicit label', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'select',
       attributes: {
         title: ''
@@ -157,15 +275,15 @@ describe('select-name virtual-rule', function () {
     });
     node.parent = null;
 
-    var results = axe.runVirtualRule('select-name', node);
+    const results = axe.runVirtualRule('select-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass for role=presentation', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should pass for role=presentation', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'select',
       attributes: {
         role: 'presentation'
@@ -173,15 +291,15 @@ describe('select-name virtual-rule', function () {
     });
     node.parent = null;
 
-    var results = axe.runVirtualRule('select-name', node);
+    const results = axe.runVirtualRule('select-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
     assert.lengthOf(results.incomplete, 0);
   });
 
-  it('should pass for role=none', function () {
-    var node = new axe.SerialVirtualNode({
+  it('should pass for role=none', () => {
+    const node = new axe.SerialVirtualNode({
       nodeName: 'select',
       attributes: {
         role: 'none'
@@ -189,7 +307,7 @@ describe('select-name virtual-rule', function () {
     });
     node.parent = null;
 
-    var results = axe.runVirtualRule('select-name', node);
+    const results = axe.runVirtualRule('select-name', node);
 
     assert.lengthOf(results.passes, 0);
     assert.lengthOf(results.violations, 1);
