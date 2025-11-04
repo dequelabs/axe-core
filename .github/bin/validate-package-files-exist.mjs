03bf74968db82061b2537600ade8c333fdb9d4fb
Jonathan Garbee <jonathan.garbee@deque.com>
2025-11-04 10:44:03 -0500

    add check for ability to import files

--- .github/bin/validate-package-files-exist.mjs
+++ .github/bin/validate-package-files-exist.mjs
@@ -21,9 +21,14 @@ const repoRoot = resolve(import.meta.dirname, '..', '..');
  * keep counting. But, if we have that many checks in here down
  * the road then all the validation will need a major refactor.
  */
-const exitCode = 0;
+let exitCode = 0;
 const missing = [];
 const summaryFile = process.env.GITHUB_STEP_SUMMARY;
+let summary = `# Package Validation
+
+**Package Name**: \`${pkg.name}\`
+**Package Version**: \`${pkg.version}\`
+`;
 
 /**
  * Checks if a file or folder exists on the filesystem.
@@ -52,14 +57,8 @@ const appendToSummaryFile = async (text) => {
   }
 };
 
-let summary = '# Package Validation\n\n';
-
-summary += `
-**Package Name**: \`${pkg.name}\`
-**Package Version**: \`${pkg.version}\`
-`;
-
-summary += `
+const fileExistenceCheck = async () => {
+  summary += `
 \n## File Existence Check
 
 The following results table shows the status of files and folders
@@ -71,27 +70,83 @@ listed in the \`files\` array of \`package.json\`.
 > could exist but be empty and still pass this check. Or
 > a file could exist but have incorrect syntax.
 
+| File | Status |\n|------|--------|
 `;
 
-summary += '\n| File | Status |\n|------|--------|\n';
+  console.log('Checking for existence of package files:');
 
-for (const file of pkg.files) {
-  if (await exists(file)) {
-    console.info(`✓ ${file}`);
-    summary += `| \`${file}\` | ✓ Found |\n`;
-  } else {
-    console.error(`✗ ${file}`);
-    summary += `| \`${file}\` | ✗ Missing |\n`;
-    missing.push(file);
+  for (const file of pkg.files) {
+    if (await exists(file)) {
+      console.info(`✓ ${file}`);
+      summary += `| \`${file}\` | ✓ Found |\n`;
+    } else {
+      console.error(`✗ ${file}`);
+      summary += `| \`${file}\` | ✗ Missing |\n`;
+      missing.push(file);
+    }
   }
-}
 
-await appendToSummaryFile(summary);
+  await appendToSummaryFile(summary);
 
-if (missing.length > 0) {
-  await appendToSummaryFile(`\n**ERROR: Missing files: ${missing.join(', ')}**\n`);
-  console.error(`::error::Missing files: ${missing.join(', ')}`);
-  exitCode++;
-}
+  if (missing.length > 0) {
+    await appendToSummaryFile(`\n**ERROR: Missing files: ${missing.join(', ')}**\n`);
+    console.error(`::error::Missing files: ${missing.join(', ')}`);
+    exitCode++;
+  }
+};
+
+/**
+ * Since this validation script is running under ESM, we can
+ * import the package files without any extra scaffolding.
+ * Allowing us to validate before publishing that the package
+ * will be usable.
+ */
+const validateImportable = async () => {
+  summary += `\n## Importability Check
+
+This check attempts to import the package. As well as all
+defined files in the \`files\` array of \`package.json\`.
+
+| File | Status |\n|------|--------|
+`;
+
+  const importTargets = [...pkg.files.map((file) => `${pkg.name}/${file}`)];
+  let anyCaught = false;
+
+  console.log('Validating package files are importable:');
+
+  for (const target of importTargets) {
+    // Skip things that can't be imported directly
+    // One day we can hopefully import anything as bytes to validate.
+    // Ref: https://github.com/tc39/proposal-import-bytes
+    if (target.endsWith('.txt') || target.endsWith('/') || target.endsWith('.d.ts')) {
+      continue;
+    }
+
+    try {
+      if (target.endsWith('.json')) {
+        await import(target, { with: { type: 'json' } });
+      } else {
+        await import(target);
+      }
+      console.info(`✓ ${target}`);
+      summary += `| \`${target}\` | ✓ Importable |\n`;
+    } catch (error) {
+      console.error(`✗ ${target}`);
+      summary += `| \`${target}\` | ✗ Not Importable |\n`;
+      summary += `\n\`\`\`\n${error.message}\n\`\`\`\n`;
+      anyCaught = true;
+    }
+  }
+
+  if (anyCaught) {
+    exitCode++;
+  }
+
+  await appendToSummaryFile(summary);
+};
+
+await fileExistenceCheck();
+await validateImportable();
 
 process.exit(exitCode);
