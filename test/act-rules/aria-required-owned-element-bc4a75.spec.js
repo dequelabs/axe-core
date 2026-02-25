6935f803ce536961fcbb0c1a8ba94ca231bec9ef
Chris Hutchins <chris.hutchins@deque.com>
2026-02-25 13:50:20 -0500

    skipped ACT test case relating to Inapplicable Example 4 for aria-busy="true"

--- test/act-rules/aria-required-owned-element-bc4a75.spec.js
+++ test/act-rules/aria-required-owned-element-bc4a75.spec.js
@@ -1,5 +1,7 @@
 require('./act-runner.js')({
   id: 'bc4a75',
   title: 'ARIA required owned elements',
-  axeRules: ['list', 'aria-required-children']
+  axeRules: ['list', 'aria-required-children'],
+  // See https://github.com/act-rules/act-rules.github.io/pull/2387
+  skipTests: ['837f92d0ac41c14e55782991cbab75975b492702']
 });
