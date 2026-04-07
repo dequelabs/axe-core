1d801636f058f2abd885c488baff954872b13846
yugo innami <58389827+nami8824@users.noreply.github.com>
2026-04-07 23:29:50 +0900

    fix(aria-allowed-attr): restrict br and wbr elements to aria-hidden only (#4974)
    Closes: https://github.com/dequelabs/axe-core/issues/3177

---
+++ test/checks/aria/aria-allowed-attr-elm.js
@@ -0,0 +1,92 @@
+describe('aria-allowed-attr-elm', () => {
+  'use strict';
+
+  const queryFixture = axe.testUtils.queryFixture;
+  const checkContext = axe.testUtils.MockCheckContext();
+
+  afterEach(() => {
+    checkContext.reset();
+  });
+
+  it('should pass for br with aria-hidden', () => {
+    const vNode = queryFixture('<br aria-hidden="true" id="target">');
+
+    assert.isTrue(
+      axe.testUtils
+        .getCheckEvaluate('aria-allowed-attr-elm')
+        .call(checkContext, null, null, vNode)
+    );
+  });
+
+  it('should pass for wbr with aria-hidden', () => {
+    const vNode = queryFixture('<wbr aria-hidden="false" id="target">');
+
+    assert.isTrue(
+      axe.testUtils
+        .getCheckEvaluate('aria-allowed-attr-elm')
+        .call(checkContext, null, null, vNode)
+    );
+  });
+
+  it('should fail for br with disallowed aria attribute', () => {
+    const vNode = queryFixture('<br aria-busy="true" id="target">');
+
+    assert.isFalse(
+      axe.testUtils
+        .getCheckEvaluate('aria-allowed-attr-elm')
+        .call(checkContext, null, null, vNode)
+    );
+    assert.deepEqual(checkContext._data, {
+      messageKey: 'singular',
+      nodeName: 'br',
+      values: 'aria-busy="true"'
+    });
+  });
+
+  it('should fail for wbr with disallowed aria attribute', () => {
+    const vNode = queryFixture('<wbr aria-busy="true" id="target">');
+
+    assert.isFalse(
+      axe.testUtils
+        .getCheckEvaluate('aria-allowed-attr-elm')
+        .call(checkContext, null, null, vNode)
+    );
+    assert.deepEqual(checkContext._data, {
+      messageKey: 'singular',
+      nodeName: 'wbr',
+      values: 'aria-busy="true"'
+    });
+  });
+
+  it('should pass for br with non-global aria attribute', () => {
+    const vNode = queryFixture('<br aria-expanded="true" id="target">');
+
+    assert.isTrue(
+      axe.testUtils
+        .getCheckEvaluate('aria-allowed-attr-elm')
+        .call(checkContext, null, null, vNode)
+    );
+  });
+
+  it('should pass for br with explicit role', () => {
+    const vNode = queryFixture(
+      '<br role="heading" aria-busy="true" id="target">'
+    );
+
+    assert.isTrue(
+      axe.testUtils
+        .getCheckEvaluate('aria-allowed-attr-elm')
+        .call(checkContext, null, null, vNode)
+    );
+  });
+
+  it('should pass for element without allowedAriaAttrs restriction', () => {
+    const vNode = queryFixture('<div aria-busy="true" id="target"></div>');
+
+    assert.isTrue(
+      axe.testUtils
+        .getCheckEvaluate('aria-allowed-attr-elm')
+        .call(checkContext, null, null, vNode)
+    );
+  });
+});
