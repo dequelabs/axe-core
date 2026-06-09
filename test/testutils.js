281fb509e43b54dab5fbccfe75ef9b3d229edf8c
straker <2433219+straker@users.noreply.github.com>
2026-06-09 20:05:39 +0000

    :robot: Automated formatting fixes

--- test/testutils.js
+++ test/testutils.js
@@ -226,10 +226,10 @@ var helpers;
     // Normalize target, allow it to be the provided string or use '#target' to query composed tree
     let shadowSelector = '#shadow';
     if (typeof targetSelector === 'object') {
-    if (typeof targetSelector === 'object' && targetSelector !== null) {
-      shadowSelector = targetSelector.shadow ?? '#shadow';
-      targetSelector = targetSelector.target ?? '#target';
-    }
+      if (typeof targetSelector === 'object' && targetSelector !== null) {
+        shadowSelector = targetSelector.shadow ?? '#shadow';
+        targetSelector = targetSelector.target ?? '#target';
+      }
     }
 
     const fixtureNode = testUtils.injectIntoFixture(content);
