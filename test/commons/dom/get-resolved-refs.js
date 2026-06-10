fa8d1d69cfdf194746b5deb698bf4e5928fe2d27
Steven Lambert <2433219+straker@users.noreply.github.com>
2026-06-10 10:29:25 -0600

    fixes

--- test/commons/dom/get-resolved-refs.js
+++ test/commons/dom/get-resolved-refs.js
@@ -15,11 +15,7 @@ describe('dom.getResolvedRefs', () => {
       getNodeFromTree(document.getElementById('target2'))
     ];
 
-    assert.deepEqual(
-      getResolvedRefs(vNode, 'aria-cats'),
-      expected,
-      'Should find it!'
-    );
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-cats'), expected);
   });
 
   it('should find only referenced nodes within the current root: shadow DOM', () => {
@@ -31,11 +27,7 @@ describe('dom.getResolvedRefs', () => {
     const node = fixture.querySelector('#shadow').shadowRoot.firstChild;
     const expected = [targetVNode];
 
-    assert.deepEqual(
-      getResolvedRefs(node, 'target'),
-      expected,
-      'should only find stuff in the shadow DOM'
-    );
+    assert.deepEqual(getResolvedRefs(node, 'target'), expected);
   });
 
   it('should find only referenced nodes within the current root: document', () => {
@@ -47,11 +39,7 @@ describe('dom.getResolvedRefs', () => {
     const node = fixture.querySelector('[target]');
     const expected = [getNodeFromTree(fixture.querySelector('#target'))];
 
-    assert.deepEqual(
-      getResolvedRefs(node, 'target'),
-      expected,
-      'should only find stuff in the document'
-    );
+    assert.deepEqual(getResolvedRefs(node, 'target'), expected);
   });
 
   it('should insert null if a reference is not found', () => {
@@ -67,11 +55,7 @@ describe('dom.getResolvedRefs', () => {
       null
     ];
 
-    assert.deepEqual(
-      getResolvedRefs(vNode, 'aria-cats'),
-      expected,
-      'Should find it!'
-    );
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-cats'), expected);
   });
 
   it('should not fail when extra whitespace is used', () => {
@@ -92,11 +76,7 @@ describe('dom.getResolvedRefs', () => {
       null
     ];
 
-    assert.deepEqual(
-      getResolvedRefs(vNode, 'aria-cats'),
-      expected,
-      'Should find it!'
-    );
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-cats'), expected);
   });
 
   it('should work with idrefs property', () => {
@@ -116,11 +96,7 @@ describe('dom.getResolvedRefs', () => {
       getNodeFromTree(document.getElementById('target2'))
     ];
 
-    assert.deepEqual(
-      getResolvedRefs(vNode, 'aria-controls'),
-      expected,
-      'Should find it!'
-    );
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-controls'), expected);
   });
 
   it('should work with idrefs elementInternals', () => {
@@ -140,11 +116,7 @@ describe('dom.getResolvedRefs', () => {
       getNodeFromTree(document.getElementById('target2'))
     ];
 
-    assert.deepEqual(
-      getResolvedRefs(vNode, 'aria-labelledby'),
-      expected,
-      'Should find it!'
-    );
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-labelledby'), expected);
   });
 
   it('should not insert null if an idrefs property reference is not connected', () => {
@@ -166,10 +138,75 @@ describe('dom.getResolvedRefs', () => {
       getNodeFromTree(document.getElementById('target2'))
     ];
 
-    assert.deepEqual(
-      getResolvedRefs(vNode, 'aria-controls'),
-      expected,
-      'Should find it!'
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-controls'), expected);
+  });
+
+  it('should remove self id references from attribute', () => {
+    const vNode = queryFixture(html`
+      <div aria-cats="target1 target target2" id="target"></div>
+      <div id="target1"></div>
+      <div id="target2"></div>
+    `);
+
+    const expected = [
+      getNodeFromTree(document.getElementById('target1')),
+      getNodeFromTree(document.getElementById('target2'))
+    ];
+
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-cats'), expected);
+  });
+
+  it('should remove self id references from elementInternals', () => {
+    const vNode = queryFixture(html`
+      <testutils-element id="target"></testutils-element>
+      <div id="target1"></div>
+      <div id="target2"></div>
+    `);
+
+    vNode.actualNode._internals.ariaLabelledByElements = [
+      document.getElementById('target1'),
+      document.getElementById('target'),
+      document.getElementById('target2')
+    ];
+
+    const expected = [
+      getNodeFromTree(document.getElementById('target1')),
+      getNodeFromTree(document.getElementById('target2'))
+    ];
+
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-labelledby'), expected);
+  });
+
+  it('should fallback to attribute if prop is undefined (IE11)', () => {
+    // any AOM property cannot be set to undefined so we'll create a custom element that does override the value
+    customElements.define(
+      'aria-resolved-refs-elm',
+      class AriaResolvedRefsElm extends HTMLElement {
+        constructor() {
+          super();
+        }
+
+        get ariaLabelledByElements() {
+          return undefined;
+        }
+        set ariaLabelledByElements(foo) {}
+      }
     );
+
+    const vNode = queryFixture(html`
+      <aria-resolved-refs-elm
+        aria-labelledby="target1 target2"
+        id="target"
+      ></aria-resolved-refs-elm>
+      <div id="target1"></div>
+      <div id="target2"></div>
+    `);
+
+    const expected = [
+      getNodeFromTree(document.getElementById('target1')),
+      getNodeFromTree(document.getElementById('target2'))
+    ];
+
+    assert.deepEqual(getResolvedRefs(vNode, 'aria-labelledby'), expected);
   });
 });
